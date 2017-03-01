255c7f8631edae3eace36166bf931be51dc06ece
Zhiyuan <jameslaizy@gmail.com>
2017-03-02 02:53:02 +0800

    Change application name

--- src/main/java/seedu/address/MainApp.java
+++ src/main/java/seedu/address/MainApp.java
@@ -163,7 +163,7 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        logger.info("Starting AddressBook " + MainApp.VERSION);
+        logger.info("Starting Task Manager " + MainApp.VERSION);
         ui.start(primaryStage);
     }
 
