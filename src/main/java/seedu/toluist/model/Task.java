e64c470691928097582f92442dbb4e4cd7a9e3e0
Louis Lai <hoangdung2506@gmail.com>
2017-03-02 13:03:20 +0800

    Add isOverdue to Task model

--- src/main/java/seedu/toluist/model/Task.java
+++ src/main/java/seedu/toluist/model/Task.java
@@ -56,4 +56,8 @@ public class Task {
     public void replaceTags(ArrayList<Tag> tags) {
         this.allTags = tags;
     }
+
+    public boolean isOverdue() {
+        return startDateTime != null && startDateTime.isBefore(LocalDateTime.now());
+    }
 }
