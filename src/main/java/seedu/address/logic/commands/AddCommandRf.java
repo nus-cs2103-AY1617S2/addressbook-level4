2cc49d7a6dabb572e5750e89d11a2c500dcaeda9
Peixuan <lee.pei.xuan.2011@vjc.sg>
2017-03-05 01:10:13 +0800

    morph logic component to suit task manager

---
+++ src/main/java/seedu/address/logic/commands/AddCommandRf.java
@@ -0,0 +1,62 @@
+package seedu.address.logic.commands;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.task.Date;
+import seedu.address.model.task.Name;
+import seedu.address.model.task.Task;
+import seedu.address.model.task.UniqueTaskList;
+import seedu.address.model.tag.Tag;
+import seedu.address.model.tag.UniqueTagList;
+
+/**
+ * Adds a task to the task manager.
+ */
+public class AddCommand extends Command {
+
+    public static final String COMMAND_WORD = "add";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a task to the task manager. "
+            + "Parameters: NAME d/DATE  [t/TAG]...\n"
+            + "Example: " + COMMAND_WORD
+            + " buy 5 broccolis d/tomorrow t/survival t/grocery";
+
+    public static final String MESSAGE_SUCCESS = "New task added: %1$s";
+    public static final String MESSAGE_DUPLICATE_TASK = "This task already exists in the task manager";
+
+    private final Task toAdd;
+
+    /**
+     * Creates an AddCommand using raw values.
+     *
+     * @throws IllegalValueException if any of the raw values are invalid
+     */
+    public AddCommand(String name, String date, Set<String> tags)
+            throws IllegalValueException {
+        final Set<Tag> tagSet = new HashSet<>();
+        for (String tagName : tags) {
+            tagSet.add(new Tag(tagName));
+        }
+        this.toAdd = new Task(
+                new Name(name),
+                new Date(date),
+                new UniqueTagList(tagSet)
+        );
+    }
+
+    @Override
+    public CommandResult execute() throws CommandException {
+        assert model != null;
+        try {
+            model.addTask(toAdd);
+            return new CommandResult(String.format(MESSAGE_SUCCESS, toAdd));
+        } catch (UniqueTaskList.DuplicateTaskException e) {
+            throw new CommandException(MESSAGE_DUPLICATE_TASK);
+        }
+
+    }
+
+}
