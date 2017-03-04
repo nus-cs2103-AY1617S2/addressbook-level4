aeb3e23a33813338b47ab5f1d162c4ee0f8b3a82
ramjanarthan <ram.janarthan11@gmail.com>
2017-03-05 00:57:49 +0800

    Modify AddCommandParser to include new keywords

--- src/main/java/seedu/doist/logic/parser/AddCommandParser.java
+++ src/main/java/seedu/doist/logic/parser/AddCommandParser.java
@@ -1,11 +1,7 @@
 package seedu.doist.logic.parser;
 
 import static seedu.doist.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.doist.logic.parser.CliSyntax.PREFIX_ADDRESS;
-import static seedu.doist.logic.parser.CliSyntax.PREFIX_EMAIL;
-import static seedu.doist.logic.parser.CliSyntax.PREFIX_PHONE;
-import static seedu.doist.logic.parser.CliSyntax.PREFIX_TAG;
-
+import static seedu.doist.logic.parser.CliSyntax.*;
 import java.util.NoSuchElementException;
 
 import seedu.doist.commons.exceptions.IllegalValueException;
@@ -25,13 +21,20 @@ public class AddCommandParser {
      */
     public Command parse(String args) {
         ArgumentTokenizer argsTokenizer =
-                new ArgumentTokenizer(PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS, PREFIX_TAG);
+                new ArgumentTokenizer(PREFIX_FROM, PREFIX_TO, PREFIX_REMIND, PREFIX_EVERY, PREFIX_AS, PREFIX_BY,  PREFIX_UNDER);
         argsTokenizer.tokenize(args);
         try {
+            System.out.println("REaching = " + argsTokenizer.getPreamble().get());
             return new AddCommand(
                     argsTokenizer.getPreamble().get(),
-                    ParserUtil.toSet(argsTokenizer.getAllValues(PREFIX_TAG))
-            );
+                    argsTokenizer.getValue(PREFIX_FROM).get(),
+                    argsTokenizer.getValue(PREFIX_TO).get(),
+                    argsTokenizer.getValue(PREFIX_REMIND).get(),
+                    argsTokenizer.getValue(PREFIX_EVERY).get(),
+                    argsTokenizer.getValue(PREFIX_AS).get(),
+                    argsTokenizer.getValue(PREFIX_BY).get(),
+                    argsTokenizer.getValue(PREFIX_UNDER).get()
+                    );
         } catch (NoSuchElementException nsee) {
             return new IncorrectCommand(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
         } catch (IllegalValueException ive) {
