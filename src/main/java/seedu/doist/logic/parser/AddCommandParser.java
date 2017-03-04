0465546102f9d12603bdc715c9352ae71c0df14e
ramjanarthan <ram.janarthan11@gmail.com>
2017-03-05 03:19:03 +0800

    Edit class to implement own parser

--- src/main/java/seedu/doist/logic/parser/AddCommandParser.java
+++ src/main/java/seedu/doist/logic/parser/AddCommandParser.java
@@ -3,10 +3,13 @@ package seedu.doist.logic.parser;
 import static seedu.doist.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.doist.logic.parser.CliSyntax.*;
 import java.util.NoSuchElementException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import seedu.doist.commons.exceptions.IllegalValueException;
 import seedu.doist.logic.commands.AddCommand;
 import seedu.doist.logic.commands.Command;
+import seedu.doist.logic.commands.HelpCommand;
 import seedu.doist.logic.commands.IncorrectCommand;
 
 /**
@@ -14,32 +17,50 @@ import seedu.doist.logic.commands.IncorrectCommand;
  */
 public class AddCommandParser {
 
+    private static final Pattern Add_Command_Regex = Pattern.compile("(?<preamble>[^\\\\]*)(?<parameters>((\\\\)(\\S+)(\\s+)([^\\\\]*))*)");
+    
     ////argsTokenizer.getValue(PREFIX_PHONE).get() is an an example of how to use the tokenizer
     /**
      * Parses the given {@code String} of arguments in the context of the AddCommand
      * and returns an AddCommand object for execution.
      */
     public Command parse(String args) {
-        ArgumentTokenizer argsTokenizer =
-                new ArgumentTokenizer(PREFIX_FROM, PREFIX_TO, PREFIX_REMIND, PREFIX_EVERY, PREFIX_AS, PREFIX_BY,  PREFIX_UNDER);
-        argsTokenizer.tokenize(args);
-        try {
-            System.out.println("REaching = " + argsTokenizer.getPreamble().get());
-            return new AddCommand(
-                    argsTokenizer.getPreamble().get(),
-                    argsTokenizer.getValue(PREFIX_FROM).get(),
-                    argsTokenizer.getValue(PREFIX_TO).get(),
-                    argsTokenizer.getValue(PREFIX_REMIND).get(),
-                    argsTokenizer.getValue(PREFIX_EVERY).get(),
-                    argsTokenizer.getValue(PREFIX_AS).get(),
-                    argsTokenizer.getValue(PREFIX_BY).get(),
-                    argsTokenizer.getValue(PREFIX_UNDER).get()
-                    );
-        } catch (NoSuchElementException nsee) {
+        final Matcher matcher = Add_Command_Regex.matcher(args.trim());
+        if (!matcher.matches() || args.trim().equals("")) {
+            System.out.println("hit");
             return new IncorrectCommand(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
+        }
+
+        final String argument = matcher.group("preamble");
+        final String parameters = matcher.group("parameters");
+              
+        ArgumentTokenizer argsTokenizer =
+              new ArgumentTokenizer(PREFIX_FROM, PREFIX_TO, PREFIX_REMIND, PREFIX_EVERY, PREFIX_AS, PREFIX_BY, PREFIX_UNDER);
+        argsTokenizer.tokenize(parameters); 
+        
+        try {
+            return new AddCommand();
         } catch (IllegalValueException ive) {
             return new IncorrectCommand(ive.getMessage());
         }
+//       
+//        try {
+//            
+//            return new AddCommand(
+//                    argsTokenizer.getPreamble().get(),
+//                    argsTokenizer.getValue(PREFIX_FROM).get(),
+//                    argsTokenizer.getValue(PREFIX_TO).get(),
+//                    argsTokenizer.getValue(PREFIX_REMIND).get(),
+//                    argsTokenizer.getValue(PREFIX_EVERY).get(),
+//                    argsTokenizer.getValue(PREFIX_AS).get(),
+//                    argsTokenizer.getValue(PREFIX_BY).get(),
+//                    argsTokenizer.getValue(PREFIX_UNDER).get()
+//                    );
+//        } catch (NoSuchElementException nsee) {
+//            return new IncorrectCommand(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
+//        } catch (IllegalValueException ive) {
+//           
+//        }
     }
 
 }
