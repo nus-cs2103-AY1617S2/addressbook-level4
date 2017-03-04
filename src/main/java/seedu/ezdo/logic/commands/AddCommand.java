03c9c93a55904dd885c2fcf5fd828dce66f68752
Acer\zhi liang <chenzhiliang94@gmail.com>
2017-03-04 21:31:17 +0800

    removed some wrong imports in AddCommand

--- src/main/java/seedu/ezdo/logic/commands/AddCommand.java
+++ src/main/java/seedu/ezdo/logic/commands/AddCommand.java
@@ -11,15 +11,10 @@ import seedu.ezdo.model.todo.Address;
 import seedu.ezdo.model.todo.StartDate;
 import seedu.ezdo.model.todo.Email;
 import seedu.ezdo.model.todo.Name;
-<<<<<<< HEAD
 import seedu.ezdo.model.todo.Priority;
-import seedu.ezdo.model.todo.Person;
-import seedu.ezdo.model.todo.UniquePersonList;
-=======
 import seedu.ezdo.model.todo.Task;
-import seedu.ezdo.model.todo.Phone;
 import seedu.ezdo.model.todo.UniqueTaskList;
->>>>>>> 6228ae6b03115b0e64fed2b189d86d8b94d2fa7e
+
 
 /**
  * Adds a task to ezDo.
@@ -28,13 +23,10 @@ public class AddCommand extends Command {
 
     public static final String COMMAND_WORD = "add";
 
-<<<<<<< HEAD
+
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a person to the address book. "
             + "Parameters: NAME p/PRIORITY e/EMAIL a/ADDRESS  [t/TAG]...\n"
-=======
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a task to ezDo. "
-            + "Parameters: NAME p/PHONE e/EMAIL a/ADDRESS  [t/TAG]...\n"
->>>>>>> 6228ae6b03115b0e64fed2b189d86d8b94d2fa7e
+
             + "Example: " + COMMAND_WORD
             + " John Doe p/1 e/johnd@gmail.com a/311, Clementi Ave 2, #02-25 t/friends t/owesMoney";
 
@@ -48,11 +40,7 @@ public class AddCommand extends Command {
      *
      * @throws IllegalValueException if any of the raw values are invalid
      */
-<<<<<<< HEAD
-    public AddCommand(String name, int priority, String email, String address, Set<String> tags)
-=======
-    public AddCommand(String name, String phone, String email, String startDate, Set<String> tags)
->>>>>>> 6228ae6b03115b0e64fed2b189d86d8b94d2fa7e
+    public AddCommand(String name, int priority, String email, String startDate, Set<String> tags)
             throws IllegalValueException {
         final Set<Tag> tagSet = new HashSet<>();
         for (String tagName : tags) {
