fe995e9ce755441bb3d1dd487d5d15140d875ea5
Joseph Tang <josephtang765@gmail.com>
2017-03-04 13:59:05 +0800

    Update names of sample tasks

--- src/main/java/seedu/ezdo/model/util/SampleDataUtil.java
+++ src/main/java/seedu/ezdo/model/util/SampleDataUtil.java
@@ -15,24 +15,24 @@ public class SampleDataUtil {
     public static Task[] getSampleTasks() {
         try {
             return new Task[] {
-                new Task(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@gmail.com"),
+                new Task(new Name("Buy some milk"), new Phone("87438807"), new Email("alexyeoh@gmail.com"),
                     new StartDate("Blk 30 Geylang Street 29, #06-40"),
-                    new UniqueTagList("friends")),
-                new Task(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@gmail.com"),
+                    new UniqueTagList("groceries")),
+                new Task(new Name("Study for CS2103"), new Phone("99272758"), new Email("berniceyu@gmail.com"),
                     new StartDate("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
-                    new UniqueTagList("colleagues", "friends")),
-                new Task(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@yahoo.com"),
+                    new UniqueTagList("school", "exams")),
+                new Task(new Name("Buy new phone"), new Phone("93210283"), new Email("charlotte@yahoo.com"),
                     new StartDate("Blk 11 Ang Mo Kio Street 74, #11-04"),
-                    new UniqueTagList("neighbours")),
-                new Task(new Name("David Li"), new Phone("91031282"), new Email("lidavid@google.com"),
+                    new UniqueTagList("personal")),
+                new Task(new Name("Visit neighbours"), new Phone("91031282"), new Email("lidavid@google.com"),
                     new StartDate("Blk 436 Serangoon Gardens Street 26, #16-43"),
-                    new UniqueTagList("family")),
-                new Task(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@outlook.com"),
+                    new UniqueTagList("personal")),
+                new Task(new Name("Prepare for midterms"), new Phone("92492021"), new Email("irfan@outlook.com"),
                     new StartDate("Blk 47 Tampines Street 20, #17-35"),
-                    new UniqueTagList("classmates")),
-                new Task(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@gmail.com"),
+                    new UniqueTagList("school", "exams")),
+                new Task(new Name("Prepare for OP2"), new Phone("92624417"), new Email("royb@gmail.com"),
                     new StartDate("Blk 45 Aljunied Street 85, #11-31"),
-                    new UniqueTagList("colleagues"))
+                    new UniqueTagList("school", "exams"))
             };
         } catch (IllegalValueException e) {
             throw new AssertionError("sample data cannot be invalid", e);
