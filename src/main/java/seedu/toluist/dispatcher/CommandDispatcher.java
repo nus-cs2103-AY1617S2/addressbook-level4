4d979198b98306176c2c799220e00054d393ef2f
Louis Lai <hoangdung2506@gmail.com>
2017-03-04 14:02:22 +0800

    Add AliasController

--- src/main/java/seedu/toluist/dispatcher/CommandDispatcher.java
+++ src/main/java/seedu/toluist/dispatcher/CommandDispatcher.java
@@ -3,9 +3,14 @@ package seedu.toluist.dispatcher;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import seedu.toluist.commons.core.EventsCenter;
 import seedu.toluist.commons.events.ui.NewResultAvailableEvent;
+import seedu.toluist.controller.AliasController;
 import seedu.toluist.controller.Controller;
 import seedu.toluist.controller.ExitController;
 import seedu.toluist.controller.ListController;
@@ -13,11 +18,13 @@ import seedu.toluist.controller.RedoController;
 import seedu.toluist.controller.StoreController;
 import seedu.toluist.controller.TaskController;
 import seedu.toluist.controller.UndoController;
+import seedu.toluist.model.CommandAliasConfig;
 import seedu.toluist.ui.Ui;
 
 public class CommandDispatcher extends Dispatcher {
 
     private final EventsCenter eventsCenter = EventsCenter.getInstance();
+    private final CommandAliasConfig aliasConfig = new CommandAliasConfig();
 
     public void dispatch(Ui renderer, String command) {
         final Controller controller = getBestFitController(renderer, command);
@@ -26,7 +33,12 @@ public class CommandDispatcher extends Dispatcher {
     }
 
     private Controller getBestFitController(Ui renderer, String command) {
-        return getAllControllers(renderer)
+        Collection<Controller> controllerCollection = getAllControllers(renderer);
+        Set<String> reservedKeywords = getControllerKeywords(controllerCollection);
+        // It is okay to do this because the commands are the same throughout
+        aliasConfig.setReservedKeywords(reservedKeywords);
+
+        return controllerCollection
                 .stream()
                 .filter(controller -> controller.matchesCommand(command))
                 .findFirst()
@@ -40,7 +52,17 @@ public class CommandDispatcher extends Dispatcher {
             new UndoController(renderer),
             new RedoController(renderer),
             new ExitController(renderer),
+            new AliasController(renderer, aliasConfig),
             new ListController(renderer)
         }));
+    }
+
+    private Set<String> getControllerKeywords(Collection<Controller> controllerCollection) {
+        List<String> keywordList = controllerCollection
+                .stream()
+                .map(controller -> Arrays.asList(controller.getCommandWords()))
+                .flatMap(List::stream)
+                .collect(Collectors.toList());
+        return new HashSet<>(keywordList);
     }
 }
