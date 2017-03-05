81422bf7a09c6d29c3756b55c72d0455cc042448
Nguyen Quoc Bao <ryuu.sakamoto.164@outlook.com>
2017-03-06 00:53:09 +0800

    Replace Person with Task in logic

---
+++ src/main/java/seedu/address/model/task/Priority.java
@@ -0,0 +1,56 @@
+package seedu.address.model.task;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+
+/**
+ * @author ryuus
+ * Represents a Task's priority in the task manager.
+ * Guarantees: immutable; is valid as declared in {@link #isValidEmail(String)}
+ *
+ */
+public class Priority {
+
+    public static final String MESSAGE_PRIORITY_CONSTRAINTS =
+            "Task priority should be 1 alphanumeric string";
+    public static final String PRIORITY_VALIDATION_REGEX = "[\\w\\.]+";
+
+    public final String value;
+
+    /**
+     * Validates given email.
+     *
+     * @throws IllegalValueException if given email address string is invalid.
+     */
+    public Priority(String priority) throws IllegalValueException {
+        assert priority != null;
+        String trimmedPriority = priority.trim();
+        if (!isValidEmail(trimmedPriority)) {
+            throw new IllegalValueException(MESSAGE_PRIORITY_CONSTRAINTS);
+        }
+        this.value = trimmedPriority;
+    }
+
+    /**
+     * Returns if a given string is a valid person email.
+     */
+    public static boolean isValidEmail(String test) {
+        return test.matches(PRIORITY_VALIDATION_REGEX);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Priority // instanceof handles nulls
+                && this.value.equals(((Priority) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+}
