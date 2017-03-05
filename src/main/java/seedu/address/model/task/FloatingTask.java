585b9cadb1bfffc98070b0dc376f37a74740c6db
Tan Jian Sin <e0003810@u.nus.edu>
2017-03-05 19:24:36 +0800

    Added Task Model with field classes

---
+++ src/main/java/seedu/address/model/task/FloatingTask.java
@@ -0,0 +1,9 @@
+package seedu.address.model.task;
+
+/**
+ * Represents a Floating Task in the task manager.
+ * Guarantees: details are present and not null, field values are validated.
+ */
+public class FloatingTask {
+
+}
