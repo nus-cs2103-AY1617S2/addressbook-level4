535ab660f114fb2aa3d09ec00d402c17570a3d58
ramjanarthan <ram.janarthan11@gmail.com>
2017-03-05 16:18:55 +0800

    Modify AddCommandParser to reflect changes in AddCommand class

--- src/main/java/seedu/doist/logic/parser/AddCommandParser.java
+++ src/main/java/seedu/doist/logic/parser/AddCommandParser.java
@@ -2,6 +2,8 @@ package seedu.doist.logic.parser;
 
 import static seedu.doist.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.doist.logic.parser.CliSyntax.*;
+
+import java.util.ArrayList;
 import java.util.NoSuchElementException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -27,19 +29,32 @@ public class AddCommandParser {
     public Command parse(String args) {
         final Matcher matcher = Add_Command_Regex.matcher(args.trim());
         if (!matcher.matches() || args.trim().equals("")) {
-            System.out.println("hit");
             return new IncorrectCommand(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
         }
 
         final String argument = matcher.group("preamble");
-        final String parameters = matcher.group("parameters");
-              
+        final String parameters = matcher.group("parameters").trim();
+        ArrayList<String> tokens = new ArrayList<String>();
+        for(int i = 0; i < parameters.length(); i ++) {
+            if(parameters.charAt(i) == '\\') {
+                int start = i;
+                while(parameters.charAt(i) != ' '){
+                    i ++;
+                }
+                tokens.add(parameters.substring(start, i));     
+            }
+        }
+        
         ArgumentTokenizer argsTokenizer =
               new ArgumentTokenizer(PREFIX_FROM, PREFIX_TO, PREFIX_REMIND, PREFIX_EVERY, PREFIX_AS, PREFIX_BY, PREFIX_UNDER);
-        argsTokenizer.tokenize(parameters); 
         
+        if(!argsTokenizer.validateTokens(tokens)){
+            return new IncorrectCommand(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
+        }
+        argsTokenizer.tokenize(parameters); 
+
         try {
-            return new AddCommand();
+            return new AddCommand(argument, argsTokenizer.getTokenizedArguments());
         } catch (IllegalValueException ive) {
             return new IncorrectCommand(ive.getMessage());
         }
