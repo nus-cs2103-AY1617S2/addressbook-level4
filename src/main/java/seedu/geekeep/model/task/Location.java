36e600bdc2d8d4b555a8be4072c8aa5d963e5b92
Zhang Hanming <zhang.hanming.official@gmail.com>
2017-03-06 14:32:28 +0800

    add Location Title StartDateTime class

--- src/main/java/seedu/geekeep/model/task/Location.java
+++ src/main/java/seedu/geekeep/model/task/Location.java
@@ -1,5 +1,31 @@
 package seedu.geekeep.model.task;
 
+/**
+ * Represents location of a task
+ */
 public class Location {
+    public final String value;
 
+    public Location(String location) {
+        assert location != null;
+        String trimmedLocation = location.trim();
+        this.value = trimmedLocation;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return obj == this
+                || (obj instanceof Location
+                        && this.value.equals(((Location)obj).value));
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
 }
--- src/main/java/seedu/geekeep/model/task/StartDateTime.java
+++ src/main/java/seedu/geekeep/model/task/StartDateTime.java
@@ -1,5 +1,62 @@
 package seedu.geekeep.model.task;
 
+import java.time.LocalDateTime;
+import java.util.Calendar;
+
+import seedu.geekeep.commons.exceptions.InvalidDateTimeException;
+
+/**
+ * Represents the starting datetime of a task. Guarantees: immutable; is valid as declared in
+ * {@link #isValidDateTime()}
+ */
 public class StartDateTime {
 
+    /**
+     * Validates given dateTime.
+     *
+     * @throws InvalidDateTimeException
+     *             if given datetime is invalid.
+     */
+    public static boolean isValidDateTime(LocalDateTime dateTime) {
+        Calendar cal = Calendar.getInstance();
+        cal.setLenient(false);
+        cal.set(dateTime.getYear(),
+                dateTime.getMonthValue(),
+                dateTime.getDayOfMonth(),
+                dateTime.getHour(),
+                dateTime.getMinute());
+        try {
+            cal.getTime();
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public final LocalDateTime dateTime;
+
+    public StartDateTime(int year, int month, int day, int hour, int minute) throws InvalidDateTimeException{
+        LocalDateTime dateTime = LocalDateTime.of(year, month, day, hour, minute);
+        if(!isValidDateTime(dateTime)) {
+            throw new InvalidDateTimeException();
+        }
+        this.dateTime = dateTime;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof StartDateTime // instanceof handles nulls
+                        && this.dateTime.equals(((StartDateTime) other).dateTime)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return dateTime.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return dateTime.toString();
+    }
 }
