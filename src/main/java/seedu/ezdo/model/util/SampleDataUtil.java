e9ceaff08de3c598f74a56d2416aea224384abbe
Loh Zi Bin Robin <robinloh@u.nus.edu>
2017-03-07 02:48:06 +0800

    Update DueDate in every Task constructor

--- src/main/java/seedu/ezdo/model/util/SampleDataUtil.java
+++ src/main/java/seedu/ezdo/model/util/SampleDataUtil.java
@@ -4,6 +4,7 @@ import seedu.ezdo.commons.exceptions.IllegalValueException;
 import seedu.ezdo.model.EzDo;
 import seedu.ezdo.model.ReadOnlyEzDo;
 import seedu.ezdo.model.tag.UniqueTagList;
+import seedu.ezdo.model.todo.DueDate;
 import seedu.ezdo.model.todo.Email;
 import seedu.ezdo.model.todo.Name;
 import seedu.ezdo.model.todo.Priority;
@@ -17,21 +18,27 @@ public class SampleDataUtil {
             return new Task[] {
                 new Task(new Name("Buy one cherry"), new Priority("1"), new Email("1alexyeoh@gmail.com"),
                     new StartDate("Blk 130 Geylang Street 29, #06-40"),
+                    new DueDate("Blk 130 Geylang Street 29, #06-40"),
                     new UniqueTagList("groceries")),
                 new Task(new Name("Study for two midterms"), new Priority("2"), new Email("2berniceyu@gmail.com"),
                     new StartDate("Blk 230 Lorong 3 Serangoon Gardens, #07-18"),
+                    new DueDate("Blk 230 Lorong 3 Serangoon Gardens, #07-18"),
                     new UniqueTagList("school", "exams")),
                 new Task(new Name("Buy ps three"), new Priority("3"), new Email("3charlotte@yahoo.com"),
                     new StartDate("Blk 311 Ang Mo Kio Street 74, #11-04"),
+                    new DueDate("Blk 311 Ang Mo Kio Street 74, #11-04"),
                     new UniqueTagList("personal")),
                 new Task(new Name("Visit four neighbours"), new Priority("2"), new Email("4lidavid@google.com"),
                     new StartDate("Blk 436 Serangoon Gardens Street 26, #16-43"),
+                    new DueDate("Blk 436 Serangoon Gardens Street 26, #16-43"),
                     new UniqueTagList("personal")),
                 new Task(new Name("Prepare for five finals"), new Priority("3"), new Email("5irfan@outlook.com"),
                     new StartDate("Blk 547 Tampines Street 20, #17-35"),
+                    new DueDate("Blk 547 Tampines Street 20, #17-35"),
                     new UniqueTagList("school", "exams")),
                 new Task(new Name("Prepare six presentations"), new Priority("3"), new Email("6royb@gmail.com"),
                     new StartDate("Blk 645 Aljunied Street 85, #11-31"),
+                    new DueDate("Blk 645 Aljunied Street 85, #11-31"),
                     new UniqueTagList("school", "exams"))
             };
         } catch (IllegalValueException e) {
