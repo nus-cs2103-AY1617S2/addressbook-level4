b821ca636f2dde23b5e7f31d0b500cc3f698d85e
gjlam95 <gj_lam@hotmail.com>
2017-03-07 23:56:22 +0800

    Modify test cases

--- src/test/java/seedu/address/model/task/PriorityTest.java
+++ src/test/java/seedu/address/model/task/PriorityTest.java
@@ -1,22 +1,17 @@
-package seedu.address.model.person;
+package seedu.address.model.task;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-import seedu.address.model.task.Priority;
-
-public class PhoneTest {
+public class PriorityTest {
 
     @Test
     public void isValidPriority() {
         // invalid phone numbers
         assertFalse(Priority.isValidPriority("")); // empty string
         assertFalse(Priority.isValidPriority(" ")); // spaces only
-        assertFalse(Priority.isValidPriority("phone")); // non-numeric
-        assertFalse(Priority.isValidPriority("9011p041")); // alphabets within digits
-        assertFalse(Priority.isValidPriority("9312 1534")); // spaces within digits
 
         // valid phone numbers
         assertTrue(Priority.isValidPriority("93121534"));
