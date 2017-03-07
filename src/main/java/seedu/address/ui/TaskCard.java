d62a6b54c7fc0d1e19c335f79dd9b1192964eb12
Desktop\Clement <clementtan13@hotmail.com>
2017-03-08 02:15:52 +0800

    Add TaskCard class
    Add TaskCard class with proper extends and methods

---
+++ src/main/java/seedu/address/ui/TaskCard.java
@@ -0,0 +1,42 @@
+package seedu.address.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+import seedu.address.model.task.ReadOnlyTask;
+
+public class TaskCard extends UiPart<Region> {
+
+    private static final String FXML = "TaskListCard.fxml";
+
+    @FXML
+    private HBox cardPane;
+    @FXML
+    private Label name;
+    @FXML
+    private Label id;
+    @FXML
+    private Label deadline;
+    @FXML
+    private Label description;
+    @FXML
+    private Label idno;
+    @FXML
+    private FlowPane tags;
+
+    public TaskCard(ReadOnlyTask task, int displayedIndex) {
+        super(FXML);
+        name.setText(task.getName().name);
+        id.setText(displayedIndex + ". ");
+        deadline.setText(task.getDeadline().date.toString());
+        description.setText(task.getDescription().description);
+        idno.setText(task.getID().value.toString());
+        initTags(task);
+    }
+
+    private void initTags(ReadOnlyTask task) {
+        task.getTags().forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
+    }
+}
