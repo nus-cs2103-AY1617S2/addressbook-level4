8a6e57719cc5bfc9111e8bdda2534ac322f720f5
Yi Rui Goh <YiRui@75-252.priv23.nus.edu.sg>
2017-03-07 17:43:37 +0800

    Remove new model

--- src/main/java/seedu/geekeep/model/task/ReadOnlyTask.java
+++
@@ -1,9 +0,0 @@
-package seedu.geekeep.model.task;
-
-public interface ReadOnlyTask {
-    DateTime getEndDateTime();
-    Location getLocation();
-    DateTime getStartDateTime();
-    Title getTitle();
-
-}
