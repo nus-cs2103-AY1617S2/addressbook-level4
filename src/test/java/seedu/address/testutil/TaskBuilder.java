be53c39d152299dd8f26e296773b4683f86bae77
Desktop\Clement <clementtan13@hotmail.com>
2017-03-08 05:58:22 +0800

    Add TaskBuilder class
    Add TaskManagerGuiTest class with proper variables and methods.

---
+++ src/test/java/seedu/address/testutil/TaskBuilder.java
@@ -0,0 +1,61 @@
+package seedu.address.testutil;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.tag.Tag;
+import seedu.address.model.tag.UniqueTagList;
+import seedu.address.model.task.Deadline;
+import seedu.address.model.task.Description;
+import seedu.address.model.task.IdentificationNumber;
+import seedu.address.model.task.Name;
+
+/**
+ *
+ */
+public class TaskBuilder {
+
+    private TestTask task;
+
+    public TaskBuilder() {
+        this.task = new TestTask();
+    }
+
+    /**
+     * Initializes the TaskBuilder with the data of {@code taskToCopy}.
+     */
+    public TaskBuilder(TestTask taskToCopy) {
+        this.task = new TestTask(taskToCopy);
+    }
+
+    public TaskBuilder withName(String name) throws IllegalValueException {
+        this.task.setName(new Name(name));
+        return this;
+    }
+
+    public TaskBuilder withTags(String ... tags) throws IllegalValueException {
+        task.setTags(new UniqueTagList());
+        for (String tag: tags) {
+            task.getTags().add(new Tag(tag));
+        }
+        return this;
+    }
+
+    public TaskBuilder withDeadline(String deadline) throws IllegalValueException {
+        this.task.setDeadline(new Deadline(deadline));
+        return this;
+    }
+
+    public TaskBuilder withDescription(String description) throws IllegalValueException {
+        this.task.setDescription(new Description(description));
+        return this;
+    }
+
+    public TaskBuilder withID(String id) throws IllegalValueException {
+        this.task.setID(new IdentificationNumber(id));
+        return this;
+    }
+
+    public TestTask build() {
+        return this.task;
+    }
+
+}
