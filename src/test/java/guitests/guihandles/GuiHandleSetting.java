89e6a5a98c61b1a59d40f73d5abfd6d8cb8991c5
LuoYuyang <royl8@outlook.com>
2017-03-07 18:00:06 +0800

    - further extend the sleep time for testing dialogue so that it will fail accidentally

--- src/test/java/guitests/guihandles/GuiHandleSetting.java
+++ src/test/java/guitests/guihandles/GuiHandleSetting.java
@@ -3,5 +3,5 @@ package guitests.guihandles;
 public class GuiHandleSetting {
     static final int COMMAND_RUN_TIME = 80;
     static final int SLEEP_LENGTH = 50;
-    static final int LONG_SLEEP_LENGTH = 200;
+    static final int LONG_SLEEP_LENGTH = 600;
 }
