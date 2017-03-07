d24ca9e778d9b3c432865a7e05494f787edbcb92
Jordan May <jordaanmay@gmail.com>
2017-03-07 14:17:40 +0800

    Change packages in AddCommand.java

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -5,12 +5,11 @@ import java.util.Set;
 
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.logic.commands.exceptions.CommandException;
-import seedu.address.model.person.Address;
-import seedu.address.model.person.Email;
-import seedu.address.model.person.Name;
-import seedu.address.model.person.Person;
-import seedu.address.model.person.Phone;
-import seedu.address.model.person.UniquePersonList;
+import seedu.address.model.todo.Name;
+import seedu.address.model.todo.ToDo;
+import seedu.address.model.todo.StartTime;
+import seedu.address.model.todo.EndTime;
+import seedu.address.model.todo.UniqueToDoList;
 import seedu.address.model.tag.Tag;
 import seedu.address.model.tag.UniqueTagList;
 
