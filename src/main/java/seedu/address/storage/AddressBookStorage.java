355039bb5631569a398ff34ecf4d91e6eb96569a
Yanzhang Shi <yanzhang.cgc@gmail.com>
2017-03-07 18:13:03 +0800

    Refactor old addressBook references to taskManager in AddressBookStorage

--- src/main/java/seedu/address/storage/AddressBookStorage.java
+++ src/main/java/seedu/address/storage/AddressBookStorage.java
@@ -17,7 +17,7 @@ public interface AddressBookStorage {
     String getAddressBookFilePath();
 
     /**
-     * Returns AddressBook data as a {@link ReadOnlyTaskManager}.
+     * Returns TaskManager data as a {@link ReadOnlyTaskManager}.
      *   Returns {@code Optional.empty()} if storage file is not found.
      * @throws DataConversionException if the data in storage is not in the expected format.
      * @throws IOException if there was any problem when reading from the storage.
@@ -31,14 +31,14 @@ public interface AddressBookStorage {
 
     /**
      * Saves the given {@link ReadOnlyTaskManager} to the storage.
-     * @param addressBook cannot be null.
+     * @param taskManager cannot be null.
      * @throws IOException if there was any problem writing to the file.
      */
-    void saveAddressBook(ReadOnlyTaskManager addressBook) throws IOException;
+    void saveAddressBook(ReadOnlyTaskManager taskManager) throws IOException;
 
     /**
      * @see #saveAddressBook(ReadOnlyTaskManager)
      */
-    void saveAddressBook(ReadOnlyTaskManager addressBook, String filePath) throws IOException;
+    void saveAddressBook(ReadOnlyTaskManager taskManager, String filePath) throws IOException;
 
 }
