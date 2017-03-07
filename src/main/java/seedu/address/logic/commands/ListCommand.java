6682897cff0dcae1c81ae9371c5a7ed61f5672a7
riveria94 <timelesschaos94@gmail.com>
2017-03-07 16:18:25 +0800

    Add different command trigger in the ListCommand object

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -7,8 +7,11 @@ package seedu.address.logic.commands;
 public class ListCommand extends Command {
 
     public static final String COMMAND_WORD = "list";
+    public static final String COMMAND_WORD_SUFFIX_TITLE = "list title";
+    public static final String COMMAND_WORD_SUFFIX_DATE = "list date";
+    public static final String COMMAND_WORD_SUFFIX_TAG = "list tag";
 
-    public static final String MESSAGE_SUCCESS = "Listed all persons";
+    public static final String MESSAGE_SUCCESS = "Listed all items";
 
 
     @Override
