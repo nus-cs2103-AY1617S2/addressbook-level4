12afae24cde1495f2cd94cbd7ed6233841db3a23
Yi Rui Goh <YiRui@75-252.priv23.nus.edu.sg>
2017-03-07 21:17:29 +0800

    Refactor AddCommand to reference Task instead of Person

--- src/main/java/seedu/geekeep/logic/commands/AddCommand.java
+++ src/main/java/seedu/geekeep/logic/commands/AddCommand.java
@@ -22,12 +22,12 @@ public class AddCommand extends Command {
     public static final String COMMAND_WORD = "add";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a task to the Task Manager. "
-            + "Parameters: TITLE st/STARTING_DATETIME et/EMAIL a/ADDRESS  [t/TAG]...\n"
+            + "Parameters: TITLE st/STARTING_DATETIME et/ENDING_DATETIME l/LOCATION [t/TAG]...\n"
             + "Example: " + COMMAND_WORD
-            + " John Doe p/98765432 e/johnd@gmail.com a/311, Clementi Ave 2, #02-25 t/friends t/owesMoney";
+            + " Meeting 1 st/2017-04-01T10:16:30 et/2017-04-01T10:17:30 l/311, Clementi Ave 2, #02-25 t/friends";
 
-    public static final String MESSAGE_SUCCESS = "New person added: %1$s";
-    public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the address book";
+    public static final String MESSAGE_SUCCESS = "New task added: %1$s";
+    public static final String MESSAGE_DUPLICATE_TASK = "This task already exists in the address book";
 
     private final Task toAdd;
 
@@ -36,17 +36,17 @@ public class AddCommand extends Command {
      *
      * @throws IllegalValueException if any of the raw values are invalid
      */
-    public AddCommand(String name, String phone, String email, String address, Set<String> tags)
+    public AddCommand(String title, String startDateTime, String endDateTime, String location, Set<String> tags)
             throws IllegalValueException {
         final Set<Tag> tagSet = new HashSet<>();
         for (String tagName : tags) {
             tagSet.add(new Tag(tagName));
         }
         this.toAdd = new Task(
-                new Title(name),
-                new StartDateTime(phone),
-                new EndDateTime(email),
-                new Location(address),
+                new Title(title),
+                new StartDateTime(startDateTime),
+                new EndDateTime(endDateTime),
+                new Location(location),
                 new UniqueTagList(tagSet)
         );
     }
@@ -58,7 +58,7 @@ public class AddCommand extends Command {
             model.addTask(toAdd);
             return new CommandResult(String.format(MESSAGE_SUCCESS, toAdd));
         } catch (UniqueTaskList.DuplicateTaskException e) {
-            throw new CommandException(MESSAGE_DUPLICATE_PERSON);
+            throw new CommandException(MESSAGE_DUPLICATE_TASK);
         }
 
     }
