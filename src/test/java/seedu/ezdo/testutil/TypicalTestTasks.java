1bb1db7a646923f922596e330d04d5833e3b3744
Loh Zi Bin Robin <robinloh@u.nus.edu>
2017-03-07 13:22:32 +0800

    Add due dates in all test tasks

--- src/test/java/seedu/ezdo/testutil/TypicalTestTasks.java
+++ src/test/java/seedu/ezdo/testutil/TypicalTestTasks.java
@@ -15,27 +15,27 @@ public class TypicalTestTasks {
     public TypicalTestTasks() {
         try {
             alice = new TaskBuilder().withName("Alice Pauline")
-                    .withStartDate("123, Jurong West Ave 6, #08-111").withEmail("alice@gmail.com")
-                    .withPriority("1")
+                    .withStartDate("123, Jurong West Ave 6, #08-113").withEmail("alice@gmail.com")
+                    .withPriority("1").withDueDate("14/03/2015")
                     .withTags("friends").build();
             benson = new TaskBuilder().withName("Benson Meier").withStartDate("311, Clementi Ave 2, #02-25")
-                    .withEmail("johnd@gmail.com").withPriority("1")
+                    .withEmail("johnd@gmail.com").withDueDate("12/02/2015").withPriority("1")
                     .withTags("owesMoney", "friends").build();
-            carl = new TaskBuilder().withName("Carl Kurz").withPriority("2")
+            carl = new TaskBuilder().withName("Carl Kurz").withPriority("2").withDueDate("13/03/2015")
                     .withEmail("heinz@yahoo.com").withStartDate("wall street").build();
-            daniel = new TaskBuilder().withName("Daniel Meier").withPriority("2")
+            daniel = new TaskBuilder().withName("Daniel Meier").withPriority("2").withDueDate("14/04/2015")
                     .withEmail("cornelia@google.com").withStartDate("10th street").build();
-            elle = new TaskBuilder().withName("Elle Meyer").withPriority("3")
+            elle = new TaskBuilder().withName("Elle Meyer").withPriority("3").withDueDate("15/05/2015")
                     .withEmail("werner@gmail.com").withStartDate("michegan ave").build();
-            fiona = new TaskBuilder().withName("Fiona Kunz").withPriority("2")
+            fiona = new TaskBuilder().withName("Fiona Kunz").withPriority("2").withDueDate("16/06/2015")
                     .withEmail("lydia@gmail.com").withStartDate("little tokyo").build();
-            george = new TaskBuilder().withName("George Best").withPriority("3")
+            george = new TaskBuilder().withName("George Best").withPriority("3").withDueDate("17/07/2015")
                     .withEmail("anna@google.com").withStartDate("4th street").build();
 
             // Manually added
-            hoon = new TaskBuilder().withName("Hoon Meier").withPriority("1")
+            hoon = new TaskBuilder().withName("Hoon Meier").withPriority("1").withDueDate("18/08/2015")
                     .withEmail("stefan@mail.com").withStartDate("little india").build();
-            ida = new TaskBuilder().withName("Ida Mueller").withPriority("2")
+            ida = new TaskBuilder().withName("Ida Mueller").withPriority("2").withDueDate("19/09/2015")
                     .withEmail("hans@google.com").withStartDate("chicago ave").build();
         } catch (IllegalValueException e) {
             e.printStackTrace();
