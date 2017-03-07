dc5407b7cfc90a22dd800f9ed62482a449337958
Jordan May <jordaanmay@gmail.com>
2017-03-07 17:27:56 +0800

    Refactor TodoListChangedEvent.java

--- src/main/java/seedu/address/commons/events/model/TodoListChangedEvent.java
+++ src/main/java/seedu/address/commons/events/model/TodoListChangedEvent.java
@@ -1,19 +1,19 @@
 package seedu.address.commons.events.model;
 
 import seedu.address.commons.events.BaseEvent;
-import seedu.address.model.ReadOnlyTaskList;
+import seedu.address.model.ReadOnlyTodoList;
 
-/** Indicates the AddressBook in the model has changed*/
-public class AddressBookChangedEvent extends BaseEvent {
+/** Indicates the TodoList in the model has changed*/
+public class TodoListChangedEvent extends BaseEvent {
 
-    public final ReadOnlyTaskList data;
+    public final ReadOnlyTodoList data;
 
-    public AddressBookChangedEvent(ReadOnlyTaskList data) {
+    public TodoListChangedEvent(ReadOnlyTodoList data) {
         this.data = data;
     }
 
     @Override
     public String toString() {
-        return "number of persons " + data.getPersonList().size() + ", number of tags " + data.getTagList().size();
+        return "number of todos " + data.getTodoList().size() + ", number of tags " + data.getTagList().size();
     }
 }
