08f4de8603b3de34c8da44f02831f5e1a9888196
DESKTOP-49OHI99\user <pengchengan@u.nus.edu>
2017-03-08 21:53:19 +0800

    fix minor problem

--- src/test/java/seedu/address/logic/LogicManagerTest.java
+++ src/test/java/seedu/address/logic/LogicManagerTest.java
@@ -188,7 +188,7 @@ public class LogicManagerTest {
         assertCommandFailure("add Valid Name p/not_numbers e/valid@e.mail a/valid, address",
                 Name.MESSAGE_NAME_CONSTRAINTS);
         assertCommandFailure("add Valid Name p/12345 e/notAnEmail a/valid, address",
-        	    Name.MESSAGE_NAME_CONSTRAINTS);
+                Name.MESSAGE_NAME_CONSTRAINTS);
         assertCommandFailure("add Valid Name p/12345 e/valid@e.mail a/valid, address t/invalid_-[.tag",
                 Tag.MESSAGE_TAG_CONSTRAINTS);
 
