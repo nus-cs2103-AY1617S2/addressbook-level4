f1af177dad59b9bf6dbf5be30f4c253f56601eee
Brea <dionisio@live.unc.edu>
2017-03-08 21:48:57 +0800

    Reduce AddCommand to 3 parameters

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -37,7 +37,7 @@ public class AddCommand extends Command {
      *
      * @throws IllegalValueException if any of the raw values are invalid
      */
-    public AddCommand(String name, String deadlineTime, String priority, String description, Set<String> tags)
+    public AddCommand(String name, String description, Set<String> tags)
             throws IllegalValueException {
         final Set<Tag> tagSet = new HashSet<>();
         for (String tagName : tags) {
