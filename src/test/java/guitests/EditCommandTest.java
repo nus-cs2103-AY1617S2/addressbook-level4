8cf86cdb1df1bd2cc7b9c8644be515183861c4ef
Evan Yeung <evan.yeung1@gmail.com>
2017-03-08 22:57:29 +0800

    Rename AddressBookGuiTest to TaskListGuiTest

--- src/test/java/guitests/EditCommandTest.java
+++ src/test/java/guitests/EditCommandTest.java
@@ -14,7 +14,7 @@ import seedu.task.testutil.TaskBuilder;
 import seedu.task.testutil.TestTask;
 
 // TODO: reduce GUI tests by transferring some tests to be covered by lower level tests.
-public class EditCommandTest extends AddressBookGuiTest {
+public class EditCommandTest extends TaskListGuiTest {
 
     // The list of tasks in the task list panel is expected to match this list.
     // This list is updated with every successful call to assertEditSuccess().
--- src/test/java/guitests/SelectCommandTest.java
+++ src/test/java/guitests/SelectCommandTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 
 import seedu.task.model.task.ReadOnlyTask;
 
-public class SelectCommandTest extends AddressBookGuiTest {
+public class SelectCommandTest extends TaskListGuiTest {
 
 
     @Test
