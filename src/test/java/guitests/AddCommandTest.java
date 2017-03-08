285355148ad0b1ff923eb5e92fb8dacd802538c6
DESKTOP-49OHI99\user <pengchengan@u.nus.edu>
2017-03-08 21:01:54 +0800

    delete white space

--- src/test/java/guitests/AddCommandTest.java
+++ src/test/java/guitests/AddCommandTest.java
@@ -17,7 +17,7 @@ public class AddCommandTest extends TodoListGuiTest {
         //add one todo
         TestTodo[] currentList = td.getTypicalTodos();
         TestTodo todoToAdd = td.hoon;
-        
+
         assertAddSuccess(todoToAdd, currentList);
         currentList = TestUtil.addTodosToList(currentList, todoToAdd);
 
@@ -38,7 +38,7 @@ public class AddCommandTest extends TodoListGuiTest {
         //invalid command
         commandBox.runCommand("adds Johnny");
         assertResultMessage(Messages.MESSAGE_UNKNOWN_COMMAND);
-        
+
     }
 
     private void assertAddSuccess(TestTodo todoToAdd, TestTodo... currentList) {
