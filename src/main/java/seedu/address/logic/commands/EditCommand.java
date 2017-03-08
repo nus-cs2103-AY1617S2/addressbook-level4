e55672ad2cb34b43c0184eb7ae89418d4e557f57
nuslarry <e0147977@u.nus.edu>
2017-03-09 01:51:48 +0800

    fix wrong lexicographical error

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -7,11 +7,11 @@ import seedu.address.commons.core.Messages;
 import seedu.address.commons.util.CollectionUtil;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.tag.UniqueTagList;
-import seedu.address.model.task.Email;
 import seedu.address.model.task.Description;
+import seedu.address.model.task.Email;
 import seedu.address.model.task.Priority;
-import seedu.address.model.task.Task;
 import seedu.address.model.task.ReadOnlyTask;
+import seedu.address.model.task.Task;
 import seedu.address.model.task.UniqueTaskList;
 
 /**
