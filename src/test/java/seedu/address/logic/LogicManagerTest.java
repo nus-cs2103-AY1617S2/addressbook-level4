8425036cc89afbe5288e7acbff25de6acc856ee8
nuslarry <e0147977@u.nus.edu>
2017-03-09 02:24:27 +0800

    indentation fix

--- src/test/java/seedu/address/logic/LogicManagerTest.java
+++ src/test/java/seedu/address/logic/LogicManagerTest.java
@@ -185,13 +185,15 @@ public class LogicManagerTest {
 
 
     @Test
-    public void execute_add_invalidArgsFormat() {
-        String expectedMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE);
-        assertCommandFailure("add wrong args wrong args", expectedMessage);
-assertCommandFailure("add Valid Description 12345 e/valid@email.butNoPhonePrefix a/valid,address",expectedMessage);
-assertCommandFailure("add Valid Description p/12345 valid@email.butNoPrefix a/valid, address",expectedMessage);
-assertCommandFailure("add Valid Description p/12345 e/valid@email.butNoAddressPrefix valid, address", expectedMessage);
-    }
+	public void execute_add_invalidArgsFormat() {
+		String expectedMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE);
+		assertCommandFailure("add wrong args wrong args", expectedMessage);
+		assertCommandFailure("add Valid Description 12345 e/valid@email.butNoPhonePrefix a/valid,address",
+				expectedMessage);
+		assertCommandFailure("add Valid Description p/12345 valid@email.butNoPrefix a/valid, address", expectedMessage);
+		assertCommandFailure("add Valid Description p/12345 e/valid@email.butNoAddressPrefix valid, address",
+				expectedMessage);
+	}
 
     @Test
     public void execute_add_invalidPersonData() {
