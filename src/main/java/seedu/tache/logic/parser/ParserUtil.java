307eeddccc037fc2014218f8ff291787adca2b10
Tan Jian Sin <e0003810@u.nus.edu>
2017-03-08 11:03:29 +0800

    Added regular expression and corressponding methods to check validity of Date, Time and Duration

--- src/main/java/seedu/tache/logic/parser/ParserUtil.java
+++ src/main/java/seedu/tache/logic/parser/ParserUtil.java
@@ -26,6 +26,9 @@ import seedu.tache.model.tag.UniqueTagList;
 public class ParserUtil {
 
     private static final Pattern INDEX_ARGS_FORMAT = Pattern.compile("(?<targetIndex>.+)");
+    private static final Pattern DATE_FORMAT = Pattern.compile("^[0-3]?[0-9]/[0-1]?[0-9]/(?:[0-9]{2})?[0-9]{2}$|^[0-3]?[0-9]-[0-1]?[0-9]-(?:[0-9]{2})?[0-9]{2}$|^[0-3]{1}[0-9]{1}[0-1]{1}[0-9]{1}(?:[0-9]{2})?[0-9]{2}$");
+    private static final Pattern TIME_FORMAT = Pattern.compile("^[0-2][0-9][0-5][0-9]|^([0-1][0-2]|[0-9])([.][0-5][0-9])?\\s?(am|pm){1}");
+    private static final Pattern DURATION_FORMAT = Pattern.compile("^\\d+\\s?((h|hr|hrs)|(m|min|mins))");
 
     /**
      * Returns the specified index in the {@code command} if it is a positive unsigned integer
@@ -108,5 +111,38 @@ public class ParserUtil {
             tagSet.add(new Tag(tagName));
         }
         return new UniqueTagList(tagSet);
+    }
+    /**
+     * Returns True if input is a valid date
+     * Returns False otherwise.
+     */
+    public static boolean isValidDate(String input) {
+        final Matcher matcher = DATE_FORMAT.matcher(input.trim());
+        if (matcher.matches()) {
+            return true;
+        }
+        return false;
+    }
+    /**
+     * Returns True if input is a valid time
+     * Returns False otherwise.
+     */
+    public static boolean isValidTime(String input) {
+        final Matcher matcher = TIME_FORMAT.matcher(input.trim());
+        if (matcher.matches()) {
+            return true;
+        }
+        return false;
+    }
+    /**
+     * Returns True if input is a valid duration
+     * Returns False otherwise.
+     */
+    public static boolean isValidDuration(String input) {
+        final Matcher matcher = DURATION_FORMAT.matcher(input.trim());
+        if (matcher.matches()) {
+            return true;
+        }
+        return false;
     }
 }
