e10a3c777070c06d60a819466a8893d5686c2d56
Lee Jin Shun <ljsrockz@gmail.com>
2017-03-08 22:44:20 +0800

    Refactor addressBook to TaskManager

--- src/test/java/seedu/doit/storage/StorageManagerTest.java
+++ src/test/java/seedu/doit/storage/StorageManagerTest.java
@@ -58,7 +58,7 @@ public class StorageManagerTest {
     }
 
     @Test
-    public void addressBookReadSave() throws Exception {
+    public void taskManagerReadSave() throws Exception {
         /*
          * Note: This is an integration test that verifies the StorageManager is properly wired to the
          * {@link XmlTaskManagerStorage} class.
