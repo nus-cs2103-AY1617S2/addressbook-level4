67f276f2107c34230469356979034c59df5711b1
Lok You Tan <e0003133@u.nus.edu>
2017-03-09 00:41:04 +0800

    Add SaveCommandTest.java

---
+++ src/test/java/guitests/SaveCommandTest.java
@@ -0,0 +1,31 @@
+package guitests;
+
+import org.junit.Test;
+
+import seedu.ezdo.logic.commands.SaveCommand;
+import static seedu.ezdo.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+public class SaveCommandTest extends EzDoGuiTest {
+
+    private final String validDirectory = "data/";
+    private final String invalidDirectory = "data/COWABUNGA";
+
+    @Test
+    public void save_validDirectory_success() {
+        commandBox.runCommand("save " + validDirectory);
+        assertResultMessage(String.format(SaveCommand.MESSAGE_SAVE_TASK_SUCCESS, validDirectory));
+    }
+
+    @Test
+    public void save_invalidFormat_failure() {
+        commandBox.runCommand("save");
+        assertResultMessage(String.format(MESSAGE_INVALID_COMMAND_FORMAT, SaveCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void save_invalidDirectory_failure() {
+        commandBox.runCommand("save " + invalidDirectory);
+        assertResultMessage(String.format(SaveCommand.MESSAGE_DIRECTORY_PATH_INVALID));
+    }
+
+}
