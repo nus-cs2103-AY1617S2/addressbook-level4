5cc78df9a8756907a0baf38c0c553fa8f8d5097f
Azraelos <andrewsugiarto93@gmail.com>
2017-03-08 22:53:54 +0800

    renamed address book to watodo

--- src/main/java/seedu/address/commons/core/Config.java
+++ src/main/java/seedu/address/commons/core/Config.java
@@ -15,7 +15,7 @@ public class Config {
     private Level logLevel = Level.INFO;
     private String userPrefsFilePath = "preferences.json";
     private String watodoFilePath = "data/watodo.xml";
-    private String addressBookName = "MyAddressBook";
+    private String watodoName = "My Tasks";
 
 
     public String getAppTitle() {
@@ -42,20 +42,20 @@ public class Config {
         this.userPrefsFilePath = userPrefsFilePath;
     }
 
-    public String getAddressBookFilePath() {
+    public String getWatodoFilePath() {
         return watodoFilePath;
     }
 
-    public void setAddressBookFilePath(String watodoFilePath) {
+    public void setWatodoFilePath(String watodoFilePath) {
         this.watodoFilePath = watodoFilePath;
     }
 
-    public String getAddressBookName() {
-        return addressBookName;
+    public String getWatodoName() {
+        return watodoName;
     }
 
-    public void setAddressBookName(String addressBookName) {
-        this.addressBookName = addressBookName;
+    public void setWatodoName(String watodoName) {
+        this.watodoName = watodoName;
     }
 
 
@@ -74,12 +74,12 @@ public class Config {
                 && Objects.equals(logLevel, o.logLevel)
                 && Objects.equals(userPrefsFilePath, o.userPrefsFilePath)
                 && Objects.equals(watodoFilePath, o.watodoFilePath)
-                && Objects.equals(addressBookName, o.addressBookName);
+                && Objects.equals(watodoName, o.watodoName);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(appTitle, logLevel, userPrefsFilePath, watodoFilePath, addressBookName);
+        return Objects.hash(appTitle, logLevel, userPrefsFilePath, watodoFilePath, watodoName);
     }
 
     @Override
@@ -89,7 +89,7 @@ public class Config {
         sb.append("\nCurrent log level : " + logLevel);
         sb.append("\nPreference file Location : " + userPrefsFilePath);
         sb.append("\nLocal data file location : " + watodoFilePath);
-        sb.append("\nAddressBook name : " + addressBookName);
+        sb.append("\nWatodo name : " + watodoName);
         return sb.toString();
     }
 
