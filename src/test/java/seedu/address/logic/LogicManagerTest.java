f289d4e7ea3cdb8b48fdf415ba5bd6d9f0166d65
nuslarry <e0147977@u.nus.edu>
2017-03-09 02:41:10 +0800

    fix 3

--- src/test/java/seedu/address/logic/LogicManagerTest.java
+++ src/test/java/seedu/address/logic/LogicManagerTest.java
@@ -190,7 +190,7 @@ public class LogicManagerTest {
 	    assertCommandFailure("add wrong args wrong args", expectedMessage);
 	    assertCommandFailure("add Valid Description 12345 e/valid@email.butNoPhonePrefix a/valid,address",
 				expectedMessage);
-	    assertCommandFailure("add Valid Description p/1234 valid@email.butNoPrefix a/valid, address", 
+	    assertCommandFailure("add Valid Description p/1234 valid@email.butNoPrefix a/valid, address",
 				expectedMessage);
 	    assertCommandFailure("add Valid Description p/12345 e/valid@email.butNoAddressPrefix valid, address",
 				expectedMessage);
