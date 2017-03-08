993679301cc24d0195c9446ae3bc012e85a624c5
WUHEYANG\wuheyang0617 <wuheyang0617@gmail.com>
2017-03-08 16:09:10 +0800

    Fixing checkstyle

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -3,7 +3,6 @@ package seedu.address.model.util;
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.AddressBook;
 import seedu.address.model.ReadOnlyAddressBook;
-import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
