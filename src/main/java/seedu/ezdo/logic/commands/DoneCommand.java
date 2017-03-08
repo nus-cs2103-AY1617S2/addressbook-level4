e087c3a8f4bac07a40f9c740230b608b5eb088a3
Acer\zhi liang <chenzhiliang94@gmail.com>
2017-03-09 00:49:24 +0800

    Add DoneCommand class

---
+++ src/main/java/seedu/ezdo/logic/commands/DoneCommand.java
@@ -0,0 +1,51 @@
+package seedu.ezdo.logic.commands;
+
+import seedu.ezdo.commons.core.Messages;
+import seedu.ezdo.commons.core.UnmodifiableObservableList;
+import seedu.ezdo.logic.commands.exceptions.CommandException;
+import seedu.ezdo.model.todo.ReadOnlyTask;
+import seedu.ezdo.model.todo.UniqueTaskList.TaskNotFoundException;
+
+/**
+ * Marks a task as identified using its last displayed index from ezDo as done
+ */
+public class DoneCommand extends Command {
+
+    public static final String COMMAND_WORD = "done";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Marks the task identified by the index number used in the last task listing as done\n"
+            + "Parameters: INDEX (must be a positive integer)\n"
+            + "Example: " + COMMAND_WORD + " 1";
+
+    public static final String MESSAGE_DONE_TASK_SUCCESS = "Done Task: %1$s";
+
+    public final int targetIndex;
+
+    public DoneCommand(int targetIndex) {
+        this.targetIndex = targetIndex;
+    }
+
+
+    @Override
+    public CommandResult execute() throws CommandException {
+
+        UnmodifiableObservableList<ReadOnlyTask> lastShownList = model.getFilteredTaskList();
+
+        if (lastShownList.size() < targetIndex) {
+            throw new CommandException(Messages.MESSAGE_INVALID_TASK_DISPLAYED_INDEX);
+        }
+
+        ReadOnlyTask taskToDone = lastShownList.get(targetIndex - 1);
+
+        try {
+            model.killTask(taskToDone);
+            
+        } catch (TaskNotFoundException tnfe) {
+            assert false : "The target task cannot be missing";
+        }
+
+        return new CommandResult(String.format(MESSAGE_DONE_TASK_SUCCESS, taskToDone));
+    }
+
+}
