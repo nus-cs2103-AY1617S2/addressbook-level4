596039760206cbe5bde0736e9af2a9c88dcaa93b
Evan Yeung <evan.yeung1@gmail.com>
2017-03-09 00:04:10 +0800

    Refactor addressbook.xml to tasklist.xml

--- src/test/java/seedu/task/commons/core/ConfigTest.java
+++ src/test/java/seedu/task/commons/core/ConfigTest.java
@@ -17,7 +17,7 @@ public class ConfigTest {
         String defaultConfigAsString = "App title : Address App\n" +
                 "Current log level : INFO\n" +
                 "Preference file Location : preferences.json\n" +
-                "Local data file location : data/addressbook.xml\n" +
+                "Local data file location : data/tasklist.xml\n" +
                 "AddressBook name : MyAddressBook";
 
         assertEquals(defaultConfigAsString, new Config().toString());
