8b36f7d4a42993e1311ab2f22563ef2be8390bfe
Lok You Tan <e0003133@u.nus.edu>
2017-03-09 00:11:28 +0800

    Create SaveCommandParser.java

---
+++ src/main/java/seedu/ezdo/logic/parser/SaveCommandParser.java
@@ -0,0 +1,32 @@
+package seedu.ezdo.logic.parser;
+
+import static seedu.ezdo.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.ezdo.logic.parser.CliSyntax.PREFIX_DUEDATE;
+import static seedu.ezdo.logic.parser.CliSyntax.PREFIX_PRIORITY;
+import static seedu.ezdo.logic.parser.CliSyntax.PREFIX_STARTDATE;
+import static seedu.ezdo.logic.parser.CliSyntax.PREFIX_TAG;
+
+import java.util.NoSuchElementException;
+
+import seedu.ezdo.commons.exceptions.IllegalValueException;
+import seedu.ezdo.logic.commands.Command;
+import seedu.ezdo.logic.commands.IncorrectCommand;
+import seedu.ezdo.logic.commands.SaveCommand;
+
+public class SaveCommandParser {
+
+    public Command parse(String args) {
+        assert args != null;
+        System.out.println("Arguments are:" + args);
+        ArgumentTokenizer argsTokenizer = new ArgumentTokenizer();
+        argsTokenizer.tokenize(args);
+        try {
+            return new SaveCommand(argsTokenizer.getPreamble().get());
+        } catch (IllegalValueException ive) {
+            return new IncorrectCommand(ive.getMessage());
+        } catch (NoSuchElementException nsee) {
+            return new IncorrectCommand(String.format(MESSAGE_INVALID_COMMAND_FORMAT, SaveCommand.MESSAGE_USAGE));
+        }
+    }
+
+}
