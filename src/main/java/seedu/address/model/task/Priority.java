a0457c2c5e298606ce26dec5989cc784dca7c6b1
LWWenz <leongww7@gmail.com>
2017-03-09 18:06:43 +0800

    remove trailing whitespace

--- src/main/java/seedu/address/model/task/Priority.java
+++ src/main/java/seedu/address/model/task/Priority.java
@@ -3,10 +3,10 @@ package seedu.address.model.task;
 public class Priority {
 	
 	private boolean priority;
-	
+
 	public static final String MESSAGE_PRIORITY_CONSTRAINTS =
 			"Priority should be either in true or false.";
-	
+
 	public Priority () {
 		this.priority = false;
 	}
@@ -14,7 +14,7 @@ public class Priority {
 	public Priority(boolean priority) {
 		this.priority = priority;
 	}
-	
+
 	public Priority(String priority) {
 		String amended = priority.substring(2).trim();
 		if(amended.toLowerCase().compareTo("true") == 0) {
@@ -24,11 +24,11 @@ public class Priority {
 			this.priority = false;
 		}
 	}
-	
+
 	public boolean getPriority() {
 		return priority;
 	}
-	
+
 
     @Override
     public int hashCode() {
