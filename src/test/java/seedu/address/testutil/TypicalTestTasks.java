c981a2f11a2761269bba4bc5e4e221e8a83b892c
Zhiyuan <jameslaizy@gmail.com>
2017-03-09 12:50:21 +0800

    Update various test cases to include time for nlp date parser

--- src/test/java/seedu/address/testutil/TypicalTestTasks.java
+++ src/test/java/seedu/address/testutil/TypicalTestTasks.java
@@ -15,18 +15,18 @@ public class TypicalTestTasks {
     public TypicalTestTasks() {
         try {
             task1 = new TaskBuilder().withTitle("Complete task 1")
-                    .withDeadline("11-11-2017").withLabels("friends").build();
-            task2 = new TaskBuilder().withTitle("Complete task 2").withDeadline("11-11-2017")
+                    .withDeadline("11-11-2017 2300").withLabels("friends").build();
+            task2 = new TaskBuilder().withTitle("Complete task 2").withDeadline("11-11-2017 2300")
                     .withLabels("owesMoney", "friends").build();
-            task3 = new TaskBuilder().withTitle("Complete task 3").withDeadline("11-11-2017").build();
-            task4 = new TaskBuilder().withTitle("Complete task 4").withDeadline("11-11-2017").build();
-            task5 = new TaskBuilder().withTitle("Complete task 5").withDeadline("11-11-2017").build();
-            task6 = new TaskBuilder().withTitle("Complete task 6").withDeadline("11-11-2017").build();
-            task7 = new TaskBuilder().withTitle("Complete task 7").withDeadline("11-11-2017").build();
+            task3 = new TaskBuilder().withTitle("Complete task 3").withDeadline("11-11-2017 2300").build();
+            task4 = new TaskBuilder().withTitle("Complete task 4").withDeadline("11-11-2017 2300").build();
+            task5 = new TaskBuilder().withTitle("Complete task 5").withDeadline("11-11-2017 2300").build();
+            task6 = new TaskBuilder().withTitle("Complete task 6").withDeadline("10-11-2017 2300").build();
+            task7 = new TaskBuilder().withTitle("Complete task 7").withDeadline("11-11-2017 2300").build();
 
             // Manually added
-            task8 = new TaskBuilder().withTitle("Complete task 8").withDeadline("11-11-2017").build();
-            task9 = new TaskBuilder().withTitle("Complete task 9").withDeadline("11-11-2017").build();
+            task8 = new TaskBuilder().withTitle("Complete task 8").withDeadline("11-11-2017 2300").build();
+            task9 = new TaskBuilder().withTitle("Complete task 9").withDeadline("11-11-2017 2300").build();
         } catch (IllegalValueException e) {
             e.printStackTrace();
             assert false : "not possible";
