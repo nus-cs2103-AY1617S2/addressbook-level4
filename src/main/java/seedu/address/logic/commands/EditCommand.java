60258ffafe3aa83d7008f75c95ab118a13b6e5a1
Jia Yilin <e0007663@u.nus.edu>
2017-03-10 00:15:27 +0800

    Update EditCommand:reorder import files

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -6,12 +6,12 @@ import java.util.Optional;
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.util.CollectionUtil;
 import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.tag.UniqueTagList;
 import seedu.address.model.task.EndTime;
 import seedu.address.model.task.ReadOnlyTask;
 import seedu.address.model.task.StartTime;
 import seedu.address.model.task.Task;
 import seedu.address.model.task.Title;
-import seedu.address.model.tag.UniqueTagList;
 import seedu.address.model.task.UniqueTaskList;
 import seedu.address.model.task.Venue;
 
