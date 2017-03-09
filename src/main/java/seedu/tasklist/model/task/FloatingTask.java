74e11524f92e4e2af8f2ed5916c39cff5af62049
Vesali <vesali@Vesalis-MacBook-Pro.local>
2017-03-09 22:08:29 +0800

    Update FloatingTask.java methods

--- src/main/java/seedu/tasklist/model/task/FloatingTask.java
+++ src/main/java/seedu/tasklist/model/task/FloatingTask.java
@@ -35,9 +35,19 @@ public class FloatingTask implements ReadOnlyFloatingTask {
         this(source.getName(), source.getComment(), source.getPriority(), source.getStatus(), source.getTags());
     }
     
+    public void setName(Name name) {
+        assert name != null;
+        this.name = name;
+    }
+    
     @Override
     public Name getName() {
         return name;
+    }
+    
+    public void setComment(Comment comment) {
+        assert comment != null;
+        this.comment = comment;
     }
 
     @Override
@@ -50,13 +60,45 @@ public class FloatingTask implements ReadOnlyFloatingTask {
         return tags;
     }
     
+    public void setPriority(Priority priority){
+        assert priority != null;
+        this.priority = priority;
+    }
+    
     public Priority getPriority() {
         return priority;
     }
    
+    public void setStatus(Status status){
+        assert status != null;
+        this.status = status;
+    }
+
+    
     public Status getStatus() {
         return status;
     }
+    
+    /**
+     * Replaces this person's tags with the tags in the argument tag list.
+     */
+    public void setTags(UniqueTagList replacement) {
+        tags.setTags(replacement);
+    }
+
+    /**
+     * Updates this person with the details of {@code replacement}.
+     */
+    public void resetData(ReadOnlyFloatingTask replacement) {
+        assert replacement != null;
+
+        this.setName(replacement.getName());
+        this.setComment(replacement.getComment());
+        this.setPriority(replacement.getPriority());
+        this.setStatus(replacement.getStatus());
+        this.setTags(replacement.getTags());
+    }
+
 
 
 }
