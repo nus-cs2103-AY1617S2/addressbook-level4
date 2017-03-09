005ebf50a3d7aa0b2be8764797bd506685a4dfbb
Vesali <vesali@Vesalis-MacBook-Pro.local>
2017-03-09 21:48:19 +0800

    Update ReadOnlyFloatingTask.java

--- src/main/java/seedu/tasklist/model/task/ReadOnlyFloatingTask.java
+++ src/main/java/seedu/tasklist/model/task/ReadOnlyFloatingTask.java
@@ -10,6 +10,8 @@ import seedu.tasklist.model.tag.UniqueTagList;
 public interface ReadOnlyFloatingTask extends ReadOnlyTask {
     Name getName();
     Comment getComment();
+    Priority getPriority();
+    Status getStatus();
 
     /**
      * The returned TagList is a deep copy of the internal TagList,
@@ -39,4 +41,5 @@ public interface ReadOnlyFloatingTask extends ReadOnlyTask {
         getTags().forEach(builder::append);
         return builder.toString();
     }
+
 }
