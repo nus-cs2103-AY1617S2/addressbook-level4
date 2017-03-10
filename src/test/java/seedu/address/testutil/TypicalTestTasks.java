5ba06a341eea2e5e25450de5ccde18b1b9e48530
Nguyen Quang Dung <nqdung14@gmail.com>
2017-03-10 12:16:24 +0800

    Fix ID of Typical Test Tasks

--- src/test/java/seedu/address/testutil/TypicalTestTasks.java
+++ src/test/java/seedu/address/testutil/TypicalTestTasks.java
@@ -14,28 +14,45 @@ public class TypicalTestTasks {
     public TypicalTestTasks() {
         try {
             alice = new TaskBuilder().withName("Alice Pauline")
-                    .withDeadline("12/06/8111").withDescription("alice@gmail.com")
-                    .withID("85355255")
+                    .withDeadline("12/06/8111")
+                    .withID("1")
+                    .withDescription("alice@gmail.com")
                     .withTags("friends").build();
-            benson = new TaskBuilder().withName("Benson Meier").withDeadline("27/02/2025")
-                    .withDescription("johnd@gmail.com").withID("98765432")
+            benson = new TaskBuilder().withName("Benson Meier")
+                    .withDeadline("27/02/2025")
+                    .withID("2")
+                    .withDescription("johnd@gmail.com")
                     .withTags("owesMoney", "friends").build();
-            carl = new TaskBuilder().withName("Carl Kurz").withID("95352563")
-                    .withDescription("heinz@yahoo.com").withDeadline("11/11/2011").build();
-            daniel = new TaskBuilder().withName("Daniel Meier").withID("87652533")
-                    .withDescription("cornelia@google.com").withDeadline("10/10/2010").build();
-            elle = new TaskBuilder().withName("Elle Meyer").withID("9482224")
-                    .withDescription("werner@gmail.com").withDeadline("12/12/2012").build();
-            fiona = new TaskBuilder().withName("Fiona Kunz").withID("9482427")
-                    .withDescription("lydia@gmail.com").withDeadline("22/02/2222").build();
-            george = new TaskBuilder().withName("George Best").withID("9482442")
-                    .withDescription("anna@google.com").withDeadline("04/04/2004").build();
+            carl = new TaskBuilder() .withName("Carl Kurz")
+                    .withDescription("heinz@yahoo.com")
+                    .withID("3")
+                    .withDeadline("11/11/2011").build();
+            daniel = new TaskBuilder().withName("Daniel Meier")
+                    .withDescription("cornelia@google.com")
+                    .withID("4")
+                    .withDeadline("10/10/2010").build();
+            elle = new TaskBuilder().withName("Elle Meyer")
+                    .withDescription("werner@gmail.com")
+                    .withID("5")
+                    .withDeadline("12/12/2012").build();
+            fiona = new TaskBuilder().withName("Fiona Kunz")
+                    .withDescription("lydia@gmail.com")
+                    .withID("6")
+                    .withDeadline("22/02/2222").build();
+            george = new TaskBuilder().withName("George Best")
+                    .withDescription("anna@google.com")
+                    .withID("7")
+                    .withDeadline("04/04/2004").build();
 
             // Manually added
-            hoon = new TaskBuilder().withName("Hoon Meier").withID("8482424")
-                    .withDescription("stefan@mail.com").withDeadline("01/01/2001").build();
-            ida = new TaskBuilder().withName("Ida Mueller").withID("8482131")
-                    .withDescription("hans@google.com").withDeadline("02/02/2002").build();
+            hoon = new TaskBuilder().withName("Hoon Meier")
+                    .withDescription("stefan@mail.com")
+                    .withID("8")
+                    .withDeadline("01/01/2001").build();
+            ida = new TaskBuilder().withName("Ida Mueller")
+                    .withDescription("hans@google.com")
+                    .withID("9")
+                    .withDeadline("02/02/2002").build();
         } catch (IllegalValueException e) {
             e.printStackTrace();
             assert false : "not possible";
