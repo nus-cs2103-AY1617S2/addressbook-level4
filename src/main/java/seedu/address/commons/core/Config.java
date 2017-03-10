6650faf0ce1c3245cd3d05adc244be081ee05acb
Li Hao Tan <LiHaoTan@users.noreply.github.com>
2017-03-10 09:02:27 +0800

    Update Config and tests

--- src/main/java/seedu/address/commons/core/Config.java
+++ src/main/java/seedu/address/commons/core/Config.java
@@ -89,7 +89,7 @@ public class Config {
         sb.append("\nCurrent log level : " + logLevel);
         sb.append("\nPreference file Location : " + userPrefsFilePath);
         sb.append("\nLocal data file location : " + addressBookFilePath);
-        sb.append("\nAddressBook name : " + addressBookName);
+        sb.append("\nTaskList name : " + addressBookName);
         return sb.toString();
     }
 
