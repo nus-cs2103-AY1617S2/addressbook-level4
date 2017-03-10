88239b063bd6748d6b62be4d607095f16304f105
Zihan Li <lizihan97@gmail.com>
2017-03-11 02:50:12 +0800

    [v0.2][storage]create XmlAdaptedActivity

---
+++ src/main/java/seedu/address/storage/XmlAdaptedActivity.java
@@ -0,0 +1,75 @@
+package seedu.address.storage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlElement;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.person.Activity;
+import seedu.address.model.person.Description;
+import seedu.address.model.person.Email;
+import seedu.address.model.person.Location;
+import seedu.address.model.person.Phone;
+import seedu.address.model.person.ReadOnlyActivity;
+import seedu.address.model.tag.Tag;
+import seedu.address.model.tag.UniqueTagList;
+
+/**
+ * JAXB-friendly version of the Activity.
+ */
+public class XmlAdaptedActivity {
+
+    @XmlElement(required = true)
+    private String description;
+    @XmlElement(required = true)
+    private String phone;
+    @XmlElement(required = true)
+    private String email;
+    @XmlElement(required = true)
+    private String location;
+
+    @XmlElement
+    private List<XmlAdaptedTag> tagged = new ArrayList<>();
+
+    /**
+     * Constructs an XmlAdaptedActivity.
+     * This is the no-arg constructor that is required by JAXB.
+     */
+    public XmlAdaptedActivity() {}
+
+
+    /**
+     * Converts a given Activity into this class for JAXB use.
+     *
+     * @param source future changes to this will not affect the created XmlAdaptedActivity
+     */
+    public XmlAdaptedActivity(ReadOnlyActivity source) {
+        description = source.getDescription().description;
+        phone = source.getPhone().value;
+        email = source.getEmail().value;
+        location = source.getLocation().value;
+        tagged = new ArrayList<>();
+        for (Tag tag : source.getTags()) {
+            tagged.add(new XmlAdaptedTag(tag));
+        }
+    }
+
+    /**
+     * Converts this jaxb-friendly adapted person object into the model's Activity object.
+     *
+     * @throws IllegalValueException if there were any data constraints violated in the adapted person
+     */
+    public Activity toModelType() throws IllegalValueException {
+        final List<Tag> personTags = new ArrayList<>();
+        for (XmlAdaptedTag tag : tagged) {
+            personTags.add(tag.toModelType());
+        }
+        final Description description = new Description(this.description);
+        final Phone phone = new Phone(this.phone);
+        final Email email = new Email(this.email);
+        final Location location = new Location(this.location);
+        final UniqueTagList tags = new UniqueTagList(personTags);
+        return new Activity(description, phone, email, location, tags);
+    }
+}
