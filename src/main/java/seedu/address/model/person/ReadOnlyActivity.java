85a413a618db210c558883bc04a114be404b6753
Zihan Li <lizihan97@gmail.com>
2017-03-11 02:39:23 +0800

    [v0.2][model]add new ReadOnlyActivity class

---
+++ src/main/java/seedu/address/model/person/ReadOnlyActivity.java
@@ -0,0 +1,51 @@
+package seedu.address.model.person;
+
+import seedu.address.model.tag.UniqueTagList;
+
+/**
+ * A read-only immutable interface for an Activity in WhatsLeft.
+ * Implementations should guarantee: details are present and not null, field values are validated.
+ */
+public interface ReadOnlyActivity {
+
+    Description getDescription();
+    Phone getPhone();
+    Email getEmail();
+    Location getLocation();
+
+    /**
+     * The returned TagList is a deep copy of the internal TagList,
+     * changes on the returned list will not affect the activity's internal tags.
+     */
+    UniqueTagList getTags();
+
+    /**
+     * Returns true if both have the same state. (interfaces cannot override .equals)
+     */
+    default boolean isSameStateAs(ReadOnlyActivity other) {
+        return other == this // short circuit if same object
+                || (other != null // this is first to avoid NPE below
+                && other.getDescription().equals(this.getDescription()) // state checks here onwards
+                && other.getPhone().equals(this.getPhone())
+                && other.getEmail().equals(this.getEmail())
+                && other.getLocation().equals(this.getLocation()));
+    }
+
+    /**
+     * Formats the activity as text, showing all contact details.
+     */
+    default String getAsText() {
+        final StringBuilder builder = new StringBuilder();
+        builder.append(getDescription())
+                .append(" Phone: ")
+                .append(getPhone())
+                .append(" Email: ")
+                .append(getEmail())
+                .append(" Location: ")
+                .append(getLocation())
+                .append(" Tags: ");
+        getTags().forEach(builder::append);
+        return builder.toString();
+    }
+
+}
