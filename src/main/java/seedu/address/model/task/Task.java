9f7091e1d05cbaec7a68df4c9a30ab03673f5cd1
LoneGunman001 <cwm20071128@gmail.com>
2017-03-10 17:32:05 +0800

    Add DateTime class and modify Task class

--- src/main/java/seedu/address/model/task/Task.java
+++ src/main/java/seedu/address/model/task/Task.java
@@ -6,79 +6,81 @@ import seedu.address.commons.util.CollectionUtil;
 import seedu.address.model.tag.UniqueTagList;
 
 /**
- * Represents a Task in the address book.
- * Guarantees: details are present and not null, field values are validated.
+ * Represents a Task in the address book. Guarantees: details are present and
+ * not null, field values are validated.
  */
 public class Task implements ReadOnlyTask {
 
-    private Name name;
+  private Name name;
 
-    private UniqueTagList tags;
+  private UniqueTagList tags;
 
-    /**
-     * Every field must be present and not null.
-     */
-    public Task(Name name, UniqueTagList tags) {
-        assert !CollectionUtil.isAnyNull(name, tags);
-        this.name = name;
-        this.tags = new UniqueTagList(tags); // protect internal tags from changes in the arg list
-    }
+  /**
+   * Every field must be present and not null.
+   */
+  public Task(Name name, UniqueTagList tags) {
+    assert !CollectionUtil.isAnyNull(name, tags);
+    this.name = name;
+    this.tags = new UniqueTagList(tags); // protect internal tags from changes
+                                         // in the arg list
+  }
 
-    /**
-     * Creates a copy of the given ReadOnlyTask.
-     */
-    public Task(ReadOnlyTask source) {
-        this(source.getName(), source.getTags());
-    }
+  /**
+   * Creates a copy of the given ReadOnlyTask.
+   */
+  public Task(ReadOnlyTask source) {
+    this(source.getName(), source.getTags());
+  }
 
-    public void setName(Name name) {
-        assert name != null;
-        this.name = name;
-    }
+  public void setName(Name name) {
+    assert name != null;
+    this.name = name;
+  }
 
-    @Override
-    public Name getName() {
-        return name;
-    }
+  @Override
+  public Name getName() {
+    return name;
+  }
 
-    @Override
-    public UniqueTagList getTags() {
-        return new UniqueTagList(tags);
-    }
+  @Override
+  public UniqueTagList getTags() {
+    return new UniqueTagList(tags);
+  }
 
-    /**
-     * Replaces this task's tags with the tags in the argument tag list.
-     */
-    public void setTags(UniqueTagList replacement) {
-        tags.setTags(replacement);
-    }
+  /**
+   * Replaces this task's tags with the tags in the argument tag list.
+   */
+  public void setTags(UniqueTagList replacement) {
+    tags.setTags(replacement);
+  }
 
-    /**
-     * Updates this task with the details of {@code replacement}.
-     */
-    public void resetData(ReadOnlyTask replacement) {
-        assert replacement != null;
+  /**
+   * Updates this task with the details of {@code replacement}.
+   */
+  public void resetData(ReadOnlyTask replacement) {
+    assert replacement != null;
 
-        this.setName(replacement.getName());
-        this.setTags(replacement.getTags());
-    }
+    this.setName(replacement.getName());
+    this.setTags(replacement.getTags());
+  }
 
-    @Override
-    public boolean equals(Object other) {
-        return other == this // short circuit if same object
-                || (other instanceof ReadOnlyTask // instanceof handles nulls
-                && this.isSameStateAs((ReadOnlyTask) other));
-    }
+  @Override
+  public boolean equals(Object other) {
+    return other == this // short circuit if same object
+        || (other instanceof ReadOnlyTask // instanceof handles nulls
+            && this.isSameStateAs((ReadOnlyTask) other));
+  }
 
-    @Override
-    public int hashCode() {
-        // use this method for custom fields hashing instead of implementing your own
-        return Objects.hash(name, tags);
-    }
+  @Override
+  public int hashCode() {
+    // use this method for custom fields hashing instead of implementing your
+    // own
+    return Objects.hash(name, tags);
+  }
 
-    @Override
-    public String toString() {
-        return getAsText();
-    }
+  @Override
+  public String toString() {
+    return getAsText();
+  }
 
 }
