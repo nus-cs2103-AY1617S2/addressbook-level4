84c328be8ac103977093f012d4626e5bae717e4d
ruolan <fionaxrl@gmail.com>
2017-03-10 16:26:39 +0800

    make priority & information input optional

--- src/main/java/seedu/taskboss/logic/parser/AddCommandParser.java
+++ src/main/java/seedu/taskboss/logic/parser/AddCommandParser.java
@@ -3,9 +3,11 @@ package seedu.taskboss.logic.parser;
 import static seedu.taskboss.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.taskboss.logic.parser.CliSyntax.PREFIX_CATEGORY;
 import static seedu.taskboss.logic.parser.CliSyntax.PREFIX_INFORMATION;
+import static seedu.taskboss.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.taskboss.logic.parser.CliSyntax.PREFIX_PRIORITY;
 
 import java.util.NoSuchElementException;
+import java.util.Optional;
 
 import seedu.taskboss.commons.exceptions.IllegalValueException;
 import seedu.taskboss.logic.commands.AddCommand;
@@ -16,6 +18,10 @@ import seedu.taskboss.logic.commands.IncorrectCommand;
  * Parses input arguments and creates a new AddCommand object
  */
 public class AddCommandParser {
+    
+    private String taskName;
+    private String priority;
+    private String information;
 
     /**
      * Parses the given {@code String} of arguments in the context of the AddCommand
@@ -23,19 +29,27 @@ public class AddCommandParser {
      */
     public Command parse(String args) {
         ArgumentTokenizer argsTokenizer =
-                new ArgumentTokenizer(PREFIX_PRIORITY, PREFIX_INFORMATION, PREFIX_CATEGORY);
+                new ArgumentTokenizer(PREFIX_NAME, PREFIX_PRIORITY, PREFIX_INFORMATION, PREFIX_CATEGORY);
         argsTokenizer.tokenize(args);
         try {
             return new AddCommand(
-                    argsTokenizer.getPreamble().get(),
-                    argsTokenizer.getValue(PREFIX_PRIORITY).get(),
-                    argsTokenizer.getValue(PREFIX_INFORMATION).get(),
+                    argsTokenizer.getValue(PREFIX_NAME).get(),
+                    checkEmpty(argsTokenizer.getValue(PREFIX_PRIORITY)),
+                    checkEmpty(argsTokenizer.getValue(PREFIX_INFORMATION)),
                     ParserUtil.toSet(argsTokenizer.getAllValues(PREFIX_CATEGORY))
             );
         } catch (NoSuchElementException nsee) {
             return new IncorrectCommand(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
         } catch (IllegalValueException ive) {
             return new IncorrectCommand(ive.getMessage());
+        }
+    }
+
+    private String checkEmpty(Optional<String> test) {
+        try {
+            return test.get();
+        } catch (NoSuchElementException nsee) {
+            return "";
         }
     }
 
