9229e819cd0e16c5edd6a28773e9a4a0228462f7
bernardyip <bernard_yip40@hotmail.com>
2017-03-10 14:41:50 +0800

    Update itextpdf dependencies and create pdf testcase

---
+++ src/test/java/seedu/address/commons/util/PdfUtilTest.java
@@ -0,0 +1,25 @@
+package seedu.address.commons.util;
+
+import java.io.FileNotFoundException;
+
+import org.junit.Test;
+
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Paragraph;
+
+/**
+ * Test util for exporting to pdf
+ */
+public class PdfUtilTest {
+
+    @Test
+    public void testPDFExport() throws FileNotFoundException {
+        PdfWriter writer = new PdfWriter("C:\\Desktop\\Test.pdf");
+        PdfDocument pdf = new PdfDocument(writer);
+        Document document = new Document(pdf);
+        document.add(new Paragraph("Hello World!"));
+        document.close();
+    }
+}
