8d4cc09d56d88dabb63d156401e3da8bd3e92b15
Zihan Li <lizihan97@gmail.com>
2017-03-11 03:05:33 +0800

    [v0.2][test][guitests]update SampleDataTest

--- src/test/java/guitests/SampleDataTest.java
+++ src/test/java/guitests/SampleDataTest.java
@@ -4,14 +4,14 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-import seedu.address.model.AddressBook;
-import seedu.address.model.person.Person;
+import seedu.address.model.WhatsLeft;
+import seedu.address.model.person.Activity;
 import seedu.address.model.util.SampleDataUtil;
 import seedu.address.testutil.TestUtil;
 
-public class SampleDataTest extends AddressBookGuiTest {
+public class SampleDataTest extends WhatsLeftGuiTest {
     @Override
-    protected AddressBook getInitialData() {
+    protected WhatsLeft getInitialData() {
         // return null to force test app to load data from file only
         return null;
     }
@@ -24,7 +24,7 @@ public class SampleDataTest extends AddressBookGuiTest {
 
     @Test
     public void addressBook_dataFileDoesNotExist_loadSampleData() throws Exception {
-        Person[] expectedList = SampleDataUtil.getSamplePersons();
-        assertTrue(personListPanel.isListMatching(expectedList));
+        Activity[] expectedList = SampleDataUtil.getSampleActivities();
+        assertTrue(activityListPanel.isListMatching(expectedList));
     }
 }
