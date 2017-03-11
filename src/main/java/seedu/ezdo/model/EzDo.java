958e78021cd9d3c27bf69f0b076b1af5b6520288
Acer\zhi liang <chenzhiliang94@gmail.com>
2017-03-11 11:29:15 +0800

    Remove unnecessary import

--- src/main/java/seedu/ezdo/model/EzDo.java
+++ src/main/java/seedu/ezdo/model/EzDo.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
-import java.util.function.Predicate;
 
 import javafx.collections.ObservableList;
 import seedu.ezdo.commons.core.UnmodifiableObservableList;
