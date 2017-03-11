56e8cad5cc61686cc2249755976dead86138bc70
YIM CHIA HUI <E0003264@u.nus.edu>
2017-03-11 21:17:46 +0800

    Added TaskBuilder class to Task

--- src/main/java/seedu/typed/model/task/Task.java
+++ src/main/java/seedu/typed/model/task/Task.java
@@ -2,8 +2,11 @@ package seedu.typed.model.task;
 
 import java.util.Objects;
 
+import seedu.typed.commons.exceptions.IllegalValueException;
 import seedu.typed.commons.util.CollectionUtil;
+import seedu.typed.model.tag.Tag;
 import seedu.typed.model.tag.UniqueTagList;
+import seedu.typed.model.tag.UniqueTagList.DuplicateTagException;
 
 /**
  * Represents a Task in the task manager. Guarantees: details are present and
@@ -96,6 +99,48 @@ public class Task implements ReadOnlyTask {
     @Override
     public String toString() {
         return getAsText();
+    }
+    /**
+     * TaskBuilder helps to build a Task object by being flexible in
+     * what attributes a Task object will initialise with. In particular,
+     * only a name is compulsory whereas other attributes are optional.
+     * Also takes in existing task for modifications to attributes.
+     * @author YIM CHIA HUI
+     *
+     */
+    public static class TaskBuilder {
+        private Name name;
+        private Date date;
+        private UniqueTagList tags;
+
+        public TaskBuilder() {
+            this.tags = new UniqueTagList();
+        }
+
+        public TaskBuilder(ReadOnlyTask task) {
+            this.name = task.getName();
+            this.date = task.getDate();
+            this.tags = task.getTags();
+        }
+
+        public TaskBuilder setName(String name) {
+            this.name = new Name(name);
+            return this;
+        }
+
+        public TaskBuilder setDate(String date) throws IllegalValueException {
+            this.date = new Date(date);
+            return this;
+        }
+
+        public TaskBuilder setTags(String tag) throws DuplicateTagException, IllegalValueException {
+            this.tags.add(new Tag(tag));
+            return this;
+        }
+
+        public Task build() {
+            return new Task(this.name, this.date, this.tags);
+        }
     }
 
 }
