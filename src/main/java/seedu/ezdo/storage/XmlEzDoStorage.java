4cde9a7ad84aa86946c82b7a6d22fee7bd6038b9
Lok You Tan <e0003133@u.nus.edu>
2017-03-11 19:26:33 +0800

    Remove unused import and fix import order

--- src/main/java/seedu/ezdo/storage/XmlEzDoStorage.java
+++ src/main/java/seedu/ezdo/storage/XmlEzDoStorage.java
@@ -5,7 +5,6 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.CopyOption;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.Optional;
