6699f6ea35f3c3116b8cc0d342bbcfff693a1cc8
francischeng070 <francischenghp@gmail.com>
2017-03-11 22:55:14 +0800

    fix indentation

--- src/main/java/seedu/address/model/task/Priority.java
+++ src/main/java/seedu/address/model/task/Priority.java
@@ -2,32 +2,32 @@ package seedu.address.model.task;
 
 public class Priority {
 
-	private boolean priority;
+    private boolean priority;
 
-	public static final String MESSAGE_PRIORITY_CONSTRAINTS =
-			"Priority should be either in true or false.";
+    public static final String MESSAGE_PRIORITY_CONSTRAINTS =
+            "Priority should be either in true or false.";
 
-	public Priority () {
-		this.priority = false;
-	}
+    public Priority () {
+        this.priority = false;
+    }
 
-	public Priority(boolean priority) {
-		this.priority = priority;
-	}
+    public Priority(boolean priority) {
+        this.priority = priority;
+    }
 
-	public Priority(String priority) {
-		String amended = priority.substring(2).trim();
-		if(amended.toLowerCase().compareTo("true") == 0) {
-			this.priority = true;
-		}
-		else {
-			this.priority = false;
-		}
-	}
+    public Priority(String priority) {
+        String amended = priority.substring(2).trim();
+        if(amended.toLowerCase().compareTo("true") == 0) {
+            this.priority = true;
+        }
+        else {
+            this.priority = false;
+        }
+    }
 
-	public boolean getPriority() {
-		return priority;
-	}
+    public boolean getPriority() {
+        return priority;
+    }
 
 
     @Override
