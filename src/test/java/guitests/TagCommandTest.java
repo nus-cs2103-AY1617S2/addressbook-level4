9a636efbe88325de76e7d6f57c12c3c285d74e6b
Louis Lai <hoangdung2506@gmail.com>
2017-03-11 21:37:57 +0800

    Add basic gui tests for tagging

---
+++ src/test/java/guitests/TagCommandTest.java
@@ -0,0 +1,25 @@
+package guitests;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import seedu.toluist.model.Tag;
+import seedu.toluist.model.Task;
+import seedu.toluist.testutil.TypicalTestTodoLists;
+
+/**
+ * Gui Tests for tag command
+ */
+public class TagCommandTest extends ToLuistGuiTest {
+
+    @Test
+    public void addTag_SingleTag() {
+        String tagName = "aTag";
+        Task task = new TypicalTestTodoLists().getTypicalTasks()[0];
+        task.addTag(new Tag(tagName));
+        String command = "tag 1 " + tagName;
+        commandBox.runCommand(command);
+        assertTrue(isTaskShown(task));
+    }
+}
