cdfe28ac8aa3fb2b67e7ec3a22e8de8ef8229d30
Crsytral <shouyu2@hotmail.com>
2017-03-11 23:51:32 +0800

    Fix Checkstyle Violations

--- src/test/java/guitests/guihandles/TaskListPanel2Handle.java
+++ src/test/java/guitests/guihandles/TaskListPanel2Handle.java
@@ -5,9 +5,9 @@ import javafx.stage.Stage;
 
 public class TaskListPanel2Handle extends TaskListPanelHandle {
 
-	public TaskListPanel2Handle(GuiRobot guiRobot, Stage primaryStage) {
-		super(guiRobot, primaryStage);
-		// TODO Auto-generated constructor stub
-	}
+    public TaskListPanel2Handle(GuiRobot guiRobot, Stage primaryStage) {
+        super(guiRobot, primaryStage);
+        // TODO Auto-generated constructor stub
+    }
 
 }
