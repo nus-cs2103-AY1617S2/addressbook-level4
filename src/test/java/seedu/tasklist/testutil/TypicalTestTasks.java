e6c62bd09ef0ff4220704f2cc657cf471df28bcb
Lim Jie <limjie1994@gmail.com>
2017-03-13 02:03:02 +0800

    Changed TypicalTestTasks to have different types of Task instead of the abstract Task

--- src/test/java/seedu/tasklist/testutil/TypicalTestTasks.java
+++ src/test/java/seedu/tasklist/testutil/TypicalTestTasks.java
@@ -1,7 +1,12 @@
 package seedu.tasklist.testutil;
 
+import java.util.Date;
+
 import seedu.tasklist.commons.exceptions.IllegalValueException;
 import seedu.tasklist.model.TaskList;
+import seedu.tasklist.model.task.DeadlineTask;
+import seedu.tasklist.model.task.EventTask;
+import seedu.tasklist.model.task.FloatingTask;
 import seedu.tasklist.model.task.Task;
 import seedu.tasklist.model.task.UniqueTaskList;
 
@@ -10,26 +15,26 @@ import seedu.tasklist.model.task.UniqueTaskList;
  */
 public class TypicalTestTasks {
 
-    public TestTask tutorial, homework, groceries, lifegoals, java, CS2103T, drink, internship, ida;
+    public TestTask tutorial, homework, groceries, java, CS2103T, drink, internship, ida;
 
     public TypicalTestTasks() {
         try {
-            tutorial = new TaskBuilder().withName("CS2103T tutorial")
+            tutorial = new EventTaskBuilder().withName("CS2103T tutorial")
                     .withComment("prepare V0.2 presentation")
+                    .withTags("class").withStartDate(new Date(2017, 5, 5, 15, 0).toString())
+                    .withEndDate(new Date(2017, 5, 5, 17, 0).toString()).build();
+            homework = new FloatingTaskBuilder().withName("CS3245 homework 3").withComment("discuss with classmates")
                     .withTags("class").build();
-            homework = new TaskBuilder().withName("CS3245 homework 3").withComment("discuss with classmates")
-                    .withTags("class").build();
-            groceries = new TaskBuilder().withName("Buy groceries").withComment("go NTUC").build();
-            lifegoals = new TaskBuilder().withName("Find ways to not feel left out")
-                    .withComment("#IFeelLeftOut").build();
-            java = new TaskBuilder().withName("Update Java for CS2103T")
+            groceries = new FloatingTaskBuilder().withName("Buy groceries").withComment("go NTUC").build();
+            java = new FloatingTaskBuilder().withName("Update Java for CS2103T")
                     .withComment("Find out why jdk is not displaying the correct ver").build();
-            CS2103T = new TaskBuilder().withName("Implement undo for this").withComment("By today").build();
-            drink = new TaskBuilder().withName("Drink water").withComment("To improve brain function").build();
+            CS2103T = new DeadlineTaskBuilder().withName("Implement undo for this").withComment("By today")
+                    .withDeadline(new Date().toString()).build();
+            drink = new FloatingTaskBuilder().withName("Drink water").withComment("To improve brain function").build();
 
             // Manually added
-            internship = new TaskBuilder().withName("Internship interview").withComment("at mediacorp").build();
-            ida = new TaskBuilder().withName("Yet another interview").withComment("also at mediacorp").build();
+            internship = new EventTaskBuilder().withName("Internship interview").withComment("at mediacorp").build();
+            ida = new EventTaskBuilder().withName("Yet another interview").withComment("also at mediacorp").build();
         } catch (IllegalValueException e) {
             e.printStackTrace();
             assert false : "not possible";
@@ -39,7 +44,22 @@ public class TypicalTestTasks {
     public static void loadTaskListWithSampleData(TaskList ab) {
         for (TestTask task : new TypicalTestTasks().getTypicalTasks()) {
             try {
-                ab.addTask(new Task(task));
+                Task currentTask;
+                String type = task.getType();
+                switch (type) {
+                case FloatingTask.TYPE:
+                    currentTask = new FloatingTask((TestFloatingTask) task);
+                    break;
+                case DeadlineTask.TYPE:
+                    currentTask = new DeadlineTask((TestDeadlineTask) task);
+                    break;
+                case EventTask.TYPE:
+                    currentTask = new EventTask((TestEventTask) task);
+                    break;
+                default:
+                    currentTask = null;
+                }
+                ab.addTask(currentTask);
             } catch (UniqueTaskList.DuplicateTaskException e) {
                 assert false : "not possible";
             }
@@ -47,7 +67,7 @@ public class TypicalTestTasks {
     }
 
     public TestTask[] getTypicalTasks() {
-        return new TestTask[]{tutorial, homework, groceries, lifegoals, java, CS2103T, drink};
+        return new TestTask[]{tutorial, homework, groceries, java, CS2103T, drink};
     }
 
     public TaskList getTypicalTaskList() {
