7167755b71e9b7c3fa7e189c268f5f6722ae5b67
InfinityFrost <iceazul_awakendragon@hotmail.com>
2017-03-12 22:20:13 +0800

    Added UndoCommand case into Parser

--- src/main/java/seedu/taskboss/logic/parser/Parser.java
+++ src/main/java/seedu/taskboss/logic/parser/Parser.java
@@ -69,6 +69,9 @@ public class Parser {
 
         case HelpCommand.COMMAND_WORD:
             return new HelpCommand();
+                
+        case UndoCommand.COMMAND_WORD:
+            return new UndoCommand();
 
         default:
             return new IncorrectCommand(MESSAGE_UNKNOWN_COMMAND);
