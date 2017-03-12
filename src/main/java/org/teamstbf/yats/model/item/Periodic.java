931889bea6ffdd4f9c0d25a84e33ca7d60aaf945
HORIZON\riveria94 <timelesschaos94@gmail.com>
2017-03-13 00:48:51 +0800

    Update Periodic to include ArrayList of schedules

--- src/main/java/org/teamstbf/yats/model/item/Periodic.java
+++ src/main/java/org/teamstbf/yats/model/item/Periodic.java
@@ -1,5 +1,7 @@
 package org.teamstbf.yats.model.item;
 
+import java.util.ArrayList;
+
 import org.teamstbf.yats.commons.exceptions.IllegalValueException;
 
 /**
@@ -8,10 +10,20 @@ import org.teamstbf.yats.commons.exceptions.IllegalValueException;
  */
 
 public class Periodic {
+	
+	private ArrayList<Schedule> scheduleArray;
+	private final String PERIODIC_DAILY = "daily";
+	private final String PERIODIC_WEEKLY = "weekly";
+	private final String PERIODIC_MONTHLY = "monthly";
+	private final int REPEAT_FREQUENCY_NONE = 1;
+	private final int REPEAT_FREQUENCY_DAILY = 365;
+	private final int REPEAT_FREQUENCY_WEEKLY = 52;
+	private final int REPEAT_FREQUENCY_MONTHLY = 12;
+	
 
     public static final String MESSAGE_PERIODIC_CONSTRAINTS =
             "Periodicity must be none, daily, weekly or monthly";
-    public static final String PERIODIC_VALIDATION_REGEX = ".*(none|daily|monthly|weekly).*";
+    public static final String PERIODIC_VALIDATION_REGEX = ".*(none|daily|weekly|monthly).*";
 
     public final String value;
 
@@ -27,6 +39,31 @@ public class Periodic {
             throw new IllegalValueException(MESSAGE_PERIODIC_CONSTRAINTS);
         }
         this.value = trimmedPeriod;
+        int repeatFrequency = parsePeriod(trimmedPeriod);
+        setupScheduleArray(scheduleArray, repeatFrequency);
+    }
+    
+    public int parsePeriod(String value) {
+    	int repeatFrequency = REPEAT_FREQUENCY_NONE;
+    	switch(value) {
+    	case(PERIODIC_DAILY):
+    		repeatFrequency = REPEAT_FREQUENCY_DAILY;
+    	case(PERIODIC_WEEKLY):
+    		repeatFrequency = REPEAT_FREQUENCY_WEEKLY;
+    	case(PERIODIC_MONTHLY):
+    		repeatFrequency = REPEAT_FREQUENCY_MONTHLY;
+    	}
+    	return repeatFrequency;
+    }
+    
+    public ArrayList<Schedule> getScheduleArray() {
+    	return scheduleArray;
+    }
+    
+    private void setupScheduleArray(ArrayList<Schedule> scheduleArray, int repeatFrequency) {
+    	for(int i=0; i<repeatFrequency; i++) {
+    		scheduleArray.add(new Schedule());
+    	}
     }
 
     /**
