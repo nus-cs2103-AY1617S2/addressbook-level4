1b9cce54c332a08cf78824d36ffadaa8198bc574
gjlam95 <gj_lam@hotmail.com>
2017-03-12 16:45:02 +0800

    Resolve conflict

--- src/test/java/seedu/address/model/task/NoteTest.java
+++ src/test/java/seedu/address/model/task/NoteTest.java
@@ -8,11 +8,12 @@ public class NoteTest {
 
     @Test
     public void isValidNote() {
-        // invalid notes
-
-        // valid notes
-        assertTrue(Note.isValidNote("Blk 456, Den Road, #01-355"));
         assertTrue(Note.isValidNote("-")); // one character
-        assertTrue(Note.isValidNote("Leng Inc; 1234 Market St; San Francisco CA 2349879; USA")); // long address
+        assertTrue(Note.isValidNote("2 dozens of eggs, 1 packet of milk, 1 loaf of bread, cheese")); // long note
+        assertTrue(Note.isValidNote("2 dozens of eggs, 1 packet of milk, 1 loaf of bread, cheese")); // long note
+        assertTrue(Note.isValidNote("Individually, review the sample user guides (on IVLE) using the "
+                                    + "User Guide checklist in CA 3: User Guide and Developer Guide. In "
+                                    + "your project team, discuss your findings with each other. Be ready "
+                                    + "to share your team’s discussion with the class.")); // very long note
     }
 }
