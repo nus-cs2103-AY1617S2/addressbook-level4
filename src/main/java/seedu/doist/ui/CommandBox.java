6782e24e414ba14b164790b134be1e8abd1de1e9
ramjanarthan <ram.janarthan11@gmail.com>
2017-03-12 18:37:28 +0800

    Edit UI to allow command history feature

--- src/main/java/seedu/doist/ui/CommandBox.java
+++ src/main/java/seedu/doist/ui/CommandBox.java
@@ -5,12 +5,15 @@ import java.util.logging.Logger;
 import javafx.fxml.FXML;
 import javafx.scene.control.SplitPane;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Region;
 import seedu.doist.commons.core.LogsCenter;
 import seedu.doist.commons.events.ui.NewResultAvailableEvent;
 import seedu.doist.commons.util.FxViewUtil;
 import seedu.doist.logic.Logic;
+import seedu.doist.logic.commands.CommandHistory;
 import seedu.doist.logic.commands.CommandResult;
 import seedu.doist.logic.commands.exceptions.CommandException;
 
@@ -38,24 +41,49 @@ public class CommandBox extends UiPart<Region> {
     }
 
     @FXML
-    private void handleCommandInputChanged() {
-        try {
-            CommandResult commandResult = logic.execute(commandTextField.getText());
+    private void handleKeyPressed(KeyEvent event) {
+        if (event.getCode() == KeyCode.ENTER) {
+            try {
+                String userCommandText = commandTextField.getText();
+                CommandHistory.restore();
+                if (!CommandHistory.addCommandHistory(userCommandText)) {
+                    throw new ArrayIndexOutOfBoundsException();
+                }
+                CommandResult commandResult = logic.execute(userCommandText);
+                // process result of the command
+                setStyleToIndicateCommandSuccess();
+                commandTextField.setText("");
+                logger.info("Result: " + commandResult.feedbackToUser);
+                raise(new NewResultAvailableEvent(commandResult.feedbackToUser));
 
-            // process result of the command
-            setStyleToIndicateCommandSuccess();
-            commandTextField.setText("");
-            logger.info("Result: " + commandResult.feedbackToUser);
-            raise(new NewResultAvailableEvent(commandResult.feedbackToUser));
-
-        } catch (CommandException e) {
-            // handle command failure
-            setStyleToIndicateCommandFailure();
-            logger.info("Invalid command: " + commandTextField.getText());
-            raise(new NewResultAvailableEvent(e.getMessage()));
+            } catch (CommandException e) {
+                // handle command failure
+                setStyleToIndicateCommandFailure();
+                commandTextField.setText("");
+                logger.info("Invalid command: " + commandTextField.getText());
+                raise(new NewResultAvailableEvent(e.getMessage()));
+            }
+        } else if (event.getCode() == KeyCode.UP) {
+            String userCommandText = CommandHistory.getPreviousCommand();
+            if (userCommandText == null) {
+                setCommandInput("");
+            } else {
+                setCommandInput(userCommandText);
+            }
+        } else if (event.getCode() == KeyCode.DOWN) {
+            String userCommandText = CommandHistory.getNextCommand();
+            if (userCommandText == null) {
+                setCommandInput("");
+            } else {
+                setCommandInput(userCommandText);
+            }
         }
     }
 
+
+    private void setCommandInput(String string) {
+        commandTextField.setText(string);
+    }
 
     /**
      * Sets the command box style to indicate a successful command.
