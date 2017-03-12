6b8ac9dcff21f69d0f780e112b06d32696332e62
peng229 <e0003632@u.nus.edu>
2017-03-12 12:05:39 +0800

    Refactor package name to task, and changed the test cases in NameTest and CommentTask (to add more parameters tests in the future)

---
+++ src/test/java/seedu/tasklist/model/task/CommentTest.java
@@ -0,0 +1,23 @@
+package seedu.tasklist.model.task;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import seedu.tasklist.model.task.Comment;
+
+public class CommentTest {
+
+    @Test
+    public void isValidComment() {
+        // invalid addresses
+        assertFalse(Comment.isValidComment("")); // empty string
+        assertFalse(Comment.isValidComment(" ")); // spaces only
+
+        // valid addresses
+        assertTrue(Comment.isValidComment("Find ways to not feel left out"));
+        assertTrue(Comment.isValidComment("-")); // one character
+        assertTrue(Comment.isValidComment("Find out why jdk is not displaying the correct ver")); // long comment
+    }
+}
