2e5bfcf78fe8f5c100000e4e92ee79a9b90baf17
InfinityFrost <iceazul_awakendragon@hotmail.com>
2017-03-12 22:41:12 +0800

    Fix trailing white space

--- src/main/java/seedu/taskboss/logic/commands/UndoCommand.java
+++ src/main/java/seedu/taskboss/logic/commands/UndoCommand.java
@@ -5,17 +5,17 @@ import java.util.EmptyStackException;
 /**
  * Undoes the most recent operation entered by user.
  */
- 
+
 public class UndoCommand extends Command {
-	
+
     public static final String COMMAND_WORD = "undo";
-    
+
     public static final String MESSAGE_USAGE = COMMAND_WORD
     		+ ": Undoes the most recent operation entered by user\n"
     		+ "Example: " + COMMAND_WORD;
-    
+
     public static final String MESSAGE_SUCCESS = "The most recent operation has been undone!";
-    
+
     public static final String MESSAGE_WITHOUT_PREVIOUS_OPERATION = "Cannot undo.";
 
     public UndoCommand() {}
@@ -23,10 +23,10 @@ public class UndoCommand extends Command {
 
     @Override
     public CommandResult execute() {
-        assert model != null; 
+        assert model != null;
         try {
             model.undoTaskboss();
-            return new CommandResult(MESSAGE_SUCCESS);	
+            return new CommandResult(MESSAGE_SUCCESS);
         } catch (EmptyStackException ese) {
             return new CommandResult(MESSAGE_WITHOUT_PREVIOUS_OPERATION);
         }
