42b7e98b098f0497b0d951bc10a37c17fe4d191d
HORIZON\riveria94 <timelesschaos94@gmail.com>
2017-03-13 01:02:40 +0800

    Update Periodic class

--- src/main/java/org/teamstbf/yats/model/item/Periodic.java
+++ src/main/java/org/teamstbf/yats/model/item/Periodic.java
@@ -10,8 +10,9 @@ import org.teamstbf.yats.commons.exceptions.IllegalValueException;
  */
 
 public class Periodic {
-	
+
 	private ArrayList<Schedule> scheduleArray;
+	private final String PERIODIC_NONE = "none";
 	private final String PERIODIC_DAILY = "daily";
 	private final String PERIODIC_WEEKLY = "weekly";
 	private final String PERIODIC_MONTHLY = "monthly";
@@ -19,75 +20,101 @@ public class Periodic {
 	private final int REPEAT_FREQUENCY_DAILY = 365;
 	private final int REPEAT_FREQUENCY_WEEKLY = 52;
 	private final int REPEAT_FREQUENCY_MONTHLY = 12;
+
+	private static boolean isPeriodic = false;
+
+	public static final String MESSAGE_PERIODIC_CONSTRAINTS =
+			"Periodicity must be none, daily, weekly or monthly";
+	public static final String PERIODIC_VALIDATION_REGEX = ".*(none|daily|weekly|monthly).*";
+
+	public final String value;
+
+	/**
+	 * Validates given period.
+	 *
+	 * @throws IllegalValueException if given period string is invalid.
+	 */
+	public Periodic(String period) throws IllegalValueException {
+		assert period != null;
+		String trimmedPeriod = period.trim();
+		if (!isValidPeriod(trimmedPeriod)) {
+			throw new IllegalValueException(MESSAGE_PERIODIC_CONSTRAINTS);
+		}
+		this.value = trimmedPeriod;
+		int repeatFrequency = parsePeriod(trimmedPeriod);
+		setupScheduleArray(scheduleArray, repeatFrequency);
+	}
+
+	/**
+	 * Parses the {@code String} period to {@code int}     
+	 * @param value
+	 * @return period as integer
+	 */
+	public int parsePeriod(String value) {
+		int repeatFrequency = REPEAT_FREQUENCY_NONE;
+		switch(value) {
+		case(PERIODIC_NONE): 
+			setPeriodicityFalse();
+		case(PERIODIC_DAILY): {
+			repeatFrequency = REPEAT_FREQUENCY_DAILY;
+			setPeriodicityTrue();
+		}
+		case(PERIODIC_WEEKLY): {
+			repeatFrequency = REPEAT_FREQUENCY_WEEKLY;
+			setPeriodicityTrue();
+		}
+		case(PERIODIC_MONTHLY): {
+			repeatFrequency = REPEAT_FREQUENCY_MONTHLY;
+			setPeriodicityTrue();
+		}
+		}
+		return repeatFrequency;
+	}
+
+	public ArrayList<Schedule> getScheduleArray() {
+		return scheduleArray;
+	}
+
+	private void setupScheduleArray(ArrayList<Schedule> scheduleArray, int repeatFrequency) {
+		for(int i=0; i<repeatFrequency; i++) {
+			scheduleArray.add(new Schedule());
+		}
+	}
+
+	/**
+	 * Returns if a given string is a valid period.
+	 */
+	public static boolean isValidPeriod(String test) {
+		return test.matches(PERIODIC_VALIDATION_REGEX);
+	}
+
+	public static boolean isPeriodic() {
+		return isPeriodic;
+	}
+
+	public static void setPeriodicityTrue() {
+		isPeriodic = true;
+	}
 	
+	public static void setPeriodicityFalse() {
+		isPeriodic = false;
+	}
 
-    public static final String MESSAGE_PERIODIC_CONSTRAINTS =
-            "Periodicity must be none, daily, weekly or monthly";
-    public static final String PERIODIC_VALIDATION_REGEX = ".*(none|daily|weekly|monthly).*";
+	@Override
+	public String toString() {
+		return value;
+	}
 
-    public final String value;
+	@Override
+	public boolean equals(Object other) {
+		return other == this // short circuit if same object
+				|| (other instanceof Periodic // instanceof handles nulls
+						&& this.value.equals(((Periodic) other).value)); // state check
+	}
 
-    /**
-     * Validates given period.
-     *
-     * @throws IllegalValueException if given period string is invalid.
-     */
-    public Periodic(String period) throws IllegalValueException {
-        assert period != null;
-        String trimmedPeriod = period.trim();
-        if (!isValidPeriod(trimmedPeriod)) {
-            throw new IllegalValueException(MESSAGE_PERIODIC_CONSTRAINTS);
-        }
-        this.value = trimmedPeriod;
-        int repeatFrequency = parsePeriod(trimmedPeriod);
-        setupScheduleArray(scheduleArray, repeatFrequency);
-    }
-    
-    public int parsePeriod(String value) {
-    	int repeatFrequency = REPEAT_FREQUENCY_NONE;
-    	switch(value) {
-    	case(PERIODIC_DAILY):
-    		repeatFrequency = REPEAT_FREQUENCY_DAILY;
-    	case(PERIODIC_WEEKLY):
-    		repeatFrequency = REPEAT_FREQUENCY_WEEKLY;
-    	case(PERIODIC_MONTHLY):
-    		repeatFrequency = REPEAT_FREQUENCY_MONTHLY;
-    	}
-    	return repeatFrequency;
-    }
-    
-    public ArrayList<Schedule> getScheduleArray() {
-    	return scheduleArray;
-    }
-    
-    private void setupScheduleArray(ArrayList<Schedule> scheduleArray, int repeatFrequency) {
-    	for(int i=0; i<repeatFrequency; i++) {
-    		scheduleArray.add(new Schedule());
-    	}
-    }
-
-    /**
-     * Returns if a given string is a valid period.
-     */
-    public static boolean isValidPeriod(String test) {
-        return test.matches(PERIODIC_VALIDATION_REGEX);
-    }
-
-    @Override
-    public String toString() {
-        return value;
-    }
-
-    @Override
-    public boolean equals(Object other) {
-        return other == this // short circuit if same object
-                || (other instanceof Periodic // instanceof handles nulls
-                && this.value.equals(((Periodic) other).value)); // state check
-    }
-
-    @Override
-    public int hashCode() {
-        return value.hashCode();
-    }
+	@Override
+	public int hashCode() {
+		return value.hashCode();
+	}
 
 }
