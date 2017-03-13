9c17b3aca7cbeac8b576bc8f96ee0d2ca7d1fb8b
WUHEYANG\wuheyang0617 <wuheyang0617@gmail.com>
2017-03-13 17:41:05 +0800

    Renaming message string for deleting and finding index

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -53,7 +53,7 @@ public class EditCommand extends Command {
         List<ReadOnlyTask> lastShownList = model.getFilteredTaskList();
 
         if (filteredPersonListIndex >= lastShownList.size()) {
-            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+            throw new CommandException(Messages.MESSAGE_INVALID_TASK_DISPLAYED_INDEX);
         }
 
         ReadOnlyTask personToEdit = lastShownList.get(filteredPersonListIndex);
