f624061ef00ec1fb7b2c54ff61741110c370766b
Joelleejh <bossleejh@gmail.com>
2017-03-13 12:37:29 +0800

    Change Find to Search

--- src/main/java/seedu/taskmanager/logic/commands/SearchCommand.java
+++ src/main/java/seedu/taskmanager/logic/commands/SearchCommand.java
@@ -3,21 +3,21 @@ package seedu.taskmanager.logic.commands;
 import java.util.Set;
 
 /**
- * Finds and lists all tasks in task manager whose taskname contains any of the argument keywords.
+ * Searches and lists all tasks in task manager whose taskname contains any of the argument keywords.
  * Keyword matching is case sensitive.
  */
-public class FindCommand extends Command {
+public class SearchCommand extends Command {
 
     public static final String COMMAND_WORD = "SEARCH";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all tasks whose tasknames contain any of "
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Searches all tasks whose tasknames contain any of "
             + "the specified keywords (case-sensitive) and displays them as a list with index numbers.\n"
             + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
             + "Example: " + COMMAND_WORD + " eat food lunch ";
 
     private final Set<String> keywords;
 
-    public FindCommand(Set<String> keywords) {
+    public SearchCommand(Set<String> keywords) {
         this.keywords = keywords;
     }
 
