652380d7adcae5fcc35ae281fad891e883a28905
gngster <gjhsmiles@gmail.com>
2017-03-14 02:56:32 +0800

    add commandAndState class

---
+++ src/main/java/seedu/todolist/logic/CommandAndState.java
@@ -0,0 +1,22 @@
+package seedu.todolist.logic;
+
+import seedu.todolist.logic.commands.Command;
+import seedu.todolist.model.ReadOnlyToDoList;
+
+public class CommandAndState {
+	Command command;
+	ReadOnlyToDoList state;
+
+	public CommandAndState(Command command, ReadOnlyToDoList state) {
+	    this.command = command;
+	    this.state = state;
+	}
+
+    public Command getCommand() {
+        return command;
+    }
+
+    public ReadOnlyToDoList getState() {
+        return state;
+    }
+}
