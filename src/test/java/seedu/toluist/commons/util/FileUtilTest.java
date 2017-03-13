6127b87b48dfb50d44985fdaf794741167dc6c8a
Louis Lai <hoangdung2506@gmail.com>
2017-03-14 00:51:33 +0800

    Linting

--- src/test/java/seedu/toluist/commons/util/FileUtilTest.java
+++ src/test/java/seedu/toluist/commons/util/FileUtilTest.java
@@ -3,12 +3,13 @@ package seedu.toluist.commons.util;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-import org.junit.Before;
-import org.junit.Test;
-import seedu.toluist.testutil.TestUtil;
-
 import java.io.File;
 import java.io.IOException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import seedu.toluist.testutil.TestUtil;
 
 /**
  * Tests for FileUtil
