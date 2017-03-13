f4ca6df780d08bbcac5309eb91b1bc633a6a47dc
Bili Xu <xubili93@gmail.com>
2017-03-13 20:37:24 +0800

    Implement test cases for MarkCommand

---
+++ src/test/java/guitests/MarkCommandTest.java
@@ -0,0 +1,48 @@
+package guitests;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import guitests.guihandles.TaskCardHandle;
+import seedu.address.logic.commands.EditCommand;
+import seedu.address.testutil.TaskBuilder;
+import seedu.address.testutil.TestTask;
+
+public class MarkCommandTest extends TaskManagerGuiTest {
+
+    TestTask[] expectedTasksList = td.getTypicalTasks();
+
+    @Test
+    public void mark_task_success() throws Exception {
+        int taskManagerIndex = 1;
+
+        TestTask taskToMark = expectedTasksList[taskManagerIndex - 1];
+        TestTask markedTask = new TaskBuilder(taskToMark).withStatus("complete").build();
+
+        assertMarkSuccess(taskManagerIndex, taskManagerIndex, markedTask);
+    }
+
+    /**
+     * Checks whether the marked task has the correct updated details.
+     *
+     * @param filteredTaskListIndex index of task to edit in filtered list.
+     * @param taskManagerIndex index of task to edit in the task manager.
+     *      Must refer to the same task as {@code filteredTaskListIndex}
+     * @param editedTask the expected task after editing the task's details.
+     */
+    private void assertMarkSuccess(int filteredTaskListIndex, int taskManagerIndex,
+            TestTask markedTask) {
+        commandBox.runCommand("mark " + filteredTaskListIndex);
+
+        // confirm the new card contains the right data
+        TaskCardHandle editedCard = taskListPanel.navigateToTask(markedTask.getName().fullName);
+        assertMatching(markedTask, editedCard);
+
+        // confirm the list now contains all previous tasks plus the task with updated details
+        expectedTasksList[taskManagerIndex - 1] = markedTask;
+        assertTrue(taskListPanel.isListMatching(expectedTasksList));
+        assertResultMessage(String.format(EditCommand.MESSAGE_EDIT_TASK_SUCCESS, markedTask));
+    }
+
+}
