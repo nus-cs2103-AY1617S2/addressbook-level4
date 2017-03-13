5af6cd324f4ef4992a5e114644e80723598480b4
LWWenz <leongww7@gmail.com>
2017-03-13 10:30:49 +0800

    remove the trailing whitespace

--- src/main/java/typetask/model/task/CompletedStatus.java
+++ src/main/java/typetask/model/task/CompletedStatus.java
@@ -7,16 +7,13 @@ public class CompletedStatus {
     private boolean completed;
     private Task task; //Task that is associated with the current completedStatus.
     private static Vector<Task> completedList = new Vector<Task>(); //Keeps a list of all tasks that are completed.
-    
     public CompletedStatus(Task task) {
         this.task = task;
         this.completed = false;
     }
-    
     public boolean getCompleted() {
         return this.completed;
     }
-    
     /**
      * May change the name of both methods completedTrue and completedFalse
      */
@@ -24,7 +21,6 @@ public class CompletedStatus {
         this.completed = true;
         completedList.addElement(task);
     }
-    
     public void completedFalse() {
         this.completed = false;
         completedList.remove(task);
