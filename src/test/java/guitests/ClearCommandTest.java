55902081a7b3e09afe3fd688ae92a984db308bf4
Joseph Tang <josephtang765@gmail.com>
2017-03-13 17:33:31 +0800

    Add test for shortened clear command

--- src/test/java/guitests/ClearCommandTest.java
+++ src/test/java/guitests/ClearCommandTest.java
@@ -11,7 +11,12 @@ public class ClearCommandTest extends EzDoGuiTest {
 
         //verify a non-empty list can be cleared
         assertTrue(taskListPanel.isListMatching(td.getTypicalTasks()));
-        assertClearCommandSuccess();
+        assertClearCommandSuccess(false);
+
+        //verify a non-empty list can be cleared using the short clear command
+        commandBox.runCommand(td.jack.getAddCommand(true));
+        assertListSize(1);
+        assertClearCommandSuccess(true);
 
         //verify other commands can work after a clear command
         commandBox.runCommand(td.hoon.getAddCommand(false));
@@ -20,11 +25,15 @@ public class ClearCommandTest extends EzDoGuiTest {
         assertListSize(0);
 
         //verify clear command works when the list is empty
-        assertClearCommandSuccess();
+        assertClearCommandSuccess(false);
     }
 
-    private void assertClearCommandSuccess() {
-        commandBox.runCommand("clear");
+    private void assertClearCommandSuccess(boolean usesShortCommand) {
+        if (usesShortCommand) {
+            commandBox.runCommand("c");
+        } else {
+            commandBox.runCommand("clear");
+        }
         assertListSize(0);
         assertResultMessage("EzDo has been cleared!");
     }
