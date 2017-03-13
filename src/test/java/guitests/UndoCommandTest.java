c0a6056e8be3c0ca49f64b3cda92586bc7af944f
InfinityFrost <iceazul_awakendragon@hotmail.com>
2017-03-13 12:49:39 +0800

    Update UndoCommandTest.java

--- src/test/java/guitests/UndoCommandTest.java
+++ src/test/java/guitests/UndoCommandTest.java
@@ -19,7 +19,7 @@ public class UndoCommandTest extends TaskBossGuiTest {
 
         //undo one command
         TestTask[] currentList = td.getTypicalTasks();
-        TestTask taskToAdd = td.wedding;
+        TestTask taskToAdd = td.ida;
         commandBox.runCommand(taskToAdd.getAddCommand());
         commandBox.runCommand("delete " + currentList.length);
         TestTask[] expectedList = TestUtil.addTasksToList(currentList, taskToAdd);
