284ca7c1cc4545b78d57277e23ae41c3f106dfbc
Loh Zi Bin Robin <robinloh@u.nus.edu>
2017-03-14 07:35:25 +0800

    Remove extra semi-colon in line 13

--- src/main/java/seedu/ezdo/model/todo/TaskDate.java
+++ src/main/java/seedu/ezdo/model/todo/TaskDate.java
@@ -10,7 +10,7 @@ public abstract class TaskDate {
     public final String value;
 
     public static final String TASKDATE_VALIDATION_REGEX =
-        "^$|([12][0-9]|3[01]|0?[1-9])/(0?[1-9]|1[012])/((?:19|20)\\d\\d)";;
+        "^$|([12][0-9]|3[01]|0?[1-9])/(0?[1-9]|1[012])/((?:19|20)\\d\\d)";
 
     public TaskDate(String taskDate) {
     	Parser parser = new Parser();
