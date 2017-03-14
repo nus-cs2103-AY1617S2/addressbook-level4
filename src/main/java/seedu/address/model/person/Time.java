0e4d281d2450c38196648843d31b70c87224ed39
Daniel Mullen <dmullen3@gatech.edu>
2017-03-14 07:30:52 -0400

    Modified time

--- src/main/java/seedu/address/model/person/Time.java
+++ src/main/java/seedu/address/model/person/Time.java
@@ -3,10 +3,12 @@ package seedu.address.model.person;
 public class Time {
 	private int hour;
 	private int minute;
+	private String hourStr;
+	private String minuteStr;
 	public final String value;
 	public final String TIME_DELIMITER = ":";
-	public final int MINUTE_ARRAY_INDEX = 1;
-	public final int HOUR_ARRAY_INDEX = 0;
+	public final int MINUTE_ARRAY_INDEX = 0;
+	public final int HOUR_ARRAY_INDEX = 1;
 	public Time(String time) {
 		time = time.trim();
 		value = time;
@@ -16,10 +18,9 @@ public class Time {
 	}
 	
 	public String toString() {
-		return hour + TIME_DELIMITER + minute;
+		return hourStr + TIME_DELIMITER + minuteStr;
 	}
 	public void setHour(int hour) {
-		this.hour = hour;
 		if (0 <= hour && hour <= 23) {
 			this.hour = hour;
 		} else {
@@ -27,24 +28,45 @@ public class Time {
 		}
 	}
 	public void setMinute(int minute) {
-		this.minute = minute;
 		if (0 <= minute && minute <= 59) {
 			this.minute = minute;
 		} else {
 			throw new IllegalArgumentException("Invalid minute");
 		}
 	}
-	public static int[] timeFormatConverter(String time) {
-		if (time.length() != 4) {
+	public int[] timeFormatConverter(String time) {
+		if (time.length() < 3 || time.length() > 5) {
 			throw new IllegalArgumentException("Invalid time format");
+		} else if (time.length() == 3) {
+			this.minuteStr = time.substring(1,3);
+			this.hourStr = time.substring(0,1);
+		} else if (time.length() == 4) {
+			if (time.substring(1,2).equals(":")) {
+				this.minuteStr = time.substring(2,4);
+				this.hourStr = "0" + time.substring(0,1);
+			} else {
+				this.minuteStr = time.substring(2,4);
+				this.hourStr = time.substring(0,2);
+			}
+		} else {
+			this.minuteStr = time.substring(3,5);
+			this.hourStr = time.substring(0,2);
 		}
-		int minute = Integer.parseInt(time.substring(0,2));
-		int hour = Integer.parseInt(time.substring(2,4));
+		int minute = Integer.parseInt(minuteStr);
+		int hour = Integer.parseInt(hourStr);
 		int[] timeArray = {minute, hour};
 		return timeArray;
 	}
 	public static void main(String[] args) {
-		Time t = new Time("1231");
+		Time t = new Time("0204");
+		System.out.println(t);
+		t = new Time("305");
+		System.out.println(t);
+		t = new Time("4:10");
+		System.out.println(t);
+		t = new Time("12:30");
+		System.out.println(t);
+		t = new Time("0:01");
 		System.out.println(t);
 	}
 }
