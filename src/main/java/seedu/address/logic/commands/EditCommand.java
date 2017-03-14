5799f36d0e1b37d546e8ab8a0eda40d4a5b181c4
wangchi <wangchi92@gmail.com>
2017-03-14 23:33:42 +0800

    Clear Junit test

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -25,7 +25,7 @@ public class EditCommand extends Command {
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Edits the details of the task identified "
             + "by the index number used in the last task listing. "
             + "Existing values will be overwritten by the input values.\n"
-            + "Parameters: INDEX (must be a positive integer) [TITLE] [@@VENUE] [from/STARTTIME] [to/ENDTIME] [##TAG]...\n"
+            + "Parameters: INDEX (must be a positive integer)[TITLE][@@VENUE][from:STARTTIME][to:ENDTIME][##TAG]..\n"
             + "Example: " + COMMAND_WORD + " 1 @@ johndoe@yahoo.com";
 
     public static final String MESSAGE_EDIT_TASK_SUCCESS = "Edited Task: %1$s";
