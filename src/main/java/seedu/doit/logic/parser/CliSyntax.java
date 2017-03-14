edeeae4f38293cf7a59bd06ee2e96625d928322f
Lee Jin Shun <ljsrockz@gmail.com>
2017-03-14 12:39:54 +0800

    Specify tasks into floating task, tasks and events

--- src/main/java/seedu/doit/logic/parser/CliSyntax.java
+++ src/main/java/seedu/doit/logic/parser/CliSyntax.java
@@ -11,6 +11,7 @@ public class CliSyntax {
 
     /* Prefix definitions */
     public static final Prefix PREFIX_PRIORITY = new Prefix("p/");
+    public static final Prefix PREFIX_START = new Prefix("s/");
     public static final Prefix PREFIX_END = new Prefix("e/");
     public static final Prefix PREFIX_DESCRIPTION = new Prefix("d/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
