285f1c1fd308433c42443c68504d85d9495d00ec
Lok You Tan <e0003133@u.nus.edu>
2017-03-14 19:00:34 +0800

    Remove unused imports

--- src/main/java/seedu/ezdo/model/ModelManager.java
+++ src/main/java/seedu/ezdo/model/ModelManager.java
@@ -2,7 +2,6 @@ package seedu.ezdo.model;
 
 import java.util.EmptyStackException;
 import java.util.Set;
-import java.util.Stack;
 import java.util.logging.Logger;
 
 import javafx.collections.transformation.FilteredList;
