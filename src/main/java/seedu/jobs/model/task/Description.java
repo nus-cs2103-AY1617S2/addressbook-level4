676c2961658fa23d4b747937b140b7451464bc4f
chrishendra93 <chrishendra93@gmail.com>
2017-03-15 01:02:29 +0800

    Add constraint for description's length

--- src/main/java/seedu/jobs/model/task/Description.java
+++ src/main/java/seedu/jobs/model/task/Description.java
@@ -1,16 +1,32 @@
 package seedu.jobs.model.task;
 
+import seedu.jobs.commons.exceptions.IllegalValueException;
+
 /**
  * Represents a Task's description in JOBS.
  */
 
 public class Description {
 	
-	public static final String DEFAULT_VALUE = "";
+	public static final String DEFAULT_DESCRIPTION = "";
+	public static final String  MESSAGE_DESCRIPTION_CONSTRAINT = "Description cannot be more than 150 characters";
+	public static final int DESCRIPTION_LENGTH_CONSTRAINT = 150;
 	public final String value;
 	
-	public Description(String description){
-		this.value = (description==null) ? DEFAULT_VALUE : description;
+	public Description(String description) throws IllegalValueException{
+		if(description==null){
+			this.value = DEFAULT_DESCRIPTION;
+		}
+		else{
+			if(!isValidDescription(description)){
+				throw new IllegalValueException(MESSAGE_DESCRIPTION_CONSTRAINT);
+			}
+			this.value = description;
+		}
+	}
+	
+	public boolean isValidDescription(String description){
+		return description.length() <= DESCRIPTION_LENGTH_CONSTRAINT;
 	}
 	
 	@Override
@@ -24,7 +40,7 @@ public class Description {
                 || (other instanceof Description // instanceof handles nulls
                 && this.value.equals(((Description) other).value)); // state check
     }
-
+	
     @Override
     public int hashCode() {
         return value.hashCode();
