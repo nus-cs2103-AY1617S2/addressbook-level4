00060b252fa5603525c911bd22335a07b6da9146
Yi Rui Goh <yirui94@gmail.com>
2017-03-14 15:08:36 +0800

    Implement Done and Undone Command logic

--- src/main/java/seedu/geekeep/logic/commands/UndoneCommand.java
+++ src/main/java/seedu/geekeep/logic/commands/UndoneCommand.java
@@ -1,16 +1,43 @@
 package seedu.geekeep.logic.commands;
 
+import seedu.geekeep.commons.core.Messages;
+import seedu.geekeep.commons.core.UnmodifiableObservableList;
 import seedu.geekeep.logic.commands.exceptions.CommandException;
+import seedu.geekeep.model.task.ReadOnlyTask;
 
 /**
  * Marks 'undone' for task identified using it's last displayed index from the address book.
  */
 public class UndoneCommand extends Command {
+    public static final String COMMAND_WORD = "undone";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Marks the task identified by the index number used in the last task listing as undone.\n"
+            + "Parameters: INDEX (must be a positive integer)\n"
+            + "Example: " + COMMAND_WORD + " 1";
+
+    public static final String MESSAGE_UNDONE_TASK_SUCCESS = "Marked as undone: %1$s";
+
+
+    public final int targetIndex;
+
+    public UndoneCommand(int targetIndex) {
+        this.targetIndex = targetIndex;
+    }
 
     @Override
     public CommandResult execute() throws CommandException {
-        // TODO Auto-generated method stub
-        return null;
+        UnmodifiableObservableList<ReadOnlyTask> lastShownList = model.getFilteredPersonList();
+
+        if (lastShownList.size() < targetIndex) {
+            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        }
+
+        ReadOnlyTask taskToMark = lastShownList.get(targetIndex - 1);
+
+        model.markTaskUndone(targetIndex);
+
+        return new CommandResult(String.format(MESSAGE_UNDONE_PERSON_SUCCESS, taskToMark));
     }
 
 }
--- src/main/java/seedu/geekeep/logic/parser/DoneCommandParser.java
+++ src/main/java/seedu/geekeep/logic/parser/DoneCommandParser.java
@@ -1,10 +1,30 @@
 package seedu.geekeep.logic.parser;
 
+import static seedu.geekeep.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import java.util.Optional;
+import seedu.geekeep.logic.commands.Command;
+import seedu.geekeep.logic.commands.DoneCommand;
+import seedu.geekeep.logic.commands.IncorrectCommand;
+
 /** 
  * 
- * Parses input argument and 
+ * Parses input argument and creates a new DoneCommand object
  *
  */
 public class DoneCommandParser {
+    /**
+     * Parses the given {@code String} of arguments in the context of the DoneCommand
+     * and returns an DoneCommand object for execution.
+     */
+    public Command parse(String args) {
+
+        Optional<Integer> index = ParserUtil.parseIndex(args);
+        if (!index.isPresent()) {
+            return new IncorrectCommand(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, DoneCommand.MESSAGE_USAGE));
+        }
+
+        return new DoneCommand(index.get());
+    }
 
 }
