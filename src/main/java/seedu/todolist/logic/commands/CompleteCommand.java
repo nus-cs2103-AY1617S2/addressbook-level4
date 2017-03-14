698e5ad0a62b1d95a5e428bf19eb6f3b329a0118
vitquay1996 <leminhphuc10t1@gmail.com>
2017-03-14 21:16:44 +0800

    Add CompleteCommand class

---
+++ src/main/java/seedu/todolist/logic/commands/CompleteCommand.java
@@ -0,0 +1,50 @@
+package seedu.todolist.logic.commands;
+
+import seedu.todolist.commons.core.Messages;
+import seedu.todolist.commons.core.UnmodifiableObservableList;
+import seedu.todolist.logic.commands.exceptions.CommandException;
+import seedu.todolist.model.task.ReadOnlyTask;
+import seedu.todolist.model.task.UniqueTaskList.TaskNotFoundException;
+
+/**
+ * Mark a Task as completed using its last displayed index from the ToDoList
+ */
+public class CompleteCommand {
+    public static final String COMMAND_WORD = "complete";
+    
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Mark as completed the task identified by the index number used in the last task listing.\n"
+            + "Parameters: INDEX (must be a positive integer)\n"
+            + "Example: " + COMMAND_WORD + " 1";
+
+    public static final String MESSAGE_COMPLETE_TASK_SUCCESS = "Completed Task: %1$s";
+
+    public final int targetIndex;
+
+    public CompleteCommand(int targetIndex) {
+        this.targetIndex = targetIndex;
+    }
+    
+    public CommandResult execute() throws CommandException {
+        
+        UnmodifiableObservableList<ReadOnlyTask> lastShownList = model.getFilteredTaskList();
+
+        if (lastShownList.size() < targetIndex) {
+            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        }
+
+        ReadOnlyTask taskToComplete = lastShownList.get(targetIndex - 1);
+
+        try {
+            model.completeTask(taskToComplete);
+        } catch (TaskNotFoundException pnfe) {
+            assert false : "The target task cannot be missing";
+        }
+
+        return new CommandResult(String.format(MESSAGE_COMPLETE_TASK_SUCCESS, taskToComplete));
+    }
+    
+    public boolean isMutating() {
+        return true;
+    }
+}
