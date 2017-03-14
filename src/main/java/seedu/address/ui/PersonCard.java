f98524111b5fc08d2c590de4f87fd26d8593e7b0
nuslarry <e0147977@u.nus.edu>
2017-03-14 21:55:19 +0800

    add priorty description, add color to complete tags

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -30,13 +30,24 @@ public class PersonCard extends UiPart<Region> {
         super(FXML);
         name.setText(person.getDescription().description);
         id.setText(displayedIndex + ". ");
-        phone.setText(person.getPriority().value);
+        phone.setText("Priority:"+person.getPriority().value);
         email.setText(person.getStartDate().value);
 //        endDate.setText(person.getEndDate().value);
         initTags(person);
     }
 
     private void initTags(ReadOnlyTask person) {
-        person.getTags().forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
+//        person.getTags().forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
+    	
+        person.getTags().forEach(tag -> {
+        	String complete = "complete";
+        	System.out.println(tag.tagName);
+        	Label label1 = new Label(tag.tagName);
+        	//cannot use == to do string comparison
+        	if(tag.tagName.equals(complete)){
+        		label1.setStyle("-fx-border-color:pink; -fx-background-color:green;");
+        	}
+        	tags.getChildren().add(label1);   
+        });
     }
 }
