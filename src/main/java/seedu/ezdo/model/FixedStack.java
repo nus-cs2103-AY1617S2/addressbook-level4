9e247573516f5e3a2f458ec8b6a5e39d5b75e636
Lok You Tan <e0003133@u.nus.edu>
2017-03-14 16:55:01 +0800

    Add FixedStack.java (array-based implementation of a fixed-size stack)

---
+++ src/main/java/seedu/ezdo/model/FixedStack.java
@@ -0,0 +1,40 @@
+package seedu.ezdo.model;
+
+import java.util.EmptyStackException;
+
+/*
+ * Array-based implementation for a stack with fixed size. Used for undo & redo stacks.
+ * If stack goes past max capacity, the oldest item to be pushed is replaced.
+ */
+public class FixedStack<Item> {
+    private int index;
+    private Item[] array;
+
+    public FixedStack(int capacity) {
+        array = (Item[]) new Object[capacity];
+        index = -1;
+    }
+
+    public void push(Item item) {
+        index = (index + 1) % 5; // wraps around
+        array[index] = item;
+    }
+
+    public Item pop() throws EmptyStackException {
+        if (array[index] == null) {
+            throw new EmptyStackException();
+        }
+        Item item = array[index];
+        array[index] = null;
+        if (index == 0) {
+            index = array.length - 1;
+        } else {
+            index = index - 1;
+        }
+        return item;
+    }
+
+    public boolean isEmpty() {
+        return array.length == 0;
+    }
+}
