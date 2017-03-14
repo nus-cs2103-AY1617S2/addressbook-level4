1005bd70be97b284349f5a9d351412803032fe06
Lok You Tan <e0003133@u.nus.edu>
2017-03-14 15:54:46 +0800

    Rename UndoCommand messages and strings

--- src/main/java/seedu/ezdo/logic/commands/UndoCommand.java
+++ src/main/java/seedu/ezdo/logic/commands/UndoCommand.java
@@ -5,9 +5,9 @@ import seedu.ezdo.logic.commands.exceptions.CommandException;
 public class UndoCommand extends Command {
     public static final String COMMAND_WORD = "undo";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Undo the previous undoable task.";
-    public static final String MESSAGE_SUCCESS = "Previous undoable task has been undone!";
-    public static final String MESSAGE_NO_PREV_TASK = "There is no previous undoable task!";
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Undo the previous undoable command.";
+    public static final String MESSAGE_SUCCESS = "Previous undoable command has been undone!";
+    public static final String MESSAGE_NO_PREV_COMMAND = "There is no previous undoable command!";
 
     @Override
     public CommandResult execute() throws CommandException {
