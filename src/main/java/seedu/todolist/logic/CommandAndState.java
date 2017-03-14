29a5ae8d8594c109313506414960a0b78a96b3f4
gngster <gjhsmiles@gmail.com>
2017-03-14 15:59:35 +0800

    edit todolist and CommandAndState class

--- src/main/java/seedu/todolist/logic/CommandAndState.java
+++ src/main/java/seedu/todolist/logic/CommandAndState.java
@@ -4,14 +4,13 @@ import seedu.todolist.logic.commands.Command;
 import seedu.todolist.model.ReadOnlyToDoList;
 
 public class CommandAndState {
-	Command command;
-	ReadOnlyToDoList state;
+    Command command;
+    ReadOnlyToDoList state;
 
-	public CommandAndState(Command command, ReadOnlyToDoList state) {
-	    this.command = command;
-	    this.state = state;
-
-	}
+    public CommandAndState(Command command, ReadOnlyToDoList state) {
+        this.command = command;
+        this.state = state;
+    }
 
     public Command getCommand() {
         return command;
