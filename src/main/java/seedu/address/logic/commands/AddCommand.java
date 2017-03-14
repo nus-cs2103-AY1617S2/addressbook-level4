6e1000036eb5ab2523c9312e785aaa254aab1e5e
Penghao Chen <PHMAN45@119-65.priv23.nus.edu.sg>
2017-03-14 18:56:31 +0800

    Update AddCommand and EditCommand: Change to the new prefixes defined

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -22,7 +22,7 @@ public class AddCommand extends Command {
     public static final String COMMAND_WORD = "add";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a Task to the to-do list. "
-            + "Parameters: TITLE v/VENUE s/STARTTIME e/ENDTIME  [t/TAG]...\n"
+            + "Parameters: TITLE @@VENUE from/STARTTIME to/ENDTIME  [##TAG]...\n"
             + "Example: " + COMMAND_WORD
             + " CS2103 Tutorial v/COM1-B110 s/March 8,10.00am e/March 8, 11.00am t/lesson t/project";
 
