2bf57f1286241f0be4cf43b6016167096e341bfa
vitquay1996 <leminhphuc10t1@gmail.com>
2017-03-14 22:07:42 +0800

    minor changes to CompleteCommandParser class

--- src/main/java/seedu/todolist/logic/parser/CompleteCommandParser.java
+++ src/main/java/seedu/todolist/logic/parser/CompleteCommandParser.java
@@ -18,7 +18,7 @@ public class CompleteCommandParser {
      * Parses the given {@code String} of arguments in the context of the CompleteCommand
      * and returns a CompleteCommand object for execution.
      */
-    public Command parse(String args) throws IllegalValueException {
+    public Command parse(String args) {
 
         Optional<Integer> index = ParserUtil.parseIndex(args);
         if (!index.isPresent()) {
@@ -26,6 +26,11 @@ public class CompleteCommandParser {
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, CompleteCommand.MESSAGE_USAGE));
         }
 
-        return new CompleteCommand(index.get());
+        try {
+            CompleteCommand newCompleteCommand = new CompleteCommand(index.get());
+            return newCompleteCommand;
+        } catch (IllegalValueException ive) {
+            return new IncorrectCommand(ive.getMessage());
+        }
     }
 }
