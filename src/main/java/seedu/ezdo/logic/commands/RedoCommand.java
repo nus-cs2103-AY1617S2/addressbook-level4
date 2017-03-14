c7bf8da58839e4c56066402b2be675a05253362f
Lok You Tan <e0003133@u.nus.edu>
2017-03-14 15:58:35 +0800

    Create RedoCommand.java

---
+++ src/main/java/seedu/ezdo/logic/commands/RedoCommand.java
@@ -0,0 +1,20 @@
+package seedu.ezdo.logic.commands;
+
+import seedu.ezdo.logic.commands.exceptions.CommandException;
+
+public class RedoCommand extends Command {
+
+    public static final String COMMAND_WORD = "redo";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Redo the last command you have undone.";
+    public static final String MESSAGE_SUCCESS = "Last command undone has been redone!";
+    public static final String MESSAGE_NO_PREV_COMMAND = "There is no redoable command!";
+
+
+    @Override
+    public CommandResult execute() throws CommandException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
