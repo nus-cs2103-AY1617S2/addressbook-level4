f45139be196f81913a1b2735b687e3b1d3752230
Lim Jie <limjie1994@gmail.com>
2017-03-16 00:28:05 +0800

    Changed TaskCardHandle to check for everything when checking if a card and a task is the same

--- src/test/java/guitests/guihandles/TaskCardHandle.java
+++ src/test/java/guitests/guihandles/TaskCardHandle.java
@@ -9,6 +9,11 @@ import javafx.scene.control.Labeled;
 import javafx.scene.layout.Region;
 import javafx.stage.Stage;
 import seedu.tasklist.model.tag.UniqueTagList;
+import seedu.tasklist.model.task.DeadlineTask;
+import seedu.tasklist.model.task.EventTask;
+import seedu.tasklist.model.task.FloatingTask;
+import seedu.tasklist.model.task.ReadOnlyDeadlineTask;
+import seedu.tasklist.model.task.ReadOnlyEventTask;
 import seedu.tasklist.model.task.ReadOnlyTask;
 
 /**
@@ -18,6 +23,11 @@ public class TaskCardHandle extends GuiHandle {
     private static final String NAME_FIELD_ID = "#name";
     private static final String COMMENT_FIELD_ID = "#comment";
     private static final String TAGS_FIELD_ID = "#tags";
+    private static final String PRIORITY_FIELD_ID = "#priority";
+    private static final String STATUS_FIELD_ID = "#status";
+    private static final String DEADLINE_FIELD_ID = "#deadline";
+    private static final String STARTDATE_FIELD_ID = "#startDate";
+    private static final String ENDDATE_FIELD_ID = "#endDate";
 
     private Node node;
 
@@ -63,9 +73,58 @@ public class TaskCardHandle extends GuiHandle {
     }
 
     public boolean isSameTask(ReadOnlyTask task) {
+        boolean isDeadlineSame;
+        boolean isStartDateSame;
+        boolean isEndDateSame;
+        String type = task.getType();
+        switch (type) {
+        case FloatingTask.TYPE:
+            isDeadlineSame = true;
+            isStartDateSame = true;
+            isEndDateSame = true;
+            break;
+        case DeadlineTask.TYPE:
+            isDeadlineSame = getDeadline().equals(((ReadOnlyDeadlineTask) task).getDeadlineString());
+            isStartDateSame = true;
+            isEndDateSame = true;
+            break;
+        case EventTask.TYPE:
+            isDeadlineSame = true;
+            isStartDateSame = getStartDate().equals(((ReadOnlyEventTask) task).getStartDateString());
+            isEndDateSame = getEndDate().equals(((ReadOnlyEventTask) task).getEndDateString());
+            break;
+        default:
+            isDeadlineSame = true;
+            isStartDateSame = true;
+            isEndDateSame = true;
+        }
         return getFullName().equals(task.getName().fullName)
                 && getComment().equals(task.getComment().value)
-                && getTags().equals(getTags(task.getTags()));
+                && getTags().equals(getTags(task.getTags()))
+                //TODO
+                //&& getStatus() == task.getStatus().value
+                && getPriority().equals(task.getPriority().value)
+                && isDeadlineSame && isStartDateSame && isEndDateSame;
+    }
+
+    private String getEndDate() {
+        return getTextFromLabel(ENDDATE_FIELD_ID).split(" ", 3)[2];
+    }
+
+    private String getStartDate() {
+        return getTextFromLabel(STARTDATE_FIELD_ID).split(" ", 3)[2];
+    }
+
+    private String getDeadline() {
+        return getTextFromLabel(DEADLINE_FIELD_ID).split(" ", 2)[1];
+    }
+
+    private boolean getStatus() {
+        return Boolean.parseBoolean(getTextFromLabel(STATUS_FIELD_ID));
+    }
+
+    private String getPriority() {
+        return getTextFromLabel(PRIORITY_FIELD_ID).split(" ")[1];
     }
 
     @Override
