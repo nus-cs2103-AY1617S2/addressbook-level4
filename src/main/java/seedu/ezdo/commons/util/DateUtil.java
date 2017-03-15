c0f0572627d013c113d9419005a17929c745a7b1
Lok You Tan <e0003133@u.nus.edu>
2017-03-15 18:33:00 +0800

    Create DateUtil class with checkTaskDate method(skeleton)

---
+++ src/main/java/seedu/ezdo/commons/util/DateUtil.java
@@ -0,0 +1,12 @@
+package seedu.ezdo.commons.util;
+
+import seedu.ezdo.model.todo.ReadOnlyTask;
+
+/*
+ * Checks for dates 
+ */
+public class DateUtil {
+    public boolean checkTaskDate(ReadOnlyTask task) {
+	return true;
+    }
+}
