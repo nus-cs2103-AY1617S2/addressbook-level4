497aade4a37c1d81a90e37a3bb488e14f2042ac9
YING-PC\m_a_t_000 <vpsy28@gmail.com>
2017-03-15 12:13:17 +0800

    Modified UI + cleaned up code

--- src/main/java/werkbook/task/ui/BrowserPanel.java
+++ src/main/java/werkbook/task/ui/BrowserPanel.java
@@ -49,10 +49,7 @@ public class BrowserPanel extends UiPart<Region> {
         description1.setText(task.getDescription().toString());
         start_time.setText(task.getStartDateTime().toString());
         end_time.setText(task.getEndDateTime().toString());
-  //    tags.setAccessibleText(task.getTags().toString());
         initTags(task);
-  //    task.getTags().forEach(tag -> tags.getChildren().toString());
-  //    task.getTags().forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
     }
     
     private void initTags(ReadOnlyTask task) {
