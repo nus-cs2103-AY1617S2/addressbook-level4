f078314551d3157551074f0b562ca6f78ff4fd2f
Vesali <e0006008@u.nus.edu>
2017-03-15 21:47:43 +0800

    Improve code quality

--- src/main/java/seedu/tasklist/logic/parser/LoadCommandParser.java
+++ src/main/java/seedu/tasklist/logic/parser/LoadCommandParser.java
@@ -12,17 +12,22 @@ import seedu.tasklist.logic.commands.LoadCommand;
  * Parses input arguments and creates a new LoadCommand object
  */
 public class LoadCommandParser {
+    public static final int CORRECT_LENGTH = 1;
+    public static final int FILE_PATH = 0;
+
 
     /**
      * Parses the given {@code String} of arguments in the context of the LoadCommand
      * and returns an LoadCommand object for execution.
      */
     public Command parse(String args) {
+
         final String[] path = args.trim().split("\\s+");
-        if (path.length > 1 || path[0].isEmpty()) {
+
+        if (path.length > CORRECT_LENGTH || path[FILE_PATH].isEmpty()) {
             return new IncorrectCommand(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, LoadCommand.MESSAGE_USAGE));
         }
-        return new LoadCommand(path[0]);
+        return new LoadCommand(path[FILE_PATH]);
     }
 }
