1135433bfe6ba4b9156b817043831152379b76e8
Vesali <e0006008@u.nus.edu>
2017-03-16 02:14:19 +0800

    correct checkstyle errors

--- src/main/java/seedu/tasklist/logic/commands/SaveCommand.java
+++ src/main/java/seedu/tasklist/logic/commands/SaveCommand.java
@@ -5,42 +5,43 @@ import java.nio.file.InvalidPathException;
 import java.nio.file.Paths;
 
 public class SaveCommand extends Command {
-    
+
     public static final String COMMAND_WORD = "save";
-    
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Change the directory and file name of the data that is being saved.\n"
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Change the directory and file name of the data that is being saved.\n"
             + "Parameters: FILE_PATH/FILE_NAME.xml\n"
             + "Example: " + COMMAND_WORD + " data/mytasklist.xml";
-    
+
     public static final String MESSAGE_SUCCESS = "File is successfully saved to: %1$s";
     public static final String MESSAGE_INVALID_PATH = "Invalid file path: %1$s";
     public static final String MESSAGE_FAILURE = "Unable to save file at specified path: %1$s";
-    
+
     private final String filePath;
     private static final String XML = ".xml";
-    
-    public SaveCommand(String filePath){
+
+    public SaveCommand(String filePath) {
         this.filePath = filePath;
     }
-    
+
     @Override
     public CommandResult execute() {
-        assert filePath != null; 
+        assert filePath != null;
         assert model != null;
-        
+
         if (!isValidPath(filePath)) {
             return new CommandResult(String.format(MESSAGE_INVALID_PATH, filePath));
         }
 
-        try{
+        try {
             model.saveTaskList(filePath);
             return new CommandResult(String.format(MESSAGE_SUCCESS, filePath));
-        }catch (IOException e){
+        } catch (IOException e) {
             return new CommandResult(String.format(MESSAGE_FAILURE, filePath));
         }
-        
+
     }
-    
+
 
     /**
      * Returns true if the given path is a valid path
