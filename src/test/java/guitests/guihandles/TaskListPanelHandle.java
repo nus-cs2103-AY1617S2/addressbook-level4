254d55b539205e6892b0197b211371b31795cc75
Yi Rui Goh <yirui94@gmail.com>
2017-03-15 11:46:17 +0800

    Fix test tasklistpanel #upcomingListView to #allListView

--- src/test/java/guitests/guihandles/TaskListPanelHandle.java
+++ src/test/java/guitests/guihandles/TaskListPanelHandle.java
@@ -25,7 +25,7 @@ public class TaskListPanelHandle extends GuiHandle {
     public static final int NOT_FOUND = -1;
     public static final String CARD_PANE_ID = "#cardPane";
 
-    private static final String PERSON_LIST_VIEW_ID = "#upcomingListView";
+    private static final String PERSON_LIST_VIEW_ID = "#allListView";
 
     public TaskListPanelHandle(GuiRobot guiRobot, Stage primaryStage) {
         super(guiRobot, primaryStage, TestApp.APP_TITLE);
