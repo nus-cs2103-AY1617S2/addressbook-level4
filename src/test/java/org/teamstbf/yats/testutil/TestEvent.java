2751f536e7788c31b49607de227ea9bf0790868c
HORIZON\riveria94 <timelesschaos94@gmail.com>
2017-03-15 21:29:52 +0800

    Remove trailing whitespaces

--- src/test/java/org/teamstbf/yats/testutil/TestEvent.java
+++ src/test/java/org/teamstbf/yats/testutil/TestEvent.java
@@ -14,115 +14,115 @@ import org.teamstbf.yats.model.tag.UniqueTagList;
  */
 public class TestEvent implements ReadOnlyEvent {
 
-    private Title name;
-    private Periodic period;
-    private Schedule startTime;
-    private Schedule endTime;
-    private Description description;
-    private boolean isDone;
-    private Location location;
-    private UniqueTagList tags;
+	private Title name;
+	private Periodic period;
+	private Schedule startTime;
+	private Schedule endTime;
+	private Description description;
+	private boolean isDone;
+	private Location location;
+	private UniqueTagList tags;
 
-    public TestEvent() {
-        tags = new UniqueTagList();
-    }
+	public TestEvent() {
+		tags = new UniqueTagList();
+	}
 
-    /**
-     * Creates a copy of {@code personToCopy}.
-     */
-    public TestEvent(TestEvent personToCopy) {
-        this.name = personToCopy.getTitle();
-        this.period = personToCopy.getPeriod();
-        this.location = personToCopy.getLocation();
-        this.startTime = personToCopy.getStartTime();
-        this.endTime = personToCopy.getEndTime();
-        this.description = personToCopy.getDescription();
-        this.tags = personToCopy.getTags();
-    }
+	/**
+	 * Creates a copy of {@code personToCopy}.
+	 */
+	public TestEvent(TestEvent personToCopy) {
+		this.name = personToCopy.getTitle();
+		this.period = personToCopy.getPeriod();
+		this.location = personToCopy.getLocation();
+		this.startTime = personToCopy.getStartTime();
+		this.endTime = personToCopy.getEndTime();
+		this.description = personToCopy.getDescription();
+		this.tags = personToCopy.getTags();
+	}
 
-    public void setTitle(Title name) {
-        this.name = name;
-    }
+	public String getAddCommand() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("add " + this.getTitle().fullName + " ");
+		sb.append("l/" + this.getLocation().value + " ");
+		sb.append("p/" + this.getPeriod().value + " ");
+		sb.append("s/" + this.getStartTime().value + " ");
+		sb.append("e/" + this.getEndTime().value + " ");
+		sb.append("d/" + this.getDescription().value + " ");
+		this.getTags().asObservableList().stream().forEach(s -> sb.append("t/" + s.tagName + " "));
+		return sb.toString();
+	}
 
-    public void setTags(UniqueTagList tags) {
-        this.tags = tags;
-    }
+	@Override
+	public Date getDeadline() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-    @Override
-    public Title getTitle() {
-        return name;
-    }
+	@Override
+	public Description getDescription() {
+		return description;
+	}
 
-    @Override
-    public Description getDescription() {
-        return description;
-    }
+	@Override
+	public Schedule getEndTime() {
+		return endTime;
+	}
 
-    @Override
-    public UniqueTagList getTags() {
-        return tags;
-    }
+	@Override
+	public Location getLocation() {
+		return location;
+	}
 
-    @Override
-    public String toString() {
-        return getAsText();
-    }
+	@Override
+	public Periodic getPeriod() {
+		return period;
+	}
 
-    public String getAddCommand() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("add " + this.getTitle().fullName + " ");
-        sb.append("l/" + this.getLocation().value + " ");
-        sb.append("p/" + this.getPeriod().value + " ");
-        sb.append("s/" + this.getStartTime().value + " ");
-        sb.append("e/" + this.getEndTime().value + " ");
-        sb.append("d/" + this.getDescription().value + " ");
-        this.getTags().asObservableList().stream().forEach(s -> sb.append("t/" + s.tagName + " "));
-        return sb.toString();
-    }
+	@Override
+	public Schedule getStartTime() {
+		return startTime;
+	}
 
-    @Override
-    public Location getLocation() {
-        return location;
-    }
+	@Override
+	public UniqueTagList getTags() {
+		return tags;
+	}
 
-    @Override
-    public Schedule getStartTime() {
-        return startTime;
-    }
+	@Override
+	public Title getTitle() {
+		return name;
+	}
 
-    @Override
-    public Schedule getEndTime() {
-        return endTime;
-    }
+	public void setDescription(Description description) {
+		this.description = description;
+	}
 
-    @Override
-    public Periodic getPeriod() {
-        return period;
-    }
+	public void setEndTime(Schedule schedule) {
+		this.endTime = schedule;
+	}
 
-    public void setDescription(Description description) {
-        this.description = description;
-    }
+	public void setLocation(Location location) {
+		this.location = location;
+	}
 
-    public void setPeriod(Periodic periodic) {
-        this.period = periodic;  
-    }
-    
-    public void setLocation(Location location) {
-        this.location = location;  
-    }
-    
-    public void setStartTime(Schedule schedule) {
-        this.startTime = schedule;
-    }
+	public void setPeriod(Periodic periodic) {
+		this.period = periodic;
+	}
 
-    public void setEndTime(Schedule schedule) {
-        this.endTime = schedule;
-    }
+	public void setStartTime(Schedule schedule) {
+		this.startTime = schedule;
+	}
 
-    @Override
-    public Date getDeadline() {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	public void setTags(UniqueTagList tags) {
+		this.tags = tags;
+	}
+
+	public void setTitle(Title name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return getAsText();
+	}
 }
