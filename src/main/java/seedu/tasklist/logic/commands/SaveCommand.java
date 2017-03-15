6f5a3a0b6c48a594e263ee18c0a3bcdc76d3b223
Vesali <e0006008@u.nus.edu>
2017-03-16 02:08:34 +0800

    Update SaveCommand.java

--- src/main/java/seedu/tasklist/logic/commands/SaveCommand.java
+++ src/main/java/seedu/tasklist/logic/commands/SaveCommand.java
@@ -1,6 +1,8 @@
 package seedu.tasklist.logic.commands;
 
 import java.io.IOException;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Paths;
 
 public class SaveCommand extends Command {
     
@@ -12,8 +14,10 @@ public class SaveCommand extends Command {
     
     public static final String MESSAGE_SUCCESS = "File is successfully saved to: %1$s";
     public static final String MESSAGE_INVALID_PATH = "Invalid file path: %1$s";
+    public static final String MESSAGE_FAILURE = "Unable to save file at specified path: %1$s";
     
- private final String filePath;
+    private final String filePath;
+    private static final String XML = ".xml";
     
     public SaveCommand(String filePath){
         this.filePath = filePath;
@@ -21,13 +25,41 @@ public class SaveCommand extends Command {
     
     @Override
     public CommandResult execute() {
+        assert filePath != null; 
+        assert model != null;
+        
+        if (!isValidPath(filePath)) {
+            return new CommandResult(String.format(MESSAGE_INVALID_PATH, filePath));
+        }
+
         try{
             model.saveTaskList(filePath);
             return new CommandResult(String.format(MESSAGE_SUCCESS, filePath));
         }catch (IOException e){
-            return new CommandResult(String.format(MESSAGE_INVALID_PATH, filePath));
+            return new CommandResult(String.format(MESSAGE_FAILURE, filePath));
         }
         
     }
+    
+
+    /**
+     * Returns true if the given path is a valid path
+     */
+    private boolean isValidPath(String path) {
+        try {
+            Paths.get(path);
+            return isXML(path);
+        } catch (InvalidPathException e) {
+            return false;
+        }
+    }
+
+    /**
+     * Returns true if the given path ends with the .xml extension.
+     */
+    private static boolean isXML(String path) {
+        return path.endsWith(XML);
+    }
+
 
 }
