a06ca4db08501225ab9f3a9e167e64bd1c5490c6
CHQ\Ching <hqching@gmail.com>
2017-03-15 11:38:00 +0800

    edit addCommand class

--- src/main/java/seedu/watodo/logic/commands/AddCommand.java
+++ src/main/java/seedu/watodo/logic/commands/AddCommand.java
@@ -7,11 +7,12 @@ import seedu.watodo.commons.exceptions.IllegalValueException;
 import seedu.watodo.logic.commands.exceptions.CommandException;
 import seedu.watodo.model.tag.Tag;
 import seedu.watodo.model.tag.UniqueTagList;
-import seedu.watodo.model.task.Address;
 import seedu.watodo.model.task.Description;
-import seedu.watodo.model.task.Email;
+import seedu.watodo.model.task.EventTask;
 import seedu.watodo.model.task.FloatingTask;
-import seedu.watodo.model.task.Phone;
+import seedu.watodo.model.task.Task;
+import seedu.watodo.model.task.DateTime;
+import seedu.watodo.model.task.DeadlineTask;
 import seedu.watodo.model.task.UniqueTaskList;
 
 /**
@@ -21,32 +22,68 @@ public class AddCommand extends Command {
 
     public static final String COMMAND_WORD = "add";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a floating task to the task manager. "
-            + "Parameters: TASK [#TAG]...\n"
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a task to the task manager. "
+            + "Parameters: TASK [by/ DATETIME] [from/ START_DATETIME] [to/ END_DATE_TIME] [#TAG]...\n"
             + "Example: " + COMMAND_WORD
-            + " read Lord of the Rings #personal";
+            + " read Lord of the rings by/ next thurs #personal";
 
     public static final String MESSAGE_SUCCESS = "New task added: %1$s";
     public static final String MESSAGE_DUPLICATE_TASK = "This task already exists in the task manager";
 
-    private final FloatingTask toAdd;
+    private enum taskTypes {FLOAT, DEADLINE, EVENT}; 
+    private Task toAdd;
 
     /**
-     * Creates an AddCommand using raw values.
+     * Creates an AddCommand using raw values
      *
      * @throws IllegalValueException if any of the raw values are invalid
      */
-    public AddCommand(String description, Set<String> tags)
+    public AddCommand(String description, String deadline, String startDate, String endDate, Set<String> tags)
             throws IllegalValueException {
+        
+        assert description != null;
+        
         final Set<Tag> tagSet = new HashSet<>();
         for (String tagName : tags) {
             tagSet.add(new Tag(tagName));
         }
-        this.toAdd = new FloatingTask(
-                new Description(description),
-                new UniqueTagList(tagSet)
-        );
+        
+        taskTypes taskType = checkTaskType(deadline, startDate, endDate);
+        
+        switch (taskType) {
+        case FLOAT:
+            this.toAdd = new FloatingTask(new Description(description), new UniqueTagList(tagSet));
+        case DEADLINE:
+            this.toAdd = new DeadlineTask(new Description(description), new DateTime(deadline), 
+                    new UniqueTagList(tagSet));
+        case EVENT:
+            this.toAdd = new EventTask(new Description(description), new DateTime(startDate), 
+                    new DateTime(endDate), new UniqueTagList(tagSet));
+        }
+        
     }
+    
+    /**
+     * Checks the type of task(floating, deadline or event) to be added
+     * based on the DATETIME parameters entered by the user. 
+     * @throws IllegalValueException if both deadline and startDate are entered, or if only one of startDate or endDate is entered
+     */
+    private taskTypes checkTaskType(String deadline, String startDate, String endDate) throws IllegalValueException {
+        if (deadline == null && startDate == null && endDate == null) { 
+            return taskTypes.FLOAT;
+        } else if (deadline != null) {
+                assert startDate == null && endDate == null;
+                return taskTypes.DEADLINE;
+        } else if (startDate != null && endDate != null) {
+            assert deadline == null;
+            return taskTypes.EVENT;
+        } else {
+            throw new IllegalValueException("Too many/few DATETIME arguments!");
+            //TODO possible re-factor?
+        }
+        
+    }
+
 
     @Override
     public CommandResult execute() throws CommandException {
