22ad64ee5d595b285b7136cc1aa1f2d0a2f75093
leeyh20 <leeyh20@hotmail.com>
2017-03-15 13:07:40 +0800

    Remove test browser panel

--- src/test/java/guitests/HelpWindowTest.java
+++ src/test/java/guitests/HelpWindowTest.java
@@ -21,11 +21,11 @@ public class HelpWindowTest extends DoistGUITest {
         personListPanel.clickOnListView();
         assertHelpWindowOpen(mainMenu.openHelpWindowUsingAccelerator());
 
-        browserPanel.clickOnWebView();
+//        browserPanel.clickOnWebView();
 //        assertHelpWindowNotOpen(mainMenu.openHelpWindowUsingAccelerator());
 
         //use menu button
-//        assertHelpWindowOpen(mainMenu.openHelpWindowUsingMenu());
+        assertHelpWindowOpen(mainMenu.openHelpWindowUsingMenu());
 
         //use command
         assertHelpWindowOpen(commandBox.runHelpCommand());
