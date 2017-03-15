1a308e27145e34297a012d1c5b7c5006a1501496
yoshi-1224 <yoshiaki.bb@gmail.com>
2017-03-16 02:36:43 +0800

    Uncomment guihandles

--- src/test/java/guitests/guihandles/MainGuiHandle.java
+++ src/test/java/guitests/guihandles/MainGuiHandle.java
@@ -1,40 +1,40 @@
-//package guitests.guihandles;
-//
-//import guitests.GuiRobot;
-//import javafx.stage.Stage;
-//import t15b1.taskcrusher.TestApp;
-//
-///**
-// * Provides a handle for the main GUI.
-// */
-//public class MainGuiHandle extends GuiHandle {
-//
-//    public MainGuiHandle(GuiRobot guiRobot, Stage primaryStage) {
-//        super(guiRobot, primaryStage, TestApp.APP_TITLE);
-//    }
-//
-//    public PersonListPanelHandle getPersonListPanel() {
-//        return new PersonListPanelHandle(guiRobot, primaryStage);
-//    }
-//
-//    public ResultDisplayHandle getResultDisplay() {
-//        return new ResultDisplayHandle(guiRobot, primaryStage);
-//    }
-//
-//    public CommandBoxHandle getCommandBox() {
-//        return new CommandBoxHandle(guiRobot, primaryStage, TestApp.APP_TITLE);
-//    }
-//
-//    public MainMenuHandle getMainMenu() {
-//        return new MainMenuHandle(guiRobot, primaryStage);
-//    }
-//
-//    public BrowserPanelHandle getBrowserPanel() {
-//        return new BrowserPanelHandle(guiRobot, primaryStage);
-//    }
-//
-//    public AlertDialogHandle getAlertDialog(String title) {
-//        guiRobot.sleep(1000);
-//        return new AlertDialogHandle(guiRobot, primaryStage, title);
-//    }
-//}
+package guitests.guihandles;
+
+import guitests.GuiRobot;
+import javafx.stage.Stage;
+import project.taskcrusher.TestApp;
+
+/**
+ * Provides a handle for the main GUI.
+ */
+public class MainGuiHandle extends GuiHandle {
+
+    public MainGuiHandle(GuiRobot guiRobot, Stage primaryStage) {
+        super(guiRobot, primaryStage, TestApp.APP_TITLE);
+    }
+
+    public PersonListPanelHandle getPersonListPanel() {
+        return new PersonListPanelHandle(guiRobot, primaryStage);
+    }
+
+    public ResultDisplayHandle getResultDisplay() {
+        return new ResultDisplayHandle(guiRobot, primaryStage);
+    }
+
+    public CommandBoxHandle getCommandBox() {
+        return new CommandBoxHandle(guiRobot, primaryStage, TestApp.APP_TITLE);
+    }
+
+    public MainMenuHandle getMainMenu() {
+        return new MainMenuHandle(guiRobot, primaryStage);
+    }
+
+    public BrowserPanelHandle getBrowserPanel() {
+        return new BrowserPanelHandle(guiRobot, primaryStage);
+    }
+
+    public AlertDialogHandle getAlertDialog(String title) {
+        guiRobot.sleep(1000);
+        return new AlertDialogHandle(guiRobot, primaryStage, title);
+    }
+}
