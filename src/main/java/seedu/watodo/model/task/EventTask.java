88addd56da2d6ed3df31fc40b0c7c20f9486e995
CHQ\Ching <hqching@gmail.com>
2017-03-15 11:33:02 +0800

    create new class for DeadlineTask, EventTask

---
+++ src/main/java/seedu/watodo/model/task/EventTask.java
@@ -0,0 +1,34 @@
+package seedu.watodo.model.task;
+
+import seedu.watodo.model.tag.UniqueTagList;
+
+public class EventTask extends Task {
+    
+    private DateTime startDateTime;
+    private DateTime endDateTime;
+    
+    public EventTask(Description description, DateTime startDate, DateTime dueDate, UniqueTagList tags) {
+        super(description, tags);
+        this.setStartDateTime(startDate);
+        this.setEndDateTime(dueDate);
+    }
+
+    public DateTime getStartDateTime() {
+        return startDateTime;
+    }
+
+    public void setStartDateTime(DateTime startDateTime) {
+        this.startDateTime = startDateTime;
+    }
+
+    public DateTime getEndDateTime() {
+        return endDateTime;
+    }
+
+    public void setEndDateTime(DateTime endDateTime) {
+        this.endDateTime = endDateTime;
+    }
+    
+    
+
+}
---
+++ src/main/java/seedu/watodo/model/task/ReadOnlyTask.java
@@ -0,0 +1,42 @@
+package seedu.watodo.model.task;
+
+import seedu.watodo.model.tag.UniqueTagList;
+import seedu.watodo.model.task.Task.Status;
+
+/**
+ * A read-only immutable interface for a Task in the task manager.
+ * Implementations should guarantee: details are present and not null, field values are validated.
+ */
+public interface ReadOnlyTask {
+
+    Description getDescription();
+    Status getStatus();
+
+    /**
+     * The returned TagList is a deep copy of the internal TagList,
+     * changes on the returned list will not affect the person's internal tags.
+     */
+    UniqueTagList getTags();
+
+    /**
+     * Returns true if both have the same state. (interfaces cannot override .equals)
+     */
+    default boolean isSameStateAs(ReadOnlyTask other) {
+        return other == this // short circuit if same object
+                || (other != null // this is first to avoid NPE below
+                && other.getDescription().equals(this.getDescription()) // state checks here onwards
+                );
+    }
+
+    /**
+     * Formats the task as text, showing all internal details.
+     */
+    default String getAsText() {
+        final StringBuilder builder = new StringBuilder();
+        builder.append(getDescription())
+                .append(" Tags: ");
+        getTags().forEach(builder::append);
+        return builder.toString();
+    }
+
+}
