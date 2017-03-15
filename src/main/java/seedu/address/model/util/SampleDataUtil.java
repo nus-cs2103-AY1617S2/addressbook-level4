241751cd354f1529708c68018aca3ef15dbbd010
Nguyen Quoc Bao <ryuu.sakamoto.164@outlook.com>
2017-03-15 21:58:51 +0800

    Fix sample data

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -15,24 +15,24 @@ public class SampleDataUtil {
     public static Task[] getSampleTasks() {
         try {
             return new Task[] {
-                new Task(new Title("Alex Yeoh"), new Deadline("tomorrow"), new Priority("urgent"),
-                    new Instruction("Blk 30 Geylang Street 29, #06-40"),
-                    new UniqueTagList("friends")),
-                new Task(new Title("Bernice Yu"), new Deadline("tomorrow"), new Priority("urgent"),
-                    new Instruction("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
-                    new UniqueTagList("colleagues", "friends")),
-                new Task(new Title("Charlotte Oliveiro"), new Deadline("tomorrow"), new Priority("urgent"),
-                    new Instruction("Blk 11 Ang Mo Kio Street 74, #11-04"),
-                    new UniqueTagList("neighbours")),
-                new Task(new Title("David Li"), new Deadline("tomorrow"), new Priority("urgent"),
-                    new Instruction("Blk 436 Serangoon Gardens Street 26, #16-43"),
-                    new UniqueTagList("family")),
-                new Task(new Title("Irfan Ibrahim"), new Deadline("tomorrow"), new Priority("urgent"),
-                    new Instruction("Blk 47 Tampines Street 20, #17-35"),
-                    new UniqueTagList("classmates")),
-                new Task(new Title("Roy Balakrishnan"), new Deadline("tomorrow"), new Priority("urgent"),
-                    new Instruction("Blk 45 Aljunied Street 85, #11-31"),
-                    new UniqueTagList("colleagues"))
+                new Task(new Title("Buy groceries"), new Deadline("today"), new Priority("1"),
+                    new Instruction("eggs x2, bread x1, milk x4"),
+                    new UniqueTagList("chores")),
+                new Task(new Title("Fill ice tray"), new Deadline("tonight at 9pm"), new Priority("2"),
+                    new Instruction("or tomorrow no coffee, no coffee no work, no work no life"),
+                    new UniqueTagList("everyday", "trivialButCanKill")),
+                new Task(new Title("Finish React Native course"), new Deadline("floating"), new Priority("3"),
+                    new Instruction("clear section 7 and final test"),
+                    new UniqueTagList("selfLearn")),
+                new Task(new Title("Revise CS3243"), new Deadline("sunday"), new Priority("3"),
+                    new Instruction("lecture 9"),
+                    new UniqueTagList("school")),
+                new Task(new Title("Finish CS2103T issues 23"), new Deadline("tomorrow"), new Priority("5"),
+                    new Instruction("clear up for Travis"),
+                    new UniqueTagList("project", "school")),
+                new Task(new Title("Release v01"), new Deadline("last sunday"), new Priority("-2"),
+                    new Instruction("done"),
+                    new UniqueTagList("project", "school"))
             };
         } catch (IllegalValueException e) {
             throw new AssertionError("sample data cannot be invalid", e);
