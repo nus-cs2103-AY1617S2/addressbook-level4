cabf2bc12f7d3d58e77363f50200c4c2fd26b9e7
MaggieMeow <maggieliyd@gmail.com>
2017-03-16 19:22:09 +0800

    Add alternative prefixes in CliSyntax as comment for now

--- src/main/java/seedu/taskmanager/logic/parser/CliSyntax.java
+++ src/main/java/seedu/taskmanager/logic/parser/CliSyntax.java
@@ -14,6 +14,11 @@ public class CliSyntax {
     public static final Prefix PREFIX_ENDDATE = new Prefix("e/");
     public static final Prefix PREFIX_DESCRIPTION = new Prefix("d/");
     public static final Prefix PREFIX_TAG = new Prefix("#");
+    
+    /* Alternative prefix definitions for natural variations of user input*/
+//    public static final Prefix ALTERNATIVE_PREFIX_STARTDATE = new Prefix("start on ");
+//    public static final Prefix ALTERNATIVE_PREFIX_ENDDATE = new Prefix("end on ");
+//    public static final Prefix ALTERNATIVE_PREFIX_DESCRIPTION = new Prefix("with description ");
 
     /* Patterns definitions */
     public static final Pattern KEYWORDS_ARGS_FORMAT =
