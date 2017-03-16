3ee762bb0587f38aa77fab5037bb72fb38490f8e
Nguyen Quang Dung <nqdung14@gmail.com>
2017-03-17 01:00:45 +0800

    Remove menu bar

--- src/test/java/guitests/HelpWindowTest.java
+++ src/test/java/guitests/HelpWindowTest.java
@@ -11,18 +11,6 @@ public class HelpWindowTest extends TaskManagerGuiTest {
 
     @Test
     public void openHelpWindow() {
-        //use accelerator
-        commandBox.clickOnTextField();
-        assertHelpWindowOpen(mainMenu.openHelpWindowUsingAccelerator());
-
-        resultDisplay.clickOnTextArea();
-        assertHelpWindowOpen(mainMenu.openHelpWindowUsingAccelerator());
-
-        taskListPanel.clickOnListView();
-        assertHelpWindowOpen(mainMenu.openHelpWindowUsingAccelerator());
-
-        //use menu button
-        assertHelpWindowOpen(mainMenu.openHelpWindowUsingMenu());
 
         //use command
         assertHelpWindowOpen(commandBox.runHelpCommand());
