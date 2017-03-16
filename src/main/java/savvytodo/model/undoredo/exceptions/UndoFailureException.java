44285a589d81bf030f694a65df6fc4b33a487816
coyotestarrkwsq <espada_wsq@hotmail.com>
2017-03-16 16:24:45 +0800

    create new classes to support undo of edit, delete, add, clear

---
+++ src/main/java/savvytodo/model/undoredo/exceptions/UndoFailureException.java
@@ -0,0 +1,14 @@
+package savvytodo.model.undoredo.exceptions;
+
+/**
+ * 
+ * @author A0124863A
+ * Signals that undo cannot be performed
+ *
+ */
+public class UndoFailureException extends Exception {
+
+    public UndoFailureException(String s) {
+        super(s);
+    }
+}
