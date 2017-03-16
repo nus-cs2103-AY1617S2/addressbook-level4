686170170bf98c235a0449e043f0a42a979a2ad0
Semsettin Oguz Yuksek <semsyuksek@gmail.com>
2017-03-16 14:12:40 +0800

    trying to pass travis

--- src/test/java/seedu/address/testutil/TypicalTestTasks.java
+++ src/test/java/seedu/address/testutil/TypicalTestTasks.java
@@ -14,10 +14,17 @@ public class TypicalTestTasks {
 
     public TypicalTestTasks() {
         try {
-            alice = new TaskBuilder().withName("Alice Pauline")
-                    .withTime("17/02/2017")
-                    .withTags("friends").build();
+            alice = new TaskBuilder().withName("Alice Pauline").withTime("17/02/2017").withTags("friends").build();
+            benson = new TaskBuilder().withName("Benson Meier").withTime("17/02/2017").withTags("owesMoney", "friends").build();
+            carl = new TaskBuilder().withName("Carl Kurz").withTime("17/02/2017").build();
+            daniel = new TaskBuilder().withName("Daniel Meier").withTime("17/02/2017").build();
+            elle = new TaskBuilder().withName("Elle Meyer").withTime("17/02/2017").build();
+            fiona = new TaskBuilder().withName("Fiona Kunz").withTime("17/02/2017").build();
+            george = new TaskBuilder().withName("George Best").withTime("17/02/2017").build();
 
+            // Manually added
+            hoon = new TaskBuilder().withName("Hoon Meier").withTime("17/02/2017").build();
+            ida = new TaskBuilder().withName("Ida Mueller").withTime("17/02/2017").build();
         } catch (IllegalValueException e) {
             e.printStackTrace();
             assert false : "not possible";
