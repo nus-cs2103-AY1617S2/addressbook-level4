5f3b30541d7616456c81ed9b9d2934ba32e59de4
Fanyi <zfy926@gmail.com>
2017-03-16 14:15:25 +0800

    modify SelectCommand

--- src/main/java/seedu/address/logic/commands/SelectCommand.java
+++ src/main/java/seedu/address/logic/commands/SelectCommand.java
@@ -21,7 +21,7 @@ public class SelectCommand extends Command {
             + "Parameters: INDEX (must be a positive integer)\n"
             + "Example: " + COMMAND_WORD + " 1";
 
-    public static final String MESSAGE_SELECT_TASK_SUCCESS = "Selected Person: %1$s";
+    public static final String MESSAGE_SELECT_TASK_SUCCESS = "Selected Task: %1$s";
 
     public SelectCommand(int targetIndex) {
         this.targetIndex = targetIndex;
