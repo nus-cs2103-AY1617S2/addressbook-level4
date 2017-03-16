88b00ddc84e1763c5499b42f4a53420bef813a27
Vesali <e0006008@u.nus.edu>
2017-03-17 00:55:23 +0800

    Update SortCommand.java

--- src/main/java/seedu/tasklist/logic/commands/SortCommand.java
+++ src/main/java/seedu/tasklist/logic/commands/SortCommand.java
@@ -1,6 +1,5 @@
 package seedu.tasklist.logic.commands;
 
-import seedu.tasklist.logic.commands.exceptions.CommandException;
 
 public class SortCommand extends Command{
     
@@ -8,20 +7,43 @@ public class SortCommand extends Command{
     private final String parameter;
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ":Sorts task according to the parameter specified. Only 1 of the 4. Name:n"
-            + "Priority:p or StartDate: sd or EndDate:ed\n"
+            + ":Sorts task according to the parameter specified. Only 1 of the 5. Name:n"
+            + "Priority:p or StartDate: sd or EndDate:ed or Deadline:d\n"
             + "Parameters: n, p, sd, ed\n"
-            + "Example: " + COMMAND_WORD + "n";
-
+            + "Example: " + COMMAND_WORD + " n";
+    public static final String MESSAGE_FAILURE = "Invalid sorting parameter used!" + MESSAGE_USAGE;
+    public static final String MESSAGE_SUCCESS = "Task sorted according to given parameter.";
     
     public SortCommand(String parameter) {
-        this.parameter = parameter;
+        switch (parameter) {
+        case "d":
+            this.parameter = "Deadline";
+            break;
+        case "sd":
+            this.parameter = "Start Date";
+            break;
+        case "ed":
+            this.parameter = "End Date";
+            break;
+        case "n":
+            this.parameter = "Name";
+            break;
+        case "p":
+            this.parameter = "Priority";
+            break;
+        default:
+            this.parameter = null;
+        }
     }
 
     @Override
-    public CommandResult execute() throws CommandException {
+    public CommandResult execute() {
        
-        return null;
+        if(parameter == null) {
+            return new CommandResult(MESSAGE_FAILURE);
+        }
+        model.sortTaskList(parameter);
+        return new CommandResult(MESSAGE_SUCCESS); 
     }
 
 }
