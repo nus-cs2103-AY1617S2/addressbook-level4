ebd7677d1931d8799b2fb8fbe7b1065191d81e7d
LENOVO-PC-LYL\Liu yulin <yulinliu668@gmail.com>
2017-03-16 22:02:24 +0800

    Fixed coding standard issues

--- src/main/java/seedu/address/commons/events/storage/FileLocationChangedEvent.java
+++ src/main/java/seedu/address/commons/events/storage/FileLocationChangedEvent.java
@@ -3,20 +3,22 @@ package seedu.address.commons.events.storage;
 
 import seedu.address.commons.events.BaseEvent;
 
-public class FileLocationChangedEvent extends BaseEvent{
-	private String filePath;
-	
-	public FileLocationChangedEvent(String filePath){
-		this.filePath = filePath;
-	}
-	
-	public String getFilePath(){
-		return filePath;
-	}
-	
-	
-	@Override
-	public String toString(){
-		return"The file has been changed to" +filePath;
-	}
+public class FileLocationChangedEvent extends BaseEvent {
+
+    private String filePath;
+
+    public FileLocationChangedEvent(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+
+    @Override
+    public String toString() {
+        return "The file has been changed to" + filePath;
+    }
+
 }
