fcde29bba453ffd9421d9f208d16c4f393fdd8f1
Penghao Chen <xmchenph@gmail.com>
2017-03-16 14:53:26 +0800

    Update TitleTest: change test cases for title

--- src/test/java/seedu/address/model/task/TitleTest.java
+++ src/test/java/seedu/address/model/task/TitleTest.java
@@ -11,17 +11,15 @@ public class TitleTest {
     public void isValidTitle() {
         // invalid name
         assertFalse(Title.isValidTitle("")); // empty string
-        //assertFalse(Title.isValidTitle(" ")); // spaces only
-        //assertFalse(Title.isValidTitle("^")); // only non-alphanumeric
-                                              // characters
-        //assertFalse(Title.isValidTitle("peter*")); // contains non-alphanumeric
-                                                   // characters
-
+        assertFalse(Title.isValidTitle(" ")); // spaces only
+        assertFalse(Title.isValidTitle("^")); // only non-alphanumeric characters
+        assertFalse(Title.isValidTitle("????")); //several non-alphanumeric characters
+        
         // valid name
         assertTrue(Title.isValidTitle("debugging")); // alphabets only
         assertTrue(Title.isValidTitle("12345")); // numbers only
-        assertTrue(Title.isValidTitle("debug for CS2103")); // alphanumeric
-                                                         // characters
+        assertTrue(Title.isValidTitle("My-SQL testing")); //first word contain symbols
+        assertTrue(Title.isValidTitle("debug for CS2103")); // alphanumeric characters
         assertTrue(Title.isValidTitle("Debugging")); // with capital letters
         assertTrue(Title.isValidTitle("Debugging until the sun rises again at 7am")); // long
                                                                           // names
