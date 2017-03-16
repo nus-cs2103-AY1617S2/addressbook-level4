908eaa0456f468a35f2686d06d4e846bb86e82fc
CHQ\Ching <hqching@gmail.com>
2017-03-16 10:26:49 +0800

    updated user feedback messages

--- src/main/java/seedu/watodo/model/task/EventTask.java
+++ src/main/java/seedu/watodo/model/task/EventTask.java
@@ -1,5 +1,7 @@
 package seedu.watodo.model.task;
 
+import java.util.Objects;
+
 import seedu.watodo.model.tag.UniqueTagList;
 
 public class EventTask extends Task implements ReadOnlyTask {
@@ -29,6 +31,40 @@ public class EventTask extends Task implements ReadOnlyTask {
         this.endDateTime = endDateTime;
     }
     
-    
+    @Override
+    public void resetData(ReadOnlyTask replacement) {
+        assert replacement != null;
+
+        this.setDescription(replacement.getDescription());
+        this.setTags(replacement.getTags());
+        this.setStatus(replacement.getStatus());
+        //TODO the deadline in 
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof DeadlineTask // instanceof handles nulls
+                        && this.isSameStateAs((ReadOnlyTask) other) 
+                        && this.getStartDateTime().equals(((EventTask) other).getStartDateTime())
+                        && this.getEndDateTime().equals(((EventTask) other).getEndDateTime()));
+    }
+
+    @Override
+    public int hashCode() {
+        // use this method for custom fields hashing instead of implementing your own
+        return Objects.hash(description, status, startDateTime, endDateTime, tags);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+        builder.append(getDescription())
+                .append(" from: ").append(getStartDateTime().toString())
+                .append(" to: ").append(getEndDateTime().toString())
+                .append(" Tags: ");
+        getTags().forEach(builder::append);
+        return builder.toString();
+    }
 
 }
--- src/main/java/seedu/watodo/model/task/Task.java
+++ src/main/java/seedu/watodo/model/task/Task.java
@@ -11,7 +11,7 @@ import seedu.watodo.model.tag.UniqueTagList;
  */
 public class Task implements ReadOnlyTask {
 
-    public enum Status {UNDONE, DONE, OVERDUE} // Represents a Task's current status in the task manager.
+    public enum Status { UNDONE, DONE, OVERDUE } // Represents a Task's current status in the task manager.
     
     protected Description description;
     protected Status status;
@@ -83,7 +83,7 @@ public class Task implements ReadOnlyTask {
     @Override
     public int hashCode() {
         // use this method for custom fields hashing instead of implementing your own
-        return Objects.hash(description, tags);
+        return Objects.hash(description, status, tags);
     }
 
     @Override
