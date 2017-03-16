770a600b42921fb1be75d53ce22b6eaa46dc9ebe
LENOVO-PC-LYL\Liu yulin <yulinliu668@gmail.com>
2017-03-16 21:46:10 +0800

    Create FileLocationChangedEvent

---
+++ src/main/java/seedu/address/commons/events/storage/FileLocationChangedEvent.java
@@ -0,0 +1,22 @@
+package seedu.address.commons.events.storage;
+
+
+import seedu.address.commons.events.BaseEvent;
+
+public class FileLocationChangedEvent extends BaseEvent{
+	private String filePath;
+	
+	public FileLocationChangedEvent(String filePath){
+		this.filePath = filePath;
+	}
+	
+	public String getFilePath(){
+		return filePath;
+	}
+	
+	
+	@Override
+	public String toString(){
+		return"The file has been changed to" +filePath;
+	}
+}
