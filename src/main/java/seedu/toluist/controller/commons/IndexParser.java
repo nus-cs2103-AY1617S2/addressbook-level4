14e4a3c0a8f733dab6ee92ac2f0683fdf1d233e2
Melvin <melvin0719@hotmail.com>
2017-03-17 23:46:23 +0800

    Change all IndexTokenizer to IndexParser

--- src/main/java/seedu/toluist/controller/commons/IndexParser.java
+++ src/main/java/seedu/toluist/controller/commons/IndexParser.java
@@ -11,7 +11,7 @@ import seedu.toluist.commons.util.StringUtil;
 /**
  * Tokenize string of indexes into a list of integer index.
  */
-public class IndexTokenizer {
+public class IndexParser {
     private static final String HYPHEN = "-";
 
     /**
