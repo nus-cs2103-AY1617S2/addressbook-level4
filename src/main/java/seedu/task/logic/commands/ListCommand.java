4b89c4455313501b442d8f657374bc05469c1db2
walter tay <waltertay4749@gmail.com>
2017-03-17 11:38:58 +0800

    travis checkstyle

--- src/main/java/seedu/task/logic/commands/ListCommand.java
+++ src/main/java/seedu/task/logic/commands/ListCommand.java
@@ -9,7 +9,7 @@ public class ListCommand extends Command {
     public static final String COMMAND_WORD = "list";
     public static final String COMMAND_WORD_SHORTFORM = "ls";
     public static final String COMMAND_WORD_HOTKEY = "l";
-    
+
 
     public static final String MESSAGE_SUCCESS = "Listed all tasks";
 
