d326cb4cd46611c0572b5f4a3f7d2761fccac844
Aditya Hendraatmaja <ahendraatmaja@gmail.com>
2017-03-17 23:39:09 +0800

    Fix ChangeDirectory feature

---
+++ src/main/java/seedu/taskmanager/commons/events/storage/TaskManagerStorageDirectoryChangedEvent.java
@@ -0,0 +1,31 @@
+package seedu.taskmanager.commons.events.storage;
+
+import seedu.taskmanager.commons.events.BaseEvent;
+import seedu.taskmanager.commons.core.Config;
+
+/**
+ * Indicates a change in directory of the TaskManager initiated by user
+ */
+public class TaskManagerStorageDirectoryChangedEvent extends BaseEvent {
+    private final String newFilePath;
+    private final Config newConfig;
+    
+    public TaskManagerStorageDirectoryChangedEvent (String newFilePath, Config newConfig) {
+        this.newFilePath = newFilePath;
+        this.newConfig = newConfig;
+    }
+    
+    public String getNewFilePath() {
+        return this.newFilePath;
+    }
+    
+    public Config getNewConfig() {
+        return this.newConfig;
+    }
+
+    @Override
+    public String toString() {
+        return "TaskManager Directory  changed to " + this.newFilePath;
+    }
+
+}
--- src/main/java/seedu/taskmanager/logic/parser/ChangeDirectoryCommandParser.java
+++ src/main/java/seedu/taskmanager/logic/parser/ChangeDirectoryCommandParser.java
@@ -1,25 +1,25 @@
 package seedu.taskmanager.logic.parser;
 
 import static seedu.taskmanager.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.taskmanager.logic.parser.CliSyntax.PATH_ARGS_FORMAT;
+import static seedu.taskmanager.logic.parser.CliSyntax.FILEPATH_ARGS_FORMAT;
 
 import java.util.regex.Matcher;
 
 import seedu.taskmanager.logic.commands.Command;
-import seedu.taskmanager.logic.commands.ChangeDirCommand;
+import seedu.taskmanager.logic.commands.ChangeDirectoryCommand;
 import seedu.taskmanager.logic.commands.IncorrectCommand;
 
-public class ChangeDirCommandParser {
+public class ChangeDirectoryCommandParser {
     /**
      * Parses the given {@code String} of path in the context of the ChangeDirCommand
      * and returns an ChangeDirCommand object for execution.
      */
     public Command parse(String args) {
-        final Matcher matcher = PATH_ARGS_FORMAT.matcher(args.trim());
+        final Matcher matcher = FILEPATH_ARGS_FORMAT.matcher(args.trim());
         if (!matcher.matches()) {
             return new IncorrectCommand(
-                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, ChangeDirCommand.MESSAGE_USAGE));
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, ChangeDirectoryCommand.MESSAGE_USAGE));
         }
-        return new ChangeDirCommand(args.trim());
+        return new ChangeDirectoryCommand(args.trim());
     }
 }
