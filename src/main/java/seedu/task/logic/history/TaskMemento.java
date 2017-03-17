59e8c419f69f018b9d9705567729627f4d84b66a
Evan Yeung <evan.yeung1@gmail.com>
2017-03-17 23:20:10 +0800

    Add the datastructure to support undo/redo history

---
+++ src/main/java/seedu/task/logic/history/TaskMemento.java
@@ -0,0 +1,16 @@
+package seedu.task.logic.history;
+
+import seedu.task.model.task.Task;
+
+/**
+ * A memento contains the information to restore the system to some state. In this case, it
+ * only contains a Task. This task object may be used to undo/redo the effects of commands on a
+ * Task. The Task ID may be used to match up the memento's task with that in the Task list.
+ */
+public class TaskMemento {
+    public final Task task;
+
+    public TaskMemento(Task task) {
+        this.task = task;
+    }
+}
