74b07a972199def6da45a5c826793a83fc81e759
InfinityFrost <iceazul_awakendragon@hotmail.com>
2017-03-17 17:36:39 +0800

    Import storage into command

--- src/main/java/seedu/taskboss/logic/commands/Command.java
+++ src/main/java/seedu/taskboss/logic/commands/Command.java
@@ -4,12 +4,14 @@ import seedu.taskboss.commons.core.Messages;
 import seedu.taskboss.commons.exceptions.IllegalValueException;
 import seedu.taskboss.logic.commands.exceptions.CommandException;
 import seedu.taskboss.model.Model;
+import seedu.todolist.storage.Storage;
 
 /**
  * Represents a command with hidden internal logic and the ability to be executed.
  */
 public abstract class Command {
     protected Model model;
+    protected Storage storage;
 
     /**
      * Constructs a feedback message to summarise an operation that displayed a listing of tasks.
