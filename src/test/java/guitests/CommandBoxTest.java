7d06c369a40bb53f8db4f730bb14ea5599d39370
Yi Rui Goh <yirui94@users.noreply.github.com>
2017-03-17 15:21:17 +0800

    Update commandBox test to use Find instead of Select

--- src/test/java/guitests/CommandBoxTest.java
+++ src/test/java/guitests/CommandBoxTest.java
@@ -12,7 +12,7 @@ import seedu.geekeep.ui.CommandBox;
 
 public class CommandBoxTest extends AddressBookGuiTest {
 
-    private static final String COMMAND_THAT_SUCCEEDS = "select 3";
+    private static final String COMMAND_THAT_SUCCEEDS = "find NOSUCHTASKEXIST";
     private static final String COMMAND_THAT_FAILS = "invalid command";
 
     private ArrayList<String> defaultStyleOfCommandBox;
