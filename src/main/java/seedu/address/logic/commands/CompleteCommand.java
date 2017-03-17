78b045a3ca98aa3c2ccef03733ca8a7f318bb430
Jordan May <jordaanmay@gmail.com>
2017-03-17 18:36:10 +0800

    add CompleteCommand.java

--- src/main/java/seedu/address/logic/commands/CompleteCommand.java
+++ src/main/java/seedu/address/logic/commands/CompleteCommand.java
@@ -3,29 +3,29 @@ package seedu.address.logic.commands;
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.UnmodifiableObservableList;
 import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
 import seedu.address.model.todo.ReadOnlyTodo;
 import seedu.address.model.todo.UniqueTodoList.TodoNotFoundException;
 
 /**
- * Deletes a todo identified using it's last displayed index from the todo list.
+ * Completes a todo identified using its last displayed index from the todo list
  */
-public class DeleteCommand extends Command {
+public class CompleteCommand extends Command {
 
-    public static final String COMMAND_WORD = "delete";
+    public static final String COMMAND_WORD = "complete";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Deletes the todo identified by the index number used in the last todo listing.\n"
+            + ": Undoes last action if it involves modifying a todo.\n"
             + "Parameters: INDEX (must be a positive integer)\n"
             + "Example: " + COMMAND_WORD + " 1";
 
-    public static final String MESSAGE_DELETE_TODO_SUCCESS = "Deleted Todo: %1$s";
+    public static final String MESSAGE_COMPLETE_TODO_SUCCESS = "Completed Todo: %1$s";
 
     public final int targetIndex;
 
-    public DeleteCommand(int targetIndex) {
+    public CompleteCommand(int targetIndex) {
         this.targetIndex = targetIndex;
     }
-
 
     @Override
     public CommandResult execute() throws CommandException {
@@ -36,14 +36,15 @@ public class DeleteCommand extends Command {
             throw new CommandException(Messages.MESSAGE_INVALID_TODO_DISPLAYED_INDEX);
         }
 
-        ReadOnlyTodo todoToDelete = lastShownList.get(targetIndex - 1);
+        ReadOnlyTodo todoToComplete = lastShownList.get(targetIndex - 1);
 
         try {
-            model.deleteTodo(todoToDelete);
+            model.completeTodo(todoToComplete);
         } catch (TodoNotFoundException tnfe) {
             assert false : "The target todo cannot be missing";
         }
 
-        return new CommandResult(String.format(MESSAGE_DELETE_TODO_SUCCESS, todoToDelete));
+        return new CommandResult(String.format(MESSAGE_COMPLETE_TODO_SUCCESS, todoToComplete));
     }
+
 }
