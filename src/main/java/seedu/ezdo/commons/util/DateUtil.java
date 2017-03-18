2a84f85a3ce1c14320ac2cda737dd6c1a6cc9032
Lok You Tan <e0003133@u.nus.edu>
2017-03-18 20:37:19 +0800

    Add assert Change return boolean to one line

--- src/main/java/seedu/ezdo/commons/util/DateUtil.java
+++ src/main/java/seedu/ezdo/commons/util/DateUtil.java
@@ -11,15 +11,13 @@ import seedu.ezdo.model.todo.ReadOnlyTask;
  */
 public class DateUtil {
     public static boolean isTaskDateValid(ReadOnlyTask task) throws ParseException {
+        assert task != null;
         SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy H:mm");
         Date startDate = df.parse(task.getStartDate().toString());
         Date dueDate = df.parse(task.getDueDate().toString());
         if (task.getStartDate().toString().equals("") || task.getDueDate().toString().equals("")) {
             return true;
         }
-        if (startDate.compareTo(dueDate) >= 0) {
-            return false;
-        }
-        return true;
+        return (startDate.compareTo(dueDate) < 0);
     }
 }
