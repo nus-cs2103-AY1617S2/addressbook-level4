a58db94f73cee05dc7854768b132236e66dd1ec9
Shao Hui <shinlos@hotmail.com>
2017-03-18 12:45:22 +0800

    refactor NameTest.java

--- src/test/java/seedu/todolist/model/task/NameTest.java
+++ src/test/java/seedu/todolist/model/task/NameTest.java
@@ -9,17 +9,17 @@ public class NameTest {
 
     @Test
     public void isValidName() {
-        // invalid name
+        // invalid task name
         assertFalse(Name.isValidName("")); // empty string
         assertFalse(Name.isValidName(" ")); // spaces only
         assertFalse(Name.isValidName("^")); // only non-alphanumeric characters
-        assertFalse(Name.isValidName("peter*")); // contains non-alphanumeric characters
+        assertFalse(Name.isValidName("laundry*")); // contains non-alphanumeric characters
 
-        // valid name
-        assertTrue(Name.isValidName("peter jack")); // alphabets only
-        assertTrue(Name.isValidName("12345")); // numbers only
-        assertTrue(Name.isValidName("peter the 2nd")); // alphanumeric characters
-        assertTrue(Name.isValidName("Capital Tan")); // with capital letters
-        assertTrue(Name.isValidName("David Roger Jackson Ray Jr 2nd")); // long names
+        // valid task name
+        assertTrue(Name.isValidName("buy milk")); // alphabets only
+        assertTrue(Name.isValidName("20170727")); // numbers only
+        assertTrue(Name.isValidName("buy 2 loaves of bread")); // alphanumeric characters
+        assertTrue(Name.isValidName("send email to Bill Cosby")); // with capital letters
+        assertTrue(Name.isValidName("Pick up dinner and some fruits before going home")); // long task names
     }
 }
