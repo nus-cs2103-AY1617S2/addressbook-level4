9e48c875e39eb510d54d283ce1e9d459ec329fd8
Shao Hui <shinlos@hotmail.com>
2017-03-19 01:04:29 +0800

    refactor list_command

--- src/main/java/seedu/todolist/logic/commands/ListCommand.java
+++ src/main/java/seedu/todolist/logic/commands/ListCommand.java
@@ -1,5 +1,9 @@
 package seedu.todolist.logic.commands;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+//@@author A0139633B
 /**
  * Lists all tasks in the to-do list to the user.
  */
@@ -7,10 +11,15 @@ public class ListCommand extends Command {
 
     public static final String COMMAND_WORD = "list";
 
+    public static final String TYPE_DEFAULT = "";
     public static final String TYPE_ALL = "all";
     public static final String TYPE_INCOMPLETE = "incomplete";
     public static final String TYPE_COMPLETE = "complete";
     public static final String TYPE_OVERDUE = "overdue";
+
+    private static ArrayList<String> validCommands = new ArrayList<String>(Arrays.asList(
+            TYPE_DEFAULT, TYPE_ALL, TYPE_INCOMPLETE, TYPE_COMPLETE, TYPE_OVERDUE
+            ));
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Lists the type of tasks specified.\n"
@@ -25,22 +34,23 @@ public class ListCommand extends Command {
         this.taskType = taskType;
     }
 
+    public static boolean isValidCommand(String command) {
+        return validCommands.contains(command);
+    }
+
     @Override
     public CommandResult execute() {
         switch(taskType) {
 
         case TYPE_INCOMPLETE:
-            System.out.println("Executing task type " + TYPE_INCOMPLETE);
             model.getFilteredIncompleteTaskList();
             break;
 
         case TYPE_COMPLETE:
-            System.out.println("Executing task type " + TYPE_COMPLETE);
             model.getFilteredCompleteTaskList();
             break;
 
         case TYPE_OVERDUE:
-            System.out.println("Executing task type " + TYPE_OVERDUE);
             model.getFilteredOverdueTaskList();
             break;
 
--- src/main/java/seedu/todolist/logic/parser/ListCommandParser.java
+++ src/main/java/seedu/todolist/logic/parser/ListCommandParser.java
@@ -6,6 +6,7 @@ import seedu.todolist.logic.commands.Command;
 import seedu.todolist.logic.commands.IncorrectCommand;
 import seedu.todolist.logic.commands.ListCommand;
 
+//@@author A0139633B
 /**
  * Parses input arguments and creates a new DeleteCommand object
  */
@@ -18,32 +19,11 @@ public class ListCommandParser {
     public Command parse(String args) {
         String taskType = args.trim().toLowerCase();
 
-        if (!validTaskType(taskType)) {
+        if (!ListCommand.isValidCommand(taskType)) {
             return new IncorrectCommand(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, ListCommand.MESSAGE_USAGE));
         }
 
         return new ListCommand(taskType);
-    }
-
-    public boolean validTaskType(String taskType) {
-
-        switch(taskType) {
-
-        case "":
-            return true;
-
-        case "complete":
-            return true;
-
-        case "incomplete":
-            return true;
-
-        case "overdue":
-            return true;
-
-        default:
-            return false;
-        }
     }
 }
