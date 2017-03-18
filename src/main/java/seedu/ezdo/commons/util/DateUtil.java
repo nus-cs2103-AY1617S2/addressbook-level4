08ee67f2420ada2ace5e484d9d0b8c4fbcf79ede
Lok You Tan <e0003133@u.nus.edu>
2017-03-18 19:30:39 +0800

    Proper indentation

--- src/main/java/seedu/ezdo/commons/util/DateUtil.java
+++ src/main/java/seedu/ezdo/commons/util/DateUtil.java
@@ -11,15 +11,15 @@ import seedu.ezdo.model.todo.ReadOnlyTask;
  */
 public class DateUtil {
     public static boolean isTaskDateValid(ReadOnlyTask task) throws ParseException {
-    SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy H:mm");
-	Date startDate = df.parse(task.getStartDate().toString());
-	Date dueDate = df.parse(task.getDueDate().toString());
-	if (task.getStartDate().toString().equals("") || task.getDueDate().toString().equals("")) {
-	    return true;
-	}
-	if (startDate.compareTo(dueDate) >= 0) {
-	    return false;
-	}
-	return true;
+        SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy H:mm");
+        Date startDate = df.parse(task.getStartDate().toString());
+        Date dueDate = df.parse(task.getDueDate().toString());
+        if (task.getStartDate().toString().equals("") || task.getDueDate().toString().equals("")) {
+            return true;
+        }
+        if (startDate.compareTo(dueDate) >= 0) {
+            return false;
+        }
+        return true;
     }
 }
