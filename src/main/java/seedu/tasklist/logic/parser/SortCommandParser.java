5bcc19a3df088c360746723b02733c8b42deda46
Vesali <e0006008@u.nus.edu>
2017-03-18 20:45:06 +0800

    Update SortCommandParser.java

--- src/main/java/seedu/tasklist/logic/parser/SortCommandParser.java
+++ src/main/java/seedu/tasklist/logic/parser/SortCommandParser.java
@@ -3,6 +3,9 @@ package seedu.tasklist.logic.parser;
 import static seedu.tasklist.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.tasklist.logic.parser.CliSyntax.KEYWORDS_ARGS_FORMAT;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.regex.Matcher;
 
 import seedu.tasklist.logic.commands.Command;
@@ -24,7 +27,9 @@ public class SortCommandParser {
         if (!matcher.matches()) {
             return new IncorrectCommand(String.format(MESSAGE_INVALID_COMMAND_FORMAT, SortCommand.MESSAGE_USAGE));
         }
-        final String keyword = matcher.group("keywords");
+        
+        final String[] keywords = matcher.group("keywords").split(" ");
+        final Set<String> keyword = new HashSet<>(Arrays.asList(keywords));
         return new SortCommand(keyword);
     }
 
