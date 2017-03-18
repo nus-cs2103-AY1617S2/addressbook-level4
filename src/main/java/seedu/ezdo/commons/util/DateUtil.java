43e52e4aff1b0b12cddeb2d13c228e20197161df
Lok You Tan <e0003133@u.nus.edu>
2017-03-18 19:02:25 +0800

    Add DateUtil class

--- src/main/java/seedu/ezdo/commons/util/DateUtil.java
+++ src/main/java/seedu/ezdo/commons/util/DateUtil.java
@@ -1,12 +1,25 @@
 package seedu.ezdo.commons.util;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import seedu.ezdo.model.todo.ReadOnlyTask;
 
 /*
  * Checks for dates 
  */
 public class DateUtil {
-    public boolean checkTaskDate(ReadOnlyTask task) {
+    public static boolean checkTaskDate(ReadOnlyTask task) throws ParseException {
+	SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy H:mm");
+	Date startDate = df.parse(task.getStartDate().toString());
+	Date dueDate = df.parse(task.getDueDate().toString());
+	if (task.getStartDate().toString().equals("") || task.getDueDate().toString().equals("")) {
+	    return true;
+	}
+	if (startDate.compareTo(dueDate) >= 0) {
+	    return false;
+	}
 	return true;
     }
 }
