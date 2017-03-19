973d414294587c479c045496784fe20263453389
JingRong ( <jing_rong92@hotmail.com>
2017-03-19 23:18:27 +0800

    Added undo functionality from StateManager class

--- src/main/java/seedu/address/logic/commands/UndoCommand.java
+++ src/main/java/seedu/address/logic/commands/UndoCommand.java
@@ -1,14 +1,19 @@
 package seedu.address.logic.commands;
 
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.StateManager;
+
 /**
  * Undo most recent command
  */
 public class UndoCommand extends Command {
     public static final String COMMAND_WORD = "undo";
     public static final String MESSAGE_SUCCESS = "Undid most recent command";
+    private final StateManager stateManager = StateManager.getInstance();
 
     @Override
-    public CommandResult execute() {
+    public CommandResult execute() throws CommandException {
+        stateManager.undo();
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
