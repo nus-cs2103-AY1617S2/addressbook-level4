d6cddc48b92696adea9ea7ab71402f1acc8cc585
mynameisyz <yanzhang_shi@outlook.com>
2017-03-19 23:05:52 +0800

    Change to Optional.get(). Add isPresent() to check validity of priority

--- src/main/java/seedu/address/storage/XmlAdaptedTask.java
+++ src/main/java/seedu/address/storage/XmlAdaptedTask.java
@@ -49,10 +49,9 @@ public class XmlAdaptedTask {
      */
     public XmlAdaptedTask(ReadOnlyTask source) {
         name = source.getName().fullName;
-        priority = source.getPriority()
-                .map(Priority::getValue)
-                .map(Priority.Type::name)
-                .orElse(null);
+        if (source.getPriority().isPresent()) {
+            priority = source.getPriority().get().getValue().name();
+        }
         status = source.getStatus().value;
         note = source.getNote().map(Note::toString).orElse("");
         deadline = source.getDeadline().map(Deadline::toString).orElse("");
