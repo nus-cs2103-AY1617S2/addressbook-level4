b57752da0eb82c03354d48301df2f53f68d5d10f
Penghao Chen <xmchenph@gmail.com>
2017-03-19 20:20:43 +0800

    Update XmlAdaptedTask: change accordingly

--- src/main/java/seedu/address/storage/XmlAdaptedTask.java
+++ src/main/java/seedu/address/storage/XmlAdaptedTask.java
@@ -11,7 +11,6 @@ import seedu.address.model.tag.UniqueTagList;
 import seedu.address.model.task.Description;
 import seedu.address.model.task.EndTime;
 import seedu.address.model.task.ReadOnlyTask;
-import seedu.address.model.task.StartTime;
 import seedu.address.model.task.Task;
 import seedu.address.model.task.Title;
 import seedu.address.model.task.UrgencyLevel;
@@ -24,16 +23,16 @@ public class XmlAdaptedTask {
 
     @XmlElement(required = true)
     private String title;
-    @XmlElement(required = true)
+    @XmlElement
     private String venue;
-    @XmlElement(required = true)
-    private String startTime;
-    @XmlElement(required = true)
+    @XmlElement
     private String endTime;
-    @XmlElement(required = true)
+    @XmlElement
     private String urgencyLevel;
-    @XmlElement(required = true)
+    @XmlElement
     private String description;
+    @XmlElement
+    private Boolean status;
 
     @XmlElement
     private List<XmlAdaptedTag> tagged = new ArrayList<>();
@@ -44,19 +43,18 @@ public class XmlAdaptedTask {
      */
     public XmlAdaptedTask() {}
 
-
+    //@@author A0122017Y
     /**
      * Converts a given Task into this class for JAXB use.
      *
      * @param source future changes to this will not affect the created XmlAdaptedTask
      */
     public XmlAdaptedTask(ReadOnlyTask source) {
-        title = source.getTitle().title;
-        venue = source.getVenue().value;
-        startTime = source.getStartTime().value;
-        endTime = source.getEndTime().value;
-        urgencyLevel = source.getUrgencyLevel().value;
-        description = source.getDescription().value;
+        title = source.getTitle().toString();
+        venue = source.getVenue().toString();
+        endTime = source.getEndTime().toString();
+        urgencyLevel = source.getUrgencyLevel().toString();
+        description = source.getDescription().toString();
         tagged = new ArrayList<>();
         for (Tag tag : source.getTags()) {
             tagged.add(new XmlAdaptedTag(tag));
@@ -75,11 +73,11 @@ public class XmlAdaptedTask {
         }
         final Title title = new Title(this.title);
         final Venue venue = new Venue(this.venue);
-        final StartTime startTime = new StartTime(this.startTime);
         final EndTime endTime = new EndTime(this.endTime);
         final UrgencyLevel urgencyLevel = new UrgencyLevel(this.urgencyLevel);
         final Description description = new Description(this.description);
+        final Boolean status = new Boolean(this.status);
         final UniqueTagList tags = new UniqueTagList(taskTags);
-        return new Task(title, venue, startTime, endTime, urgencyLevel, description, tags);
+        return new Task(title, venue, endTime, urgencyLevel, description, status, tags);
     }
 }
