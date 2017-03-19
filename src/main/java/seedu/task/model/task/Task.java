6010a40a33562d4069b47f6a3b89945a5702df8d
Tyler Rocha <tyler.rocha@utexas.edu>
2017-03-19 20:22:16 +0800

    implement comparable in Timing.java

--- src/main/java/seedu/task/model/task/Task.java
+++ src/main/java/seedu/task/model/task/Task.java
@@ -6,8 +6,8 @@ import seedu.task.commons.util.CollectionUtil;
 import seedu.task.model.tag.UniqueTagList;
 
 /**
- * Represents a Task in the ToDo List.
- * Guarantees: details are present and not null, field values are validated.
+ * Represents a Task in the ToDo List. Guarantees: details are present and not
+ * null, field values are validated.
  */
 public class Task implements ReadOnlyTask, Comparable<Task> {
 
@@ -29,15 +29,16 @@ public class Task implements ReadOnlyTask, Comparable<Task> {
         this.startTiming = startTiming;
         this.endTiming = endTiming;
         this.complete = false;
-        this.tags = new UniqueTagList(tags); // protect internal tags from changes in the arg list
+        this.tags = new UniqueTagList(tags); // protect internal tags from
+                                             // changes in the arg list
     }
 
     /**
      * Creates a copy of the given ReadOnlyTask.
      */
     public Task(ReadOnlyTask source) {
-        this(source.getDescription(), source.getPriority(), source.getStartTiming(),
-            source.getEndTiming(), source.getTags());
+        this(source.getDescription(), source.getPriority(), source.getStartTiming(), source.getEndTiming(),
+                source.getTags());
     }
 
     public void setDescription(Description description) {
@@ -84,6 +85,7 @@ public class Task implements ReadOnlyTask, Comparable<Task> {
         this.complete = true;
     }
 
+    @Override
     public boolean isComplete() {
         return this.complete;
     }
@@ -117,12 +119,13 @@ public class Task implements ReadOnlyTask, Comparable<Task> {
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof ReadOnlyTask // instanceof handles nulls
-                && this.isSameStateAs((ReadOnlyTask) other));
+                        && this.isSameStateAs((ReadOnlyTask) other));
     }
 
     @Override
     public int hashCode() {
-        // use this method for custom fields hashing instead of implementing your own
+        // use this method for custom fields hashing instead of implementing
+        // your own
         return Objects.hash(description, priority, startTiming, endTiming, tags);
     }
 
@@ -130,23 +133,25 @@ public class Task implements ReadOnlyTask, Comparable<Task> {
     public String toString() {
         return getAsText();
     }
-    /**
-    * Results in Tasks sorted by priority, followed by endDate, then startDate.
-    * Note: If a and b are tasks and a.compareTo(b) == 0, that does not imply a.equals(b).
-    */
-   @Override
-   public int compareTo(Task compareTask) {
 
-       int compareToResult = this.priority.compareTo(compareTask.priority);
-       
-       if (compareToResult == 0) {
-           compareToResult = this.endDate.compareTo(compareTask.endDate);
-       }
-       
-       if (compareToResult == 0) {
-           compareToResult = this.startDate.compareTo(compareTask.startDate);
-       }
-       return compareToResult;
-   }
+    /**
+     * Results in Tasks sorted by priority, followed by endDate, then startDate.
+     * Note: If a and b are tasks and a.compareTo(b) == 0, that does not imply
+     * a.equals(b).
+     */
+    @Override
+    public int compareTo(Task compareTask) {
+
+        int compareToResult = this.priority.compareTo(compareTask.priority);
+
+        if (compareToResult == 0) {
+            compareToResult = this.endTiming.compareTo(compareTask.endTiming);
+        }
+
+        if (compareToResult == 0) {
+            compareToResult = this.startTiming.compareTo(compareTask.startTiming);
+        }
+        return compareToResult;
+    }
 
 }
--- src/main/java/seedu/task/model/task/Timing.java
+++ src/main/java/seedu/task/model/task/Timing.java
@@ -2,29 +2,29 @@ package seedu.task.model.task;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import seedu.task.commons.exceptions.IllegalValueException;
 
 /**
- * Represents a Task's date in the address book.
- * Guarantees: immutable; is valid as declared in {@link #isValidTiming(String)}
+ * Represents a Task's date in the address book. Guarantees: immutable; is valid
+ * as declared in {@link #isValidTiming(String)}
  */
-public class Timing {
+public class Timing implements Comparable<Timing> {
 
-    public static final String MESSAGE_TIMING_CONSTRAINTS =
-            "Task date should be in the format HH:mm dd/MM/yyyy OR dd/MM/yyyy";
+    public static final String MESSAGE_TIMING_CONSTRAINTS = "Task date should be in the format HH:mm dd/MM/yyyy OR dd/MM/yyyy";
     public static final String[] TIMING_FORMAT = {
-//          "dd/MM/yyyy HH:mm",
-        "HH:mm dd/MM/yyyy",
-        "dd/MM/yyyy"
-    };
+            // "dd/MM/yyyy HH:mm",
+            "HH:mm dd/MM/yyyy", "dd/MM/yyyy" };
 
     public final String value;
+    private Date date;
 
     /**
      * Validates given date.
      *
-     * @throws IllegalValueException if given date string is invalid.
+     * @throws IllegalValueException
+     *             if given date string is invalid.
      */
     public Timing(String timing) throws IllegalValueException {
         if (timing != null) {
@@ -41,7 +41,7 @@ public class Timing {
     /**
      * Returns if a given string is a valid date.
      */
-    public static boolean isValidTiming(String test) {
+    private boolean isValidTiming(String test) {
         boolean isValid = false;
         if (test.equals("n/a")) {
             isValid = true;
@@ -50,9 +50,10 @@ public class Timing {
                 SimpleDateFormat sdf = new SimpleDateFormat(TIMING_FORMAT[i]);
                 sdf.setLenient(false);
                 try {
-                    //throws ParseException if date is not valid
-                    sdf.parse(test);
+                    // throws ParseException if date is not valid
+                    Date date = sdf.parse(test);
                     isValid = true;
+                    this.date = date;
                     break;
                 } catch (ParseException e) {
                 }
@@ -73,12 +74,21 @@ public class Timing {
         }
         return other == this // short circuit if same object
                 || (other instanceof Timing // instanceof handles nulls
-                && this.value.equals(((Timing) other).value)); // state check
+                        && this.value.equals(((Timing) other).value)); // state
+                                                                       // check
     }
 
     @Override
     public int hashCode() {
         return value.hashCode();
+    }
+
+    /**
+     * Results in Timing sorted in ascending order.
+     */
+    @Override
+    public int compareTo(Timing compareTiming) {
+        return this.date.compareTo(compareTiming.date);
     }
 
 }
