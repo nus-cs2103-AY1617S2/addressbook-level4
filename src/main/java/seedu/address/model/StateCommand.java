8ca30d2ca5c797590b63d5ed5bb8597ddb354327
JingRong ( <jing_rong92@hotmail.com>
2017-03-19 16:41:21 +0800

    Init sample statecommand

---
+++ src/main/java/seedu/address/model/StateCommand.java
@@ -0,0 +1,19 @@
+package seedu.address.model;
+
+import seedu.address.logic.commands.*;
+
+/**
+ * Class to store an action, and it's inverse
+ */
+public class StateCommand {
+    private String executeCommand;
+    private String undoCommand;
+    
+    public void execute() {
+        System.out.println(executeCommand);
+    }
+    
+    public void executeInvese() {
+        System.out.println(undoCommand);
+    }
+}
