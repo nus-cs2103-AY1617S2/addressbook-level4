bd183c1e514ea7e38789c65dbf293f5a3e038450
Jia Yilin <e0007663@u.nus.edu>
2017-03-19 20:08:49 +0800

    Update AddCommand to add in new feature:undo

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -1,11 +1,14 @@
 package seedu.address.logic.commands;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
 
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.ReadOnlyToDoList;
+import seedu.address.model.ToDoList;
 import seedu.address.model.tag.Tag;
 import seedu.address.model.tag.UniqueTagList;
 import seedu.address.model.task.Description;
@@ -16,33 +19,36 @@ import seedu.address.model.task.Title;
 import seedu.address.model.task.UniqueTaskList;
 import seedu.address.model.task.UrgencyLevel;
 import seedu.address.model.task.Venue;
+import seedu.address.logic.commands.CommandResult;
 
 /**
  * Adds a Task to the address book.
  */
-public class AddCommand extends Command {
+public class AddCommand extends UndoableCommand {
 
     public static final String COMMAND_WORD = "add";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a Task to the to-do list. "
             + "Parameters: TITLE @@VENUE from:STARTTIME to:ENDTIME **URGENCYLEVEL d:DESCRIPTION  ##[TAG]...\n"
-            + "Example: " + COMMAND_WORD
-            + " CS2103 Tutorial @@COM1-B110 from:March 8,10.00am to:March 8, 11.00am "
+            + "Example: " + COMMAND_WORD + " CS2103 Tutorial @@COM1-B110 from:March 8,10.00am to:March 8, 11.00am "
             + "**5 d:have to present V0.2 ##lesson ##project";
 
     public static final String MESSAGE_SUCCESS = "New Task added: %1$s";
     public static final String MESSAGE_DUPLICATE_TASK = "This Task already exists in the address book";
 
     private final Task toAdd;
+    private ReadOnlyToDoList originalToDoList;
+    private CommandResult commandResultToUndo;
 
     /**
      * Creates an AddCommand using raw values.
      *
-     * @throws IllegalValueException if any of the raw values are invalid
+     * @throws IllegalValueException
+     *             if any of the raw values are invalid
      */
     public AddCommand(String title, Optional<String> venue, Optional<String> starttime, Optional<String> endtime,
             Optional<String> urgencyLevel, Optional<String> description, Set<String> tags)
-                    throws IllegalValueException {
+            throws IllegalValueException {
         final Set<Tag> tagSet = new HashSet<>();
         for (String tagName : tags) {
             tagSet.add(new Tag(tagName));
@@ -81,27 +87,42 @@ public class AddCommand extends Command {
             tempDescription = new Description("");
         }
 
-        this.toAdd = new Task(
-                tempTitle,
-                tempVenue,
-                tempStartTime,
-                tempEndTime,
-                tempUrgencyLevel,
-                tempDescription,
-                new UniqueTagList(tagSet)
-                );
+        this.toAdd = new Task(tempTitle, tempVenue, tempStartTime, tempEndTime, tempUrgencyLevel, tempDescription,
+                new UniqueTagList(tagSet));
     }
-
+  //@@author A0143648Y
     @Override
     public CommandResult execute() throws CommandException {
         assert model != null;
         try {
+            originalToDoList = new ToDoList(model.getToDoList());
+            
             model.addTask(toAdd);
+            commandResultToUndo = new CommandResult(String.format(MESSAGE_SUCCESS, toAdd));
+            updateUndoLists();
+            
             return new CommandResult(String.format(MESSAGE_SUCCESS, toAdd));
         } catch (UniqueTaskList.DuplicateTaskException e) {
             throw new CommandException(MESSAGE_DUPLICATE_TASK);
         }
 
+    }
+
+    @Override
+    public void updateUndoLists() {
+        if (previousToDoLists == null) {
+            previousToDoLists = new ArrayList<ReadOnlyToDoList>(3);
+            previousCommandResults = new ArrayList<CommandResult>(3);
+        }
+        if (previousToDoLists.size() >= 3) {
+            previousToDoLists.remove(0);
+            previousCommandResults.remove(0);
+            previousToDoLists.add(originalToDoList);
+            previousCommandResults.add(commandResultToUndo);
+        } else {
+            previousToDoLists.add(originalToDoList);
+            previousCommandResults.add(commandResultToUndo);
+        }
     }
 
 }
