4742d380d13dfc2bf4372344b3b2631114031f82
Penghao Chen <xmchenph@gmail.com>
2017-03-19 11:52:21 +0800

    Update EndTime: incorporate LocalDateTime

--- src/main/java/seedu/address/model/task/EndTime.java
+++ src/main/java/seedu/address/model/task/EndTime.java
@@ -1,65 +1,67 @@
 package seedu.address.model.task;
 
+import java.time.LocalDateTime;
 
 import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.commons.util.StringUtil;
 
 /**
- * Represents a task's end time in the to-do list.
+ * Represents a Task's end time in the to-do list.
  * Guarantees: immutable; is valid as declared in {@link #isValidEndTime(String)}
  */
-public class EndTime {
+public class EndTime implements Comparable<EndTime>{
 
-    public static final String MESSAGE_ENDTIME_CONSTRAINTS =
-            "Task end time can take any values, and it should not be blank";
-
-    /*
-     * The first character of the address must not be a whitespace,
-     * otherwise " " (a blank string) becomes a valid input.
-     */
+    public static final String MESSAGE_ENDTIME_CONSTRAINTS = "Task starttime numbers should only contain numbers";
     public static final String ENDTIME_VALIDATION_REGEX = ".+";
 
-    public final String value;
+    public final LocalDateTime endTime;
 
     /**
      * Validates given end time.
      *
      * @throws IllegalValueException if given end time string is invalid.
      */
-    public EndTime(String endtime) throws IllegalValueException {
-        assert endtime != null;
-        if (endtime.isEmpty()) {
-            this.value = endtime;
-        } else {
-            String trimmedEndTime = endtime.trim();
-            if (!isValidEndTime(trimmedEndTime)) {
-                throw new IllegalValueException(MESSAGE_ENDTIME_CONSTRAINTS);
-            }
-            this.value = trimmedEndTime;
+    public EndTime(String endTimeArg) throws IllegalValueException {
+        assert endTimeArg != null;
+        endTimeArg = endTimeArg.trim();
+        try {
+            this.endTime = StringUtil.parseStringToTime(endTimeArg);
+        } catch (IllegalValueException e){
+            throw new IllegalValueException(MESSAGE_ENDTIME_CONSTRAINTS);
         }
     }
 
     /**
-     * Returns true if a given string is a valid person email.
+     * Returns true if a given string is a valid task end time.
      */
     public static boolean isValidEndTime(String test) {
         return test.matches(ENDTIME_VALIDATION_REGEX);
     }
-
+    
+    public LocalDateTime getEndTime(){
+        return this.endTime;
+    }
+    
     @Override
     public String toString() {
-        return value;
+        return this.endTime.format(StringUtil.DATE_FORMATTER);
     }
 
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof EndTime // instanceof handles nulls
-                        && this.value.equals(((EndTime) other).value)); // state check
+                        && this.endTime.equals(((EndTime) other).endTime)); // state check
     }
 
     @Override
     public int hashCode() {
-        return value.hashCode();
+        return endTime.toString().hashCode();
+    }
+
+    @Override
+    public int compareTo(EndTime other) {
+        return this.getEndTime().compareTo(other.getEndTime());
     }
 
 }
