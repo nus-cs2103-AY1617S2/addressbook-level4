5ec52a8a9c7be3479b321e807cf67179d0bbd829
LWWenz <leongww7@gmail.com>
2017-03-19 16:22:35 +0800

    Add ModifyConfigData to be able to modify the ConfigData and saving the changes.

---
+++ src/main/java/typetask/storage/ModifyConfigData.java
@@ -0,0 +1,33 @@
+package typetask.storage;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import typetask.commons.core.Config;
+import typetask.commons.exceptions.DataConversionException;
+import typetask.commons.util.JsonUtil;
+
+public class ModifyConfigData {
+
+    private String configPath = new String("config.json");
+    private Config config;
+
+    public ModifyConfigData(String configPathName) throws DataConversionException {
+        Optional<Config> importConfig = JsonUtil.readJsonFile(configPathName, Config.class);
+        config = importConfig.orElse(new Config());
+    }
+
+    public ModifyConfigData() throws DataConversionException {
+        Optional<Config> importConfig = JsonUtil.readJsonFile(configPath, Config.class);
+        config = importConfig.orElse(new Config());
+    }
+
+    public void setTaskManagerFilePath(String newPath) throws IOException {
+        config.setTaskManagerFilePath(newPath);
+        JsonUtil.saveJsonFile(config, configPath);
+    }
+
+    public String getTaskManagerFilePath() {
+        return config.getTaskManagerFilePath();
+    }
+}
