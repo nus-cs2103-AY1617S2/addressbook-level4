d9e517664e94ffc470efaba65bf4fcd7245a7186
Acer\zhi liang <chenzhiliang94@gmail.com>
2017-03-19 14:39:07 +0800

    Overload constructor of StartDate for convenience of searching tasks

--- src/main/java/seedu/ezdo/model/todo/StartDate.java
+++ src/main/java/seedu/ezdo/model/todo/StartDate.java
@@ -24,5 +24,17 @@ public class StartDate extends TaskDate {
             throw new IllegalValueException(MESSAGE_STARTDATE_CONSTRAINTS);
         }
     }
+    
+    /**
+     * Validates given start date when user finds by start date
+     *
+     * @throws IllegalValueException if given start date string is invalid.
+     */
+    public StartDate(String startDate, boolean isFind) throws IllegalValueException {
+        super(startDate, isFind);
+        if (!isValidTaskDate(this.value, true)) {
+            throw new IllegalValueException(MESSAGE_FIND_DATE_CONSTRAINTS);
+        }
+    }
 
 }
