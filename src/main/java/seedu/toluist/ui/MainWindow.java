3663f253a408be4359d8740398f18b3942af8430
Louis Lai <hoangdung2506@gmail.com>
2017-03-19 18:51:25 +0800

    Change min width

--- src/main/java/seedu/toluist/ui/MainWindow.java
+++ src/main/java/seedu/toluist/ui/MainWindow.java
@@ -29,7 +29,7 @@ public class MainWindow extends UiPart<Region> {
     private static final String ICON = "/images/address_book_32.png";
     private static final String FXML = "MainWindow.fxml";
     private static final int MIN_HEIGHT = 600;
-    private static final int MIN_WIDTH = 450;
+    private static final int MIN_WIDTH = 800;
 
     private Stage primaryStage;
     private Dispatcher dispatcher;
