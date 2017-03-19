9b0f4ae98a10ad7180ff09ad4b8a0a4c26dcb55b
JingRong ( <jing_rong92@hotmail.com>
2017-03-19 17:19:12 +0800

    Added init function, and stub function to evaluate the inverse of the command

--- src/main/java/seedu/address/model/StateCommandPair.java
+++ src/main/java/seedu/address/model/StateCommandPair.java
@@ -7,14 +7,34 @@ import seedu.address.logic.commands.*;
  * Class to store an action, and it's inverse
  */
 public class StateCommandPair {
-    private String executeCommand;
-    private String undoCommand;
+    private Command executeCommand;
+    private Command undoCommand;
     
+    public StateCommandPair(Command cmd) {
+        this.executeCommand = cmd;
+        this.undoCommand = this.evaluateInverse(cmd);
+    }
+    
+    /**
+     * Executes the command previously entered (for redo)
+     */
     public void execute() {
         System.out.println(executeCommand);
     }
     
+    /**
+     * Executes the inverse of the command previously entered (for undo)
+     */
     public void executeInvese() {
         System.out.println(undoCommand);
+    }
+    
+    /**
+     * 
+     * @param Command
+     * @return an Inverse action of the Command
+     */
+    private Command evaluateInverse(Command cmd) {
+        return cmd;// stub
     }
 }
