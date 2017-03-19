248ed5fe2264155489b7f632bc5b4c1f4cd5f96e
JingRong ( <jing_rong92@hotmail.com>
2017-03-19 23:36:00 +0800

    Added support for parsing Redo/Undo commands

--- src/main/java/seedu/address/logic/parser/Parser.java
+++ src/main/java/seedu/address/logic/parser/Parser.java
@@ -17,6 +17,8 @@ import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.IncorrectCommand;
 import seedu.address.logic.commands.ListCommand;
 import seedu.address.logic.commands.SelectCommand;
+import seedu.address.logic.commands.UndoCommand;
+import seedu.address.logic.commands.RedoCommand;
 
 /**
  * Parses user input.
@@ -85,7 +87,13 @@ public class Parser {
 
         case HelpCommand.COMMAND_WORD:
             return new HelpCommand();
-
+        
+        case UndoCommand.COMMAND_WORD:
+            return new UndoCommand();        
+        
+        case RedoCommand.COMMAND_WORD:
+            return new RedoCommand();            
+            
         default:
             return new IncorrectCommand(MESSAGE_UNKNOWN_COMMAND);
         }
