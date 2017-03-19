bf9e509e1715f786719c6a62046bda4eadf0ff96
JingRong ( <jing_rong92@hotmail.com>
2017-03-19 23:19:20 +0800

    Added redo class

---
+++ src/main/java/seedu/address/logic/commands/RedoCommand.java
@@ -0,0 +1,19 @@
+package seedu.address.logic.commands;
+
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.StateManager;
+
+/**
+ * Redo most recent command
+ */
+public class RedoCommand extends Command {
+    public static final String COMMAND_WORD = "Redo";
+    public static final String MESSAGE_SUCCESS = "Redid most recent command";
+    private final StateManager stateManager = StateManager.getInstance();
+
+    @Override
+    public CommandResult execute() throws CommandException {
+        stateManager.redo();
+        return new CommandResult(MESSAGE_SUCCESS);
+    }
+}
