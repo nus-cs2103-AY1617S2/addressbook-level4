802129cc1a80c33be06c617b838b71e4bb033ae5
Acer\zhi liang <chenzhiliang94@gmail.com>
2017-03-19 14:41:26 +0800

    Ensures Regex matching for TaskDate is relaxed when searching tasks

--- src/main/java/seedu/ezdo/model/todo/TaskDate.java
+++ src/main/java/seedu/ezdo/model/todo/TaskDate.java
@@ -1,5 +1,6 @@
 package seedu.ezdo.model.todo;
 
+import seedu.ezdo.commons.exceptions.IllegalValueException;
 import seedu.ezdo.logic.parser.DateParser;
 
 /**
@@ -12,10 +13,21 @@ public abstract class TaskDate {
     public static final String TASKDATE_VALIDATION_REGEX = "^$|(0[1-9]|"
             + "[12][0-9]|3[01])[/](0[1-9]|1[012])[/](19|20)[0-9]{2} "
             + "(2[0-3]|[0-1][0-9])[:][0-5][0-9]";
-
+    public static final String FIND_TASKDATE_VALIDATION_REGEX = "^$|(0[1-9]|[12][0-9]|3[01])[/](0[1-9]|1[012])[/](19|20)[0-9]{2}";
+    public static final String MESSAGE_FIND_DATE_CONSTRAINTS = "Please enter a date in this form: DD/MM/YYYY";
+    
     public TaskDate(String taskDate) {
         DateParser dateParser = new DateParser(taskDate);
         this.value = dateParser.value;
+    }
+
+    public TaskDate(String taskDate, boolean isFind) throws IllegalValueException {
+        taskDate = taskDate.trim();
+        assert taskDate != null;
+        if (!isValidTaskDate (taskDate, isFind)) {
+            throw new IllegalValueException(MESSAGE_FIND_DATE_CONSTRAINTS);
+        }
+        this.value = taskDate;
     }
 
     @Override
@@ -42,4 +54,9 @@ public abstract class TaskDate {
     public static boolean isValidTaskDate(String test) {
         return test.matches(TASKDATE_VALIDATION_REGEX);
     }
+    
+    public static boolean isValidTaskDate(String test, boolean isFind) {
+        return test.matches(FIND_TASKDATE_VALIDATION_REGEX);
+    }
+
 }
