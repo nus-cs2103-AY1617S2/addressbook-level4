c8af19d90894a3d7c08bf9d87e707195105a981b
JingRong ( <jing_rong92@hotmail.com>
2017-03-19 16:06:31 +0800

    Added stacks for undo/redo command history addition

--- src/main/java/seedu/address/model/StateManager.java
+++ src/main/java/seedu/address/model/StateManager.java
@@ -1,12 +1,20 @@
 package seedu.address.model;
 
+import java.util.Stack;
+
 /**
  * Singleton class to handle Undo/Redo commands
  */
 public class StateManager {
+    
     private static StateManager instance = null;
+    private Stack<String> undoStack;
+    private Stack<String> redoStack;
+    
     // Exists only to defeat instantiation.
     protected StateManager() {
+        undoStack = new Stack<String>();
+        redoStack = new Stack<String>();
     }
     // Returns the singleton instance
     public static StateManager getInstance() {
