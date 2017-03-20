93c96f3a3c3e7c47b38f7016af081145093ffda5
gngster <gjhsmiles@gmail.com>
2017-03-20 23:38:49 +0800

    fix config test

--- src/test/java/seedu/todolist/commons/core/ConfigTest.java
+++ src/test/java/seedu/todolist/commons/core/ConfigTest.java
@@ -14,7 +14,7 @@ public class ConfigTest {
 
     @Test
     public void toString_defaultObject_stringReturned() {
-        String defaultConfigAsString = "App title : ToDo App\n" +
+        String defaultConfigAsString = "App title : DoMe!\n" +
                 "Current log level : INFO\n" +
                 "Preference file Location : preferences.json\n" +
                 "Local data file location : data/todolist.xml\n" +
