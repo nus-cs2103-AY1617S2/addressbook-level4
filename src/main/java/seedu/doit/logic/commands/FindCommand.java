038cc97c32869be3c0cbec5ddd35291d0e7afb62
drtrifle <drtrifle@gmail.com>
2017-03-20 18:51:09 +0800

    Updated findCommand.java

--- src/main/java/seedu/doit/logic/commands/FindCommand.java
+++ src/main/java/seedu/doit/logic/commands/FindCommand.java
@@ -34,7 +34,7 @@ public class FindCommand extends Command {
 
     @Override
     public CommandResult execute() {
-        model.updateFilteredTaskList(namekeywords);
+        model.updateFilteredTaskList(namekeywords, prioritykeywords, desckeywords);
         return new CommandResult(getMessageForTaskListShownSummary(model.getFilteredTaskList().size()));
     }
 
