7c5b09ceeff5045a174dc0d19907f1b53a8d49f7
InfinityFrost <iceazul_awakendragon@hotmail.com>
2017-03-20 12:37:10 +0800

    Update Command.java

--- src/main/java/seedu/taskboss/logic/commands/Command.java
+++ src/main/java/seedu/taskboss/logic/commands/Command.java
@@ -40,4 +40,8 @@ public abstract class Command {
     public void setData(Model model) {
         this.model = model;
     }
+
+    public void setStorage(Storage storage) {
+        this.storage = storage;
+    }
 }
