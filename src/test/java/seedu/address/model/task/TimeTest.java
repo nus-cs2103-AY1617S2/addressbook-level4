e68b65d50b3a73cfdf7905cea7c8b2171df328fb
Penghao Chen <xmchenph@gmail.com>
2017-03-21 02:48:59 +0800

    Create TimeTest

---
+++ src/test/java/seedu/address/model/task/TimeTest.java
@@ -0,0 +1,27 @@
+package seedu.address.model.task;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TimeTest {
+
+    @Test
+    public void isValidEndTime() {
+        // blank EndTime
+        assertFalse(Time.isValidTime("")); // empty string
+        //assertFalse(EndTime.isValidEndTime(" ")); // spaces only
+
+        // valid Time 
+        assertTrue(Time.isValidTime("March Fifteenth"));//alphabets
+        assertTrue(Time.isValidTime("20170315")); // number
+        assertTrue(Time.isValidTime("By end of March")); // alphabets with capital
+        assertTrue(Time.isValidTime("March 15 2017")); // numeric and alphabet
+                                                           // and domain name
+        assertTrue(Time.isValidTime("March 15, 2017")); // mixture of
+                                                               // alphanumeric
+                                                               // and comma
+                                                               // characters
+    }
+}
