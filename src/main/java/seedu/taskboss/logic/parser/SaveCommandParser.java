b2b9c7a6b66dd6844dfc20dfff8fd00fd49fb119
InfinityFrost <iceazul_awakendragon@hotmail.com>
2017-03-20 11:45:56 +0800

    Update SaveCommandParser.java

--- src/main/java/seedu/taskboss/logic/parser/SaveCommandParser.java
+++ src/main/java/seedu/taskboss/logic/parser/SaveCommandParser.java
@@ -19,7 +19,7 @@ public class SaveCommandParser {
         File file = new File(args.trim());
 
         if (!file.exists() && !file.isDirectory()) {
-            file.mkfiles();
+            file.mkdirs();
         }
 
         return new SaveCommand(args.trim());
