6074190f54dbc0a1b43c254cfad1fe2d17546216
WANGCE\WangC <wangce999@hotmail.com>
2017-03-21 01:19:25 +0800

    added container for command words

---
+++ src/main/java/werkbook/task/commons/core/CommandTexts.java
@@ -0,0 +1,22 @@
+//@@author A0140462R
+package werkbook.task.commons.core;
+
+/*
+ * Container for command words used
+ */
+
+public class CommandTexts {
+    
+    public static final String ADD_COMMAND_WORD = "add";
+    public static final String EDIT_COMMAND_WORD = "edit";
+    public static final String SELECT_COMMAND_WORD = "select";
+    public static final String DELETE_COMMAND_WORD = "delete";
+    public static final String CLEAR_COMMAND_WORD = "clear";
+    public static final String FIND_COMMAND_WORD = "find";
+    public static final String LIST_COMMAND_WORD = "list";
+    public static final String EXIT_COMMAND_WORD = "exit";
+    public static final String HELP_COMMAND_WORD = "help";
+    public static final String UNDO_COMMAND_WORD = "undo";
+    public static final String REDO_COMMAND_WORD = "redo";
+    public static final String MARK_COMMAND_WORD = "mark";
+}
