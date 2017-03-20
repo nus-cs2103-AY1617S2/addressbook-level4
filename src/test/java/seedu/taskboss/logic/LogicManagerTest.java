bb2f55de9bc303342b586ff9cc4ffdff01b64708
Haya Alsharef <haya.fayez@gmail.com>
2017-03-21 03:31:52 +0800

    fix travis errors part 7

--- src/test/java/seedu/taskboss/logic/LogicManagerTest.java
+++ src/test/java/seedu/taskboss/logic/LogicManagerTest.java
@@ -226,16 +226,14 @@ public class LogicManagerTest {
 
     @Test
     public void execute_add_invalidTaskData() throws IllegalValueException, InvalidDatesException {
-        assertCommandFailure("add n/[]\\[;] p/Yes sd/today ed/tomorrow i/valid, information",
+        assertCommandFailure("add n/[]\\[;] sd/today ed/tomorrow i/valid, information",
                 Name.MESSAGE_NAME_CONSTRAINTS);
-        assertCommandFailure("add n/Valid Name p/not_Yes_No sd/today ed/tomorrow i/valid, information",
-                PriorityLevel.MESSAGE_PRIORITY_CONSTRAINTS);
-        assertCommandFailure("add n/Valid Name p/Yes sd/today ed/tomorrow "
+        assertCommandFailure("add n/Valid Name! sd/today ed/tomorrow "
                 + "i/valid, information c/invalid_-[.category",
                 Category.MESSAGE_CATEGORY_CONSTRAINTS);
-        assertCommandFailure("add n/Valid Name p/Yes sd/today to next week ed/tomorrow i/valid, information",
+        assertCommandFailure("add n/Valid Name sd/today to next week ed/tomorrow i/valid, information",
                 DateTimeParser.getMultipleDatesError());
-        assertCommandFailure("add n/Valid Name p/Yes sd/invalid date ed/tomorroq i/valid, information",
+        assertCommandFailure("add n/Valid Name sd/invalid date ed/tomorroq i/valid, information",
                 DateTime.MESSAGE_DATE_CONSTRAINTS);
     }
 
@@ -519,7 +517,6 @@ public class LogicManagerTest {
             cmd.append("add ");
 
             cmd.append(" n/").append(p.getName().toString());
-            cmd.append(" p/").append(p.getPriorityLevel());
             cmd.append(" sd/").append(p.getStartDateTime().toString());
             cmd.append(" ed/").append(p.getEndDateTime().toString());
             cmd.append(" i/").append(p.getInformation());
