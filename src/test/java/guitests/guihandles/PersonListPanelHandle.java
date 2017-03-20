299d6843e9eff7a9e521c8549d48643fec944298
clementtan13 <clementtan13@hotmail.com>
2017-03-20 04:29:08 -0700

    Rename TestApp.java location

--- src/test/java/guitests/guihandles/PersonListPanelHandle.java
+++ src/test/java/guitests/guihandles/PersonListPanelHandle.java
@@ -11,10 +11,10 @@ import javafx.geometry.Point2D;
 import javafx.scene.Node;
 import javafx.scene.control.ListView;
 import javafx.stage.Stage;
-import seedu.address.TestApp;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.ReadOnlyPerson;
 import seedu.address.testutil.TestUtil;
+import seedu.task.TestApp;
 
 /**
  * Provides a handle for the panel containing the person list.
