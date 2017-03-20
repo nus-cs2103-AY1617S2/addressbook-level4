1c03067b96dda4af9dbf29e9341cede57a5d58f9
Yi Rui Goh <yirui94@users.noreply.github.com>
2017-03-21 01:05:35 +0800

    LocationTest.java : update tests to include empty strings

--- src/test/java/seedu/geekeep/model/person/LocationTest.java
+++ src/test/java/seedu/geekeep/model/person/LocationTest.java
@@ -1,6 +1,5 @@
 package seedu.geekeep.model.person;
 
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -11,11 +10,9 @@ public class LocationTest {
 
     @Test
     public void isValidLocation() {
-        // invalid locations
-        assertFalse(Location.isValidLocation("")); // empty string
-        assertFalse(Location.isValidLocation(" ")); // spaces only
-
         // valid locations
+        assertTrue(Location.isValidLocation("")); // empty string
+        assertTrue(Location.isValidLocation(" ")); // spaces only
         assertTrue(Location.isValidLocation("Blk 456, Den Road, #01-355"));
         assertTrue(Location.isValidLocation("-")); // one character
         assertTrue(Location.isValidLocation("Leng Inc; 1234 Market St; San Francisco CA 2349879; USA")); // long address
