b563f888c32af56376d80fee8c4a4068535c18f0
Ying <ying1996@gmail.com>
2017-03-20 23:44:31 +0800

    create save command unit test

---
+++ src/test/java/guitests/SaveCommandTest.java
@@ -0,0 +1,18 @@
+package guitests;
+
+import static org.junit.Assert.assertTrue;
+import static seedu.task.logic.commands.SaveCommand.MESSAGE_SUCCESS;
+
+import org.junit.Test;
+
+import seedu.task.testutil.TestTask;
+import seedu.task.testutil.TestUtil;
+
+public class SaveCommandTest extends TaskManagerGuiTest {
+
+    @Test
+    public void save() {
+
+    }
+
+}
