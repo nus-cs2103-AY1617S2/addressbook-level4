dd302ffcdda157ede863fda2cae51d2ec168d3f9
jinshunlee <ljsrockz@gmail.com>
2017-03-20 17:41:11 +0800

    Implement find to allow searching by priority, start time, end time, description, tags

--- src/main/java/seedu/doit/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/doit/logic/parser/FindCommandParser.java
@@ -2,8 +2,14 @@ package seedu.doit.logic.parser;
 
 import static seedu.doit.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.doit.logic.parser.CliSyntax.KEYWORDS_ARGS_FORMAT;
+import static seedu.doit.logic.parser.CliSyntax.PREFIX_DESCRIPTION;
+import static seedu.doit.logic.parser.CliSyntax.PREFIX_END;
+import static seedu.doit.logic.parser.CliSyntax.PREFIX_PRIORITY;
+import static seedu.doit.logic.parser.CliSyntax.PREFIX_START;
+import static seedu.doit.logic.parser.CliSyntax.PREFIX_TAG;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.regex.Matcher;
@@ -21,7 +27,26 @@ public class FindCommandParser {
      * Parses the given {@code String} of arguments in the context of the FindCommand
      * and returns an FindCommand object for execution.
      */
+
+
     public Command parse(String args) {
+
+        ArgumentTokenizer argsTokenizer =
+            new ArgumentTokenizer(PREFIX_PRIORITY, PREFIX_START, PREFIX_END, PREFIX_DESCRIPTION, PREFIX_TAG);
+        argsTokenizer.tokenize(args);
+
+        Set<String> priorityKeyWordSet = new HashSet<>(argsTokenizer.getAllValues(PREFIX_PRIORITY)
+            .orElse(Collections.emptyList()));
+        Set<String> startKeyWordSet = new HashSet<>(argsTokenizer.getAllValues(PREFIX_START)
+            .orElse(Collections.emptyList()));
+        Set<String> deadlineKeyWordSet = new HashSet<>(argsTokenizer.getAllValues(PREFIX_END)
+            .orElse(Collections.emptyList()));
+        Set<String> tagsKeyWordSet = new HashSet<>(argsTokenizer.getAllValues(PREFIX_TAG)
+            .orElse(Collections.emptyList()));
+        Set<String> descKeyWordSet = new HashSet<>(argsTokenizer.getAllValues(PREFIX_DESCRIPTION)
+            .orElse(Collections.emptyList()));
+
+
         final Matcher matcher = KEYWORDS_ARGS_FORMAT.matcher(args.trim());
         if (!matcher.matches()) {
             return new IncorrectCommand(
@@ -31,7 +56,9 @@ public class FindCommandParser {
         // keywords delimited by whitespace
         final String[] keywords = matcher.group("keywords").split("\\s+");
         final Set<String> keywordSet = new HashSet<>(Arrays.asList(keywords));
-        return new FindCommand(keywordSet);
+        return new FindCommand(keywordSet, startKeyWordSet, deadlineKeyWordSet,
+            priorityKeyWordSet, tagsKeyWordSet, descKeyWordSet);
+
     }
 
 }
