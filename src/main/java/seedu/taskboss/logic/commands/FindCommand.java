f976f1f6a05f6a57653d5552da65c327069b5c70
ruolan <fionaxrl@gmail.com>
2017-03-20 22:24:31 +0800

    add find by information feature in findCommand; update command usage

--- src/main/java/seedu/taskboss/logic/commands/FindCommand.java
+++ src/main/java/seedu/taskboss/logic/commands/FindCommand.java
@@ -2,6 +2,7 @@ package seedu.taskboss.logic.commands;
 
 import static seedu.taskboss.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.taskboss.logic.parser.CliSyntax.PREFIX_START_DATE;
+import static seedu.taskboss.logic.parser.CliSyntax.PREFIX_END_DATE;
 
 import java.util.Arrays;
 import java.util.HashSet;
@@ -18,9 +19,9 @@ public class FindCommand extends Command {
     public static final String COMMAND_WORD_SHORT = "f";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + "/" + COMMAND_WORD_SHORT
-            + ": Finds all tasks whose names contain any of "
+            + ": Finds all tasks whose names/information/datetime contain any of "
             + "the specified keywords (case-sensitive) and displays them as a list with index numbers.\n"
-            + "Parameters: n/NAME or sd/STARTDATETIME or ed/ENDDATETIME \n"
+            + "Parameters: n/NAME or sd/STARTDATETIME or ed/ENDDATETIME or i/INFORMATION \n"
             + "Example: " + COMMAND_WORD + " n/meeting" + " || " + COMMAND_WORD_SHORT + " sd/Mar";
 
     private final String keywords;
@@ -41,8 +42,13 @@ public class FindCommand extends Command {
         } else if (prefix.equals(PREFIX_START_DATE.toString())) {
             model.updateFilteredTaskListByStartDateTime(keywords);
             return new CommandResult(getMessageForTaskListShownSummary(model.getFilteredTaskList().size()));
-        } else {
+        } else if (prefix.equals(PREFIX_END_DATE.toString())){
             model.updateFilteredTaskListByEndDateTime(keywords);
+            return new CommandResult(getMessageForTaskListShownSummary(model.getFilteredTaskList().size()));
+        } else { //prefix.equals(PREFIX_INFORMATION.toString()))
+            String[] keywordsList = keywords.split("\\s+");
+            final Set<String> keywordSet = new HashSet<String>(Arrays.asList(keywordsList));
+            model.updateFilteredTaskListByInformation(keywordSet);
             return new CommandResult(getMessageForTaskListShownSummary(model.getFilteredTaskList().size()));
         }
     }
