e00adced33b3e23dd4acd426034f3078deb154eb
Lim Jie <limjie1994@gmail.com>
2017-03-21 00:17:45 +0800

    Fix Merge Errors

--- src/main/java/seedu/tasklist/model/ModelManager.java
+++ src/main/java/seedu/tasklist/model/ModelManager.java
@@ -14,7 +14,6 @@ import seedu.tasklist.commons.core.UnmodifiableObservableList;
 import seedu.tasklist.commons.events.model.TaskListChangedEvent;
 
 import seedu.tasklist.commons.exceptions.DataConversionException;
-import seedu.tasklist.commons.exceptions.EmptyModelStackException;
 import seedu.tasklist.commons.util.CollectionUtil;
 import seedu.tasklist.commons.util.StringUtil;
 import seedu.tasklist.model.task.ReadOnlyTask;
@@ -133,16 +132,8 @@ public class ModelManager extends ComponentManager implements Model {
         undoStack.push(new TaskList(taskList));
     }
 
-    @SuppressWarnings("serial")
-    public static class EmptyUndoRedoStackException extends EmptyModelStackException {
-        protected EmptyUndoRedoStackException() {
-            super("No available states");
-        }
-    }
-
     @Override
     public synchronized void loadTaskList(String filePath) throws IOException {
-
         Optional<ReadOnlyTaskList> flexiTaskOptional;
         try {
             flexiTaskOptional = storage.readTaskList(filePath);
