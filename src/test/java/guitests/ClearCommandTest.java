a759fd182f4dbeec1121ae726b367ef64ead637b
DESKTOP-49OHI99\user <pengchengan@u.nus.edu>
2017-03-20 17:22:45 +0800

    fix trvias

--- src/test/java/guitests/ClearCommandTest.java
+++ src/test/java/guitests/ClearCommandTest.java
@@ -4,8 +4,6 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-import seedu.address.testutil.TestTodo;
-
 public class ClearCommandTest extends TodoListGuiTest {
 
     @Test
