889e6ff639dd6a99498a5f5b607b1c6efa0f511c
ang-zy <angzy95@yahoo.com.sg>
2017-03-21 22:54:15 +0800

    Add author tag

--- src/main/java/seedu/tache/logic/commands/Undoable.java
+++ src/main/java/seedu/tache/logic/commands/Undoable.java
@@ -1,16 +1,15 @@
 package seedu.tache.logic.commands;
 
 import seedu.tache.logic.commands.exceptions.CommandException;
-
+//@@author A0150120H
 public interface Undoable {
 
-    //@@author A0150120H
+
     /**
      * Returns true if the command can be undone.
      */
     public abstract boolean isUndoable();
 
-    //@@author A0150120H
     /**
      * Attempts to undo the command.
      * If the undo is successful, returns the CommandResult feedback string of the original command
