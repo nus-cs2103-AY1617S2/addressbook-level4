c577eee7178c551f1d685e2e5a37d9bcc25dd18d
DESKTOP-7RLT252\gjlam95 <gj_lam@hotmail.com>
2017-03-21 23:34:45 +0800

    Extract predicates out to variables

--- src/main/java/seedu/address/model/task/EndTimeComparator.java
+++ src/main/java/seedu/address/model/task/EndTimeComparator.java
@@ -4,7 +4,9 @@ import java.util.Comparator;
 
 public class EndTimeComparator implements Comparator<ReadOnlyTask> {
     public int compare(ReadOnlyTask p, ReadOnlyTask q) {
-        if (p.getEndTime().isPresent() && q.getEndTime().isPresent()) {
+        boolean hasStartAndEnd = p.getEndTime().isPresent() && q.getEndTime().isPresent();
+        boolean hasStartOrEnd = p.getEndTime().isPresent() || q.getEndTime().isPresent();
+    	if (hasStartAndEnd) {
             if (p.getEndTime().get().dateTime.isBefore(q.getEndTime().get().dateTime)) {
                 return -1;
             } else if (p.getEndTime().get().dateTime.isAfter(q.getEndTime().get().dateTime)) {
@@ -12,7 +14,7 @@ public class EndTimeComparator implements Comparator<ReadOnlyTask> {
             } else {
                 return 0;
             }
-        } else if (p.getEndTime().isPresent() || q.getEndTime().isPresent()) {
+        } else if (hasStartOrEnd) {
             return -1;
         } else {
             return 0;
