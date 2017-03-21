26dddfbdc86d9978fd83c0d296cda503794531b9
JingRong ( <jing_rong92@hotmail.com>
2017-03-21 13:47:25 +0800

    Refactor get undo edit command string into EditCommandParser

--- src/main/java/seedu/address/logic/parser/EditCommandParser.java
+++ src/main/java/seedu/address/logic/parser/EditCommandParser.java
@@ -8,11 +8,13 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
+import seedu.address.commons.core.UnmodifiableObservableList;
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.EditCommand;
 import seedu.address.logic.commands.EditCommand.EditTaskDescriptor;
 import seedu.address.logic.commands.IncorrectCommand;
+import seedu.address.model.person.ReadOnlyTask;
 import seedu.address.model.tag.UniqueTagList;
 
 /**
@@ -49,6 +51,27 @@ public class EditCommandParser {
         }
 
         return new EditCommand(index.get(), editTaskDescriptor);
+    }
+
+    //@@author A0114395E
+    /*
+     * Helper method to parse a get the inverse arguments of an Edit command
+     * @param String, UnmodifiableObservableList<ReadOnlyTask>
+     * @returns String of arguments for the inverse of the input edit command
+     */
+    public Command parseInverse(String arguments,
+            UnmodifiableObservableList<ReadOnlyTask> lastShownList) {
+        ArgumentTokenizer argsTokenizer = new ArgumentTokenizer(PREFIX_TAG);
+        argsTokenizer.tokenize(arguments);
+        List<Optional<String>> preambleFields = ParserUtil.splitPreamble(argsTokenizer.getPreamble().orElse(""), 2);
+        Optional<Integer> index = preambleFields.get(0).flatMap(ParserUtil::parseIndex);
+        ReadOnlyTask taskToEdit = lastShownList.get(index.get() - 1);
+        final StringBuilder editBuilder = new StringBuilder();
+        editBuilder.append(" ");
+        editBuilder.append(index.get().toString());
+        editBuilder.append(" ");
+        editBuilder.append(ParserUtil.getTaskArgs(taskToEdit));
+        return parse(editBuilder.toString());
     }
 
     /**
