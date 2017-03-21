a1741224dc3d2e43091b5aabc7ce21bdd47419cc
DESKTOP-7RLT252\gjlam95 <gj_lam@hotmail.com>
2017-03-21 20:22:21 +0800

    Edit EndTimeComparator to check for empty end dates

--- src/main/java/seedu/address/model/task/EndTimeComparator.java
+++ src/main/java/seedu/address/model/task/EndTimeComparator.java
@@ -4,9 +4,17 @@ import java.util.Comparator;
 
 public class EndTimeComparator implements Comparator<ReadOnlyTask> {
     public int compare(ReadOnlyTask p, ReadOnlyTask q) {
-        if (p.getEndTime().get().dateTime.isBefore(q.getEndTime().get().dateTime)) {
+        if (p.getEndTime().isPresent() && q.getEndTime().isPresent()) {
+    	    if (p.getEndTime().get().dateTime.isBefore(q.getEndTime().get().dateTime)) {
+                return -1;
+             } else if (p.getEndTime().get().dateTime.isAfter(q.getEndTime().get().dateTime)) {
+                return 1;
+            } else {
+                return 0;
+            }
+        } else if (p.getEndTime().isPresent()) {
             return -1;
-        } else if (p.getEndTime().get().dateTime.isAfter(q.getEndTime().get().dateTime)) {
+        } else if (q.getEndTime().isPresent()) {
             return 1;
         } else {
             return 0;
