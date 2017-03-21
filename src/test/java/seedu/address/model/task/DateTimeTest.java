ccae6a59b4f145fff1e97515c9e4dd794dddd88d
Bili Xu <xubili93@gmail.com>
2017-03-21 11:59:23 +0800

    Implement more test cases for isCurrentWeek around boundary values

--- src/test/java/seedu/address/model/task/DateTimeTest.java
+++ src/test/java/seedu/address/model/task/DateTimeTest.java
@@ -28,12 +28,25 @@ public class DateTimeTest {
         try {
             // Today should be in current week
             DateTime today = new DateTime(now);
+            assertTrue(today.isCurrentWeek());
 
             // Next week is not in current week
             DateTime nextWeek = new DateTime(now.plusDays(7));
-
-            assertTrue(today.isCurrentWeek());
             assertFalse(nextWeek.isCurrentWeek());
+
+            // This Sunday should be in current week
+            int daysToSunday = 7 - now.getDayOfWeek().getValue();
+            DateTime sunday = new DateTime(now.plusDays(daysToSunday));
+            assertTrue(sunday.isCurrentWeek());
+
+            // Next Monday should not be in current week
+            DateTime nextMonday = new DateTime(now.plusDays(daysToSunday + 1));
+            assertFalse(nextMonday.isCurrentWeek());
+
+            // Last Sunday should not be in current week
+            DateTime lastSunday = new DateTime(now.minusDays(now.getDayOfWeek().getValue()));
+            assertFalse(lastSunday.isCurrentWeek());
+
         } catch (IllegalValueException e) {
             fail("Exception should not be thrown.");
         }
