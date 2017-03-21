c8e23cc5fb48e068d621a6c926b0f597bd371fbc
Jacob Levy <levyjr@bu.edu>
2017-03-21 17:33:47 +0800

    Updated Help Center

--- src/main/java/seedu/task/ui/HelpWindow.java
+++ src/main/java/seedu/task/ui/HelpWindow.java
@@ -19,8 +19,8 @@ public class HelpWindow extends UiPart<Region> {
     private static final String ICON = "/images/help_icon.png";
     private static final String FXML = "HelpWindow.fxml";
     private static final String TITLE = "Help";
-    private static final String USERGUIDE_URL =
-            "https://se-edu.github.io/addressbook-level4/docs/UserGuide.html";
+    private static final String USERGUIDE_URL = "https://github.com/CS2103JAN2017-F11-B2/main/blob/master/docs/UserGuide.md";
+            //"https://se-edu.github.io/addressbook-level4/docs/UserGuide.html";
 
     @FXML
     private WebView browser;
