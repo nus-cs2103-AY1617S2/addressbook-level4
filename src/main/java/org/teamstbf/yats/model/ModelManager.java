c3731444d999f20a1efa94f57f6e0458fc41fee5
riveria94 <timelesschaos94@gmail.com>
2017-03-21 19:29:41 +0800

    Add ListCommand

--- src/main/java/org/teamstbf/yats/model/ModelManager.java
+++ src/main/java/org/teamstbf/yats/model/ModelManager.java
@@ -1,9 +1,9 @@
 package org.teamstbf.yats.model;
 
-import java.util.ArrayList;
 import java.util.Set;
 import java.util.Stack;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 import org.teamstbf.yats.commons.core.ComponentManager;
 import org.teamstbf.yats.commons.core.LogsCenter;
@@ -18,72 +18,19 @@ import org.teamstbf.yats.model.item.UniqueEventList.DuplicateEventException;
 import org.teamstbf.yats.model.item.UniqueEventList.EventNotFoundException;
 
 import javafx.collections.transformation.FilteredList;
-import javafx.collections.transformation.SortedList;
 
 /**
  * Represents the in-memory model of the task manager data. All changes to any
  * model should be synchronized.
  */
 public class ModelManager extends ComponentManager implements Model {
-	interface Expression {
-		boolean satisfies(ReadOnlyEvent event);
-
-		@Override
-		String toString();
-	}
-
-	private class NameQualifier implements Qualifier {
-		private Set<String> nameKeyWords;
-
-		NameQualifier(Set<String> nameKeyWords) {
-			this.nameKeyWords = nameKeyWords;
-		}
-
-		@Override
-		public boolean run(ReadOnlyEvent event) {
-			return nameKeyWords.stream()
-					.filter(keyword -> StringUtil.containsWordIgnoreCase(event.getTitle().fullName, keyword)).findAny()
-					.isPresent();
-		}
-
-		@Override
-		public String toString() {
-			return "title=" + String.join(", ", nameKeyWords);
-		}
-	}
-
-	private class PredicateExpression implements Expression {
-
-		private final Qualifier qualifier;
-
-		PredicateExpression(Qualifier qualifier) {
-			this.qualifier = qualifier;
-		}
-
-		@Override
-		public boolean satisfies(ReadOnlyEvent event) {
-			return qualifier.run(event);
-		}
-
-		@Override
-		public String toString() {
-			return qualifier.toString();
-		}
-	}
-
-	interface Qualifier {
-		boolean run(ReadOnlyEvent event);
-
-		@Override
-		String toString();
-	}
 
 	private static final Logger logger = LogsCenter.getLogger(ModelManager.class);
 
 	private final TaskManager taskManager;
-	
+
 	private static Stack<TaskManager> undoTaskManager = new Stack<TaskManager>();
-    private static Stack<TaskManager> redoTaskManager = new Stack<TaskManager>();
+	private static Stack<TaskManager> redoTaskManager = new Stack<TaskManager>();
 
 	private final FilteredList<ReadOnlyEvent> filteredEvents;
 
@@ -106,54 +53,53 @@ public class ModelManager extends ComponentManager implements Model {
 
 	@Override
 	public synchronized void addEvent(Event event) throws UniqueEventList.DuplicateEventException {
-	    saveImage();
-	    taskManager.addEvent(event);
+		saveImage();
+		taskManager.addEvent(event);
 		updateFilteredListToShowAll();
 		indicateTaskManagerChanged();
 	}
 
-    private void saveImage() {
-        TaskManager tempManager = new TaskManager();
-	    tempManager.resetData(taskManager);
-	    undoTaskManager.push(tempManager);
-	    redoTaskManager = new Stack<TaskManager>();
-    }
+	private void saveImage() {
+		TaskManager tempManager = new TaskManager();
+		tempManager.resetData(taskManager);
+		undoTaskManager.push(tempManager);
+		redoTaskManager = new Stack<TaskManager>();
+	}
 
 	@Override
 	public synchronized void deleteEvent(ReadOnlyEvent target) throws EventNotFoundException {
-	    saveImage();
+		saveImage();
 		taskManager.removeEvent(target);
 		indicateTaskManagerChanged();
 	}
-	
-	
-	//@@author A0102778B
-	
-	@Override
-    public synchronized void getPreviousState() {
-	    TaskManager tempManager = new TaskManager();
-	    tempManager.resetData(taskManager);
-	    redoTaskManager.push(tempManager);
-        taskManager.resetData((ReadOnlyTaskManager)undoTaskManager.pop());
-    }
-	
-    @Override
-    public synchronized void getNextState() {
-        saveImage();
-        taskManager.resetData((ReadOnlyTaskManager)redoTaskManager.pop());
-    }
+
+	// @@author A0102778B
 
 	@Override
-	public UnmodifiableObservableList<ReadOnlyEvent> getFilteredTaskList() {
-		return new UnmodifiableObservableList<>(filteredEvents);
+	public synchronized void getPreviousState() {
+		TaskManager tempManager = new TaskManager();
+		tempManager.resetData(taskManager);
+		redoTaskManager.push(tempManager);
+		taskManager.resetData(undoTaskManager.pop());
+	}
+
+	@Override
+	public synchronized void getNextState() {
+		saveImage();
+		taskManager.resetData(redoTaskManager.pop());
+	}
+
+	@Override
+	public ReadOnlyTaskManager getTaskManager() {
+		return taskManager;
 	}
 
 	// =========== Filtered Event List Accessors
 	// =============================================================
 
 	@Override
-	public ReadOnlyTaskManager getTaskManager() {
-		return taskManager;
+	public UnmodifiableObservableList<ReadOnlyEvent> getFilteredTaskList() {
+		return new UnmodifiableObservableList<>(filteredEvents);
 	}
 
 	/** Raises an event to indicate the model has changed */
@@ -163,15 +109,9 @@ public class ModelManager extends ComponentManager implements Model {
 
 	@Override
 	public void resetData(ReadOnlyTaskManager newData) {
-	    saveImage();
+		saveImage();
 		taskManager.resetData(newData);
 		indicateTaskManagerChanged();
-	}
-
-	@Override
-	public void sortFilteredEventList() {
-		SortedList<ReadOnlyEvent> sortedEventList = new SortedList<ReadOnlyEvent>(filteredEvents);
-		sortedEventList.sorted();
 	}
 
 	@Override
@@ -206,4 +146,165 @@ public class ModelManager extends ComponentManager implements Model {
 		filteredEvents.setPredicate(null);
 	}
 
+	// @@author A0138952W
+	@Override
+	public void updateFilteredListToShowLocation(Set<String> keywords) {
+		updateFilteredEventList(new PredicateExpression(new LocationQualifier(keywords)));
+	}
+
+	@Override
+	public void updateFilteredListToShowDate(Set<String> keywords) {
+		updateFilteredEventList(new PredicateExpression(new DateQualifier(keywords)));
+	}
+
+	@Override
+	public void updateFilteredListToShowStartTime(Set<String> keywords) {
+		updateFilteredEventList(new PredicateExpression(new StartTimeQualifier(keywords)));
+	}
+
+	@Override
+	public void updateFilteredListToShowTags(Set<String> keywords) {
+		updateFilteredEventList(new PredicateExpression(new TagQualifier(keywords)));
+	}
+
+	// Inner class used for Searching //
+	interface Qualifier {
+		boolean run(ReadOnlyEvent event);
+
+		@Override
+		String toString();
+	}
+
+	interface Expression {
+		boolean satisfies(ReadOnlyEvent event);
+
+		@Override
+		String toString();
+	}
+
+	private class PredicateExpression implements Expression {
+
+		private final Qualifier qualifier;
+
+		PredicateExpression(Qualifier qualifier) {
+			this.qualifier = qualifier;
+		}
+
+		@Override
+		public boolean satisfies(ReadOnlyEvent event) {
+			return qualifier.run(event);
+		}
+
+		@Override
+		public String toString() {
+			return qualifier.toString();
+		}
+	}
+
+	private class NameQualifier implements Qualifier {
+		private Set<String> nameKeyWords;
+
+		NameQualifier(Set<String> nameKeyWords) {
+			this.nameKeyWords = nameKeyWords;
+		}
+
+		@Override
+		public boolean run(ReadOnlyEvent event) {
+			return nameKeyWords.stream()
+					.filter(keyword -> StringUtil.containsWordIgnoreCase(event.getTitle().fullName, keyword)).findAny()
+					.isPresent();
+		}
+
+		@Override
+		public String toString() {
+			return "title=" + String.join(", ", nameKeyWords);
+		}
+	}
+
+	// @@author A0138952W
+
+	private class LocationQualifier implements Qualifier {
+
+		private Set<String> locationKeyWords;
+
+		LocationQualifier(Set<String> locationKeyWords) {
+			this.locationKeyWords = locationKeyWords;
+		}
+
+		@Override
+		public boolean run(ReadOnlyEvent event) {
+			return locationKeyWords.stream()
+					.filter(keyword -> StringUtil.containsWordIgnoreCase(event.getLocation().toString(), keyword))
+					.findAny().isPresent();
+		}
+
+		@Override
+		public String toString() {
+			return "location=" + String.join(", ", locationKeyWords);
+		}
+	}
+
+	private class DateQualifier implements Qualifier {
+
+		private Set<String> dateKeyWords;
+
+		DateQualifier(Set<String> dateKeyWords) {
+			this.dateKeyWords = dateKeyWords;
+		}
+
+		@Override
+		public boolean run(ReadOnlyEvent event) {
+			return dateKeyWords.stream().filter(
+					keyword -> StringUtil.containsWordIgnoreCase(event.getStartTime().getDate().toString(), keyword))
+					.findAny().isPresent();
+		}
+
+		@Override
+		public String toString() {
+			return "date=" + String.join(", ", dateKeyWords);
+		}
+	}
+
+	private class StartTimeQualifier implements Qualifier {
+
+		private Set<String> startTimeKeyWords;
+
+		StartTimeQualifier(Set<String> startTimeKeyWords) {
+			this.startTimeKeyWords = startTimeKeyWords;
+		}
+
+		@Override
+		public boolean run(ReadOnlyEvent event) {
+			return startTimeKeyWords.stream().filter(
+					keyword -> StringUtil.containsWordIgnoreCase(event.getStartTime().getTime().toString(), keyword))
+					.findAny().isPresent();
+		}
+
+		@Override
+		public String toString() {
+			return "startTime=" + String.join(", ", startTimeKeyWords);
+		}
+	}
+
+	private class TagQualifier implements Qualifier {
+
+		private Set<String> tagKeyWords;
+
+		TagQualifier(Set<String> tagKeyWords) {
+			this.tagKeyWords = tagKeyWords;
+		}
+
+		@Override
+		public boolean run(ReadOnlyEvent event) {
+			String tagObtain = event.getTags().asObservableList().stream().map(tagString -> tagString.tagName)
+					.distinct().collect(Collectors.joining(" "));
+			return tagKeyWords.stream().filter(keyword -> StringUtil.containsWordIgnoreCase(tagObtain, keyword))
+					.findAny().isPresent();
+		}
+
+		@Override
+		public String toString() {
+			return "tag=" + String.join(", ", tagKeyWords);
+		}
+	}
 }
