edf104e887abdd1380679930a5a38d4bf7e9b98c
ruolan <fionaxrl@gmail.com>
2017-03-21 15:12:33 +0800

    remove unused import, edit usage message

--- src/main/java/seedu/taskboss/logic/commands/FindCommand.java
+++ src/main/java/seedu/taskboss/logic/commands/FindCommand.java
@@ -1,7 +1,6 @@
 package seedu.taskboss.logic.commands;
 
 import static seedu.taskboss.logic.parser.CliSyntax.PREFIX_END_DATE;
-import static seedu.taskboss.logic.parser.CliSyntax.PREFIX_INFORMATION;
 import static seedu.taskboss.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.taskboss.logic.parser.CliSyntax.PREFIX_START_DATE;
 
@@ -20,9 +19,12 @@ public class FindCommand extends Command {
     public static final String COMMAND_WORD_SHORT = "f";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + "/" + COMMAND_WORD_SHORT
-            + ": Finds all tasks whose names/information/datetime contain any of "
-            + "the specified keywords (case-sensitive) and displays them as a list with index numbers.\n"
-            + "Parameters: n/NAME or sd/STARTDATETIME or ed/ENDDATETIME or i/INFORMATION \n"
+            + ": Finds all tasks with names containing any of "
+            + "the specified keywords (case-sensitive),\n"
+            + " or with dates specified in any of following formats"
+            + " e.g 28 / Feb / Feb 28 / Feb 28, 2017,\n"
+            + " and displays them in a list.\n"
+            + "Parameters: n/NAME or sd/START_DATE or ed/END_DATE \n"
             + "Example: " + COMMAND_WORD + " n/meeting" + " || " + COMMAND_WORD_SHORT + " sd/Mar";
 
     private final String keywords;
