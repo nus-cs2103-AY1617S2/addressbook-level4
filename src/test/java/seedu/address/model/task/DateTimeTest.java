25b7443a38ba18926734fc348e60d63eb4df1144
Bili Xu <xubili93@gmail.com>
2017-03-21 11:52:42 +0800

    Implement method in DateTime class to check if DateTime is in current week

--- src/test/java/seedu/address/model/task/DateTimeTest.java
+++ src/test/java/seedu/address/model/task/DateTimeTest.java
@@ -2,8 +2,13 @@ package seedu.address.model.task;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.time.LocalDateTime;
 
 import org.junit.Test;
+
+import seedu.address.commons.exceptions.IllegalValueException;
 
 public class DateTimeTest {
 
@@ -14,5 +19,23 @@ public class DateTimeTest {
 
         // valid dateTime
         assertTrue(DateTime.isValidDateTime("03/12/2217 12:00")); // inputs a valid date string
+    }
+
+    @Test
+    public void isInCurrentWeek() {
+        LocalDateTime now = LocalDateTime.now();
+
+        try {
+            // Today should be in current week
+            DateTime today = new DateTime(now);
+
+            // Next week is not in current week
+            DateTime nextWeek = new DateTime(now.plusDays(7));
+
+            assertTrue(today.isCurrentWeek());
+            assertFalse(nextWeek.isCurrentWeek());
+        } catch (IllegalValueException e) {
+            fail("Exception should not be thrown.");
+        }
     }
 }
