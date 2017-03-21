8eea24df77f20c970bd502823c89cb19bc1063d9
LWWenz <leongww7@gmail.com>
2017-03-21 22:56:39 +0800

    Fix AppVeyor Error

--- src/main/java/typetask/logic/parser/SettingCommandParser.java
+++ src/main/java/typetask/logic/parser/SettingCommandParser.java
@@ -3,8 +3,8 @@ package typetask.logic.parser;
 import static typetask.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import typetask.logic.commands.Command;
-import typetask.logic.commands.SettingCommand;
 import typetask.logic.commands.IncorrectCommand;
+import typetask.logic.commands.SettingCommand;
 
 public class SettingCommandParser {
     public Command parse(String args) {
@@ -13,7 +13,7 @@ public class SettingCommandParser {
             return new IncorrectCommand(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, SettingCommand.MESSAGE_USAGE));
         }
-        else if(args.substring(args.trim().length()-3).equals(".xml")) {
+        else if (args.substring(args.trim().length() - 3).equals(".xml")) {
             return new IncorrectCommand(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, "Specify the folder Location"));
         }
