13b48ecb7c904d78af3d8ee34bf9a8b4825676fa
chrishendra93 <chrishendra93@gmail.com>
2017-03-21 23:23:40 +0800

    Add test cases for TimeTest.java

--- src/test/java/seedu/jobs/model/task/TimeTest.java
+++ src/test/java/seedu/jobs/model/task/TimeTest.java
@@ -17,11 +17,14 @@ public class TimeTest {
         assertFalse(Time.isValidTime("12/12/2016 12")); // incomplete time declaration
         assertFalse(Time.isValidTime("12/12/2016 12:")); // incomplete time declaration
         assertFalse(Time.isValidTime("9/12/1993 5:2")); //no preceding zero in the minute portion
-
+        assertFalse(Time.isValidTime("9/12/1993 05:00")); //no preceding zero in the day
+        assertFalse(Time.isValidTime("09/9/1993 05:00")); //no preceding zero in the month
+        assertFalse(Time.isValidTime("31/11/1993 05:00")); //non existent date
+        assertFalse(Time.isValidTime("30/11/1993 25:00")); //non existent time
+        
         // valid Time
         assertTrue(Time.isValidTime("09/12/1993 15:00")); //preceding zero in the date
-        assertTrue(Time.isValidTime("9/12/1993 05:00")); //no preceding zero in the date 
-        assertTrue(Time.isValidTime("9/12/1993 5:00")); //no preceding zero in the hour
+        assertTrue(Time.isValidTime("9/12/1993 05:00")); //preceding zero in the hour
 
     }
 }
