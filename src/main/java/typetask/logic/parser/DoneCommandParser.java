03aefd073dc954eb2b0e670c919aab70aec2c057
Dana Baghabrah <d.baghabra@gmail.com>
2017-03-21 22:33:26 +0800

    Add done Command

---
+++ src/main/java/typetask/logic/parser/DoneCommandParser.java
@@ -0,0 +1,30 @@
+package typetask.logic.parser;
+
+import static typetask.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import java.util.Optional;
+
+import typetask.logic.commands.Command;
+import typetask.logic.commands.DoneCommand;
+import typetask.logic.commands.IncorrectCommand;
+
+/**
+ * Parses input arguments and creates a new DoneCommand object
+ * @@author: A0144902L
+ */
+public class DoneCommandParser {
+    /**
+     * Parses the given {@code String} of arguments in the context of the DoneCommand
+     * and returns an DoneCommand object for execution.
+     */
+    public Command parse(String args) {
+
+        Optional<Integer> index = ParserUtil.parseIndex(args);
+        if (!index.isPresent()) {
+            return new IncorrectCommand(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, DoneCommand.MESSAGE_USAGE));
+        }
+
+        return new DoneCommand(index.get());
+    }
+}
