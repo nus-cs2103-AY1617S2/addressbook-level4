5afe8a61d77bb1f1af580bbb3f8f96489af5bc67
WANGCE\WangC <wangce999@hotmail.com>
2017-03-22 01:07:58 +0800

    added command text list for access to command words

--- src/main/java/werkbook/task/commons/core/CommandTexts.java
+++ src/main/java/werkbook/task/commons/core/CommandTexts.java
@@ -1,12 +1,16 @@
 //@@author A0140462R
+
 package werkbook.task.commons.core;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /*
  * Container for command words used
  */
 
 public class CommandTexts {
-    
+
     public static final String ADD_COMMAND_WORD = "add";
     public static final String EDIT_COMMAND_WORD = "edit";
     public static final String SELECT_COMMAND_WORD = "select";
@@ -19,4 +23,9 @@ public class CommandTexts {
     public static final String UNDO_COMMAND_WORD = "undo";
     public static final String REDO_COMMAND_WORD = "redo";
     public static final String MARK_COMMAND_WORD = "mark";
+    public static final ArrayList<String> COMMAND_TEXT_LIST = new ArrayList<String>(
+        Arrays.asList(ADD_COMMAND_WORD, EDIT_COMMAND_WORD, SELECT_COMMAND_WORD,
+                      DELETE_COMMAND_WORD, CLEAR_COMMAND_WORD, FIND_COMMAND_WORD,
+                      LIST_COMMAND_WORD, EXIT_COMMAND_WORD, HELP_COMMAND_WORD,
+                      UNDO_COMMAND_WORD, REDO_COMMAND_WORD, MARK_COMMAND_WORD));
 }
