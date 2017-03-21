408a8af00e1dfb24e46a7848cafda4882dbf488b
ruolan <fionaxrl@gmail.com>
2017-03-21 21:25:37 +0800

    fix checkstyle errors

--- src/main/java/seedu/taskboss/model/ModelManager.java
+++ src/main/java/seedu/taskboss/model/ModelManager.java
@@ -103,8 +103,8 @@ public class ModelManager extends ComponentManager implements Model {
         int listSize = taskListWithCategory.size();
         System.out.println("list size: " + listSize);
         int i;
-        for (i = 0; i < listSize; i++ ) {
-            System.out.println("i is: "+i);
+        for (i = 0; i < listSize; i++) {
+            System.out.println("i is: " + i);
             ReadOnlyTask target = taskListWithCategory.get(0);
             try {
                 taskBoss.removeTask(target);
