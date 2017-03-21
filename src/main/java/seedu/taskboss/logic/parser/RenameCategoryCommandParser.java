9a207ed75bf370874379efc95eed6d9e7d9e20a1
Tan Wei <tanwei0319@gmail.com>
2017-03-22 04:45:23 +0800

    Check for alphanumeric names::RenameCategoryCommandParser

--- src/main/java/seedu/taskboss/logic/parser/RenameCategoryCommandParser.java
+++ src/main/java/seedu/taskboss/logic/parser/RenameCategoryCommandParser.java
@@ -11,13 +11,21 @@ public class RenameCategoryCommandParser {
     private static final int NUM_CATEGORIES_PARAM = 2;
     private static final int INDEX_OLD_CATEGORY = 0;
     private static final int INDEX_NEW_CATEGORY = 1;
+    private static final String FORMAT_ALPHANUMERIC = "[A-Za-z0-9]+";
+    private static final String ERROR_NON_ALPHANUMERIC = "Category names should be alphanumeric.";
 
     public Command parse(String args) {
         String[] categories = ParserUtil.parseRenameCategory(args);
         if (categories.length != NUM_CATEGORIES_PARAM) {
             return new IncorrectCommand(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, RenameCategoryCommand.MESSAGE_USAGE));
-        }
+        } else if (!categories[INDEX_OLD_CATEGORY].matches(FORMAT_ALPHANUMERIC) ||
+            !categories[INDEX_NEW_CATEGORY].matches(FORMAT_ALPHANUMERIC)) {
+            return new IncorrectCommand(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, ERROR_NON_ALPHANUMERIC));
+        } 
+
+        
         return new RenameCategoryCommand(categories[INDEX_OLD_CATEGORY], categories[INDEX_NEW_CATEGORY]);
     }
 }
