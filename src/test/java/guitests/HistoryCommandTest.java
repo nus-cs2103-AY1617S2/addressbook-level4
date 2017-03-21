428aa40e7a8856193e052cb5fe0e6f1b6abe6bea
A0162011A <e0112235@u.nus.edu>
2017-03-21 20:27:01 +0800

    update test cases

--- src/test/java/guitests/HistoryCommandTest.java
+++ src/test/java/guitests/HistoryCommandTest.java
@@ -7,18 +7,49 @@ import org.junit.Test;
  */
 public class HistoryCommandTest extends ToLuistGuiTest {
     @Test
-    public void viewHistory() {
+    public void viewHistoryWithoutAnythingElse() {
         String command = "history";
         commandBox.runCommand(command);
         assertResultMessage("\n" + command + "\n2 commands displayed.");
     }
 
     @Test
-    public void addTaskAndViewHistory() {
+    public void addMutatingCommandAndViewHistory() {
         String command1 = "add task";
         commandBox.runCommand(command1);
         String command2 = "history";
         commandBox.runCommand(command2);
         assertResultMessage("\n" + command1 + "\n" + command2 + "\n3 commands displayed.");
+    }
+
+    @Test
+    public void addNonMutatingCommandAndViewHistory() {
+        String command1 = "history";
+        commandBox.runCommand(command1);
+        String command2 = "history";
+        commandBox.runCommand(command2);
+        assertResultMessage("\n" + command1 + "\n" + command2 + "\n3 commands displayed.");
+    }
+
+    @Test
+    public void addMutatingAndNonMutatingCommandAndViewHistory() {
+        String command1 = "add task";
+        commandBox.runCommand(command1);
+        String command2 = "history";
+        commandBox.runCommand(command2);
+        String command3 = "history";
+        commandBox.runCommand(command3);
+        assertResultMessage("\n" + command1 + "\n" + command2 + "\n" + command3 + "\n4 commands displayed.");
+    }
+
+    @Test
+    public void addNonMutatingAndMutatingCommandAndViewHistory() {
+        String command1 = "history";
+        commandBox.runCommand(command1);
+        String command2 = "add task";
+        commandBox.runCommand(command2);
+        String command3 = "history";
+        commandBox.runCommand(command3);
+        assertResultMessage("\n" + command1 + "\n" + command2 + "\n" + command3 + "\n4 commands displayed.");
     }
 }
