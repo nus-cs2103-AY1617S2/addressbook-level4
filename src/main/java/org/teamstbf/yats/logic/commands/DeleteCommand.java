5b762c74270dfcb246623a7f2078a7048461f293
riveria94 <timelesschaos94@gmail.com>
2017-03-21 18:48:36 +0800

    Revert "Update name in deleteCommand"
    This reverts commit 8662a48628e6fefece64c2b814ad8357acb56567.

--- src/main/java/org/teamstbf/yats/logic/commands/DeleteCommand.java
+++ src/main/java/org/teamstbf/yats/logic/commands/DeleteCommand.java
@@ -7,43 +7,44 @@ import org.teamstbf.yats.model.item.ReadOnlyEvent;
 import org.teamstbf.yats.model.item.UniqueEventList.EventNotFoundException;
 
 /**
- * Deletes a task identified using it's last displayed index from the task
- * manager.
+ * Deletes a person identified using it's last displayed index from the address book.
  */
 public class DeleteCommand extends Command {
 
-	public static final String COMMAND_WORD = "delete";
+    public static final String COMMAND_WORD = "delete";
 
-	public static final String MESSAGE_USAGE = COMMAND_WORD
-			+ ": Deletes the task identified by the index number used in the last task listing.\n"
-			+ "Parameters: INDEX (must be a positive integer)\n" + "Example: " + COMMAND_WORD + " 1";
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Deletes the task identified by the index number used in the last task listing.\n"
+            + "Parameters: INDEX (must be a positive integer)\n"
+            + "Example: " + COMMAND_WORD + " 1";
 
-	public static final String MESSAGE_DELETE_TASK_SUCCESS = "Deleted Task: %1$s";
+    public static final String MESSAGE_DELETE_TASK_SUCCESS = "Deleted Task: %1$s";
 
-	public final int targetIndex;
+    public final int targetIndex;
 
-	public DeleteCommand(int targetIndex) {
-		this.targetIndex = targetIndex;
-	}
+    public DeleteCommand(int targetIndex) {
+        this.targetIndex = targetIndex;
+    }
 
-	@Override
-	public CommandResult execute() throws CommandException {
 
-		UnmodifiableObservableList<ReadOnlyEvent> lastShownList = model.getFilteredTaskList();
+    @Override
+    public CommandResult execute() throws CommandException {
 
-		if (lastShownList.size() < targetIndex) {
-			throw new CommandException(Messages.MESSAGE_INVALID_TASK_DISPLAYED_INDEX);
-		}
+        UnmodifiableObservableList<ReadOnlyEvent> lastShownList = model.getFilteredTaskList();
 
-		ReadOnlyEvent taskToDelete = lastShownList.get(targetIndex - 1);
+        if (lastShownList.size() < targetIndex) {
+            throw new CommandException(Messages.MESSAGE_INVALID_TASK_DISPLAYED_INDEX);
+        }
 
-		try {
-			model.deleteEvent(taskToDelete);
-		} catch (EventNotFoundException pnfe) {
-			assert false : "The target task cannot be missing";
-		}
+        ReadOnlyEvent personToDelete = lastShownList.get(targetIndex - 1);
 
-		return new CommandResult(String.format(MESSAGE_DELETE_TASK_SUCCESS, taskToDelete));
-	}
+        try {
+            model.deleteEvent(personToDelete);
+        } catch (EventNotFoundException pnfe) {
+            assert false : "The target task cannot be missing";
+        }
+
+        return new CommandResult(String.format(MESSAGE_DELETE_TASK_SUCCESS, personToDelete));
+    }
 
 }
