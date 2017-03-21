7d0ee9ae189c3f6a3564d75c92a552d8b19c8dc1
InfinityFrost <iceazul_awakendragon@hotmail.com>
2017-03-21 14:46:24 +0800

    Update Config.java

--- src/main/java/seedu/taskboss/commons/core/Config.java
+++ src/main/java/seedu/taskboss/commons/core/Config.java
@@ -14,7 +14,7 @@ public class Config {
     private String appTitle = "TaskBoss App";
     private Level logLevel = Level.INFO;
     private String userPrefsFilePath = "preferences.json";
-    private String taskBossFilePath = "data/taskboss.xml";
+    private String taskBossFilePath = "/taskboss.xml";
     private String taskBossName = "MyTaskBoss";
 
 
