a2eac804318719a18c5114585b2d9c436ad5e7ad
Fanyi <zfy926@gmail.com>
2017-03-23 06:31:02 +0800

    add TitleTest and fix XmlUtilTest

--- src/test/java/seedu/address/commons/util/XmlUtilTest.java
+++ src/test/java/seedu/address/commons/util/XmlUtilTest.java
@@ -21,8 +21,8 @@ public class XmlUtilTest {
     private static final String TEST_DATA_FOLDER = FileUtil.getPath("src/test/data/XmlUtilTest/");
     private static final File EMPTY_FILE = new File(TEST_DATA_FOLDER + "empty.xml");
     private static final File MISSING_FILE = new File(TEST_DATA_FOLDER + "missing.xml");
-    private static final File VALID_FILE = new File(TEST_DATA_FOLDER + "validAddressBook.xml");
-    private static final File TEMP_FILE = new File(TestUtil.getFilePathInSandboxFolder("tempAddressBook.xml"));
+    private static final File VALID_FILE = new File(TEST_DATA_FOLDER + "validTaskIt.xml");
+    private static final File TEMP_FILE = new File(TestUtil.getFilePathInSandboxFolder("tempTaskIt.xml"));
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
@@ -54,8 +54,8 @@ public class XmlUtilTest {
     @Test
     public void getDataFromFile_validFile_validResult() throws Exception {
         XmlSerializableAddressBook dataFromFile = XmlUtil.getDataFromFile(VALID_FILE, XmlSerializableAddressBook.class);
-        assertEquals(9, dataFromFile.getTaskList().size());
-        assertEquals(0, dataFromFile.getTagList().size());
+        assertEquals(4, dataFromFile.getTaskList().size());
+        assertEquals(4, dataFromFile.getTagList().size());
     }
 
     @Test
