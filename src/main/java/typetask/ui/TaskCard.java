b1597bca0655066672f23d902165150580905859
Jun Wei <Jun Wei>
2017-03-23 00:28:04 +0800

    Add red and green colour code Add red colour code for overdue Add green colour code for pending (i.e. tasks that are non-floating task)

--- src/main/java/typetask/ui/TaskCard.java
+++ src/main/java/typetask/ui/TaskCard.java
@@ -1,5 +1,10 @@
 package typetask.ui;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
@@ -10,6 +15,10 @@ import typetask.model.task.ReadOnlyTask;
 public class TaskCard extends UiPart<Region> {
 
     private static final String FXML = "TaskListCard.fxml";
+    
+    public static final String OVERDUE_STYLE_CLASS = "overdue";
+    public static final String PENDING_STYLE_CLASS = "pending";
+    public static final String PRIORITY_STYLE_CLASS = "priority";
 
     @FXML
     private HBox cardPane;
@@ -33,15 +42,62 @@ public class TaskCard extends UiPart<Region> {
     private Label endTime;
     @FXML
     private Pane colourTag;
+    
+    private ReadOnlyTask task;
+    private LocalDateTime now = LocalDateTime.now();
+    private LocalDate nowDate = now.toLocalDate();
+    private String inputPattern = "dd/MM/yyyy";
+    private DateTimeFormatter dtf = DateTimeFormatter.ofPattern(inputPattern);
+    private LocalDate parsedDate;// NOTE: only instantiated for non-floating task
+    private boolean parsedDateFlag = false;
+    
 
-
-    public TaskCard(ReadOnlyTask task, int displayedIndex) {
+    
+	public TaskCard(ReadOnlyTask task, int displayedIndex) {
         super(FXML);
         name.setText(task.getName().fullName);
         id.setText(displayedIndex + ". ");
         date.setText(task.getDate().value);
         time.setText(task.getTime().value);
+        this.task = task;
+        if (!task.getDate().value.equals("")) {
+        	parsedDateFlag = true;
+        	parsedDate = LocalDate.parse(task.getDate().value, dtf);
+        }
+//        time.setText(parsedDate.toString());
+        setColourCode();
         //add endDate.setText(...);
+        
     }
 
+    //@@author A0139154E
+    private void setColourCode() {
+//        if (task.getName().fullName.equals("lunch")) {
+//        	setStyleToIndicatePriority();
+//        }
+        if (parsedDateFlag == true) {
+        	if (parsedDate.isBefore(nowDate)) {
+        		setStyleToIndicateOverdue();
+        	} else {
+        		setStyleToIndicatePending();
+        	}
+        } else {
+        	//floating tasks has no date
+        }
+    }
+    
+    //@@author A0139154E
+    private void setStyleToIndicateOverdue() {
+        colourTag.getStyleClass().add(OVERDUE_STYLE_CLASS);
+    }
+    
+    //@@author A0139154E
+    private void setStyleToIndicatePending() {
+        colourTag.getStyleClass().add(PENDING_STYLE_CLASS);
+    }
+    
+    //@@author A0139154E
+    private void setStyleToIndicatePriority() {
+        colourTag.getStyleClass().add(PRIORITY_STYLE_CLASS);
+    }
 }
