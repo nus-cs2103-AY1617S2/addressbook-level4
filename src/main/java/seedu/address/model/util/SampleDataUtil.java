ecc7dfb268c5bed24330f00c1f15afe3f91ba212
Huan Hock Lim <huanhock.lim@gmail.com>
2017-03-22 22:35:18 +0800

    Fix implementation of Completion in Task

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -3,6 +3,7 @@ package seedu.address.model.util;
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.ReadOnlyToDoApp;
 import seedu.address.model.ToDoApp;
+import seedu.address.model.person.Completion;
 import seedu.address.model.person.Deadline;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Notes;
@@ -17,9 +18,9 @@ public class SampleDataUtil {
         try {
             return new Task[] {
                 new Task(new Name("Buy printer"), new Start("today"), new Deadline("tomorrow"),
-                    new Priority(1), new UniqueTagList("shopping"), new Notes("this is a note")),
+                    new Priority(1), new UniqueTagList("shopping"), new Notes("this is a note"), new Completion("true")),
                 new Task(new Name("Go to the gym"), new Start(""), new Deadline(""),
-                    new Priority(0), new UniqueTagList("exercise"), new Notes("")),
+                    new Priority(0), new UniqueTagList("exercise"), new Notes(""), new Completion("")),
             };
         } catch (IllegalValueException e) {
             throw new AssertionError("sample data cannot be invalid", e);
