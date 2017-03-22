9f2769cbfbf705b927ff1e2c02a6e534aea95e16
Brea <dionisio@live.unc.edu>
2017-03-22 23:29:03 +0800

    Add DeleteCommand support for Events

--- src/main/java/project/taskcrusher/logic/commands/DeleteCommand.java
+++ src/main/java/project/taskcrusher/logic/commands/DeleteCommand.java
@@ -3,48 +3,74 @@ package project.taskcrusher.logic.commands;
 import project.taskcrusher.commons.core.Messages;
 import project.taskcrusher.commons.core.UnmodifiableObservableList;
 import project.taskcrusher.logic.commands.exceptions.CommandException;
+import project.taskcrusher.model.event.ReadOnlyEvent;
+import project.taskcrusher.model.event.UniqueEventList.EventNotFoundException;
 import project.taskcrusher.model.task.ReadOnlyTask;
 import project.taskcrusher.model.task.UniqueTaskList.TaskNotFoundException;
 
 /**
- * Deletes a person identified using it's last displayed index from the address book.
+ * Deletes a person identified using it's last displayed index from the address
+ * book.
  */
 public class DeleteCommand extends Command {
 
     public static final String COMMAND_WORD = "delete";
 
+    // TODO update this as well
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Deletes the task identified by the index number used in the last task listing.\n"
-            + "Parameters: INDEX (must be a positive integer)\n"
-            + "Example: " + COMMAND_WORD + " 1";
+            + "Parameters: INDEX (must be a positive integer)\n" + "Example: " + COMMAND_WORD + " 1";
 
     public static final String MESSAGE_DELETE_TASK_SUCCESS = "Deleted Task: %1$s";
 
     public final int targetIndex;
+    public final String flag;
 
-    public DeleteCommand(int targetIndex) {
+    public DeleteCommand(String flag, int targetIndex) {
+        this.flag = flag;
         this.targetIndex = targetIndex;
     }
-
 
     @Override
     public CommandResult execute() throws CommandException {
 
-        UnmodifiableObservableList<ReadOnlyTask> lastShownList = model.getFilteredTaskList();
+        // TODO define these flags in a central place sometime perhaps
+        assert (this.flag.equals(AddCommand.EVENT_FLAG) || this.flag.equals(AddCommand.TASK_FLAG));
 
-        if (lastShownList.size() < targetIndex) {
-            throw new CommandException(Messages.MESSAGE_INVALID_TASK_DISPLAYED_INDEX);
+        if (this.flag.equals(AddCommand.TASK_FLAG)) {
+            UnmodifiableObservableList<ReadOnlyTask> lastShownList = model.getFilteredTaskList();
+
+            if (lastShownList.size() < targetIndex) {
+                throw new CommandException(Messages.MESSAGE_INVALID_TASK_DISPLAYED_INDEX);
+            }
+
+            ReadOnlyTask taskToDelete = lastShownList.get(targetIndex - 1);
+
+            try {
+                model.deleteTask(taskToDelete);
+            } catch (TaskNotFoundException pnfe) {
+                assert false : "The target person cannot be missing";
+            }
+
+            return new CommandResult(String.format(MESSAGE_DELETE_TASK_SUCCESS, taskToDelete));
+        } else {
+            UnmodifiableObservableList<ReadOnlyEvent> lastShownList = model.getFilteredEventList();
+
+            if (lastShownList.size() < targetIndex) {
+                throw new CommandException(Messages.MESSAGE_INVALID_EVENT_DISPLAYED_INDEX);
+            }
+
+            ReadOnlyEvent eventToDelete = lastShownList.get(targetIndex - 1);
+
+            try {
+                model.deleteEvent(eventToDelete);
+            } catch (EventNotFoundException pnfe) {
+                assert false : "The target person cannot be missing";
+            }
+
+            return new CommandResult(String.format(MESSAGE_DELETE_TASK_SUCCESS, eventToDelete));
         }
 
-        ReadOnlyTask taskToDelete = lastShownList.get(targetIndex - 1);
-
-        try {
-            model.deleteTask(taskToDelete);
-        } catch (TaskNotFoundException pnfe) {
-            assert false : "The target person cannot be missing";
-        }
-
-        return new CommandResult(String.format(MESSAGE_DELETE_TASK_SUCCESS, taskToDelete));
     }
 
 }
