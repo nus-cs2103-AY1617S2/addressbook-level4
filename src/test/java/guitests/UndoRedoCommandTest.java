3a705aa7c2c613a1574a04191c6025d05d58cbf6
LuoYuyang <royl8@outlook.com>
2017-03-22 10:26:17 +0800

    add test cases for new branch of history

--- src/test/java/guitests/UndoRedoCommandTest.java
+++ src/test/java/guitests/UndoRedoCommandTest.java
@@ -51,7 +51,7 @@ public class UndoRedoCommandTest extends DoistGUITest {
     public void testUndoAndRedoMoreThanHistory() {  // execute one mutating command, but undo twice
         TestTask[] currentList = td.getTypicalTasks();
 
-        TestTask taskToAdd = td.email;
+        TestTask taskToAdd = td.chores;
         commandBox.runCommand(taskToAdd.getAddCommand());
         TestTask[] newList = TestUtil.addTasksToList(currentList, taskToAdd);
         assertTrue(personListPanel.isListMatching(newList));
@@ -65,6 +65,36 @@ public class UndoRedoCommandTest extends DoistGUITest {
         assertTrue(personListPanel.isListMatching(newList));
         commandBox.runCommand("redo");
         assertTrue(personListPanel.isListMatching(newList));
+    }
+
+    @Test
+    public void testNewBranchHistory() {  // undo then execute a command, which create a new history branch
+        TestTask[] currentList = td.getTypicalTasks();
+
+        TestTask taskToAdd = td.chores;
+        commandBox.runCommand(taskToAdd.getAddCommand());
+        TestTask[] newList = TestUtil.addTasksToList(currentList, taskToAdd);
+        assertTrue(personListPanel.isListMatching(newList));
+
+        taskToAdd = td.email;
+        commandBox.runCommand(taskToAdd.getAddCommand());
+        TestTask[] newList2 = TestUtil.addTasksToList(newList, taskToAdd);
+        assertTrue(personListPanel.isListMatching(newList2));
+
+        commandBox.runCommand("undo");
+        assertTrue(personListPanel.isListMatching(newList));
+
+        taskToAdd = td.exercise;
+        commandBox.runCommand(taskToAdd.getAddCommand());
+        TestTask[] newList3 = TestUtil.addTasksToList(newList, taskToAdd);
+        assertTrue(personListPanel.isListMatching(newList3));
+
+        commandBox.runCommand("redo");
+        assertTrue(personListPanel.isListMatching(newList3));
+        commandBox.runCommand("undo");
+        assertTrue(personListPanel.isListMatching(newList));
+        commandBox.runCommand("redo");
+        assertTrue(personListPanel.isListMatching(newList3));
     }
 }
 
