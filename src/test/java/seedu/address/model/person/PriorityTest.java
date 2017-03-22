79e016014a30c1ddac6215995f271142621e8d4c
Ben2\benwong <benwongwf@gmail.com>
2017-03-22 15:35:07 +0800

    add priority tes

---
+++ src/test/java/seedu/address/model/person/PriorityTest.java
@@ -0,0 +1,19 @@
+package seedu.address.model.person;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class PriorityTest {
+    
+    @Test
+    public void isValidPriority() {
+        //valid priority
+        assertTrue(Priority.isValidPriority(0));//zero
+        assertTrue(Priority.isValidPriority(10));//integer
+        
+        //invalid priority
+        assertFalse(Priority.isValidPriority(-1));//negative integer
+    }
+}
