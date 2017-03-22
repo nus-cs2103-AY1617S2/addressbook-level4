7dc5bb2264ae88bed847e668072d575a7fc435bb
Ben2\benwong <benwongwf@gmail.com>
2017-03-22 15:12:54 +0800

    add start test

---
+++ src/test/java/seedu/address/model/person/StartTest.java
@@ -0,0 +1,18 @@
+package seedu.address.model.person;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Test;
+
+public class StartTest {
+
+    @Test
+    public void isValidStart() {
+        // blank start
+        assertTrue(Start.isValidStart("")); // empty string
+        assertTrue(Start.isValidStart("3 Mar")); // specific date
+        assertTrue(Start.isValidStart("4 pm")); // specific time
+        assertTrue(Start.isValidStart("tomorrow")); // general word
+    }
+}
