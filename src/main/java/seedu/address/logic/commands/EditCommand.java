befa1f6e884a1d605a7aeac36d8cc30c305584a9
JingRong ( <jing_rong92@hotmail.com>
2017-03-22 23:59:46 +0800

    Fixes for checkstyle

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -116,7 +116,8 @@ public class EditCommand extends Command {
          * Returns true if at least one field is edited.
          */
         public boolean isAnyFieldEdited() {
-            return CollectionUtil.isAnyPresent(this.name, this.start, this.deadline, this.priority, this.notes, this.tags);
+            return CollectionUtil.isAnyPresent(this.name, this.start,
+                    this.deadline, this.priority, this.notes, this.tags);
         }
 
         public void setName(Optional<Name> name) {
