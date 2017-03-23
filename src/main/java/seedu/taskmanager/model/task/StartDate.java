95c3ff655ef3a28f45c22d5c11de790cbf1a3a4c
Gabriel Sim <gabrielsim1993@gmail.com>
2017-03-24 00:06:55 +0800

    edit StartDate

--- src/main/java/seedu/taskmanager/model/task/StartDate.java
+++ src/main/java/seedu/taskmanager/model/task/StartDate.java
@@ -32,16 +32,16 @@ public class StartDate extends Date {
 
     private static long startDateConstructor(String startDate) throws IllegalValueException {
         assert startDate != null;
-        if (!isValidStartDate(startDate)) {
-            throw new IllegalValueException(MESSAGE_STARTDATE_CONSTRAINTS);
-        }
         try {
+            if (!isValidStartDate(startDate)) {
+                throw new IllegalValueException(MESSAGE_STARTDATE_CONSTRAINTS);
+            }
             if (startDate.trim().equals("")) {
                 Calendar cal = Calendar.getInstance();
                 return cal.getTimeInMillis();
             }
             return sdfInput.parse(startDate).getTime();
-        } catch (ParseException e) {
+        } catch (IllegalValueException | ParseException e) {
             throw new IllegalValueException(MESSAGE_STARTDATE_CONSTRAINTS);
         }
     }
