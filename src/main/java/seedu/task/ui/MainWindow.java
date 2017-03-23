9c3bd2b457b5df13cfe2a3467afc56634f38fdaa
Edwin\admin <torres12318@hotmail.com>
2017-03-23 18:07:53 +0800

    test merging

--- src/main/java/seedu/task/ui/MainWindow.java
+++ src/main/java/seedu/task/ui/MainWindow.java
@@ -18,6 +18,8 @@ import seedu.task.logic.Logic;
 import seedu.task.model.UserPrefs;
 import seedu.task.model.task.ReadOnlyTask;
 
+//original master with new updates
+
 /**
  * The Main Window. Provides the basic application layout containing a menu bar
  * and space where other JavaFX elements can be placed.
