59dd190d5ac8617e9df35a13540f63f3343c0dc0
Yi Rui Goh <yirui94@users.noreply.github.com>
2017-03-24 02:54:38 +0800

    Fix checkstyle

--- src/main/java/seedu/geekeep/logic/commands/UpdateCommand.java
+++ src/main/java/seedu/geekeep/logic/commands/UpdateCommand.java
@@ -65,7 +65,7 @@ public class UpdateCommand extends Command {
          * Returns true if at least one field is edited.
          */
         public boolean isAnyFieldEdited() {
-            if(this.startDateTime == null || this.endDateTime == null) {
+            if (this.startDateTime == null || this.endDateTime == null) {
                 return true;
             }
             return CollectionUtil.isAnyPresent(this.title, this.location, this.tags,
