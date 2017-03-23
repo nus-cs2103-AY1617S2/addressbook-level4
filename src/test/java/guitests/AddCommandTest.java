8a9af5b7c8fe981a602ad3883d8261518199b12a
yeejfe <yeejfe@hotmail.com>
2017-03-24 00:54:17 +0800

    test travis

--- src/test/java/guitests/AddCommandTest.java
+++ src/test/java/guitests/AddCommandTest.java
@@ -15,40 +15,54 @@ public class AddCommandTest extends TaskManagerGuiTest {
     @Test
     public void add() {
         //add one task
+        System.err.println("1");
         TestTask[] currentList = td.getTypicalTasks();
         TestTask taskToAdd = td.discussion;
         assertAddSuccess(taskToAdd, currentList);
         currentList = TestUtil.addTasksToList(currentList, taskToAdd);
+        System.err.println("2");
 
         //add another task
         taskToAdd = td.interview;
         assertAddSuccess(taskToAdd, currentList);
         currentList = TestUtil.addTasksToList(currentList, taskToAdd);
+        System.err.println("3");
 
         //add duplicate task
         commandBox.runCommand(td.discussion.getAddCommand());
         assertResultMessage(AddCommand.MESSAGE_DUPLICATE_TASK);
         assertTrue(taskListPanel.isListMatching(currentList));
+        System.err.println("4");
 
         //add to empty list
         commandBox.runCommand("clear");
         assertAddSuccess(td.presentation);
+        System.err.println("5");
 
         //invalid command
         commandBox.runCommand("adds Johnny");
         assertResultMessage(Messages.MESSAGE_UNKNOWN_COMMAND);
+        System.err.println("6");
     }
 
     private void assertAddSuccess(TestTask taskToAdd, TestTask... currentList) {
+
+        System.err.println("a");
         commandBox.runCommand(taskToAdd.getAddCommand());
 
+
+        System.err.println("b");
         //confirm the new card contains the right data
         TaskCardHandle addedCard = taskListPanel.navigateToTask(taskToAdd.getName().name);
         assertMatching(taskToAdd, addedCard);
 
+        System.err.println("c");
+
         //confirm the list now contains all previous tasks plus the new task
         TestTask[] expectedList = TestUtil.addTasksToList(currentList, taskToAdd);
         assertTrue(taskListPanel.isListMatching(expectedList));
+
+        System.err.println("d");
     }
 
 }
