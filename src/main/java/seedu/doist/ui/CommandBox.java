3683f5f4548880a0bf10c42c96e6b81afb2e93a0
LuoYuyang <royl8@outlook.com>
2017-03-23 12:26:25 +0800

    add more colors for parameter; TODO: 1. refactoring-abstract highlight feature to a separate class 2. implement auto complete

--- src/main/java/seedu/doist/ui/CommandBox.java
+++ src/main/java/seedu/doist/ui/CommandBox.java
@@ -66,17 +66,27 @@ public class CommandBox extends UiPart<Region> {
             commandTextField.setStyle(i, i + 1, COMMAND_WORD_STYLE);
             i++;
         }
+        String key = "";
         while (i < content.length()) {
             if (content.charAt(i) == '\\') {
+                StringBuilder keyBuilder = new StringBuilder();
                 while (i < content.length() && content.charAt(i) != ' ') {
                     commandTextField.setStyle(i, i + 1, PARAMETER_KEY_STYLE);
+                    keyBuilder.append(content.charAt(i));
                     i++;
                 }
+                key = keyBuilder.toString();
             }
             if (i >= content.length()) {
                 break;
             }
-            commandTextField.setStyle(i, i + 1, NORMAL_STYLE);
+            if (key.equals("\\by") || key.equals("\\from") || key.equals("\\to")) {
+                commandTextField.setStyle(i, i + 1, "-fx-fill: blue;");
+            } else if (key.equals("\\as")) {
+                commandTextField.setStyle(i, i + 1, "-fx-fill: #cd5c5c;");
+            } else {
+                commandTextField.setStyle(i, i + 1, NORMAL_STYLE);
+            }
             i++;
         }
     }
