d39d2ceee2163640a5acca6695541fa73b9ceac2
Li Chengcheng <lcc199533@gmail.com>
2017-03-23 16:15:29 +0800

    Update SerializableWhatsLeft.java

--- src/main/java/seedu/address/storage/XmlAdaptedEvent.java
+++ src/main/java/seedu/address/storage/XmlAdaptedEvent.java
@@ -9,53 +9,59 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
 import seedu.address.commons.exceptions.IllegalValueException;
-import seedu.address.model.person.Task;
-import seedu.address.model.person.ByDate;
-import seedu.address.model.person.ByTime;
+import seedu.address.model.person.Event;
 import seedu.address.model.person.Description;
+import seedu.address.model.person.EndDate;
+import seedu.address.model.person.EndTime;
 import seedu.address.model.person.Location;
-import seedu.address.model.person.Priority;
-import seedu.address.model.person.ReadOnlyTask;
+import seedu.address.model.person.ReadOnlyEvent;
+import seedu.address.model.person.StartDate;
+import seedu.address.model.person.StartTime;
 import seedu.address.model.tag.Tag;
 import seedu.address.model.tag.UniqueTagList;
 
 /**
- * JAXB-friendly version of the Task.
+ * JAXB-friendly version of the Event.
  */
-public class XmlAdaptedTask {
+public class XmlAdaptedEvent {
 
     @XmlElement(required = true)
     private String description;
-    @XmlElement(required = true)
-    private String priority;
     //@XmlElement(required = true) 
     @XmlJavaTypeAdapter(LocalDateAdapter.class)
-    private LocalDate bydate;
+    private LocalDate startdate;
+    //@XmlElement(required = true)
+    @XmlJavaTypeAdapter(LocalDateAdapter.class)
+    private LocalDate enddate;
     //@XmlElement(required = true)
     @XmlJavaTypeAdapter(LocalTimeAdapter.class)
-    private LocalTime bytime;
+    private LocalTime starttime;
+    //@XmlElement(required = true)
+    @XmlJavaTypeAdapter(LocalTimeAdapter.class)
+    private LocalTime endtime;
     @XmlElement(required = true)
     private String location;
     @XmlElement
     private List<XmlAdaptedTag> tagged = new ArrayList<>();
 
     /**
-     * Constructs an XmlAdaptedTask.
+     * Constructs an XmlAdaptedEvent.
      * This is the no-arg constructor that is required by JAXB.
      */
-    public XmlAdaptedTask() {}
+    public XmlAdaptedEvent() {}
 
 
     /**
-     * Converts a given Task into this class for JAXB use.
+     * Converts a given Event into this class for JAXB use.
      *
-     * @param source future changes to this will not affect the created XmlAdaptedTask
+     * @param source future changes to this will not affect the created XmlAdaptedEvent
      */
-    public XmlAdaptedTask(ReadOnlyTask source) {
+    public XmlAdaptedEvent(ReadOnlyEvent source) {
         description = source.getDescription().toString();
-        priority = source.getPriority().toString();
-        bydate = source.getByDate().getValue();
-        bytime = source.getByTime().getValue();
+        startdate = source.getStartDate().getValue();
+        enddate = source.getEndDate().getValue();
+        starttime = source.getStartTime().getValue();
+        endtime = source.getEndTime().getValue();
         location = source.getLocation().value;
         tagged = new ArrayList<>();
         for (Tag tag : source.getTags()) {
@@ -64,21 +70,22 @@ public class XmlAdaptedTask {
     }
 
     /**
-     * Converts this jaxb-friendly adapted person object into the model's Task object.
+     * Converts this jaxb-friendly adapted person object into the model's Event object.
      *
      * @throws IllegalValueException if there were any data constraints violated in the adapted person
      */
-    public Task toModelType() throws IllegalValueException {
+    public Event toModelType() throws IllegalValueException {
         final List<Tag> personTags = new ArrayList<>();
         for (XmlAdaptedTag tag : tagged) {
             personTags.add(tag.toModelType());
         }
         final Description description = new Description(this.description);
-        final Priority priority = new Priority(this.priority);
-        final ByDate bydate = new ByDate(this.bydate);
-        final ByTime bytime = new ByTime(this.bytime);
+        final StartDate startdate = new StartDate(this.startdate);
+        final EndDate enddate = new EndDate(this.startdate);
+        final StartTime starttime = new StartTime(this.starttime);
+        final EndTime endtime = new EndTime(this.endtime);
         final Location location = new Location(this.location);
         final UniqueTagList tags = new UniqueTagList(personTags);
-        return new Task(description, priority, bydate, bytime, location, tags);
+        return new Event(description, startdate, enddate, starttime, endtime, location, tags);
     }
 }
