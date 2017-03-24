4a34ba83cee951280bdd02fb680ae339accf9ffe
LuoYuyang <royl8@outlook.com>
2017-03-24 16:57:07 +0800

    refactor the implementation of command highlight manager

--- src/main/java/seedu/doist/ui/CommandHighlightManager.java
+++ src/main/java/seedu/doist/ui/CommandHighlightManager.java
@@ -1,10 +1,19 @@
 package seedu.doist.ui;
 
+import static seedu.doist.logic.parser.CliSyntax.PREFIX_AS;
+import static seedu.doist.logic.parser.CliSyntax.PREFIX_BY;
+import static seedu.doist.logic.parser.CliSyntax.PREFIX_FROM;
+import static seedu.doist.logic.parser.CliSyntax.PREFIX_TO;
+import static seedu.doist.logic.parser.CliSyntax.PREFIX_UNDER;
+
 import org.fxmisc.richtext.InlineCssTextArea;
 
 public class CommandHighlightManager {
-    private static final String COMMAND_WORD_STYLE = "-fx-fill: #796891;"; //purple
-    private static final String PARAMETER_KEY_STYLE = "-fx-fill: #55ae47;"; //green
+    private static final String COMMAND_WORD_STYLE = "-fx-fill: #1a75ff;";  // blue"
+    private static final String PARAMETER_KEY_STYLE = "-fx-fill: #55ae47;";  // green
+    private static final String TIME_VALUE_STYLE = "-fx-fill: #e68a00;";  // orange
+    private static final String PRIORITY_VALUE_STYLE = "-fx-fill: #cd5c5c;";  // red
+    private static final String TAGS_STYLE = "-fx-fill: #7300e6;";  // purple
     private static final String NORMAL_STYLE = "-fx-fill: black;";
 
     private static CommandHighlightManager instance;
@@ -36,10 +45,14 @@ public class CommandHighlightManager {
             if (i >= content.length()) {
                 break;
             }
-            if (key.equals("\\by") || key.equals("\\from") || key.equals("\\to")) {
-                commandTextField.setStyle(i, i + 1, "-fx-fill: #3075b4;"); //blue
-            } else if (key.equals("\\as")) {
-                commandTextField.setStyle(i, i + 1, "-fx-fill: #cd5c5c;"); //pink
+            if (key.equals(PREFIX_BY.toString()) ||
+                key.equals(PREFIX_FROM.toString()) ||
+                key.equals(PREFIX_TO.toString())) {
+                commandTextField.setStyle(i, i + 1, TIME_VALUE_STYLE);
+            } else if (key.equals(PREFIX_AS.toString())) {
+                commandTextField.setStyle(i, i + 1, PRIORITY_VALUE_STYLE);
+            } else if (key.equals(PREFIX_UNDER.toString())) {
+                commandTextField.setStyle(i, i + 1, TAGS_STYLE);
             } else {
                 commandTextField.setStyle(i, i + 1, NORMAL_STYLE);
             }
