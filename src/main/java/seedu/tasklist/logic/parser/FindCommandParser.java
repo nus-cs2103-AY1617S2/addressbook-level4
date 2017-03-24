f46058d0bec5f5e11e7ec1e542c4005afdb5ce8c
Lim Jie <limjie1994@gmail.com>
2017-03-24 17:06:28 +0800

    Changed FindCommand and FindCommandParser to allow finding by status

--- src/main/java/seedu/tasklist/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/tasklist/logic/parser/FindCommandParser.java
@@ -2,11 +2,12 @@ package seedu.tasklist.logic.parser;
 
 import static seedu.tasklist.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.tasklist.logic.parser.CliSyntax.KEYWORDS_ARGS_FORMAT;
+import static seedu.tasklist.logic.parser.CliSyntax.PREFIX_STATUS;
 import static seedu.tasklist.logic.parser.CliSyntax.PREFIX_TAG;
 
 import java.util.Arrays;
 import java.util.HashSet;
-import java.util.NoSuchElementException;
+import java.util.Optional;
 import java.util.Set;
 import java.util.regex.Matcher;
 
@@ -19,20 +20,33 @@ import seedu.tasklist.logic.commands.IncorrectCommand;
  */
 public class FindCommandParser {
     private boolean isByTags = false;
+    private boolean isByStatus = false;
+    @SuppressWarnings("serial")
+    private final Set<String> completed = new HashSet<String>() {{ add("completed"); }};
+    @SuppressWarnings("serial")
+    private final Set<String> notCompleted = new HashSet<String>() {{ add("not"); add("completed"); }};
     /**
      * Parses the given {@code String} of arguments in the context of the FindCommand
      * and returns an FindCommand object for execution.
      */
     public Command parse(String args) {
-        ArgumentTokenizer argsTokenizer = new ArgumentTokenizer(PREFIX_TAG);
+        ArgumentTokenizer argsTokenizer = new ArgumentTokenizer(PREFIX_TAG, PREFIX_STATUS);
         argsTokenizer.tokenize(args);
-        Matcher matcher;
-        try {
-            matcher = KEYWORDS_ARGS_FORMAT.matcher(argsTokenizer.getValue(PREFIX_TAG).get().trim());
-            isByTags = true;
-        } catch (NoSuchElementException nsee) {
-            matcher = KEYWORDS_ARGS_FORMAT.matcher(args.trim());
+        Optional<String> keywordsHolder;
+        keywordsHolder = argsTokenizer.getValue(PREFIX_TAG);
+        isByTags = true;
+        if (!keywordsHolder.isPresent()) {
+            keywordsHolder = argsTokenizer.getValue(PREFIX_STATUS);
+            isByStatus = true;
+            isByTags = false;
         }
+        if (!keywordsHolder.isPresent()) {
+            keywordsHolder = Optional.of(args.trim());
+            isByStatus = false;
+            isByTags = false;
+        }
+        assert keywordsHolder.isPresent();
+        Matcher matcher = KEYWORDS_ARGS_FORMAT.matcher(keywordsHolder.get().trim());
         if (!matcher.matches()) {
             return new IncorrectCommand(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
@@ -42,6 +56,14 @@ public class FindCommandParser {
         final Set<String> keywordSet = new HashSet<>(Arrays.asList(keywords));
         if (isByTags) {
             return new FindCommand(keywordSet).isByTags();
+        } else if (isByStatus) {
+            if ((keywordSet.size() > 2 || keywordSet.size() < 1)
+                && (!keywordSet.equals(completed) || !keywordSet.equals(notCompleted))) {
+                return new IncorrectCommand(
+                        String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+            } else {
+                return new FindCommand(keywordSet).isByStatus();
+            }
         } else {
             return new FindCommand(keywordSet);
         }
