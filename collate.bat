bff38ee9641ee23df72291dcbf952c6e7c28f142
leeyh20 <leeyh20@hotmail.com>
2017-03-24 11:28:15 +0800

    New line at end of file

--- collate.bat
+++ collate.bat
@@ -3,3 +3,4 @@ java -jar Collate-TUI.jar collate from src/main to collated/main include java, f
 java -jar Collate-TUI.jar collate from src/test to collated/test include java
 
 java -jar Collate-TUI.jar collate from unused to collated/unused include java, fxml, css
+
