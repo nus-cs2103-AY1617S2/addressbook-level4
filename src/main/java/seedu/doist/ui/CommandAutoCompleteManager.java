a95b75ebc1b6e346c8b9c3c9d8068f7872011cae
LuoYuyang <royl8@outlook.com>
2017-03-24 22:28:36 +0800

    use menu box rather than grey text to display suggestions TODO: NEED REFACTORING, current code is only for trial purpose

--- src/main/java/seedu/doist/ui/CommandAutoCompleteManager.java
+++ src/main/java/seedu/doist/ui/CommandAutoCompleteManager.java
@@ -6,13 +6,14 @@ import java.util.Set;
 
 import org.fxmisc.richtext.InlineCssTextArea;
 
+import javafx.geometry.Point2D;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
 import seedu.doist.model.Model;
 
 public class CommandAutoCompleteManager {
     private static CommandAutoCompleteManager instance;
     private static final String SUGGESTION_STYLE = "-fx-fill: grey;";
-
-    private boolean hasTriggered = false;
 
     // for singleton pattern
     protected static CommandAutoCompleteManager getInstance() {
@@ -24,53 +25,49 @@ public class CommandAutoCompleteManager {
 
     // main function method
     public void suggestCompletion(InlineCssTextArea commandTextField, Model model) {
-        // prevent "recursive" call since this method will trigger change event,
-        // which, again, will call this method
-        if (hasTriggered) {
-            return;
-        }
-
-        hasTriggered = true;
-        removeSuggestionAfterCursor(commandTextField);
-
         String[] words = commandTextField.getText().split(" +");
         String lastWord = words[words.length - 1];
         if (!lastWord.equals("")) {
-            displaySuggestion(commandTextField, lastWord, getSuggestion(lastWord, model));
+            displaySuggestion(commandTextField, getSuggestions(lastWord, model));
         }
-        hasTriggered = false;
     }
 
-    private String getSuggestion(String lastWord, Model model) {
+    private ArrayList<String> getSuggestions(String lastWord, Model model) {
         //TODO: make this method more "powerful"
         // handle different cases (command word, key, search history) differently
         // make better suggestion by using a queue to store history and store the frequency
+
+        ArrayList<String> suggestions = new ArrayList<>();
         List<String> allCommandWords = getAllCommandWords(model);
         for (String commandWord : allCommandWords) {
             if (commandWord.startsWith(lastWord)) {
-                return commandWord;
+                suggestions.add(commandWord);
             }
         }
-        return "";
+        return suggestions;
     }
 
     /**
      * display the suggested text in light grey
      */
-    private void displaySuggestion(InlineCssTextArea commandTextField, String lastWord, String suggestedWord) {
-        if (lastWord == suggestedWord) {
+    private void displaySuggestion(InlineCssTextArea commandTextField, ArrayList<String> suggestedWords) {
+
+        if (commandTextField.getPopupWindow() == null) {
+            commandTextField.setPopupWindow(new ContextMenu());
+        }
+        ContextMenu suggestionList = (ContextMenu) commandTextField.getPopupWindow();
+        commandTextField.setPopupAnchorOffset(new Point2D(-18, 18));
+        suggestionList.getItems().clear();
+        int index = 0;
+        for (String word : suggestedWords) {
+            MenuItem m = new MenuItem(word);
+            suggestionList.getItems().add(index, m);
+            index++;
+        }
+        if (commandTextField.getPopupWindow().isShowing()) {
             return;
         }
-        String remainingWord = suggestedWord.replace(lastWord, "");
-        int previousLength = commandTextField.getLength();
-        commandTextField.appendText(remainingWord);
-        commandTextField.setStyle(previousLength, previousLength + remainingWord.length(), SUGGESTION_STYLE);
-        commandTextField.moveTo(previousLength);
-    }
-
-    private void removeSuggestionAfterCursor(InlineCssTextArea commandTextField) {
-        int cursorPosition = commandTextField.getCaretPosition();
-        commandTextField.deleteText(cursorPosition, commandTextField.getLength());
+        commandTextField.getPopupWindow().show(commandTextField, suggestionList.getAnchorX(), suggestionList.getAnchorY());;;
     }
 
     private List<String> getAllCommandWords(Model model) {
