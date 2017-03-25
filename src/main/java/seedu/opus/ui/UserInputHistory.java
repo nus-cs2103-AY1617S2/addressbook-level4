a2837a773ed84320a72e46397de97157ae5ec008
mynameisyz <yanzhang_shi@outlook.com>
2017-03-26 04:32:51 +0800

    Fix history returning same entry for when pressing up and then down keys

--- src/main/java/seedu/opus/ui/UserInputHistory.java
+++ src/main/java/seedu/opus/ui/UserInputHistory.java
@@ -12,6 +12,7 @@ public class UserInputHistory {
     private LinkedList<String> commandHistory;
     private ListIterator<String> iterator;
     private final Logger logger = LogsCenter.getLogger(UserInputHistory.class);
+    private String current;
 
     public UserInputHistory() {
         this.commandHistory = new LinkedList<String>();
@@ -26,21 +27,36 @@ public class UserInputHistory {
 
     public void resetIterator() {
         iterator = commandHistory.listIterator();
+        current = null;
     }
 
     public Optional<String> getPreviousUserInput() {
-        String previousInput = null;
-        if (iterator.hasNext()) {
-            previousInput = iterator.next();
+        String previousInput;
+        if (!iterator.hasNext()) {
+            return Optional.empty();
         }
-        return Optional.ofNullable(previousInput);
+        previousInput = iterator.next();
+        if (!previousInput.equals(current)) {
+            current = previousInput;
+            return Optional.ofNullable(current);
+        } else {
+            current = iterator.next();
+            return Optional.ofNullable(current);
+        }
     }
 
     public Optional<String> getPrecedingUserInput() {
         String precedingInput = null;
-        if (iterator.hasPrevious()) {
-            precedingInput = iterator.previous();
+        if (!iterator.hasPrevious()) {
+            return Optional.empty();
         }
-        return Optional.ofNullable(precedingInput);
+        precedingInput = iterator.previous();
+        if (!precedingInput.equals(current)) {
+            current = precedingInput;
+            return Optional.ofNullable(current);
+        } else {
+            current = iterator.previous();
+            return Optional.ofNullable(current);
+        }
     }
 }
