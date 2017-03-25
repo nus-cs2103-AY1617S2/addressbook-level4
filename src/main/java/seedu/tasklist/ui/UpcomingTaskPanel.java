a542b2a677f0db83f9ff9f9c713098bc42a04e50
sherinatoh <sherinatohh@gmail.com>
2017-03-26 04:47:23 +0800

    add ListView for today and tomorrow tasklist

--- src/main/java/seedu/tasklist/ui/UpcomingTaskPanel.java
+++ src/main/java/seedu/tasklist/ui/UpcomingTaskPanel.java
@@ -1,6 +1,8 @@
 package seedu.tasklist.ui;
 
 import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Region;
 import seedu.tasklist.model.task.ReadOnlyTask;
@@ -9,10 +11,23 @@ public class UpcomingTaskPanel extends UiPart<Region> {
 
     private static final String FXML = "UpcomingTaskPanel.fxml";
 
+    @FXML
+    private ListView<ReadOnlyTask> todayTaskListView;
+    @FXML
+    private ListView<ReadOnlyTask> tomorrowTaskListView;
+
     public UpcomingTaskPanel(AnchorPane upcomingTaskPlaceholder, ObservableList<ReadOnlyTask> todayTask,
             ObservableList<ReadOnlyTask> tomorrowTask) {
         super(FXML);
-        setTodayConnections(todayTask);
-        setTomorrowConnections(tomorrowTask);
+        setTodayListView(todayTask);
+        setTomorrowListView(tomorrowTask);
+    }
+
+    public void setTodayListView(ObservableList<ReadOnlyTask> todayList) {
+        todayTaskListView.setItems(todayList);
+    }
+
+    public void setTomorrowListView(ObservableList<ReadOnlyTask> tomorrowList) {
+        tomorrowTaskListView.setItems(tomorrowList);
     }
 }
