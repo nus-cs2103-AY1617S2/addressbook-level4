3c0e3d51fedfee7b467324a2645eedd726095b5c
Lok You Tan <e0003133@u.nus.edu>
2017-03-26 03:45:11 +0800

    Remove unused imports

--- src/test/java/seedu/ezdo/logic/parser/ParserUtilTest.java
+++ src/test/java/seedu/ezdo/logic/parser/ParserUtilTest.java
@@ -1,17 +1,10 @@
 package seedu.ezdo.logic.parser;
 
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
+import static org.junit.Assert.assertEquals;
 
 import java.util.Optional;
-import java.util.regex.Matcher;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mockito;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
 
 public class ParserUtilTest {
 
