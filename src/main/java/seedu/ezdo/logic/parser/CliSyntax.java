9c798597e28d216ac7c1d24447c195856c29c3cd
Acer\zhi liang <chenzhiliang94@gmail.com>
2017-03-25 17:16:41 +0800

    Add new prefix options for future opportunities to improve functionalities

--- src/main/java/seedu/ezdo/logic/parser/CliSyntax.java
+++ src/main/java/seedu/ezdo/logic/parser/CliSyntax.java
@@ -14,7 +14,10 @@ public class CliSyntax {
     public static final Prefix PREFIX_STARTDATE = new Prefix("s/");
     public static final Prefix PREFIX_DUEDATE = new Prefix("d/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
-
+    public static final Prefix PREFIX_MONTH = new Prefix("month/");
+    public static final Prefix PREFIX_YEAR = new Prefix("year/");
+    public static final Prefix PREFIX_DAY = new Prefix("day/");
+    
     /* Patterns definitions */
     public static final Pattern KEYWORDS_ARGS_FORMAT =
             Pattern.compile("(?<keywords>\\S+(?:\\s+\\S+)*)"); // one or more keywords separated by whitespace
