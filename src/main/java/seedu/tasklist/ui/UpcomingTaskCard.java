87cb9a1458e9141ba8a6f92ad85df6c5c646b634
sherinatoh <sherinatohh@gmail.com>
2017-03-26 05:43:47 +0800

    add UpcomingTaskCard

---
+++ src/main/java/seedu/tasklist/ui/UpcomingTaskCard.java
@@ -0,0 +1,30 @@
+package seedu.tasklist.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+import seedu.tasklist.model.task.ReadOnlyTask;
+
+public class UpcomingTaskCard extends UiPart<Region> {
+
+    private static final String FXML = "TaskListCard.fxml";
+
+    @FXML
+    private HBox cardPane;
+    @FXML
+    private Label name;
+    @FXML
+    private Label id;
+
+    @FXML
+    private Label startTime;
+    @FXML
+    private Label endTime;
+
+    public UpcomingTaskCard(ReadOnlyTask task, int displayedIndex) {
+        super(FXML);
+        name.setText(task.getName().fullName);
+        id.setText(displayedIndex + ". ");
+    }
+}
