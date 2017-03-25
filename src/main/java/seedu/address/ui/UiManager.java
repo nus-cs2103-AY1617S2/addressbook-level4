7c803d9d155b8766f3c1af78e04245c657df6cf2
LESLIE HO ZONG HONG <e0011875@u.nus.edu.sg>
2017-03-25 15:47:07 +0800

    1) Fix indentation error 2) Remove mainPane from ResultDisplay.java 3) Added startEndOnLY & deadlineOnly HBox to PersonCard.java 4) Added startDate & endDate to PersonCard.java 5) Change MIN_HEIGHT & MIN_WIDTH in MainWindow.java 6) Change Application Icon 7) Disable Setting button 8) Apply RedTheme.css to UiManager

--- src/main/java/seedu/address/ui/UiManager.java
+++ src/main/java/seedu/address/ui/UiManager.java
@@ -26,7 +26,7 @@ import seedu.address.model.UserPrefs;
  */
 public class UiManager extends ComponentManager implements Ui {
     private static final Logger logger = LogsCenter.getLogger(UiManager.class);
-    private static final String ICON_APPLICATION = "/images/address_book_32.png";
+    private static final String ICON_APPLICATION = "/images/iManager_32.png";
     public static final String ALERT_DIALOG_PANE_FIELD_ID = "alertDialogPane";
 
     private Logic logic;
@@ -83,7 +83,7 @@ public class UiManager extends ComponentManager implements Ui {
     private static void showAlertDialogAndWait(Stage owner, AlertType type, String title, String headerText,
                                                String contentText) {
         final Alert alert = new Alert(type);
-        alert.getDialogPane().getStylesheets().add("view/DarkTheme.css");
+        alert.getDialogPane().getStylesheets().add("view/RedTheme.css");
         alert.initOwner(owner);
         alert.setTitle(title);
         alert.setHeaderText(headerText);
