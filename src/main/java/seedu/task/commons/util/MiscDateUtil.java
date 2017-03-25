22c039cf9be2dd5003925f3a880f763960b11032
amon <amon@nandynarwhals.org>
2017-03-25 23:15:07 +0800

    Made the parser accept case insensitive commands.

--- src/main/java/seedu/task/commons/util/MiscDateUtil.java
+++ src/main/java/seedu/task/commons/util/MiscDateUtil.java
@@ -11,6 +11,10 @@ public class MiscDateUtil {
      * Compare two dates with an allowance.
      */
     public static boolean compareDatesFuzzy(Date first, Date second, long allowance) {
+        if (first == null || second == null) {
+            return false;
+        }
+
         if (Math.abs(first.getTime() - second.getTime()) < allowance) {
            return true;
         }
