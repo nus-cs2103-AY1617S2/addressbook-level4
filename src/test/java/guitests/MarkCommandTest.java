ecd22b1bcd19024a7efcd44803c78eed1a296120
Melvin <melvin0719@hotmail.com>
2017-03-25 18:14:33 +0800

    Fix

--- src/test/java/guitests/MarkCommandTest.java
+++ src/test/java/guitests/MarkCommandTest.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
 
 import org.junit.Test;
 
