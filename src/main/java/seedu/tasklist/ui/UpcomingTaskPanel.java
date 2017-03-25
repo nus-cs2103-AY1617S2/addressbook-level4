0cc137cc16bef79102eb2d416faf607eb63246b5
sherinatoh <sherinatohh@gmail.com>
2017-03-26 04:36:42 +0800

    add constructor for UpcomingTaskPanel

--- src/main/java/seedu/tasklist/ui/UpcomingTaskPanel.java
+++ src/main/java/seedu/tasklist/ui/UpcomingTaskPanel.java
@@ -1,7 +1,18 @@
 package seedu.tasklist.ui;
 
+import javafx.collections.ObservableList;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
+import seedu.tasklist.model.task.ReadOnlyTask;
+
 public class UpcomingTaskPanel extends UiPart<Region> {
 
     private static final String FXML = "UpcomingTaskPanel.fxml";
 
+    public UpcomingTaskPanel(AnchorPane upcomingTaskPlaceholder, ObservableList<ReadOnlyTask> todayTask,
+            ObservableList<ReadOnlyTask> tomorrowTask) {
+        super(FXML);
+        setTodayConnections(todayTask);
+        setTomorrowConnections(tomorrowTask);
+    }
 }
