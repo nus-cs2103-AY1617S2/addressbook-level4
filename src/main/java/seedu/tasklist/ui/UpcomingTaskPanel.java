c65cb67cb2c336ccfed0121f9f8b0f677a44dd74
sherinatoh <sherinatohh@gmail.com>
2017-03-26 04:57:42 +0800

    add selection handler

--- src/main/java/seedu/tasklist/ui/UpcomingTaskPanel.java
+++ src/main/java/seedu/tasklist/ui/UpcomingTaskPanel.java
@@ -1,14 +1,18 @@
 package seedu.tasklist.ui;
 
+import java.util.logging.Logger;
+
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Region;
+import seedu.tasklist.commons.core.LogsCenter;
+import seedu.tasklist.commons.events.ui.TaskPanelSelectionChangedEvent;
 import seedu.tasklist.model.task.ReadOnlyTask;
 
 public class UpcomingTaskPanel extends UiPart<Region> {
-
+    private final Logger logger = LogsCenter.getLogger(UpcomingTaskPanel.class);
     private static final String FXML = "UpcomingTaskPanel.fxml";
 
     @FXML
@@ -21,13 +25,39 @@ public class UpcomingTaskPanel extends UiPart<Region> {
         super(FXML);
         setTodayListView(todayTask);
         setTomorrowListView(tomorrowTask);
+        setEventHandlerForSelectionChangeEvent();
     }
 
-    public void setTodayListView(ObservableList<ReadOnlyTask> todayList) {
+    private void setTodayListView(ObservableList<ReadOnlyTask> todayList) {
         todayTaskListView.setItems(todayList);
     }
 
-    public void setTomorrowListView(ObservableList<ReadOnlyTask> tomorrowList) {
+    private void setTomorrowListView(ObservableList<ReadOnlyTask> tomorrowList) {
         tomorrowTaskListView.setItems(tomorrowList);
+    }
+
+    private void setEventHandlerForSelectionChangeEvent() {
+        setEventHandlerForTodaySelectionChangeEvent();
+        setEventHandlerForTomorrowSelectionChangeEvent();
+    }
+
+    private void setEventHandlerForTodaySelectionChangeEvent() {
+        todayTaskListView.getSelectionModel().selectedItemProperty()
+                .addListener((observable, oldValue, newValue) -> {
+                    if (newValue != null) {
+                        logger.fine("Selection in today list panel changed to : '" + newValue + "'");
+                        raise(new TaskPanelSelectionChangedEvent(newValue));
+                    }
+                });
+    }
+
+    private void setEventHandlerForTomorrowSelectionChangeEvent() {
+        tomorrowTaskListView.getSelectionModel().selectedItemProperty()
+                .addListener((observable, oldValue, newValue) -> {
+                    if (newValue != null) {
+                        logger.fine("Selection in tomorrow list panel changed to : '" + newValue + "'");
+                        raise(new TaskPanelSelectionChangedEvent(newValue));
+                    }
+                });
     }
 }
