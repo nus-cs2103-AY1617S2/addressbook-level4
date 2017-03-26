56897f3a29d034c73681aadcaf860443e25de446
Lok You Tan <e0003133@u.nus.edu>
2017-03-26 13:06:27 +0800

    Remove unused imports

--- src/main/java/seedu/ezdo/logic/commands/DoneCommand.java
+++ src/main/java/seedu/ezdo/logic/commands/DoneCommand.java
@@ -7,7 +7,6 @@ import seedu.ezdo.commons.core.UnmodifiableObservableList;
 import seedu.ezdo.logic.commands.exceptions.CommandException;
 import seedu.ezdo.model.todo.ReadOnlyTask;
 import seedu.ezdo.model.todo.Task;
-import seedu.ezdo.model.todo.UniqueTaskList.TaskNotFoundException;
 
 /**
  * Marks a task as identified using its last displayed index from ezDo as done
