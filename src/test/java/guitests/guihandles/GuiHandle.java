e1e309dd618ec67f2f836905b5fc5484a70c5729
Bili Xu <xubili93@gmail.com>
2017-03-26 19:40:43 +0800

    Add method to allow GuiRobot to press tab

--- src/test/java/guitests/guihandles/GuiHandle.java
+++ src/test/java/guitests/guihandles/GuiHandle.java
@@ -70,6 +70,10 @@ public class GuiHandle {
         guiRobot.type(KeyCode.ENTER).sleep(500);
     }
 
+    public void pressTab() {
+        guiRobot.type(KeyCode.TAB).sleep(500);
+    }
+
     protected String getTextFromLabel(String fieldId, Node parentNode) {
         return ((Label) guiRobot.from(parentNode).lookup(fieldId).tryQuery().get()).getText();
     }
