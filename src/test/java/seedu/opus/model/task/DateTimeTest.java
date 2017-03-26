2bc73c60b26cd933a1221b8d1d6521a831b3981c
Bili Xu <xubili93@gmail.com>
2017-03-26 12:38:41 +0800

    Stubbed LocalDateTime for current week tests

--- src/test/java/seedu/opus/model/task/DateTimeTest.java
+++ src/test/java/seedu/opus/model/task/DateTimeTest.java
@@ -5,7 +5,9 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.time.Clock;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.ZoneId;
 
 import org.junit.Test;
@@ -25,36 +27,39 @@ public class DateTimeTest {
 
     @Test
     public void isInCurrentWeek() {
-        LocalDateTime now = LocalDateTime.now();
-        Clock fixedClock = Clock.fixed(now.atZone(ZoneId.systemDefault()).toInstant(), ZoneId.systemDefault());
+        // Stub the time now to be 10:30AM on 2017-03-22, Wednesday.
+        LocalDateTime stubbedNow = LocalDateTime.of(LocalDate.of(2017, 3, 22), LocalTime.of(10, 30));
+        Clock fixedClock = Clock.fixed(stubbedNow.atZone(ZoneId.systemDefault()).toInstant(), ZoneId.systemDefault());
 
         try {
             // Today should be in current week
-            DateTime today = new DateTime(now);
+            DateTime today = new DateTime(stubbedNow);
             today.setClock(fixedClock);
             assertTrue(today.isCurrentWeek());
 
-            // Next week is not in current week
-            DateTime nextWeek = new DateTime(now.plusDays(7));
-            nextWeek.setClock(fixedClock);
-            assertFalse(nextWeek.isCurrentWeek());
-
             // This Sunday should be in current week
-            int daysToSunday = 7 - now.getDayOfWeek().getValue() - 1;
-            DateTime sunday = new DateTime(now.plusDays(daysToSunday));
+            LocalDateTime stubbedSunday = LocalDateTime.of(LocalDate.of(2017, 3, 26), LocalTime.MAX);
+            DateTime sunday = new DateTime(stubbedSunday);
             sunday.setClock(fixedClock);
             assertTrue(sunday.isCurrentWeek());
 
             // Next Monday should not be in current week
-            DateTime nextMonday = new DateTime(now.plusDays(daysToSunday + 1));
+            LocalDateTime stubbedNextMonday = LocalDateTime.of(LocalDate.of(2017, 3, 27), LocalTime.MIDNIGHT);
+            DateTime nextMonday = new DateTime(stubbedNextMonday);
             nextMonday.setClock(fixedClock);
             assertFalse(nextMonday.isCurrentWeek());
 
+            // This Monday should be in current week
+            LocalDateTime stubbedMonday = LocalDateTime.of(LocalDate.of(2017, 3, 20), LocalTime.MIDNIGHT);
+            DateTime monday = new DateTime(stubbedMonday);
+            monday.setClock(fixedClock);
+            assertFalse(monday.isCurrentWeek());
+
             // Last Sunday should not be in current week
-            DateTime lastSunday = new DateTime(now.minusDays(now.getDayOfWeek().getValue()));
+            LocalDateTime stubbedLastSunday = LocalDateTime.of(LocalDate.of(2017, 3, 19), LocalTime.MAX);
+            DateTime lastSunday = new DateTime(stubbedLastSunday);
             lastSunday.setClock(fixedClock);
             assertFalse(lastSunday.isCurrentWeek());
-
         } catch (IllegalValueException e) {
             fail("Exception should not be thrown.");
         }
