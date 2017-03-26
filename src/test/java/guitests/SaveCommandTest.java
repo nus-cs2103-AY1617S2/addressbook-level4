62f6727c5964f060d08c8e1c30e31927bc6a8585
Lok You Tan <e0003133@u.nus.edu>
2017-03-26 22:17:38 +0800

    Add test to SaveCommandTest

--- src/test/java/guitests/SaveCommandTest.java
+++ src/test/java/guitests/SaveCommandTest.java
@@ -1,11 +1,24 @@
 package guitests;
 
+import static org.mockito.Mockito.when;
 import static seedu.ezdo.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
-import org.junit.Test;
+import java.io.File;
+import java.io.IOException;
 
+import org.junit.Test;
+import static org.mockito.Matchers.any;
+
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import seedu.ezdo.commons.util.FileUtil;
 import seedu.ezdo.logic.commands.SaveCommand;
 
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({FileUtil.class})
 public class SaveCommandTest extends EzDoGuiTest {
 
     private final String validDirectory = "./";
@@ -30,4 +43,11 @@ public class SaveCommandTest extends EzDoGuiTest {
         assertResultMessage(String.format(SaveCommand.MESSAGE_DIRECTORY_PATH_DOES_NOT_EXIST));
     }
 
+    @Test
+    public void save_validDirectory_noAdminPermissions_failure() throws Exception {
+        PowerMockito.spy(FileUtil.class);
+        PowerMockito.doThrow(new IOException()).when(FileUtil.class, "createIfMissing", any(File.class));
+        commandBox.runCommand("save " + validDirectory);
+        assertResultMessage(String.format(SaveCommand.MESSAGE_DIRECTORY_PATH_INVALID));
+    }
 }
