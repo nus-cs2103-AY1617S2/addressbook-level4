cec98ec8bdda797c898be2bc0dac519af5e13ac4
Lok You Tan <e0003133@u.nus.edu>
2017-03-26 13:02:12 +0800

    Add UserPrefsTest

---
+++ src/test/java/seedu/ezdo/model/UserPrefsTest.java
@@ -0,0 +1,26 @@
+package seedu.ezdo.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UserPrefsTest {
+
+    @Test
+    public void same_equals_true() {
+        UserPrefs prefs = new UserPrefs();
+        assertTrue(prefs.equals(prefs));
+    }
+
+    @Test
+    public void notUserPrefs_equals_false() {
+        UserPrefs prefs = new UserPrefs();
+        assertFalse(prefs.equals(new Integer(1)));
+    }
+
+    @Test
+    public void same_hashCode() {
+        UserPrefs prefs = new UserPrefs();
+        assertEquals(prefs.hashCode(), prefs.hashCode());
+    }
+}
