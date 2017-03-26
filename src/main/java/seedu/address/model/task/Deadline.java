5f4fb9b8e0f0bb72c2b38429fadb5c2c3e3fceee
Li Hao Tan <LiHaoTan@users.noreply.github.com>
2017-03-26 23:34:09 +0800

    Add past date check for Deadline

--- src/main/java/seedu/address/model/task/Deadline.java
+++ src/main/java/seedu/address/model/task/Deadline.java
@@ -1,19 +1,53 @@
 package seedu.address.model.task;
 
 import java.time.ZonedDateTime;
+import java.util.Objects;
 
+import seedu.address.model.task.exceptions.PastDateTimeException;
+
+//@@author A0140023E
 public class Deadline {
+    public static final String MESSAGE_DEADLINE_CONSTRAINTS = "Deadline should not be in the past";
+
     private ZonedDateTime dateTime;
 
-    public Deadline(ZonedDateTime dateTime) {
+    public Deadline(ZonedDateTime dateTime) throws PastDateTimeException {
         assert dateTime != null;
 
-        // TODO check that it is after today
+        if (dateTime.isBefore(ZonedDateTime.now())) {
+            throw new PastDateTimeException(MESSAGE_DEADLINE_CONSTRAINTS);
+        }
 
         this.dateTime = dateTime;
     }
 
     public ZonedDateTime getValue() {
         return dateTime;
+    }
+
+    @Override
+    public String toString() {
+        return dateTime.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null) {
+            return false;
+        }
+        if (o == this) {
+            return true;
+        }
+        if (this.getClass() != o.getClass()) {
+            return false;
+        }
+        final Deadline other = (Deadline) o;
+
+        return Objects.equals(getValue(), other.getValue());
+    }
+
+    @Override
+    public int hashCode() {
+        return dateTime.hashCode();
     }
 }
