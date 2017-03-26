3dd0c3c38d247ffeb72669470d40e3cbbad35d15
Tan Wei <tanwei0319@gmail.com>
2017-03-27 00:57:00 +0800

    Remove unused imports::TaskBoss

--- src/main/java/seedu/taskboss/model/TaskBoss.java
+++ src/main/java/seedu/taskboss/model/TaskBoss.java
@@ -1,8 +1,6 @@
 package seedu.taskboss.model;
 
-import java.util.Calendar;
 import java.util.Collection;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
