f5fe6a37954263a47f65d924d699f7641dfe7c5a
sherinatoh <sherinatohh@gmail.com>
2017-03-26 09:14:20 +0800

    fixed ui to show today and tomorrow

--- src/main/java/seedu/tasklist/ui/UpcomingTaskPanel.java
+++ src/main/java/seedu/tasklist/ui/UpcomingTaskPanel.java
@@ -7,10 +7,12 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.control.SplitPane;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Region;
 import seedu.tasklist.commons.core.LogsCenter;
 import seedu.tasklist.commons.events.ui.TaskPanelSelectionChangedEvent;
+import seedu.tasklist.commons.util.FxViewUtil;
 import seedu.tasklist.model.task.ReadOnlyTask;
 
 //@@author A0143355J
@@ -29,14 +31,17 @@ public class UpcomingTaskPanel extends UiPart<Region> {
         setTodayListView(todayTask);
         setTomorrowListView(tomorrowTask);
         setEventHandlerForSelectionChangeEvent();
+        addToPlaceholder(upcomingTaskPlaceholder);
     }
 
     private void setTodayListView(ObservableList<ReadOnlyTask> todayList) {
         todayTaskListView.setItems(todayList);
+        todayTaskListView.setCellFactory(listView -> new UpcomingTaskViewCell());
     }
 
     private void setTomorrowListView(ObservableList<ReadOnlyTask> tomorrowList) {
         tomorrowTaskListView.setItems(tomorrowList);
+        tomorrowTaskListView.setCellFactory(listView -> new UpcomingTaskViewCell());
     }
 
     private void setEventHandlerForSelectionChangeEvent() {
@@ -62,6 +67,12 @@ public class UpcomingTaskPanel extends UiPart<Region> {
                         raise(new TaskPanelSelectionChangedEvent(newValue));
                     }
                 });
+    }
+
+    private void addToPlaceholder(AnchorPane placeHolderPane) {
+        SplitPane.setResizableWithParent(placeHolderPane, false);
+        FxViewUtil.applyAnchorBoundaryParameters(getRoot(), 0.0, 0.0, 0.0, 0.0);
+        placeHolderPane.getChildren().add(getRoot());
     }
 
     public void todayScrollTo(int index) {
