20483f8ab11fa910c4aee1cf42fccdcd1fa2a2ab
sherinatoh <sherinatohh@gmail.com>
2017-03-26 09:31:06 +0800

    add time for DeadlineTask and EventTask

--- src/main/java/seedu/tasklist/ui/UpcomingTaskCard.java
+++ src/main/java/seedu/tasklist/ui/UpcomingTaskCard.java
@@ -4,6 +4,11 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
+import seedu.tasklist.model.task.DeadlineTask;
+import seedu.tasklist.model.task.EventTask;
+import seedu.tasklist.model.task.FloatingTask;
+import seedu.tasklist.model.task.ReadOnlyDeadlineTask;
+import seedu.tasklist.model.task.ReadOnlyEventTask;
 import seedu.tasklist.model.task.ReadOnlyTask;
 
 //@@author A0143355J
@@ -27,5 +32,19 @@ public class UpcomingTaskCard extends UiPart<Region> {
         super(FXML);
         name.setText(task.getName().fullName);
         id.setText(displayedIndex + ". ");
+        String taskType = task.getType();
+        switch (taskType) {
+        case FloatingTask.TYPE:
+            startTime.setVisible(false);
+            endTime.setVisible(false);
+            break;
+        case DeadlineTask.TYPE:
+            startTime.setText("Due: ");
+            endTime.setText(((ReadOnlyDeadlineTask) task).getTime());
+            break;
+        case EventTask.TYPE:
+            startTime.setText(((ReadOnlyEventTask) task).getStartTime());
+            endTime.setText(((ReadOnlyEventTask) task).getEndTime());
+        }
     }
 }
