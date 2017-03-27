3e8d22ff9be257d757d6df4526afe72523f3b442
gngster <gjhsmiles@gmail.com>
2017-03-27 20:13:38 +0800

    change scroll to

--- src/main/java/seedu/todolist/commons/events/model/ToDoListChangedEvent.java
+++ src/main/java/seedu/todolist/commons/events/model/ToDoListChangedEvent.java
@@ -8,10 +8,12 @@ public class ToDoListChangedEvent extends BaseEvent {
 
     public final ReadOnlyToDoList data;
     public final int index;
+    public final String typeOfCommand;
 
-    public ToDoListChangedEvent(ReadOnlyToDoList data, int index) {
+    public ToDoListChangedEvent(ReadOnlyToDoList data, int index, String typeOfCommand) {
         this.data = data;
         this.index = index;
+        this.typeOfCommand = typeOfCommand;
     }
 
     @Override
