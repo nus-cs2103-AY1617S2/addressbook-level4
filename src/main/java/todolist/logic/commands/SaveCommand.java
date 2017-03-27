fa7aa18af5de63316883c5f23f6f6830656fa5b4
Jia Yilin <e0007663@u.nus.edu>
2017-03-27 17:12:05 +0800

    format

--- src/main/java/todolist/logic/commands/SaveCommand.java
+++ src/main/java/todolist/logic/commands/SaveCommand.java
@@ -11,51 +11,52 @@ import todolist.model.ReadOnlyToDoList;
 import todolist.storage.Storage;
 import todolist.storage.StorageManager;
 
-/** Save command that saves current data file to a new filepath.
+/**
+ * Save command that saves current data file to a new filepath.
  * 
- * */
+ */
 
 public class SaveCommand extends Command {
-    
+
     private Logger logger = LogsCenter.getLogger(SaveCommand.class.getName());
-    
+
     public static final String COMMAND_WORD = "save";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + "\n" 
-            + "Saves data file to new location specified. "
+    public static final String MESSAGE_USAGE = COMMAND_WORD + "\n" + "Saves data file to new location specified. "
             + "New folders with the file can be auto-created as long as given directory is valid.\n"
-            + "Main Directory will the dafault save location for any valid but unspecifed file path\n"
-            + "Example: " + COMMAND_WORD + " C: /Users/Computin/Desktop/CS2103" + "Take note: No spacing after :\n"
-            + "Parameters: FILEPATH (must be valid)\n"
-            + "Example: " + COMMAND_WORD + " C:/Users/Computing/Desktop/CS2103";
-    
+            + "Main Directory will the dafault save location for any valid but unspecifed file path\n" + "Example: "
+            + COMMAND_WORD + " C: /Users/Computin/Desktop/CS2103" + "Take note: No spacing after :\n"
+            + "Parameters: FILEPATH (must be valid)\n" + "Example: " + COMMAND_WORD
+            + " C:/Users/Computing/Desktop/CS2103";
+
     private static final String MESSAGE_SUCCESS = "Data successfully saved to new location.";
-    private static final String MESSAGE_INVALID_PATH = "Filepath given is invalid. Filepath will be reset to old path." + "\n\n" + MESSAGE_USAGE;
-    
- //   private static Config config;
+    private static final String MESSAGE_INVALID_PATH = "Filepath given is invalid. Filepath will be reset to old path."
+            + "\n\n" + MESSAGE_USAGE;
+
+    // private static Config config;
     private String newStorageFilePath, oldStorageFilePath;
     private ReadOnlyToDoList toDoList;
     private static Storage storage;
-    
+
     public SaveCommand(String newStorageFilePath) {
         this.oldStorageFilePath = Config.getToDoListFilePath();
         logger.info("Old file path: " + oldStorageFilePath);
-        
+
         this.newStorageFilePath = newStorageFilePath.trim().replace("\\", "/") + "/todolist.xml";
         logger.info("New file path: " + this.newStorageFilePath);
-        
+
         setStorage(new StorageManager(Config.getToDoListFilePath(), Config.getUserPrefsFilePath()));
     }
-    
+
     public static void setStorage(Storage s) {
         storage = s;
     }
 
     @Override
     public CommandResult execute() {
-        
+
         toDoList = model.getToDoList();
-        
+
         Config.setToDoListFilePath(newStorageFilePath);
         try {
             storage.saveToDoList(toDoList, newStorageFilePath);
@@ -66,12 +67,13 @@ public class SaveCommand extends Command {
         model.updateFilteredListToShowAll();
         return new CommandResult(MESSAGE_SUCCESS);
     }
-    
-    private void handleInvalidFilePathException() { 
+
+    private void handleInvalidFilePathException() {
         logger.info("Error writing to filepath. Handling data save exception.");
-        
-        Config.setToDoListFilePath(oldStorageFilePath);  //set back to old filepath
-        
+
+        Config.setToDoListFilePath(oldStorageFilePath); // set back to old
+                                                        // filepath
+
         try {
             storage.saveToDoList(toDoList, newStorageFilePath);
         } catch (IOException e) {
--- src/main/java/todolist/logic/parser/SaveCommandParser.java
+++ src/main/java/todolist/logic/parser/SaveCommandParser.java
@@ -4,7 +4,6 @@ import static todolist.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 import todolist.logic.commands.Command;
 import todolist.logic.commands.IncorrectCommand;
 import todolist.logic.commands.SaveCommand;
@@ -15,7 +14,7 @@ import todolist.logic.commands.SaveCommand;
  * Parses arguments in the context of the save command.
  *
  * @param args
- * full command args string
+ *            full command args string
  * @return the prepared command
  * 
  * 
@@ -23,15 +22,16 @@ import todolist.logic.commands.SaveCommand;
 public class SaveCommandParser {
 
     private static final Pattern SAVE_ARGS_FORMAT = Pattern.compile("(?<arguments>.*)");
-    public SaveCommandParser() {}
+
+    public SaveCommandParser() {
+    }
 
     public Command parse(String args) {
         final Matcher matcher = SAVE_ARGS_FORMAT.matcher(args.trim());
-            if (!matcher.matches()) {
-            return new IncorrectCommand(
-                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, SaveCommand.MESSAGE_USAGE));
+        if (!matcher.matches()) {
+            return new IncorrectCommand(String.format(MESSAGE_INVALID_COMMAND_FORMAT, SaveCommand.MESSAGE_USAGE));
         }
-            
+
         return new SaveCommand(args);
     }
 }
