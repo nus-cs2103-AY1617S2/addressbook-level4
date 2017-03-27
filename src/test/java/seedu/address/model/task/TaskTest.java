3ddbc4c7a0bfcff5ec6c974502092ccc9dccbfd2
LoneGunman001 <cwm20071128@gmail.com>
2017-03-28 01:24:42 +0800

    Modify test cases

--- src/test/java/seedu/address/model/task/TaskTest.java
+++ src/test/java/seedu/address/model/task/TaskTest.java
@@ -1,9 +1,11 @@
 //@@author: A0144422R
 package seedu.address.model.task;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -22,6 +24,7 @@ public class TaskTest {
             assertTrue(task.getName().fullName.equals("a floating task"));
             assertTrue(task.getDeadline().isPresent() == false);
             assertTrue(task.getStartingTime().isPresent() == false);
+            assertFalse(task.isToday());
             assertTrue(task.getTags()
                     .equalsOrderInsensitive(new UniqueTagList("tag1", "tag2")));
             assertTrue(!task.isDone());
@@ -30,6 +33,7 @@ public class TaskTest {
             assertTrue(task.isDone());
             task.setToday();
             assertTrue(task.isManualToday());
+            assertTrue(task.getTaskAbsoluteDateTime().equals(""));
         } catch (IllegalValueException e) {
             fail();
         }
@@ -45,6 +49,8 @@ public class TaskTest {
             assertTrue(task.getDeadline().isPresent() == true);
             assertTrue(task.getDeadline().get().getDate().equals(date));
             assertTrue(task.getStartingTime().isPresent() == false);
+            assertTrue(task.isToday());
+            assertTrue(((DeadlineTask) task).isOverdue());
             assertTrue(task.getTags()
                     .equalsOrderInsensitive(new UniqueTagList("tag1", "tag2")));
             assertTrue(!task.isDone());
@@ -53,6 +59,10 @@ public class TaskTest {
             assertTrue(task.isDone());
             task.setToday();
             assertTrue(task.isManualToday());
+            SimpleDateFormat dateFormat = new SimpleDateFormat(
+                    "dd/MM/yyyy HH:mm:ss");
+            assertTrue(task.getTaskAbsoluteDateTime()
+                    .equals("Due: " + dateFormat.format(date)));
         } catch (IllegalValueException e) {
             fail();
         }
@@ -71,6 +81,8 @@ public class TaskTest {
             assertTrue(task.getDeadline().get().getDate().equals(date2));
             assertTrue(task.getStartingTime().isPresent() == true);
             assertTrue(task.getStartingTime().get().getDate().equals(date));
+            assertTrue(task.isToday());
+            assertFalse(((EventTask) task).isOverdue());
             assertTrue(task.getTags()
                     .equalsOrderInsensitive(new UniqueTagList("tag1", "tag2")));
             assertTrue(!task.isDone());
@@ -81,6 +93,21 @@ public class TaskTest {
             assertTrue(task.isManualToday());
         } catch (IllegalValueException e) {
             fail();
+        }
+    }
+
+    @Test
+    public void eventTaskWrongDateTimeTest() throws Exception {
+        try {
+            Date date = DateUtils.truncate(new Date(), Calendar.DAY_OF_MONTH);
+            Date date2 = DateUtils.addDays(date, 1);
+            Task task = new EventTask(new Name("a deadline task"),
+                    new UniqueTagList("tag1", "tag2"), date, date2, false,
+                    false);
+            fail();
+        } catch (IllegalValueException e) {
+            assertTrue(e.getMessage()
+                    .equals("Deadline should be after starting time."));
         }
     }
 }
