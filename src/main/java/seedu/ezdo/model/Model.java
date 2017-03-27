0a58d7d6da289049a9d76f1349fa76549783a979
Acer\zhi liang <chenzhiliang94@gmail.com>
2017-03-27 12:16:12 +0800

    Allow some methods to accept a list of arguments

--- src/main/java/seedu/ezdo/model/Model.java
+++ src/main/java/seedu/ezdo/model/Model.java
@@ -74,8 +74,7 @@ public interface Model {
     void updateFilteredListToShowAll();
 
     /** Updates the filter of the filtered task list to filter by multiple fields*/
-    void updateFilteredTaskList(Set<String> keywords, Optional toMatch1,
-            Optional toMatch2, Optional toMatch3, Set<String> toMatchTag);
+    void updateFilteredTaskList(ArrayList<Object> listToCompare, boolean searchByStartDate, boolean searchByDueDate);
 
     /** Updates the filter of the filtered task list to show done tasks*/
     void updateFilteredDoneList();
