6dff94290741c421b1b94b2c45da6823e7b38419
WUHEYANG\wuheyang0617 <wuheyang0617@gmail.com>
2017-03-28 16:20:00 +0800

    Implemented Undo/Redo feature

---
+++ src/main/java/seedu/task/logic/commands/UndoCommand.java
@@ -0,0 +1,26 @@
+package seedu.task.logic.commands;
+
+import java.util.EmptyStackException;
+
+import seedu.task.logic.commands.exceptions.CommandException;
+
+/**
+ * Undo the previous change to the TaskList
+ */
+public class UndoCommand extends Command {
+
+    public static final String COMMAND_WORD = "undo";
+    public static final String MESSAGE_SUCCESS = "Previous change is undone";
+    public static final String MESSAGE_INVALID_UNDO_COMMAND = "Nothing to undo!";
+
+    @Override
+    public CommandResult execute() throws CommandException {
+        assert model != null;
+        try {
+            model.undo();
+        } catch (EmptyStackException ese) {
+            throw new CommandException(MESSAGE_INVALID_UNDO_COMMAND);
+        }
+        return new CommandResult(MESSAGE_SUCCESS);
+    }
+}
