d0d4514f95e923f5695d694444d67bd9fdc64ea0
Li Heng <liheng6567@gmail.com>
2017-03-28 20:06:14 +0800

    Implement Undo Redo feedback messages Fix 43

--- src/main/java/seedu/onetwodo/model/history/CommandHistoryEntryInterface.java
+++ src/main/java/seedu/onetwodo/model/history/CommandHistoryEntryInterface.java
@@ -1,5 +1,10 @@
 package seedu.onetwodo.model.history;
 
+//@@author A0135739W
+/**
+ * The API of the CommandHistoryEntry.
+ */
 public interface CommandHistoryEntryInterface {
-
+    public String getFeedbackMessage();
+    public String getFeedbackMessageInReverseCommand();
 }
--- src/main/java/seedu/onetwodo/model/history/ToDoListHistory.java
+++ src/main/java/seedu/onetwodo/model/history/ToDoListHistory.java
@@ -1,18 +1,21 @@
 package seedu.onetwodo.model.history;
 
-import seedu.onetwodo.commons.exceptions.EmptyHistoryException;
 import seedu.onetwodo.model.ToDoList;
+import seedu.onetwodo.model.task.ReadOnlyTask;
 
 //@@author A0135739W
 /**
- * The API of the Model component.
+ * The API of the ToDoListHistory.
  */
 public interface ToDoListHistory {
-    void saveAsPreviousToDoList (ToDoList toDoList);
-    void saveAsPreviousToDoListAndClearRedoHistory(ToDoList toDoList);
-    void saveAsNextToDoList(ToDoList toDoList);
-    ToDoList getPreviousToDoList() throws EmptyHistoryException;
-    ToDoList getNextToDoList() throws EmptyHistoryException;
-    boolean isUndoHistoryEmpty();
-    boolean isRedoHistoryEmpty();
+    void saveUndoInformationAndClearRedoHistory(String commandWord, ReadOnlyTask task, ToDoList toDoList);
+    void saveUndoInformationAndClearRedoHistory(String commandWord, ToDoList toDoList);
+    void saveUndoInformation(ToDoList toDoList);
+    void saveRedoInformation(ToDoList toDoList);
+    ToDoList getPreviousToDoList();
+    ToDoList getNextToDoList();
+    String getUndoFeedbackMessageAndTransferToRedo();
+    String getRedoFeedbackMessageAndTransferToUndo();
+    boolean hasUndoHistory();
+    boolean hasRedoHistory();
 }
