3f93018d61a66b3934a2453f88a6d469338695c4
yufeng <e0003228@u.nus.edu>
2017-03-29 01:27:12 +0800

    add import export tests

--- src/test/java/guitests/FileTransferCommandTest.java
+++ src/test/java/guitests/FileTransferCommandTest.java
@@ -1,10 +1,8 @@
 package guitests;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import org.junit.Rule;
@@ -12,16 +10,15 @@ import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 import seedu.onetwodo.MainApp;
-import seedu.onetwodo.commons.exceptions.DataConversionException;
 import seedu.onetwodo.commons.util.FileUtil;
 import seedu.onetwodo.logic.commands.ExportCommand;
 import seedu.onetwodo.logic.commands.ImportCommand;
 import seedu.onetwodo.logic.commands.SaveToCommand;
+import seedu.onetwodo.logic.commands.exceptions.CommandException;
 import seedu.onetwodo.model.ReadOnlyToDoList;
 import seedu.onetwodo.model.ToDoList;
 import seedu.onetwodo.storage.StorageManager;
 import seedu.onetwodo.storage.ToDoListStorage;
-import seedu.onetwodo.storage.XmlToDoListStorage;
 
 //@@author A0139343E
 /**
@@ -39,6 +36,28 @@ public class FileTransferCommandTest extends ToDoListGuiTest {
     
     // SaveToCommand tests
 
+    @Test
+    public void save_correctInput_successResultMessage() throws Exception {
+        resetStorages();
+        
+        StorageManager storageManager = (StorageManager) MainApp.getInstance().getStorage();
+        ToDoListStorage toDoListStorage = storageManager.getToDoListStorage();
+        ReadOnlyToDoList toDoList = toDoListStorage.readToDoList().get();
+
+        // simple saving to another destination
+        commandBox.runCommand("save " + TEST_SAVE_FOLDER);
+        assertResultMessage(String.format(SaveToCommand.MESSAGE_SAVETO_SUCCESS,
+                TEST_SAVE_FOLDER));
+        
+        // overwrite and save
+        commandBox.runCommand("save overwrite " + TEST_SAVE_FOLDER);
+        assertResultMessage(String.format(SaveToCommand.MESSAGE_SAVETO_SUCCESS,
+                TEST_SAVE_FOLDER));
+        
+        ReadOnlyToDoList newToDoList = toDoListStorage.readToDoList().get();
+        assertEquals(newToDoList, new ToDoList(toDoList));
+    }
+    
     public void save_invalidFileName_exceptionThrown() throws Exception {
         resetStorages();
         
@@ -48,9 +67,9 @@ public class FileTransferCommandTest extends ToDoListGuiTest {
     
     public void save_fileAlreadyExist_exceptionThrown() throws Exception {
         resetStorages();
-        commandBox.runCommand(TEST_SAVE_FOLDER);
-        thrown.expect(IOException.class);
-        commandBox.runCommand(TEST_SAVE_FOLDER);
+        commandBox.runCommand("save " + TEST_SAVE_FOLDER);
+        thrown.expect(CommandException.class);
+        commandBox.runCommand("save " + TEST_SAVE_FOLDER);
     }
     
     @Test
@@ -91,42 +110,111 @@ public class FileTransferCommandTest extends ToDoListGuiTest {
         commandBox.runCommand("save SaveToTest");
         assertResultMessage(String.format(SaveToCommand.MESSAGE_SAVETO_FAILURE, 
                 SaveToCommand.MESSAGE_USAGE));
-        
-        // ImportCommand and ExportCommand tests
     }
     
+    // ImportCommand and ExportCommand tests
     
     @Test
-    public void save_correctInput_successResultMessage() throws Exception {
+    public void export_correctInput_successResultMessage() throws Exception {
+        resetStorages();
+        
         StorageManager storageManager = (StorageManager) MainApp.getInstance().getStorage();
         ToDoListStorage toDoListStorage = storageManager.getToDoListStorage();
         ReadOnlyToDoList toDoList = toDoListStorage.readToDoList().get();
-        resetStorages();
 
-        commandBox.runCommand(TEST_SAVE_FOLDER);
-        assertResultMessage(String.format(SaveToCommand.MESSAGE_SAVETO_SUCCESS,
-                TEST_SAVE_FOLDER));
+        // simple saving to another destination
+        commandBox.runCommand("export " + TEST_EXPORT_FOLDER);
+        assertResultMessage(String.format(ExportCommand.MESSAGE_EXPORT_SUCCESS,
+                TEST_EXPORT_FOLDER));
+        
+        // overwrite and save
+        commandBox.runCommand("export overwrite " + TEST_EXPORT_FOLDER);
+        assertResultMessage(String.format(ExportCommand.MESSAGE_EXPORT_SUCCESS,
+                TEST_EXPORT_FOLDER));
         
         ReadOnlyToDoList newToDoList = toDoListStorage.readToDoList().get();
         assertEquals(newToDoList, new ToDoList(toDoList));
     }
     
-    
-    @Test
-    public void save_correctOverwriteInput_successResultMessage() throws Exception {
-        StorageManager storageManager = (StorageManager) MainApp.getInstance().getStorage();
-        ToDoListStorage toDoListStorage = storageManager.getToDoListStorage();
-        ReadOnlyToDoList toDoList = toDoListStorage.readToDoList().get();
+    public void import_invalidFileName_exceptionThrown() throws Exception {
         resetStorages();
-
-        commandBox.runCommand("save overwrite " + TEST_SAVE_FOLDER);
-        assertResultMessage(String.format(SaveToCommand.MESSAGE_SAVETO_SUCCESS,
-                TEST_SAVE_FOLDER));
         
-        ReadOnlyToDoList newToDoList = toDoListStorage.readToDoList().get();
-        assertEquals(newToDoList, new ToDoList(toDoList));
+        thrown.expect(IOException.class);
+        commandBox.runCommand("import test/data/(\"O_O\".xml");
     }
     
+    public void export_invalidFileName_exceptionThrown() throws Exception {
+        resetStorages();
+        
+        thrown.expect(IOException.class);
+        commandBox.runCommand("export test/data/(\"X_X\".xml");
+    }
+    
+    public void export_fileAlreadyExist_exceptionThrown() throws Exception {
+        resetStorages();
+        commandBox.runCommand("export " + TEST_EXPORT_FOLDER);
+        thrown.expect(CommandException.class);
+        commandBox.runCommand("export " + TEST_EXPORT_FOLDER);
+    }
+    
+    @Test
+    public void fileTransfer_incorrectInput_failureResultMessage() {
+        resetStorages();
+        
+        // no file path
+        commandBox.runCommand("export");
+        assertResultMessage(String.format(ExportCommand.MESSAGE_EXPORT_FAILURE, 
+                SaveToCommand.MESSAGE_USAGE));
+        commandBox.runCommand("import");
+        assertResultMessage(String.format(ImportCommand.MESSAGE_IMPORT_FAILURE, 
+                SaveToCommand.MESSAGE_USAGE));
+        
+        // empty string file path
+        commandBox.runCommand("export ");
+        assertResultMessage(String.format(ExportCommand.MESSAGE_EXPORT_FAILURE, 
+                SaveToCommand.MESSAGE_USAGE));
+        commandBox.runCommand("import ");
+        assertResultMessage(String.format(ImportCommand.MESSAGE_IMPORT_FAILURE, 
+                SaveToCommand.MESSAGE_USAGE));
+        
+        // file has no name
+        commandBox.runCommand("export .xml");
+        assertResultMessage(String.format(ExportCommand.MESSAGE_EXPORT_FAILURE, 
+                SaveToCommand.MESSAGE_USAGE));
+        commandBox.runCommand("import .xml");
+        assertResultMessage(String.format(ImportCommand.MESSAGE_IMPORT_FAILURE, 
+                SaveToCommand.MESSAGE_USAGE));
+        
+        // too many arguments
+        commandBox.runCommand("export overwrite ExportTest1.xml ExportTest2.xml");
+        assertResultMessage(String.format(ExportCommand.MESSAGE_EXPORT_FAILURE, 
+                SaveToCommand.MESSAGE_USAGE));
+        commandBox.runCommand("import ImportTest1.xml ImportTest2.xml");
+        assertResultMessage(String.format(ImportCommand.MESSAGE_IMPORT_FAILURE, 
+                SaveToCommand.MESSAGE_USAGE));
+        
+        // not "overwrite" word
+        commandBox.runCommand("export overwrites " + TEST_EXPORT_FOLDER);
+        assertResultMessage(String.format(ExportCommand.MESSAGE_EXPORT_FAILURE, 
+                SaveToCommand.MESSAGE_USAGE));
+        
+        // file type is not xml
+        commandBox.runCommand("export ExportTestFile.html");
+        assertResultMessage(String.format(ExportCommand.MESSAGE_EXPORT_FAILURE, 
+                SaveToCommand.MESSAGE_USAGE));
+        commandBox.runCommand("import ImportTestFile.html");
+        assertResultMessage(String.format(ImportCommand.MESSAGE_IMPORT_FAILURE, 
+                SaveToCommand.MESSAGE_USAGE));
+        
+        // file has no type
+        commandBox.runCommand("export ExportTestFile");
+        assertResultMessage(String.format(ExportCommand.MESSAGE_EXPORT_FAILURE, 
+                SaveToCommand.MESSAGE_USAGE));
+        commandBox.runCommand("import ImportTestFile");
+        assertResultMessage(String.format(ImportCommand.MESSAGE_IMPORT_FAILURE, 
+                SaveToCommand.MESSAGE_USAGE));
+        
+    }
     
     private void resetStorages() {
         
@@ -142,6 +230,5 @@ public class FileTransferCommandTest extends ToDoListGuiTest {
             fileTransferFile.delete();
         }
     }
-    
 
 }
