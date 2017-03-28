450af901a169242c2448fe31065844be9a186214
A0162011A <e0112235@u.nus.edu>
2017-03-28 21:44:28 +0800

    linting

--- src/test/java/seedu/toluist/model/CommandHistoryListTest.java
+++ src/test/java/seedu/toluist/model/CommandHistoryListTest.java
@@ -2,13 +2,7 @@
 package seedu.toluist.model;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 import org.junit.Test;
 
