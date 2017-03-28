33bda9cb5cc09ea42009b66df684666732ae9dde
Daniel Mullen <dmullen3@gatech.edu>
2017-03-27 22:38:21 -0400

    Filepath fcn

---
+++ src/main/java/seedu/task/logic/commands/FilepathCommand.java
@@ -0,0 +1,14 @@
+package seedu.task.logic.commands;
+
+import seedu.task.logic.commands.exceptions.CommandException;
+
+public class FilepathCommand extends Command{
+	public static final String COMMAND_WORD = "filepath";
+
+	@Override
+	public CommandResult execute() throws CommandException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
