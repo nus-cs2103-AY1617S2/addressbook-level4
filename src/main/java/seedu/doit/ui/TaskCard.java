59eccf0485844dd925a482d8dc99e3835124b967
YE HUANHUI <huanhui.ye@gmail.com>
2017-03-29 07:35:37 +0800

    remove a few unused lines in TaskCard.java

--- src/main/java/seedu/doit/ui/TaskCard.java
+++ src/main/java/seedu/doit/ui/TaskCard.java
@@ -2,7 +2,6 @@ package seedu.doit.ui;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
@@ -38,16 +37,13 @@ public class TaskCard extends UiPart<Region> {
         super(FXML);
         this.name.setText(task.getName().fullName);
         this.id.setText(displayedIndex + ". ");
-        //this.labelBullet = new Circle(4.0);
         switch(task.getPriority().value) {
         case "high": this.labelBullet.setFill(Color.RED); break;
         case "med": this.labelBullet.setFill(Color.ORANGE); break;
         case "low": this.labelBullet.setFill(Color.GREEN); break;
-        default: this.labelBullet.setFill(Color.WHITE);
         }
         this.description.setText(task.getDescription().value);
         initTags(task);
-
         if (task.hasStartTime()) {
             this.deadline.setText(task.getStartTime().value + " - " + task.getDeadline().value);
             if (ifOverdue(task.getDeadline().value)) {
@@ -65,20 +61,14 @@ public class TaskCard extends UiPart<Region> {
 
     public boolean ifOverdue(String dateTime) {
         LocalDateTime currentDateTime = LocalDateTime.now();
-        if (dateTime.equals("")) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yy HH:mm");
+        LocalDateTime dateTimeToCompare = LocalDateTime.parse(dateTime, formatter);
+        if (currentDateTime.isBefore(dateTimeToCompare)) {
             return false;
+        } else {
+            return true;
         }
-        try {
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yy HH:mm");
-            LocalDateTime dateTimeToCompare = LocalDateTime.parse(dateTime, formatter);
-            if (currentDateTime.isBefore(dateTimeToCompare)) {
-                return false;
-            } else {
-                return true;
-            }
-        } catch (DateTimeParseException exc) {
-            throw exc;
-        }
+
     }
 
     private void initTags(ReadOnlyTask task) {
