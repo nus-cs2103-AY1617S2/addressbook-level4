29d17c8547de1ef28c8a6c40a4844ed56968de50
KenWhy-PC\KenWhy <e0003284@u.nus.edu>
2017-03-28 23:13:45 +0800

    Updated SortCommandTest

--- src/test/java/guitests/SortCommandTest.java
+++ src/test/java/guitests/SortCommandTest.java
@@ -7,18 +7,22 @@ import java.util.Comparator;
 
 import org.junit.Test;
 
+import seedu.doit.commons.exceptions.IllegalValueException;
 import seedu.doit.logic.commands.SortCommand;
 import seedu.doit.model.comparators.EndTimeComparator;
 import seedu.doit.model.comparators.PriorityComparator;
 import seedu.doit.model.comparators.StartTimeComparator;
 import seedu.doit.model.comparators.TaskNameComparator;
 import seedu.doit.model.item.ReadOnlyTask;
+import seedu.doit.testutil.TaskBuilder;
 import seedu.doit.testutil.TestTask;
 
 public class SortCommandTest extends TaskManagerGuiTest {
 
+    public static final int INDEX_TEST_MARK_VALID = 2;
+
     @Test
-    public void sort() {
+    public void sort() throws IllegalValueException {
 
         // check default sorting by name
         TestTask[] currentList = td.getTypicalTasks();
@@ -26,22 +30,23 @@ public class SortCommandTest extends TaskManagerGuiTest {
         assertSortSuccess("priority", currentList);
         assertSortSuccess("start time", currentList);
         assertSortSuccess("end time", currentList);
-        assertSortSuccess("end time", currentList);
 
         // invalid sort command
         this.commandBox.runCommand("sort invalid");
         assertResultMessage(String.format(MESSAGE_INVALID_COMMAND_FORMAT, SortCommand.MESSAGE_USAGE));
 
         // check sort with done tasks
+        commandBox.runCommand("mark " + INDEX_TEST_MARK_VALID);
 
+        TestTask[] expectedTasksList = currentList;
+        TestTask taskToMark = expectedTasksList[INDEX_TEST_MARK_VALID - 1];
+        TestTask markedTask = new TaskBuilder(taskToMark).withIsDone(true).build();
+        expectedTasksList[INDEX_TEST_MARK_VALID - 1] = markedTask;
 
-        assertSortSuccess("name", currentList);
-        assertSortSuccess("priority", currentList);
-        assertSortSuccess("start time", currentList);
-        assertSortSuccess("end time", currentList);
-        assertSortSuccess("end time", currentList);
-
-
+        assertSortSuccess("name", expectedTasksList);
+        assertSortSuccess("priority", expectedTasksList);
+        assertSortSuccess("start time", expectedTasksList);
+        assertSortSuccess("end time", expectedTasksList);
     }
 
     private void assertSortSuccess(String sortType, TestTask... currentList) {
