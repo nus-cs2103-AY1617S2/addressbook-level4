a677e6682dd940adc4015247a48e4dae72071ea2
Amro Shohoud <amro@188-145.priv23.nus.edu.sg>
2017-03-29 20:00:49 +0800

    change t/ to tag

--- src/main/java/seedu/taskit/logic/parser/CliSyntax.java
+++ src/main/java/seedu/taskit/logic/parser/CliSyntax.java
@@ -10,7 +10,7 @@ import seedu.taskit.logic.parser.ArgumentTokenizer.Prefix;
 public class CliSyntax {
 
     /* Prefix definitions */
-    public static final Prefix PREFIX_TAG = new Prefix("t/");
+    public static final Prefix PREFIX_TAG = new Prefix("tag");
     public static final Prefix PREFIX_BY= new Prefix("by");
     public static final Prefix PREFIX_FROM= new Prefix("from");
     public static final Prefix PREFIX_TO= new Prefix("to");
