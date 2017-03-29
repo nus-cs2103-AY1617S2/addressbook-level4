1d64405d67ebdeb58d0b820434a9530e32ee77ed
jay500s <jay500s@hotmail.com>
2017-03-29 21:52:49 +0800

    commented out unfinished code

--- src/main/java/seedu/task/model/ModelManager.java
+++ src/main/java/seedu/task/model/ModelManager.java
@@ -123,19 +123,19 @@ public class ModelManager extends ComponentManager implements Model {
 
     @Override
     public void updateFilteredListToShowAll() {
-        updateListWithRecurringTime();
+        //        updateListWithRecurringTime();
         filteredTasks.setPredicate(null);
     }
 
-    //@@author A0164212U
-    public void updateListWithRecurringTime() {
-        for (ReadOnlyTask task: filteredTasks) {
-            if (task.isRecurring()) {
-
-            }
-        }
-    }
-    //@@author
+    //    //@@author A0164212U
+    //    public void updateListWithRecurringTime() {
+    //        for (ReadOnlyTask task: filteredTasks) {
+    //            if (task.isRecurring()) {
+    //
+    //            }
+    //        }
+    //    }
+    //    //@@author
 
     @Override
     public void updateFilteredTaskList(Set<String> keywords) {
