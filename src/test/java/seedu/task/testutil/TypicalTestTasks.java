a913f315881f0af501c89bfaba715acec639d760
Tyler Rocha <tyler.rocha@utexas.edu>
2017-03-29 20:11:43 +0800

    use default frequency

--- src/test/java/seedu/task/testutil/TypicalTestTasks.java
+++ src/test/java/seedu/task/testutil/TypicalTestTasks.java
@@ -19,49 +19,58 @@ public class TypicalTestTasks {
         try {
             alice = new TaskBuilder().withDescription("Say hello to Alice Pauline")
                     .withOccurrences(new ArrayList<RecurringTaskOccurrence>())
+                    .withFrequency(null)
                     .withStartTiming("01/02/2017")
                     .withEndTiming("02/03/2100")
                     .withPriority("3")
                     .withTags("friends").build();
             benson = new TaskBuilder().withDescription("Say goodbye to Benson Meier")
                     .withOccurrences(new ArrayList<RecurringTaskOccurrence>())
+                    .withFrequency(null)
                     .withStartTiming("01/02/2017")
                     .withEndTiming("02/12/2110")
                     .withPriority("2")
                     .withTags("owesMoney", "friends").build();
             carl = new TaskBuilder().withDescription("Say hi to Carl Kurz")
                     .withOccurrences(new ArrayList<RecurringTaskOccurrence>())
+                    .withFrequency(null)
                     .withStartTiming("01/02/2017")
                     .withEndTiming("02/12/2110")
                     .withPriority("2").build();
             daniel = new TaskBuilder().withDescription("Say farewell to Daniel Meier")
                     .withOccurrences(new ArrayList<RecurringTaskOccurrence>())
+                    .withFrequency(null)
                     .withStartTiming("01/02/1017")
                     .withEndTiming("02/03/2100")
                     .withPriority("3").build();
             elle = new TaskBuilder().withDescription("Say howdy to Elle Meyer")
                     .withOccurrences(new ArrayList<RecurringTaskOccurrence>())
+                    .withFrequency(null)
                     .withStartTiming("01/02/2017")
                     .withEndTiming("02/03/2110")
                     .withPriority("1").build();
             fiona = new TaskBuilder().withDescription("Say so long to Fiona Kunz")
                     .withOccurrences(new ArrayList<RecurringTaskOccurrence>())
+                    .withFrequency(null)
                     .withStartTiming("01/02/2017")
                     .withEndTiming("02/03/2100")
                     .withPriority("1")
                     .withTags("owesMoney", "friends").build();
             george = new TaskBuilder().withDescription("Say greetings to George Best")
                     .withOccurrences(new ArrayList<RecurringTaskOccurrence>())
+                    .withFrequency(null)
                     .withStartTiming("01/02/2017")
                     .withEndTiming("02/03/2100")
                     .withPriority("3").build();
             ida = new TaskBuilder().withDescription("eat pizza")
                     .withOccurrences(new ArrayList<RecurringTaskOccurrence>())
+                    .withFrequency(null)
                     .withStartTiming("01/02/2020")
                     .withEndTiming("02/10/2100")
                     .withPriority("2").build();
             hoon = new TaskBuilder().withDescription("go gym")
                     .withOccurrences(new ArrayList<RecurringTaskOccurrence>())
+                    .withFrequency(null)
                     .withStartTiming("01/02/2020")
                     .withEndTiming("12/10/2100")
                     .withPriority("3").build();
