0b2575b00813a2268e89db2687fde267c90e84f2
sherinatoh <sherinatohh@gmail.com>
2017-03-29 20:17:33 +0800

    right panel ui changes

--- src/main/java/seedu/tasklist/ui/UpcomingTaskCard.java
+++ src/main/java/seedu/tasklist/ui/UpcomingTaskCard.java
@@ -3,6 +3,10 @@ package seedu.tasklist.ui;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Region;
+import seedu.tasklist.model.task.DeadlineTask;
+import seedu.tasklist.model.task.EventTask;
+import seedu.tasklist.model.task.ReadOnlyDeadlineTask;
+import seedu.tasklist.model.task.ReadOnlyEventTask;
 import seedu.tasklist.model.task.ReadOnlyTask;
 
 //@@author A0143355J
@@ -13,8 +17,39 @@ public class UpcomingTaskCard extends UiPart<Region> {
     @FXML
     private Label name;
 
+    @FXML
+    private Label startDate;
+    @FXML
+    private Label endDate;
+    @FXML
+    private Label connector;
+
     public UpcomingTaskCard(ReadOnlyTask task) {
         super(FXML);
+        setName(task);
+        setDate(task);
+    }
+
+    private void setName(ReadOnlyTask task) {
         name.setText(task.getName().fullName);
+    }
+
+    private void setDate(ReadOnlyTask task) {
+        String taskType = task.getType();
+        switch (taskType) {
+        case DeadlineTask.TYPE:
+            startDate.setText("Deadline :");
+            connector.setText("- ");
+            connector.setStyle("-fx-text-fill: white");
+            endDate.setText(((ReadOnlyDeadlineTask) task).getDeadlineStringForUpcomingTask() + " ");
+            break;
+        case EventTask.TYPE:
+            startDate.setText(((ReadOnlyEventTask) task).getStartDateStringForUpcomingTask() + " ");
+            connector.setText("- ");
+            endDate.setText(((ReadOnlyEventTask) task).getEndDateStringForUpcomingTask() + " ");
+            break;
+        default:
+            assert false;
+        }
     }
 }
