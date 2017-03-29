52532c935ce823058ac39e1e4709694b8f0b1c23
bernardyip <bernard_yip40@hotmail.com>
2017-03-30 01:56:54 +0800

    Fix editbooking

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -10,8 +10,8 @@ import seedu.address.logic.parser.ArgumentTokenizer.Prefix;
 public class CliSyntax {
 
     /* Prefix definitions */
-    public static final Prefix PREFIX_BOOK_ADD_DATE = new Prefix(" add ");
-    public static final Prefix PREFIX_BOOK_CHANGE_DATE = new Prefix(" change ");
+    public static final Prefix PREFIX_BOOK_ADD_DATE = new Prefix("add");
+    public static final Prefix PREFIX_BOOK_CHANGE_DATE = new Prefix("change");
     public static final Prefix PREFIX_BOOK_DATE = new Prefix(" on ");
     public static final Prefix PREFIX_BOOK_DATE_DELIMITER = new Prefix(",");
     public static final Prefix PREFIX_BOOK_REMOVE_DATE = new Prefix("remove");
