ec0370a7cc195a9fcfaa93525587ae3cf8f68cf9
HORIZON\riveria94 <timelesschaos94@gmail.com>
2017-03-29 09:34:17 +0800

    Implement Batch delete

--- src/main/java/org/teamstbf/yats/logic/parser/DeleteCommandParser.java
+++ src/main/java/org/teamstbf/yats/logic/parser/DeleteCommandParser.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 import java.util.Optional;
 import java.util.Stack;
 
+import org.teamstbf.yats.logic.commands.BatchDeleteCommand;
 import org.teamstbf.yats.logic.commands.Command;
 import org.teamstbf.yats.logic.commands.DeleteCommand;
 import org.teamstbf.yats.logic.commands.IncorrectCommand;
@@ -16,49 +17,76 @@ import org.teamstbf.yats.logic.commands.IncorrectCommand;
  */
 public class DeleteCommandParser {
 
-    private Stack<String> deleteStack;
+	private Stack<Integer> deleteStack = new Stack<Integer>();
 
-    /**
-     * Parses the given {@code String} of arguments in the context of the
-     * DeleteCommand and returns an DeleteCommand object for execution.
-     */
-    public Command parse(String args) {
+	/**
+	 * Parses the given {@code String} of arguments in the context of the
+	 * DeleteCommand and returns an DeleteCommand object for execution.
+	 */
+	public Command parse(String args) {
 
-	String[] deleteIndexArr = stringTokenizer(args);
-	deleteIndexArr = sortIndexArr(deleteIndexArr);
-	for (String element : deleteIndexArr) {
-	    deleteStack.push(element);
+		String[] deleteIndexArr = stringTokenizer(args);
+		if (deleteIndexArr.length > 1) {
+			if (isAllIntegers(deleteIndexArr)) {
+				deleteIndexArr = sortIndexArr(deleteIndexArr);
+				for (String element : deleteIndexArr) {
+					deleteStack.push(Integer.valueOf(element));
+				}
+				return new BatchDeleteCommand(deleteStack);
+			} else if (!isAllIntegers(deleteIndexArr)) {
+				return new IncorrectCommand(
+						String.format(MESSAGE_INVALID_COMMAND_FORMAT, BatchDeleteCommand.MESSAGE_USAGE));
+			}
+		}
+
+		Optional<Integer> index = ParserUtil.parseIndex(args);
+		if (!index.isPresent()) {
+			return new IncorrectCommand(String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
+		}
+		return new DeleteCommand(index.get());
 	}
 
-	Optional<Integer> index = ParserUtil.parseIndex(args);
-	if (!index.isPresent()) {
-	    return new IncorrectCommand(String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
+	/**
+	 * Returns a {@code String[]} of the command text tokens
+	 *
+	 * @param commandText
+	 */
+	private String[] stringTokenizer(String commandText) {
+		String[] splitText = commandText.trim().split(" ");
+		for (String element : splitText) {
+			element.trim();
+		}
+		return splitText;
 	}
-	return new DeleteCommand(index.get());
-    }
 
-    /**
-     * Returns a {@code String[]} of the command text tokens
-     *
-     * @param commandText
-     */
-    private String[] stringTokenizer(String commandText) {
-	String[] splitText = commandText.split(" ");
-	for (String element : splitText) {
-	    element.trim();
+	private String[] sortIndexArr(String[] stringArray) {
+		int[] intArray = new int[stringArray.length];
+		for (int i = 0; i < stringArray.length; i++) {
+			intArray[i] = Integer.parseInt(stringArray[i]);
+		}
+		Arrays.sort(intArray);
+		for (int i = 0; i < intArray.length; i++) {
+			stringArray[i] = Integer.toString(intArray[i]);
+		}
+		return stringArray;
 	}
-	return splitText;
-    }
 
-    private String[] sortIndexArr(String[] stringArray) {
-	int[] intArray = new int[stringArray.length];
-	for (int i = 0; i < stringArray.length; i++) {
-	    intArray[i] = Integer.parseInt(stringArray[i]);
+	private boolean isAllIntegers(String[] stringArray) {
+		for (int i = 0; i < stringArray.length; i++) {
+			if (!isInteger(stringArray[i])) {
+				return false;
+			}
+		}
+		return true;
 	}
-	Arrays.sort(intArray);
-	for (int i = 0; i < intArray.length; i++) {
-	    stringArray[i] = Integer.toString(intArray[i]);
+
+	private boolean isInteger(String toCheck) {
+		try {
+			Integer.parseInt(toCheck);
+		} catch (NumberFormatException e) {
+			return false;
+		}
+		return true;
 	}
-	return stringArray;
-    }
+
 }
