0ca3e014d360a8bf31bc0d97c53915f13a26c010
apoorva17 <apoorva_ullas@yahoo.com.sg>
2017-03-29 22:23:05 +0800

    Checkstyle fix

--- src/main/java/seedu/onetwodo/logic/parser/FileTransferCommandParser.java
+++ src/main/java/seedu/onetwodo/logic/parser/FileTransferCommandParser.java
@@ -7,10 +7,10 @@ import seedu.onetwodo.logic.commands.SaveToCommand;
 
 //@@author A0139343E
 /**
- * Parses input arguments and creates a new object related to file transfer
- * File transfer objects include save to, export and import
+ * Parses input arguments and creates a new object related to file transfer File
+ * transfer objects include save to, export and import
  */
-public class FileTransferCommandParser { 
+public class FileTransferCommandParser {
 
     public static final String EMPTY_INPUT = "";
     public static final String XML_EXTENSION = ".xml";
@@ -25,23 +25,20 @@ public class FileTransferCommandParser {
         String subString = args.substring(args.length() - 4);
         return subString.equals(XML_EXTENSION);
     }
-    
+
     protected boolean isOverWrittingFormat(String args) {
         return args.equalsIgnoreCase(SaveToCommand.COMMAND_WORD_OVERWRITE);
     }
 
     protected boolean isValidPath(String args) {
-        return  args.length() > XML_EXTENSION.length() &&
-                isXmlFormat(args);
+        return args.length() > XML_EXTENSION.length() && isXmlFormat(args);
     }
 
-//@@author A0139343E-unused
-/*    
-    protected boolean hasInvalidName(String args) {
-        String subString = args.substring(0,  args.length() - 4);
-        Pattern pattern = Pattern.compile("INVALID_FILE_NAME_REGEX");
-        Matcher matcher = pattern.matcher(subString);
-        return matcher.find();
-    }
-*/
+    // @@author A0139343E-unused
+    /*
+     * protected boolean hasInvalidName(String args) { String subString =
+     * args.substring(0, args.length() - 4); Pattern pattern =
+     * Pattern.compile("INVALID_FILE_NAME_REGEX"); Matcher matcher =
+     * pattern.matcher(subString); return matcher.find(); }
+     */
 }
