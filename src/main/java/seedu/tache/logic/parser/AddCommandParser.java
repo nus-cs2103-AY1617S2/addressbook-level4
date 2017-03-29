d0af430756a88c44ab4087a549e2adebecb85da0
ang-zy <angzy95@yahoo.com.sg>
2017-03-30 00:21:45 +0800

    Update AddCommandParser to support NLP

--- src/main/java/seedu/tache/logic/parser/AddCommandParser.java
+++ src/main/java/seedu/tache/logic/parser/AddCommandParser.java
@@ -3,6 +3,7 @@ package seedu.tache.logic.parser;
 import static seedu.tache.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.tache.logic.parser.CliSyntax.PARAMETER_DELIMITER;
 
+import java.util.Deque;
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
@@ -11,17 +12,73 @@ import seedu.tache.commons.exceptions.IllegalValueException;
 import seedu.tache.logic.commands.AddCommand;
 import seedu.tache.logic.commands.Command;
 import seedu.tache.logic.commands.IncorrectCommand;
+import seedu.tache.logic.parser.ParserUtil.DateTimeType;
+import seedu.tache.logic.parser.ParserUtil.PossibleDateTime;
 
 /**
  * Parses input arguments and creates a new AddCommand object
  */
 public class AddCommandParser {
 
+    public static final String START_DATE_IDENTIFIER = "from";
+
     /**
      * Parses the given {@code String} of arguments in the context of the AddCommand
      * and returns an AddCommand object for execution.
      */
     public Command parse(String args) {
+        Set<String> tagSet = new HashSet<String>();
+        String[] taskTag = args.split(AddCommand.TAG_SEPARATOR);
+        if (taskTag.length == 0) {
+            return new IncorrectCommand(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
+        } else if (taskTag.length > 1) {
+            for (String tag: taskTag[1].split(" ")) {
+                tagSet.add(tag);
+            }
+        }
+
+        String taskWithoutTags = taskTag[0];
+        Deque<PossibleDateTime> possibleDateTimes = ParserUtil.findDateTime(taskWithoutTags);
+        PossibleDateTime startDateTime = null;
+        PossibleDateTime endDateTime = null;
+        while (!possibleDateTimes.isEmpty()) {
+            PossibleDateTime current = possibleDateTimes.pop();
+            if (current.type == DateTimeType.END && endDateTime == null) {
+                endDateTime = current;
+            } else if (current.type == DateTimeType.START && startDateTime == null) {
+                startDateTime = current;
+            }
+        }
+        if (endDateTime == null && startDateTime != null) {
+            return new IncorrectCommand(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
+        } else if (endDateTime == null && startDateTime == null) {
+            try {
+                return new AddCommand(taskWithoutTags, Optional.empty(), Optional.empty(), tagSet);
+            } catch (IllegalValueException ex) {
+                return new IncorrectCommand(ex.getMessage());
+            }
+        } else {
+            int startOfDateTimeIndex = endDateTime.startIndex;
+            if (startDateTime != null) {
+                startOfDateTimeIndex = Math.min(startOfDateTimeIndex, startDateTime.startIndex);
+            }
+            String taskName = taskWithoutTags.substring(0, startOfDateTimeIndex);
+            Optional<String> endDateTimeStr = Optional.of(endDateTime.data);
+            Optional<String> startDateTimeStr;
+            if (startDateTime != null) {
+                startDateTimeStr = Optional.of(startDateTime.data);
+            } else {
+                startDateTimeStr = Optional.empty();
+            }
+            try {
+                return new AddCommand(taskName, startDateTimeStr, endDateTimeStr, tagSet);
+            } catch (IllegalValueException ex) {
+                return new IncorrectCommand(ex.getMessage());
+            }
+        }
+    }
+
+    public Command parseStructured(String args) {
         String[] taskFields = args.split(PARAMETER_DELIMITER);
         Set<String> tagSet = new HashSet<String>();
         if (taskFields.length == 0) {
