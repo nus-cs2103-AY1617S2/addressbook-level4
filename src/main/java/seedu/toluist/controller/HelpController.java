64a408f90914cdf00b325cbdca4bad56770f3131
A0162011A <e0112235@u.nus.edu>
2017-03-30 19:10:26 +0800

    add logic for help controller

--- src/main/java/seedu/toluist/controller/HelpController.java
+++ src/main/java/seedu/toluist/controller/HelpController.java
@@ -8,6 +8,7 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 import java.util.logging.Logger;
@@ -23,7 +24,8 @@ public class HelpController extends Controller {
     private static final Logger logger = LogsCenter.getLogger(HelpController.class);
     private static final String MESSAGE_ERROR = "Sorry, that command does not exist.\n"
                                                     + "Please type help for available commands.";
-    private static final String MESSAGE_RESULT = "Displaying Help Screen.";
+    private static final String MESSAGE_RESULT_GENERAL = "Displaying general help.";
+    private static final String MESSAGE_RESULT_SPECIFIC = "Displaying detailed help for %s.";
     private static final String COMMAND_WORD = "help";
     private static final String COMMAND_REGEX = "(?iu)^\\s*help.*";
 
@@ -32,6 +34,10 @@ public class HelpController extends Controller {
 
     private static final int NUMBER_OF_SPLITS_FOR_COMMAND_PARSE = 2;
     private static final String COMMAND_SPLITTER_REGEX = " ";
+
+    private static final String METHOD_BASIC_HELP = "getBasicHelp";
+    private static final String METHOD_DETAILED_HELP = "";
+    private static final String METHOD_CONTROLLER_KEYWORDS = "getCommandWords";
 
     private static final String HELP_DETAILS = "Marks a task to be complete or incomplete.";
     private static final String HELP_FORMAT = "mark [complete/incomplete] INDEX(ES)";
@@ -45,18 +51,54 @@ public class HelpController extends Controller {
         String commandWord = tokens.get(PARAMETER_COMMAND);
         if (commandWord.equals("")) {
             showGeneralHelp();
-            uiStore.setCommandResult(new CommandResult(MESSAGE_RESULT));
+            uiStore.setCommandResult(new CommandResult(MESSAGE_RESULT_GENERAL));
         } else if (getControllerKeywords().contains(commandWord)) {
             showSpecificHelp(commandWord);
-            uiStore.setCommandResult(new CommandResult(MESSAGE_RESULT));
+            uiStore.setCommandResult(new CommandResult(String.format(MESSAGE_RESULT_SPECIFIC, commandWord)));
         } else {
             uiStore.setCommandResult(new CommandResult(MESSAGE_ERROR));
         }
     }
 
     private void showSpecificHelp(String commandWord) {
-        // TODO Auto-generated method stub
+        Class<? extends Controller> controller = findControllerFromKeyword(commandWord);
+      //uiStore.setTasks(controller.getDetailedHelp());
+        getDetailedHelpFromController(controller);
+    }
 
+    private String[][] getDetailedHelpFromController(Class<? extends Controller> controller) {
+        String methodName = METHOD_DETAILED_HELP;
+        String[][] detailedHelp;
+        try {
+            Method method = controller.getMethod(methodName);
+            detailedHelp = (String[][]) method.invoke(null);
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            return null;
+        }
+        return detailedHelp;
+    }
+
+    private Class<? extends Controller> findControllerFromKeyword(String commandWord) {
+        Iterator iterator = getHelpControllerClasses().iterator();
+        Class<? extends Controller> nextController;
+        String[] keywordList;
+        Method method;
+        String methodName = METHOD_CONTROLLER_KEYWORDS;
+        while (iterator.hasNext()) {
+            nextController = (Class<? extends Controller>) iterator.next();
+            try {
+                method = nextController.getMethod(methodName);
+                keywordList = (String[]) method.invoke(null);
+            } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+                keywordList = null;
+            }
+            for (int i = 0; i < keywordList.length; i++) {
+                if (commandWord.equals(keywordList[i])) {
+                    return nextController;
+                }
+            }
+        }
+        return null;
     }
 
     private void showGeneralHelp() {
@@ -119,7 +161,7 @@ public class HelpController extends Controller {
                 .stream()
                 .map((Class<? extends Controller> klass) -> {
                     try {
-                        final String methodName = "getCommandWords";
+                        final String methodName = METHOD_CONTROLLER_KEYWORDS;
                         Method method = klass.getMethod(methodName);
                         return Arrays.asList((String[]) method.invoke(null));
                     } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
@@ -136,7 +178,7 @@ public class HelpController extends Controller {
                 .stream()
                 .map((Class<? extends Controller> klass) -> {
                     try {
-                        final String methodName = "getBasicHelp";
+                        final String methodName = METHOD_BASIC_HELP;
                         Method method = klass.getMethod(methodName);
                         return Arrays.asList((String[]) method.invoke(null));
                     } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
