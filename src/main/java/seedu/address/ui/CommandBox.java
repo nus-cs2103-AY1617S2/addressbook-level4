e9cf2687f19192715104f6303b68d8a5c21c01e9
Nguyen Quang Dung <nqdung14@gmail.com>
2017-03-31 05:55:21 +0800

    Handle Key UP and DOWN when typing input

--- src/main/java/seedu/address/ui/CommandBox.java
+++ src/main/java/seedu/address/ui/CommandBox.java
@@ -1,10 +1,12 @@
 package seedu.address.ui;
 
+import java.util.ArrayList;
 import java.util.logging.Logger;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.SplitPane;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Region;
 import seedu.address.commons.core.LogsCenter;
@@ -18,8 +20,12 @@ public class CommandBox extends UiPart<Region> {
     private final Logger logger = LogsCenter.getLogger(CommandBox.class);
     private static final String FXML = "CommandBox.fxml";
     public static final String ERROR_STYLE_CLASS = "error";
+    private static final String EMPTY_STRING = "";
 
     private final Logic logic;
+
+    private ArrayList<String> history = new ArrayList<String>();
+    private int historyIndex = 0;
 
     @FXML
     private TextField commandTextField;
@@ -27,6 +33,8 @@ public class CommandBox extends UiPart<Region> {
     public CommandBox(AnchorPane commandBoxPlaceholder, Logic logic) {
         super(FXML);
         this.logic = logic;
+        // Preempt history with empty string
+        history.add(EMPTY_STRING);
         addToPlaceholder(commandBoxPlaceholder);
     }
 
@@ -37,24 +45,70 @@ public class CommandBox extends UiPart<Region> {
         FxViewUtil.applyAnchorBoundaryParameters(commandTextField, 0.0, 0.0, 0.0, 0.0);
     }
 
+    //@@author A0144885R
     @FXML
-    private void handleCommandInputChanged() {
+    private void handleKeyReleased(KeyEvent event) {
+        switch (event.getCode()) {
+
+        case UP:
+        case KP_UP:
+            moveUpHistoryStack();
+            break;
+
+        case DOWN:
+        case KP_DOWN:
+            moveDownHistoryStack();
+            break;
+
+        case ENTER:
+            handleInputEntered();
+            break;
+
+        default:
+            break;
+        }
+    }
+
+    private void moveUpHistoryStack() {
+        historyIndex = Math.max(historyIndex - 1, 0);
+        String text = history.get(historyIndex);
+        commandTextField.setText(text);
+        commandTextField.end();
+        logger.info("Key UP pressed: text changed to " + text);
+    }
+
+    private void moveDownHistoryStack() {
+        historyIndex = Math.min(historyIndex + 1, history.size() - 1);
+        String text = history.get(historyIndex);
+        commandTextField.setText(text);
+        commandTextField.end();
+        logger.info("Key DOWN pressed: text changed to " + text);
+    }
+
+    private void handleInputEntered() {
+        // Update history
+        history.set(history.size() - 1, commandTextField.getText());
+
         try {
             CommandResult commandResult = logic.execute(commandTextField.getText());
+            history.add(EMPTY_STRING);
+            historyIndex = history.size() - 1;
 
             // process result of the command
             setStyleToIndicateCommandSuccess();
-            commandTextField.setText("");
+            commandTextField.clear();
             logger.info("Result: " + commandResult.feedbackToUser);
             raise(new NewResultAvailableEvent(commandResult.feedbackToUser));
 
         } catch (CommandException e) {
             // handle command failure
+            historyIndex = history.size() - 1;
             setStyleToIndicateCommandFailure();
             logger.info("Invalid command: " + commandTextField.getText());
             raise(new NewResultAvailableEvent(e.getMessage()));
         }
     }
+    //@@author
 
 
     /**
