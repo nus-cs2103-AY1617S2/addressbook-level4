031be75e9680a6254c782b536f5b7e5849dbb4e0
LENOVO-PC-LYL\Liu yulin <yulinliu668@gmail.com>
2017-03-31 03:18:44 +0800

    add config in MainApp.java

--- src/main/java/seedu/address/MainApp.java
+++ src/main/java/seedu/address/MainApp.java
@@ -1,5 +1,6 @@
 package seedu.address;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Map;
 import java.util.Optional;
@@ -17,6 +18,7 @@ import seedu.address.commons.core.Version;
 import seedu.address.commons.events.ui.ExitAppRequestEvent;
 import seedu.address.commons.exceptions.DataConversionException;
 import seedu.address.commons.util.ConfigUtil;
+import seedu.address.commons.util.FileUtil;
 import seedu.address.commons.util.StringUtil;
 import seedu.address.logic.Logic;
 import seedu.address.logic.LogicManager;
@@ -53,7 +55,8 @@ public class MainApp extends Application {
         super.init();
 
         config = initConfig(getApplicationParameter("config"));
-        storage = new StorageManager(config.getAddressBookFilePath(), config.getUserPrefsFilePath());
+        storage = new StorageManager(config.getAddressBookFilePath(), config.getUserPrefsFilePath(),
+                                     initConfigFilePath(getApplicationParameter("config")));
 
         userPrefs = initPrefs(config);
 
@@ -101,11 +104,10 @@ public class MainApp extends Application {
         Config initializedConfig;
         String configFilePathUsed;
 
-        configFilePathUsed = Config.DEFAULT_CONFIG_FILE;
+        configFilePathUsed = initConfigFilePath(configFilePath);
 
-        if (configFilePath != null) {
+        if (configFilePathUsed.equals(configFilePath)) {
             logger.info("Custom Config file specified " + configFilePath);
-            configFilePathUsed = configFilePath;
         }
 
         logger.info("Using config file : " + configFilePathUsed);
@@ -161,6 +163,18 @@ public class MainApp extends Application {
         EventsCenter.getInstance().registerHandler(this);
     }
 
+    protected String initConfigFilePath(String filePath) {
+        if (filePath == null) {
+            return Config.DEFAULT_CONFIG_FILE;
+        }
+        try {
+            File newFile = new File(filePath);
+            FileUtil.createIfMissing(newFile);
+            return filePath;
+        } catch (IOException e) {
+            return Config.DEFAULT_CONFIG_FILE;
+        }
+    }
     @Override
     public void start(Stage primaryStage) {
         logger.info("Starting AddressBook " + MainApp.VERSION);
