23fe2340ac550b1637205be1a01a66dfbbfa20bd
LENOVO-PC-LYL\Liu yulin <yulinliu668@gmail.com>
2017-03-31 03:19:29 +0800

    improvement on filestatuschanged event

--- src/main/java/seedu/address/commons/events/storage/FileLocationChangedEvent.java
+++ src/main/java/seedu/address/commons/events/storage/FileLocationChangedEvent.java
@@ -1,20 +1,25 @@
 package seedu.address.commons.events.storage;
 
-
 import seedu.address.commons.events.BaseEvent;
+import seedu.address.model.ReadOnlyTaskList;
 
 public class FileLocationChangedEvent extends BaseEvent {
 
     private String filePath;
+    private ReadOnlyTaskList data;
 
-    public FileLocationChangedEvent(String filePath) {
+    public FileLocationChangedEvent(String filePath, ReadOnlyTaskList data) {
         this.filePath = filePath;
+        this.data = data;
     }
 
     public String getFilePath() {
         return filePath;
     }
 
+    public ReadOnlyTaskList getData() {
+        return data;
+    }
 
     @Override
     public String toString() {
