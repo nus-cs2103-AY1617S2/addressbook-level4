4f4a78940a2a6d05ce372e41b35ef06a646952b0
LENOVO-PC-LYL\Liu yulin <yulinliu668@gmail.com>
2017-03-31 06:54:29 +0800

    enhanced exception handling

--- src/main/java/seedu/address/logic/commands/SaveCommand.java
+++ src/main/java/seedu/address/logic/commands/SaveCommand.java
@@ -20,12 +20,11 @@ import seedu.address.storage.XmlTaskListStorage;
 
 public class SaveCommand extends Command {
     public static final String COMMAND_WORD = "save";
-    public static final String MESSAGE_USAGE = COMMAND_WORD + "the path that you wish to save the file to/fileName.xml"
-                                               + "Example: " + COMMAND_WORD + "data/File.xml";
+    public static final String MESSAGE_USAGE = COMMAND_WORD + " the path that you wish to save the file to/fileName.xml"
+                                               + "For example: " + COMMAND_WORD + "data/File.xml";
     public static final String MESSAGE_SUCCESS = "Task list has been saved!";
-    public static final String MESSAGE_WRONG_EXTENSION = "Wrong extension. File must be .xml";
     public static final String MESSAGE_INVALID_FILE_PATH = "Do not have the permission to access the file path "
-                                                     + "chosen" + "or cannot find file." + "Please change file path.";
+                                                     + "chosen or cannot find file." + "Please change file path.";
 
     private String filePath;
 
@@ -35,36 +34,17 @@ public class SaveCommand extends Command {
 
     @Override
     public CommandResult execute() {
-        String message;
-        if (!isXMLExtension(filePath)) {
-            return new CommandResult(MESSAGE_WRONG_EXTENSION);
-        } else {
-            try {
-                File file = new File(filePath);
-                FileUtil.createIfMissing(file);
-                message = String.format(MESSAGE_SUCCESS, filePath);
-                XmlTaskListStorage storage = new XmlTaskListStorage(filePath);
-                storage.saveTaskList(model.getTaskList(), filePath);
-                EventsCenter.getInstance().post(new FileLocationChangedEvent(filePath, model.getTaskList()));
-                return new CommandResult(message);
-            } catch (IOException e) {
-                return new CommandResult(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+        try {
+            File file = new File(filePath);
+            FileUtil.createIfMissing(file);
+            String message = String.format(MESSAGE_SUCCESS, filePath);
+            XmlTaskListStorage storage = new XmlTaskListStorage(filePath);
+            storage.saveTaskList(model.getTaskList(), filePath);
+            EventsCenter.getInstance().post(new FileLocationChangedEvent(filePath, model.getTaskList()));
+            return new CommandResult(message);
+        } catch (IOException e) {
+            return new CommandResult(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
                            SaveCommand.MESSAGE_INVALID_FILE_PATH));
-            }
-        }
-    }
-
-    /*
-     * Check whether the file is .xml extension
-     * @para filePath will be checked
-     * @return true if it has
-     */
-
-    private boolean isXMLExtension(String filepath) {
-        if ("".equals(filepath)) {
-            return false;
-        } else {
-            return filePath.endsWith(".xml");
         }
     }
 
