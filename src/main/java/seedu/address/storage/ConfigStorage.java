5c51b308c88dd17b88cc2de8c1f89c99fed33f66
LENOVO-PC-LYL\Liu yulin <yulinliu668@gmail.com>
2017-03-31 03:22:03 +0800

    configstorage interface

---
+++ src/main/java/seedu/address/storage/ConfigStorage.java
@@ -0,0 +1,11 @@
+package seedu.address.storage;
+
+import java.io.IOException;
+
+import seedu.address.commons.core.Config;
+import seedu.address.commons.exceptions.DataConversionException;
+
+public interface ConfigStorage {
+    Config readConfig() throws DataConversionException, IOException;
+    void saveConfig(Config config) throws IOException;
+}
