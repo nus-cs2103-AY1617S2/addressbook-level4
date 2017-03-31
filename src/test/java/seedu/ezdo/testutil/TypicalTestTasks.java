1cfb9f4d00b8c53ad4c0400afe7869e3771b06c0
Loh Zi Bin Robin <robinloh@u.nus.edu>
2017-04-01 05:01:42 +0800

    Update recur fields in TypicalTestTasks

--- src/test/java/seedu/ezdo/testutil/TypicalTestTasks.java
+++ src/test/java/seedu/ezdo/testutil/TypicalTestTasks.java
@@ -18,25 +18,25 @@ public class TypicalTestTasks {
                     .withPriority("1")
                     .withStartDate("12/12/2016 11:22")
                     .withDueDate("14/03/2017 22:33")
-                    .withRecur("")
+                    .withRecur("daily")
                     .withTags("friends").build();
             benson = new TaskBuilder().withName("Benson Meier")
                     .withPriority("1")
                     .withStartDate("11/11/2015 14:33")
                     .withDueDate("12/02/2017 23:22")
-                    .withRecur("")
+                    .withRecur("weekly")
                     .withTags("owesMoney", "friends").build();
             carl = new TaskBuilder().withName("Carl Kurz")
                     .withPriority("2")
                     .withStartDate("12/12/2013 05:32")
                     .withDueDate("13/03/2014 00:34")
-                    .withRecur("")
+                    .withRecur("monthly")
                     .build();
             daniel = new TaskBuilder().withName("Daniel Meier")
                     .withPriority("2")
                     .withStartDate("12/12/2012 18:22")
                     .withDueDate("14/04/2016 15:17")
-                    .withRecur("")
+                    .withRecur("yearly")
                     .build();
             elle = new TaskBuilder().withName("Elle Meyer")
                     .withPriority("3")
@@ -48,13 +48,13 @@ public class TypicalTestTasks {
                     .withPriority("2")
                     .withStartDate("12/12/2015 14:15")
                     .withDueDate("16/06/2016 17:17")
-                    .withRecur("")
+                    .withRecur("daily")
                     .build();
             george = new TaskBuilder().withName("George Best")
                     .withPriority("3")
                     .withStartDate("02/07/2012 04:55")
                     .withDueDate("17/07/2015 22:22")
-                    .withRecur("")
+                    .withRecur("monthly")
                     .build();
 
             // Manually added
@@ -62,7 +62,7 @@ public class TypicalTestTasks {
                     .withPriority("1")
                     .withStartDate("12/01/2017 11:12")
                     .withDueDate("18/08/2017 17:44")
-                    .withRecur("")
+                    .withRecur("yearly")
                     .build();
             ida = new TaskBuilder().withName("Ida Mueller")
                     .withPriority("2")
@@ -74,19 +74,19 @@ public class TypicalTestTasks {
                     .withPriority("2")
                     .withStartDate("12/1/2013 00:00")
                     .withDueDate("12/09/2015 15:15")
-                    .withRecur("")
+                    .withRecur("daily")
                     .build();
             kappa = new TaskBuilder().withName("Kappa Sushi")
                     .withPriority("")
                     .withStartDate("")
                     .withDueDate("")
-                    .withRecur("")
+                    .withRecur("monthly")
                     .build();
             leroy = new TaskBuilder().withName("Leroy Jenkins")
                     .withPriority("")
                     .withStartDate("")
                     .withDueDate("")
-                    .withRecur("")
+                    .withRecur("yearly")
                     .build();
             megan = new TaskBuilder().withName("Megan Fox")
                     .withPriority("")
