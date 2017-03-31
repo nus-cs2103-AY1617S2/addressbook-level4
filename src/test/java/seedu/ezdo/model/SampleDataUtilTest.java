28313d359f6f0efb077e0dc94c391e20e4df31d4
Lok You Tan <e0003133@u.nus.edu>
2017-04-01 04:30:05 +0800

    Change to JMockit

--- src/test/java/seedu/ezdo/model/SampleDataUtilTest.java
+++ src/test/java/seedu/ezdo/model/SampleDataUtilTest.java
@@ -4,17 +4,15 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
 
+import mockit.Expectations;
+import mockit.integration.junit4.JMockit;
 import seedu.ezdo.commons.exceptions.IllegalValueException;
-import seedu.ezdo.model.todo.Task;
+import seedu.ezdo.model.todo.Name;
 import seedu.ezdo.model.todo.UniqueTaskList.DuplicateTaskException;
 import seedu.ezdo.model.util.SampleDataUtil;
 
-@RunWith(PowerMockRunner.class)
-@PrepareForTest({Task.class, SampleDataUtil.class, EzDo.class})
+@RunWith(JMockit.class)
 public class SampleDataUtilTest {
 
     @Rule
@@ -23,14 +21,19 @@ public class SampleDataUtilTest {
     @Test
     public void getSampleTasks_ive_throwsAssertionError() throws Exception {
         thrown.expect(AssertionError.class);
-        PowerMockito.whenNew(Task.class).withAnyArguments().thenThrow(new IllegalValueException("ive"));
+        new Expectations(Name.class) {{
+            new Name("Buy one cherry fruit"); result = new IllegalValueException("illegal value");
+        }};
+
         SampleDataUtil.getSampleTasks();
     }
 
     @Test
     public void getSampleEzDo_duplicateTask_throwsAssertionError() throws Exception {
         thrown.expect(AssertionError.class);
-        PowerMockito.whenNew(EzDo.class).withAnyArguments().thenThrow(new DuplicateTaskException());
+        new Expectations(EzDo.class) {{
+            new EzDo(); result = new DuplicateTaskException();
+        }};
         SampleDataUtil.getSampleEzDo();
     }
 
