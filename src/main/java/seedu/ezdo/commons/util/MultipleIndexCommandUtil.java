35af5d3c2b6ccd86beb2d36d07fcafda94a0f4f6
Lok You Tan <e0003133@u.nus.edu>
2017-04-01 03:03:25 +0800

    Wrap line

--- src/main/java/seedu/ezdo/commons/util/MultipleIndexCommandUtil.java
+++ src/main/java/seedu/ezdo/commons/util/MultipleIndexCommandUtil.java
@@ -10,7 +10,8 @@ import seedu.ezdo.model.todo.ReadOnlyTask;
  */
 public class MultipleIndexCommandUtil {
     /** checks if the indexes specified are all smaller than the size of the list and not 0 i.e. valid */
-    public static boolean isIndexValid(UnmodifiableObservableList<ReadOnlyTask> lastShownList, ArrayList<Integer> targetIndexes) {
+    public static boolean isIndexValid(UnmodifiableObservableList<ReadOnlyTask> lastShownList,
+            ArrayList<Integer> targetIndexes) {
         return targetIndexes.stream().allMatch(index -> index <= lastShownList.size() && index != 0);
     }
 }
