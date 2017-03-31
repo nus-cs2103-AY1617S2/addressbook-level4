c5d86e5ec5fc52f61581cbf060547bffda85b419
Gabriel Sim <gabrielsim1993@gmail.com>
2017-03-31 09:12:13 +0800

    implement undo

--- src/main/java/seedu/taskmanager/logic/commands/UndoCommand.java
+++ src/main/java/seedu/taskmanager/logic/commands/UndoCommand.java
@@ -1,5 +1,8 @@
 package seedu.taskmanager.logic.commands;
 
+import seedu.taskmanager.logic.commands.exceptions.CommandException;
+import seedu.taskmanager.model.HistoryManager;
+
 public class UndoCommand extends Command {
     public static final String COMMAND_WORD = "undo";
 
@@ -7,7 +10,14 @@ public class UndoCommand extends Command {
     public static final String MESSAGE_NO_MORE_UNDO = "No more actions available to undo";
 
     @Override
-    public CommandResult execute() {
+    public CommandResult execute() throws CommandException {
+        HistoryManager historyManager = HistoryManager.getInstance();
+        try {
+            System.out.println("SENDING OUT UNDO");
+            historyManager.undo();
+        } catch (Exception e) {
+            throw new CommandException(MESSAGE_NO_MORE_UNDO);
+        }
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
