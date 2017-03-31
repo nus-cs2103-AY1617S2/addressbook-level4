b7b0d8be2aa94354d0cca872fea4105cbe10280f
Acer\zhi liang <chenzhiliang94@gmail.com>
2017-04-01 01:56:06 +0800

    Create a class holder for parameters in FindCommandParser (SearchParameters)

--- src/main/java/seedu/ezdo/logic/commands/FindCommand.java
+++ src/main/java/seedu/ezdo/logic/commands/FindCommand.java
@@ -1,6 +1,8 @@
 package seedu.ezdo.logic.commands;
 
 import java.util.ArrayList;
+
+import seedu.ezdo.commons.util.SearchParameters;
 //@@author A0141010L
 /**
  * Finds and lists all tasks in ezDo whose name contains any of the argument
@@ -15,17 +17,15 @@ public class FindCommand extends Command {
             + "the specified keywords (case-sensitive) and displays them as a list with index numbers.\n"
             + "Parameters: KEYWORD [MORE_KEYWORDS]...\n" + "Example: " + COMMAND_WORD + " buy milk clean p/3";
 
-    private final ArrayList<Object> listToCompare;
-    private final ArrayList<Boolean> searchIndicatorList;
+    private final SearchParameters searchParameters;
 
-    public FindCommand(ArrayList<Object> listToCompare, ArrayList<Boolean> searchIndicatorList) {
-        this.listToCompare = listToCompare;
-        this.searchIndicatorList = searchIndicatorList;
+    public FindCommand(SearchParameters searchParameters) {
+        this.searchParameters = searchParameters;
     }
 
     @Override
     public CommandResult execute() {
-        model.updateFilteredTaskList(listToCompare, searchIndicatorList);
+        model.updateFilteredTaskList(searchParameters);
         return new CommandResult(getMessageForTaskListShownSummary(model.getFilteredTaskList().size()));
     }
 
