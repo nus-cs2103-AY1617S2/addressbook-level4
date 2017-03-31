916dfc8d81f591ffbc075d5ecac26682762ff7a7
How Si Wei <howsiwei@protonmail.com>
2017-03-31 11:44:24 +0800

    Rename SetStorage to store

--- src/main/java/seedu/geekeep/logic/commands/StoreCommand.java
+++ src/main/java/seedu/geekeep/logic/commands/StoreCommand.java
@@ -5,13 +5,13 @@ import seedu.geekeep.logic.commands.exceptions.CommandException;
 /**
  * Lists all tasks in GeeKeep to the user.
  */
-public class SetStorageCommand extends Command {
-    public static final String COMMAND_WORD = "setstorage";
+public class StoreCommand extends Command {
+    public static final String COMMAND_WORD = "store";
     public static final String MESSAGE_SUCCESS = "GeeKeep file path successfully set to %1$s";
 
     private String filePath;
 
-    public SetStorageCommand(String filePath) {
+    public StoreCommand(String filePath) {
         this.filePath = filePath;
     }
 
--- src/main/java/seedu/geekeep/logic/parser/Parser.java
+++ src/main/java/seedu/geekeep/logic/parser/Parser.java
@@ -19,7 +19,7 @@ import seedu.geekeep.logic.commands.ListCommand;
 import seedu.geekeep.logic.commands.ListDoneCommand;
 import seedu.geekeep.logic.commands.ListUndoneCommand;
 import seedu.geekeep.logic.commands.RedoCommand;
-import seedu.geekeep.logic.commands.SetStorageCommand;
+import seedu.geekeep.logic.commands.StoreCommand;
 import seedu.geekeep.logic.commands.UndoCommand;
 import seedu.geekeep.logic.commands.UndoneCommand;
 import seedu.geekeep.logic.commands.UpdateCommand;
@@ -92,8 +92,8 @@ public class Parser {
         case HelpCommand.COMMAND_WORD:
             return new HelpCommand();
 
-        case SetStorageCommand.COMMAND_WORD:
-            return new SetStorageCommand(arguments.trim());
+        case StoreCommand.COMMAND_WORD:
+            return new StoreCommand(arguments.trim());
 
         default:
             return new IncorrectCommand(MESSAGE_UNKNOWN_COMMAND);
