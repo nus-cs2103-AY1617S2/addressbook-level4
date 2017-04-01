ceb6e0f25b0cdd41b3e1907cce102485119e0bb5
Lok You Tan <e0003133@u.nus.edu>
2017-04-01 10:10:33 +0800

    Add GuiSettingsTest

---
+++ src/test/java/seedu/ezdo/commons/core/GuiSettingsTest.java
@@ -0,0 +1,21 @@
+package seedu.ezdo.commons.core;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class GuiSettingsTest {
+
+    @Test
+    public void equals_same_true() {
+        GuiSettings setting = new GuiSettings();
+        assertTrue(setting.equals(setting));
+    }
+
+    @Test
+    public void equals_oneNull_false() {
+        GuiSettings setting = new GuiSettings();
+        assertFalse(setting.equals(null));
+    }
+}
