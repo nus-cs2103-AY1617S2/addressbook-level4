cf049913855cd2138480b74d84849d2857575ff0
Jun Wei <Jun Wei>
2017-04-02 22:21:04 +0800

    Remove trailing whitespace

--- src/main/java/typetask/model/ModelManager.java
+++ src/main/java/typetask/model/ModelManager.java
@@ -295,7 +295,8 @@ public class ModelManager extends ComponentManager implements Model {
                 isEventDueToday = (taskStartDateCalendar.before(today) ||
                         (today.get(Calendar.YEAR) == taskStartDateCalendar.get(Calendar.YEAR) &&
                         today.get(Calendar.DAY_OF_YEAR) == taskStartDateCalendar.get(Calendar.DAY_OF_YEAR))) &&
-                        (taskEndDateCalendar.after(today) || (today.get(Calendar.YEAR) == taskEndDateCalendar.get(Calendar.YEAR) &&
+                        (taskEndDateCalendar.after(today) ||
+                                (today.get(Calendar.YEAR) == taskEndDateCalendar.get(Calendar.YEAR) &&
                         today.get(Calendar.DAY_OF_YEAR) == taskEndDateCalendar.get(Calendar.DAY_OF_YEAR)));
             }
 
