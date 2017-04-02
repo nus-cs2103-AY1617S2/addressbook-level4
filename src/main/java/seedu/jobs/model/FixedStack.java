35e3f4c8411a75252e68081eb5de87b1f47ca9b5
Yue Deng <yue0068@gmail.com>
2017-04-02 16:45:28 +0800

    Add comment for FixedStack

--- src/main/java/seedu/jobs/model/FixedStack.java
+++ src/main/java/seedu/jobs/model/FixedStack.java
@@ -22,6 +22,8 @@ public class FixedStack<T> extends Stack<T> {
         this.maxSize = size;
     }
 
+    // Fixed stack has fixed size which only stores the latest data.
+    // The older date will be removed from fixed stack.
     @Override
     public T push(T object) {
         while (this.size() >= maxSize) {
