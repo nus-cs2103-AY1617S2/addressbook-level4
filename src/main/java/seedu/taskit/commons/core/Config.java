ba6c562cf015583ca832bf020caf06b3c73f4978
Fanyi <zfy926@gmail.com>
2017-04-02 18:45:45 +0800

    rename addressbook to TaskManger to pass travis

--- src/main/java/seedu/taskit/commons/core/Config.java
+++ src/main/java/seedu/taskit/commons/core/Config.java
@@ -89,7 +89,7 @@ public class Config {
         sb.append("\nCurrent log level : " + logLevel);
         sb.append("\nPreference file Location : " + userPrefsFilePath);
         sb.append("\nLocal data file location : " + taskManagerFilePath);
-        sb.append("\nAddressBook name : " + taskManagerName);
+        sb.append("\nTaskManager name : " + taskManagerName);
         return sb.toString();
     }
 
