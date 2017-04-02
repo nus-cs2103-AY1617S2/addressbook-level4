17e3c1c5f935e6723edec278e2c8a1b5a46fbd33
How Si Wei <howsiwei@protonmail.com>
2017-04-02 12:35:40 +0800

    Show command redid

--- src/main/java/seedu/geekeep/logic/commands/RedoCommand.java
+++ src/main/java/seedu/geekeep/logic/commands/RedoCommand.java
@@ -11,14 +11,14 @@ public class RedoCommand extends Command {
 
     public static final String COMMAND_WORD = "redo";
 
-    public static final String MESSAGE_SUCCESS = "Redid previous undid command";
+    public static final String MESSAGE_SUCCESS = "Redid previous undid command: %1$s";
     public static final String MESSAGE_NOTHING_TO_REDO = "Nothing to redo";
 
     @Override
     public CommandResult execute() throws CommandException {
         try {
-            model.redo();
-            return new CommandResult(MESSAGE_SUCCESS);
+            String commandTextredid = model.redo();
+            return new CommandResult(String.format(MESSAGE_SUCCESS, commandTextredid));
         } catch (NothingToRedoException e) {
             throw new CommandException(MESSAGE_NOTHING_TO_REDO);
         }
