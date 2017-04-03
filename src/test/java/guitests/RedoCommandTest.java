6825a22eb79457c5ea9ffff8e6ff3627e4d5d14c
Zhuang Lei <e0003940@u.nus.edu>
2017-04-03 14:56:17 +0800

    Trying to remove trailing whitespaces

--- src/test/java/guitests/RedoCommandTest.java
+++ src/test/java/guitests/RedoCommandTest.java
@@ -22,30 +22,24 @@ import seedu.jobs.testutil.TestUtil;
 //@@author A0140055W
 
 public class RedoCommandTest extends TaskBookGuiTest {
-	
 	private final Stack<TestTask> testStack = new Stack<TestTask>();
 	//private final FixedStack<ObservableList<Task>> testUndoStack = UniqueTaskList.getUndoStack();
 	@Test
 	public void redo() throws IllegalArgumentException, IllegalTimeException, EmptyStackException {
-		
 		commandBox.runCommand("redo");
 		assertResultMessage(MESSAGE_FAILURE);
-		
 		TestTask[] currentList = td.getTypicalTasks();		
 		// add a task for undoing to enable redoing
 		TestTask taskToAdd = td.CS4101;
 		commandBox.runCommand(td.CS4101.getAddCommand());
 		currentList = TestUtil.addTasksToList(currentList, taskToAdd);
-		
 		undoForRedo(currentList);
 		assertRedoSuccess(currentList);
-		
 		undoForRedo(currentList);
 		// add another task
 		taskToAdd = td.CS4102;
 		commandBox.runCommand(td.CS4102.getAddCommand());
 		currentList = TestUtil.addTasksToList(currentList, taskToAdd);
-		
 		assertRedoSuccess(currentList);
 		assertRedoSuccess(currentList);
 	}
@@ -59,17 +53,14 @@ public class RedoCommandTest extends TaskBookGuiTest {
 	
 	private void assertRedoSuccess(TestTask[] currentList) 
 			throws IllegalArgumentException, IllegalTimeException, EmptyStackException {
-		
 		TestTask taskToBeRedone = testStack.pop();
 		//commandBox.runCommand(taskToBeRedone.toString());
 		TestTask[] expectedResult = TestUtil.addTasksToList(currentList, taskToBeRedone);
-		
 		commandBox.runCommand("redo");
 		//commandBox.runCommand("current:"+currentList[currentList.length-1].toString());
 		//commandBox.runCommand("expected:"+expectedResult[currentList.length-1].toString());
 		// confirm the resultant list after redoing matches the original
 		assertTrue(taskListPanel.isListMatching(expectedResult));
-		
 		// confirm that the result message is correct
         assertResultMessage(String.format(MESSAGE_SUCCESS, taskToBeRedone));
 	}
