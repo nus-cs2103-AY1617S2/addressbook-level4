754e6dc7d76fddb784193716ce0317d201aa7136
Loh Zi Bin Robin <robinloh@u.nus.edu>
2017-04-03 14:27:04 +0800

    Extract tests for undone into UndoneCommandTest.java

--- src/test/java/seedu/ezdo/testutil/TypicalTestTasks.java
+++ src/test/java/seedu/ezdo/testutil/TypicalTestTasks.java
@@ -11,6 +11,7 @@ import seedu.ezdo.model.todo.UniqueTaskList;
 public class TypicalTestTasks {
 
     public TestTask alice, benson, carl, daniel, elle, fiona, george, hoon, ida, jack, kappa, leroy, megan;
+    public TestTask alice2, benson2, carl2, daniel2, elle2, fiona2, george2;
 
     public TypicalTestTasks() {
         try {
@@ -56,7 +57,48 @@ public class TypicalTestTasks {
                     .withDueDate("17/07/2015 22:22")
                     .withRecur("daily")
                     .build();
-
+            alice2 = new TaskBuilder().withName("Alice Pauline")
+                    .withPriority("1")
+                    .withStartDate("12/12/2016 11:22")
+                    .withDueDate("14/03/2017 22:33")
+                    .withRecur("")
+                    .withTags("friends").build();
+            benson2 = new TaskBuilder().withName("Benson Meier")
+                    .withPriority("")
+                    .withStartDate("11/11/2015 14:33")
+                    .withDueDate("12/02/2017 23:22")
+                    .withRecur("")
+                    .withTags("owesMoney", "friends").build();
+            carl2 = new TaskBuilder().withName("Carl Kurz")
+                    .withPriority("2")
+                    .withStartDate("12/12/2013 05:32")
+                    .withDueDate("13/03/2014 00:34")
+                    .withRecur("")
+                    .build();
+            daniel2 = new TaskBuilder().withName("Daniel Meier")
+                    .withPriority("2")
+                    .withStartDate("12/12/2012 18:22")
+                    .withDueDate("14/04/2016 15:17")
+                    .withRecur("")
+                    .build();
+            elle2 = new TaskBuilder().withName("Elle Meyer")
+                    .withPriority("3")
+                    .withStartDate("01/03/2010 00:33")
+                    .withDueDate("15/05/2015 20:20")
+                    .withRecur("")
+                    .build();
+            fiona2 = new TaskBuilder().withName("Fiona Kunz")
+                    .withPriority("2")
+                    .withStartDate("12/12/2015 14:15")
+                    .withDueDate("16/06/2016 17:17")
+                    .withRecur("")
+                    .build();
+            george2 = new TaskBuilder().withName("George Best")
+                    .withPriority("3")
+                    .withStartDate("02/07/2012 04:55")
+                    .withDueDate("17/07/2015 22:22")
+                    .withRecur("")
+                    .build();
             // Manually added
             hoon = new TaskBuilder().withName("Hoon Meier")
                     .withPriority("1")
@@ -110,9 +152,23 @@ public class TypicalTestTasks {
             }
         }
     }
+    
+    public static void loadEzDoWithSampleDataNonRecurring(EzDo ez) {
+        for (TestTask task : new TypicalTestTasks().getTypicalNonRecurringTasks()) {
+            try {
+                ez.addTask(new Task(task));
+            } catch (UniqueTaskList.DuplicateTaskException e) {
+                assert false : "not possible";
+            }
+        }
+    }
 
     public TestTask[] getTypicalTasks() {
         return new TestTask[]{alice, benson, carl, daniel, elle, fiona, george};
+    }
+    
+    public TestTask[] getTypicalNonRecurringTasks() {
+        return new TestTask[]{alice2, benson2, carl2, daniel2, elle2, fiona2, george2};
     }
 
     public TestTask[] getTypicalDoneTasks() {
