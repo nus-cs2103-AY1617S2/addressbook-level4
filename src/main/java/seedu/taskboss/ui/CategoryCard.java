9a73f2af748fb15e2064d3aa4faa30626172de30
Tan Wei <tanwei0319@gmail.com>
2017-04-05 07:02:14 +0800

    Change colors of categories

--- src/main/java/seedu/taskboss/ui/CategoryCard.java
+++ src/main/java/seedu/taskboss/ui/CategoryCard.java
@@ -3,6 +3,8 @@ package seedu.taskboss.ui;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Region;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import seedu.taskboss.model.category.Category;
 
 //@@author A0143157J
@@ -16,6 +18,9 @@ public class CategoryCard extends UiPart<Region> {
     @FXML
     private Label categoryTaskCount;
 
+    @FXML
+    private Rectangle taskCountRectangle;
+
     public CategoryCard(Category category, int taskCount) {
         super(FXML);
         initCategories(category, taskCount);
@@ -24,5 +29,8 @@ public class CategoryCard extends UiPart<Region> {
     private void initCategories(Category category, int taskCount) {
         categoryText.setText(category.categoryName);
         categoryTaskCount.setText(Integer.toString(taskCount));
+        if (category.categoryName.equals("Alltasks") || category.categoryName.equals("Done")) {
+            taskCountRectangle.setFill(Color.web("F26177"));
+        }
     }
 }
