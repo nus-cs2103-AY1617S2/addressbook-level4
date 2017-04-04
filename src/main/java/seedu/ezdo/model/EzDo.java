3bbc25160aa3fb480ad78af2c7d8df2d8c234834
Loh Zi Bin Robin <robinloh@u.nus.edu>
2017-04-04 22:22:31 +0800

    Remove unused import in line 14

--- src/main/java/seedu/ezdo/model/EzDo.java
+++ src/main/java/seedu/ezdo/model/EzDo.java
@@ -11,7 +11,6 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 
-import ch.qos.logback.classic.Logger;
 import javafx.collections.ObservableList;
 import seedu.ezdo.commons.core.UnmodifiableObservableList;
 import seedu.ezdo.commons.exceptions.IllegalValueException;
