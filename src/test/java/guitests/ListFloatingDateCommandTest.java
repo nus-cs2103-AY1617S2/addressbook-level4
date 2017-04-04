af22158a57fe887ffbc16bb1cc114d9ba7076e17
walter tay <waltertay4749@gmail.com>
2017-04-04 23:04:51 +0800

    junit test for list floating test

---
+++ src/test/java/guitests/ListFloatingDateCommandTest.java
@@ -0,0 +1,41 @@
+package guitests;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import seedu.task.testutil.TestTask;
+import seedu.task.testutil.TestUtil;
+
+public class ListFloatingDateCommandTest extends TaskManagerGuiTest {
+
+    @Test
+    public void listbyfloatingTask_nonEmptyList() {
+        //add one task that does not contain start and end date, floating task 
+        TestTask[] currentList = td.getTypicalTasks();
+        TestTask taskToAdd = td.queue;
+        commandBox.runCommand(taskToAdd.getAddCommand());
+        currentList = TestUtil.addTasksToList(currentList, taskToAdd);
+        assertListByFloat(currentList);
+        
+    }
+
+
+    @Test
+    public void find_emptyList() {
+        commandBox.runCommand("clear");
+        assertListByFloat(); // no results
+       
+    }
+
+    private void assertListByFloat(TestTask... currentList) {
+
+        commandBox.runCommand("list float");
+        TestTask[] expectedList = TestUtil.filterFloatTasks(currentList);
+        assertListSize(expectedList.length);
+        assertResultMessage(expectedList.length + " floating tasks listed!");
+        assertTrue(taskListPanel.isListMatching(expectedList));
+    }
+
+  
+}
