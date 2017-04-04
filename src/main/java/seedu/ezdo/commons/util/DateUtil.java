44adf10800b3e74a2098ea72bbc1325f081640ec
Lok You Tan <e0003133@u.nus.edu>
2017-04-04 11:21:54 +0800

    Extract variables

--- src/main/java/seedu/ezdo/commons/util/DateUtil.java
+++ src/main/java/seedu/ezdo/commons/util/DateUtil.java
@@ -18,12 +18,16 @@ public class DateUtil {
      */
     public static boolean isTaskDateValid(ReadOnlyTask task) throws ParseException {
         assert task != null;
-        if (task.getStartDate().toString().isEmpty() || task.getDueDate().toString().isEmpty()) {
+        String taskStartDate = task.getStartDate().toString();
+        String taskDueDate = task.getDueDate().toString();
+        final boolean isStartDateMissing = taskStartDate.isEmpty();
+        final boolean isDueDateMissing = taskDueDate.isEmpty();
+        if (isStartDateMissing || isDueDateMissing) {
             return true;
         }
         SimpleDateFormat df = new SimpleDateFormat(DATE_REGEX);
-        Date startDate = df.parse(task.getStartDate().toString());
-        Date dueDate = df.parse(task.getDueDate().toString());
+        Date startDate = df.parse(taskStartDate);
+        Date dueDate = df.parse(taskDueDate);
         return (startDate.compareTo(dueDate) <= 0);
     }
 
