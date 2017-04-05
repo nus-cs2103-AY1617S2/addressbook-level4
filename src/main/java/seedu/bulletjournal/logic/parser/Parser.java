1e533d5c4f27a7b71a3f2e26f62e0b7b8182a950
Wang Yu <stingwangyu@gmail.com>
2017-04-05 12:43:29 +0800

    Minor change

--- src/main/java/seedu/bulletjournal/logic/parser/Parser.java
+++ src/main/java/seedu/bulletjournal/logic/parser/Parser.java
@@ -7,6 +7,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import seedu.bulletjournal.logic.commands.AddCommand;
+import seedu.bulletjournal.logic.commands.ChangeDirectoryCommand;
 import seedu.bulletjournal.logic.commands.ClearCommand;
 import seedu.bulletjournal.logic.commands.Command;
 import seedu.bulletjournal.logic.commands.DeleteCommand;
