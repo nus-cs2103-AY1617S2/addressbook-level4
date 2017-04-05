05d46551e5a9c24c28c4a3687b314eef38b6b0c1
mervynyeezl <mervynyeezl@gmail.com>
2017-04-05 15:57:54 +0800

    Remove browser panel

--- src/main/java/seedu/watodo/ui/UiManager.java
+++ src/main/java/seedu/watodo/ui/UiManager.java
@@ -64,7 +64,6 @@ public class UiManager extends ComponentManager implements Ui {
     public void stop() {
         prefs.updateLastUsedGuiSetting(mainWindow.getCurrentGuiSetting());
         mainWindow.hide();
-        mainWindow.releaseResources();
     }
 
     private void showFileOperationAlertAndWait(String description, String details, Throwable cause) {
@@ -122,7 +121,6 @@ public class UiManager extends ComponentManager implements Ui {
     @Subscribe
     private void handlePersonPanelSelectionChangedEvent(TaskPanelSelectionChangedEvent event) {
         logger.info(LogsCenter.getEventHandlingLogMessage(event));
-        mainWindow.loadPersonPage(event.getNewSelection());
     }
 
 }
