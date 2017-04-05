f5b9e6b94ec3ecf446d37432bfc8548f9356e76e
ruolan <fionaxrl@gmail.com>
2017-04-05 22:17:45 +0800

    add welcome message in result display

--- src/main/java/seedu/taskboss/commons/core/Messages.java
+++ src/main/java/seedu/taskboss/commons/core/Messages.java
@@ -9,5 +9,8 @@ public class Messages {
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_TASK_DISPLAYED_INDEX = "The task index provided is invalid";
     public static final String MESSAGE_TASKS_LISTED_OVERVIEW = "%1$d tasks listed!";
+    public static final String MESSAGE_WELCOME_TO_TASKBOSS = "Welcome to TaskBoss!\n" 
+    +"To get more information about TaskBoss features and command usage," 
+            +" you can type 'help' in the command box below.";
 
 }
