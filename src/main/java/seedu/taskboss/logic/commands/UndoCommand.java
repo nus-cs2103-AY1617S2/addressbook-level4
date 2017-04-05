64f50c66a3b34731b0047dadf4a93595a2f392f7
Tan Wei <tanwei0319@gmail.com>
2017-04-06 02:50:39 +0800

    Make undo stay in original view after execution

--- src/main/java/seedu/taskboss/logic/commands/UndoCommand.java
+++ src/main/java/seedu/taskboss/logic/commands/UndoCommand.java
@@ -30,7 +30,6 @@ public class UndoCommand extends Command {
         assert model != null;
         try {
             model.undoTaskboss();
-            model.updateFilteredListToShowAll();
             return new CommandResult(MESSAGE_SUCCESS);
         } catch (EmptyStackException ese) {
             return new CommandResult(MESSAGE_WITHOUT_PREVIOUS_OPERATION);
