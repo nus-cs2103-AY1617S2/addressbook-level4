c5fc9ffafee3af5186f43e201750f2f040039b9c
Tan Wei <tanwei0319@gmail.com>
2017-04-05 11:10:16 +0800

    Update AddCommandTest

--- src/test/java/guitests/AddCommandTest.java
+++ src/test/java/guitests/AddCommandTest.java
@@ -2,13 +2,18 @@ package guitests;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.List;
+
 import org.junit.Test;
+
+import com.google.common.collect.Lists;
 
 import guitests.guihandles.TaskCardHandle;
 import seedu.taskboss.commons.core.Messages;
 import seedu.taskboss.commons.exceptions.IllegalValueException;
 import seedu.taskboss.logic.commands.AddCommand;
 import seedu.taskboss.model.category.UniqueCategoryList.DuplicateCategoryException;
+import seedu.taskboss.testutil.TestCategory;
 import seedu.taskboss.testutil.TestTask;
 import seedu.taskboss.testutil.TestUtil;
 
@@ -19,23 +24,26 @@ public class AddCommandTest extends TaskBossGuiTest {
         TestTask[] currentList = td.getTypicalTasks();
         //add one task
         TestTask taskToAdd = td.taskH;
-
-        assertAddSuccess(false, false, taskToAdd, currentList);
+        assertAddSuccess(false, false, Lists.newArrayList(new TestCategory
+                (AddCommand.BUILT_IN_ALL_TASKS, td.taskA)), taskToAdd, currentList);
         currentList = TestUtil.addTasksToList(currentList, taskToAdd);
 
         //add another task
         taskToAdd = td.taskI;
-        assertAddSuccess(false, false, taskToAdd, currentList);
+        assertAddSuccess(false, false, Lists.newArrayList(new TestCategory
+                (AddCommand.BUILT_IN_ALL_TASKS, td.taskA)), taskToAdd, currentList);
         currentList = TestUtil.addTasksToList(currentList, taskToAdd);
 
         //add another task using short command
         taskToAdd = td.taskK;
-        assertAddSuccess(false, true, taskToAdd, currentList);
+        assertAddSuccess(false, true, Lists.newArrayList(new TestCategory
+                (AddCommand.BUILT_IN_ALL_TASKS, td.taskA)), taskToAdd, currentList);
         currentList = TestUtil.addTasksToList(currentList, taskToAdd);
 
         //add another task using short command '+'
         taskToAdd = td.taskL;
-        assertAddSuccess(true, false, taskToAdd, currentList);
+        assertAddSuccess(true, false, Lists.newArrayList(new TestCategory
+                (AddCommand.BUILT_IN_ALL_TASKS, td.taskA)), taskToAdd, currentList);
         currentList = TestUtil.addTasksToList(currentList, taskToAdd);
 
         //add duplicate task
@@ -50,24 +58,24 @@ public class AddCommandTest extends TaskBossGuiTest {
 
         //add to empty list
         commandBox.runCommand("clear");
-        assertAddSuccess(false, false, td.taskA);
+        assertAddSuccess(false, false, Lists.newArrayList(new TestCategory
+                (AddCommand.BUILT_IN_ALL_TASKS, td.taskA), new TestCategory("Friends", td.taskA)), td.taskA);
 
         //invalid command
         commandBox.runCommand("adds new task");
         assertResultMessage(Messages.MESSAGE_UNKNOWN_COMMAND);
     }
 
-    private void assertAddSuccess(boolean isPlusSign, boolean isShortCommand, TestTask taskToAdd,
-                    TestTask... currentList)
+    private void assertAddSuccess(boolean isPlusSign, boolean isShortCommand,
+            List<TestCategory> expectedCategoryList,
+            TestTask taskToAdd, TestTask... currentList)
             throws DuplicateCategoryException, IllegalValueException {
         if (isShortCommand) {
             commandBox.runCommand(taskToAdd.getShortAddCommand());
+        } else if (isPlusSign) {
+            commandBox.runCommand(taskToAdd.getAddCommandPlus());
         } else {
-            if (isPlusSign) {
-                commandBox.runCommand(taskToAdd.getAddCommandPlus());
-            } else {
-                commandBox.runCommand(taskToAdd.getAddCommand());
-            }
+            commandBox.runCommand(taskToAdd.getAddCommand());
         }
 
         //confirm the new card contains the right data
@@ -77,6 +85,8 @@ public class AddCommandTest extends TaskBossGuiTest {
         //confirm the list now contains all previous tasks plus the new task
         TestTask[] expectedList = TestUtil.addTasksToList(currentList, taskToAdd);
         assertTrue(taskListPanel.isListMatching(expectedList));
+
+        assertTrue(categoryListPanel.isListMatching(expectedCategoryList));
     }
 
 }
