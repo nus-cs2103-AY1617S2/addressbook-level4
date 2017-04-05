3325bbd57962973aaa903272735e46d2bc527ab4
Tan Jian Sin <e0003810@u.nus.edu>
2017-04-05 20:34:41 +0800

    Test Travis

--- src/main/java/seedu/tache/ui/HotkeyManager.java
+++ src/main/java/seedu/tache/ui/HotkeyManager.java
@@ -115,7 +115,7 @@ public class HotkeyManager {
      */
     public void start() {
         bindToggleSystemHotkey();
-        bindTriggerNotificationHotkey();
+        //bindTriggerNotificationHotkey();
     }
 
     /**
