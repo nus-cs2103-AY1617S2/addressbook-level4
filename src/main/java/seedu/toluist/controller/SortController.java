e012d86451a972f24e00221af90bacd49380ff47
Louis Lai <hoangdung2506@gmail.com>
2017-04-05 10:54:14 +0800

    Linting

--- src/main/java/seedu/toluist/controller/SortController.java
+++ src/main/java/seedu/toluist/controller/SortController.java
@@ -14,7 +14,6 @@ import seedu.toluist.commons.exceptions.InvalidCommandException;
 import seedu.toluist.commons.util.StringUtil;
 import seedu.toluist.model.Task;
 import seedu.toluist.model.TodoList;
-import seedu.toluist.ui.UiStore;
 import seedu.toluist.ui.commons.CommandResult;
 
 /**
