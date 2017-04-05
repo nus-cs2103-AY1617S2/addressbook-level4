04a25545b8aa040d7ac63099f92d2a78aec74848
DESKTOP-7RLT252\gjlam95 <gj_lam@hotmail.com>
2017-04-05 17:56:33 +0800

    Allow ui statusfooterbar to mutate after changing saving directory

--- src/main/java/seedu/opus/ui/StatusBarFooter.java
+++ src/main/java/seedu/opus/ui/StatusBarFooter.java
@@ -11,6 +11,7 @@ import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Region;
 import seedu.opus.commons.core.LogsCenter;
+import seedu.opus.commons.events.model.ChangeSaveLocationEvent;
 import seedu.opus.commons.events.model.TaskManagerChangedEvent;
 import seedu.opus.commons.util.FxViewUtil;
 
@@ -53,5 +54,13 @@ public class StatusBarFooter extends UiPart<Region> {
         String lastUpdated = (new Date()).toString();
         logger.info(LogsCenter.getEventHandlingLogMessage(abce, "Setting last updated status to " + lastUpdated));
         setSyncStatus("Last Updated: " + lastUpdated);
+    }
+
+    //@@author A0148081H
+    @Subscribe
+    public void handleChangeSaveLocationEvent(ChangeSaveLocationEvent event) {
+        String saveLocation = event.location;
+        logger.info(LogsCenter.getEventHandlingLogMessage(event, "Setting save location to: " + saveLocation));
+        setSaveLocation(saveLocation);
     }
 }
