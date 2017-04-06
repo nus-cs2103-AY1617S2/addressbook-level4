46e81cef549743b298984018c91932fd2c32f659
bryanleejh <bryan.lee.jh@gmail.com>
2017-04-07 03:08:50 +0800

    Checkstyle issues

--- src/main/java/seedu/bulletjournal/model/util/SampleDataUtil.java
+++ src/main/java/seedu/bulletjournal/model/util/SampleDataUtil.java
@@ -21,17 +21,17 @@ public class SampleDataUtil {
     public static Task[] getSampleTasks() {
         try {
             return new Task[] {
-                    new Task(new TaskName("Assignment for CS"), new DueDate("14th April 4pm"), new Status("undone"),
+                new Task(new TaskName("Assignment for CS"), new DueDate("14th April 4pm"), new Status("undone"),
                             new BeginDate("14th April 12pm"), new UniqueTagList("homework")),
-                    new Task(new TaskName("Other homework"), new DueDate("14th April 4pm"), new Status("undone"),
+                new Task(new TaskName("Other homework"), new DueDate("14th April 4pm"), new Status("undone"),
                             new BeginDate("14th April 12pm"), new UniqueTagList("unimportant")),
-                    new Task(new TaskName("Take shower"), new DueDate("14th April 4pm"), new Status("undone"),
+                new Task(new TaskName("Take shower"), new DueDate("14th April 4pm"), new Status("undone"),
                             new BeginDate("14th April 12pm"), new UniqueTagList("routine")),
-                    new Task(new TaskName("Play basketball"), new DueDate("14th April 4pm"), new Status("undone"),
+                new Task(new TaskName("Play basketball"), new DueDate("14th April 4pm"), new Status("undone"),
                             new BeginDate("14th April 12pm"), new UniqueTagList("love")),
-                    new Task(new TaskName("Change test cases"), new DueDate("14th April 4pm"), new Status("undone"),
+                new Task(new TaskName("Change test cases"), new DueDate("14th April 4pm"), new Status("undone"),
                             new BeginDate("14th April 12pm"), new UniqueTagList("homework")),
-                    new Task(new TaskName("Ride bicycle"), new DueDate("14th April 4pm"), new Status("undone"),
+                new Task(new TaskName("Ride bicycle"), new DueDate("14th April 4pm"), new Status("undone"),
                             new BeginDate("14th April 12pm"), new UniqueTagList("exercise")) };
         } catch (IllegalValueException e) {
             throw new AssertionError("sample data cannot be invalid", e);
