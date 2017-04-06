e358e9dd1386554976e1d07907194bbc24f99cd4
edwinyeap <torres12318@hotmail.com>
2017-04-07 00:00:09 +0800

    fix trailing whitespace issue

--- src/main/java/seedu/task/logic/Logic.java
+++ src/main/java/seedu/task/logic/Logic.java
@@ -11,7 +11,6 @@ import seedu.task.model.task.ReadOnlyTask;
 public interface Logic {
 	/**
 	 * Executes the command and returns the result.
-	 * 
 	 * @param commandText
 	 *            The command as entered by the user.
 	 * @return the result of the command execution.
