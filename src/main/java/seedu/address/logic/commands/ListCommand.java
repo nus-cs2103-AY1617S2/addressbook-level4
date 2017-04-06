3535e86b954b47d419b65f272531cba1e11773fe
Malik Jabati <malikjabati@gmail.com>
2017-04-06 14:59:31 +0800

    Correct List command to display "tasks" instead of "persons"

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -1,14 +1,15 @@
 package seedu.address.logic.commands;
 
 
+//@@author A0164466X
 /**
- * Lists all persons in the address book to the user.
+ * Lists all tasks in the task manager to the user.
  */
 public class ListCommand extends Command {
 
     public static final String COMMAND_WORD = "list";
 
-    public static final String MESSAGE_SUCCESS = "Listed all persons";
+    public static final String MESSAGE_SUCCESS = "Listed all tasks";
 
 
     @Override
