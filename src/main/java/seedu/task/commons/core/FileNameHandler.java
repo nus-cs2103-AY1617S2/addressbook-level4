b91bd47f83419de28e5d0cc2c9494c6943f1a8bb
Edwin\admin <torres12318@hotmail.com>
2017-04-07 00:11:49 +0800

    fixed trailing whitespace issue

--- src/main/java/seedu/task/commons/core/FileNameHandler.java
+++ src/main/java/seedu/task/commons/core/FileNameHandler.java
@@ -1,4 +1,5 @@
 package seedu.task.commons.core;
+
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
@@ -8,65 +9,53 @@ import java.io.IOException;
 
 //@@author A0163845X
 public class FileNameHandler {
-	private static String filePath = "fileName.txt";
-	public static final String DEFAULT_FILEPATH = "data/TaskManagerData.xml";
+    private static String filePath = "fileName.txt";
+    public static final String DEFAULT_FILEPATH = "data/TaskManagerData.xml";
 
-	public static String getFileName() {
-		try {
-			FileInputStream fis = new FileInputStream(filePath);
-			BufferedInputStream bis = new BufferedInputStream(fis);
-			byte[] data = new byte[256];
-			bis.read(data);
-			String retString = new String(data).trim();
-			if (isValid(retString)) {
-				System.out.println("isValid");
-				return retString;
-			} else {
-				return DEFAULT_FILEPATH;
-			}
-		} catch (IOException ioe) {
-			System.out.println("IOException");
-			return DEFAULT_FILEPATH;
-		}
-
-	}
-
-	public static void setFileName(String fileName) {
-		FileOutputStream fos;
-		try {
-			fos = new FileOutputStream(filePath, false);
-			BufferedOutputStream bos = new BufferedOutputStream(fos);
-			bos.write(fileName.getBytes());
-			bos.close();
-		} catch (IOException ioe) {
-			System.out.println("IOException");
-		}
-	}
-
-//	public static boolean isValid(String str) {
-//		if (str.length() <= 4) {
-//			return false;
-//		}
-//		if (str.substring(str.length() - 4, str.length()).equals(".xml")) {
-//			return true;
-//		}
-//		return false;
-//	}	
-	public static boolean isValid(String FileName) {
-	    final File File = new File(FileName);
-	    boolean isValid = true;
-	    try {
-	        if (File.createNewFile()) {
-	            File.delete();
-	        }
-	    } catch (IOException e) {
-	        isValid = false;
+    public static String getFileName() {
+	try {
+	    FileInputStream fis = new FileInputStream(filePath);
+	    BufferedInputStream bis = new BufferedInputStream(fis);
+	    byte[] data = new byte[256];
+	    bis.read(data);
+	    String retString = new String(data).trim();
+	    if (isValid(retString)) {
+		System.out.println("isValid");
+		return retString;
+	    } else {
+		return DEFAULT_FILEPATH;
 	    }
-//	    if (FileName.substring(FileName.length() - 4, FileName.length()).equals(".xml")){
-//	    	isValid = true;
-//	    }
-	    System.out.println(isValid);
-	    return isValid;
+	} catch (IOException ioe) {
+	    System.out.println("IOException");
+	    return DEFAULT_FILEPATH;
 	}
-}
 
+    }
+
+    public static void setFileName(String fileName) {
+	FileOutputStream fos;
+	try {
+	    fos = new FileOutputStream(filePath, false);
+	    BufferedOutputStream bos = new BufferedOutputStream(fos);
+	    bos.write(fileName.getBytes());
+	    bos.close();
+	} catch (IOException ioe) {
+	    System.out.println("IOException");
+	}
+    }
+
+    public static boolean isValid(String FileName) {
+	final File File = new File(FileName);
+	boolean isValid = true;
+	try {
+	    if (File.createNewFile()) {
+		File.delete();
+	    }
+	} catch (IOException e) {
+	    isValid = false;
+	}
+
+	System.out.println(isValid);
+	return isValid;
+    }
+}
