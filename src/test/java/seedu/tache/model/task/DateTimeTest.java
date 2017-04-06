bb5d3b4bdf2baf67033d71817558e47f020c9286
Crsytral <shouyu2@hotmail.com>
2017-04-06 22:30:18 +0800

    Add Unit Test for DateTime functions

--- src/test/java/seedu/tache/model/task/DateTimeTest.java
+++ src/test/java/seedu/tache/model/task/DateTimeTest.java
@@ -2,6 +2,10 @@
 package seedu.tache.model.task;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Date;
 
 import org.junit.Test;
 
@@ -53,4 +57,68 @@ public class DateTimeTest {
         assertEquals(test3.getTimeOnly(), "15:00:00");
         assertEquals(test3.getDateTimeForFullCalendar(), "2019-02-07T15:00:00");
     }
+    
+    //@@author A0139961U
+    @Test
+    public void isSameDateSuccess() throws IllegalValueException {
+        Date now = new Date();
+        DateTime test = new DateTime(now.toString());
+        Date date = new Date();
+        assertTrue(test.isSameDate(date));
+    }
+    
+    @Test
+    public void isSameDateFailure() throws IllegalValueException {
+        Date now = new Date();
+        DateTime test = new DateTime(now.toString());
+        Date date = new Date(0);
+        assertFalse(test.isSameDate(date));
+    }
+    
+    @Test
+    public void isTodaySuccess() throws IllegalValueException {
+        Date today = new Date();
+        DateTime test = new DateTime(today.toString());
+        assertTrue(test.isToday());
+    }
+    
+    @Test
+    public void isTodayFailure() throws IllegalValueException {
+        Date past = new Date(0);
+        DateTime testPast = new DateTime(past.toString());
+        assertFalse(testPast.isToday());
+        
+        DateTime yesterday = new DateTime("yesterday");
+        assertFalse(yesterday.isToday());
+        
+        DateTime tomorrow = new DateTime("tomorrow");
+        assertFalse(tomorrow.isToday());
+        
+        Date future = new Date(Long.MAX_VALUE);
+        DateTime testFuture = new DateTime(future.toString());
+        assertFalse(testFuture.isToday());
+    }
+    
+    @Test
+    public void isSameWeekSuccess() throws IllegalValueException {
+        DateTime thisWeek = new DateTime("this week");
+        assertTrue(thisWeek.isSameWeek());
+    }
+    
+    @Test
+    public void isSameWeekFailure() throws IllegalValueException {
+        DateTime lastWeek = new DateTime("last week");
+        assertFalse(lastWeek.isSameWeek());
+        
+        DateTime nextWeek = new DateTime("next week");
+        assertFalse(nextWeek.isSameWeek());
+    }
+    
+    @Test
+    public void removeTimeSuccess() throws IllegalValueException {
+        Date date = new Date();
+        DateTime today = new DateTime("today 00:00AM");
+        assertEquals(DateTime.removeTime(date), today.getDate());
+    }
+    //@@author
 }
