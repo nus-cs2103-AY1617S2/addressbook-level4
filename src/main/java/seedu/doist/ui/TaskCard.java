c80536ad65585371925ddfc87382b92dec6d5a5b
leeyh20 <leeyh20@hotmail.com>
2017-04-06 17:01:49 +0800

    Add stars to priority

--- src/main/java/seedu/doist/ui/TaskCard.java
+++ src/main/java/seedu/doist/ui/TaskCard.java
@@ -3,11 +3,13 @@ package seedu.doist.ui;
 import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
+import seedu.doist.model.task.Priority;
 import seedu.doist.model.task.ReadOnlyTask;
 
 //@@author A0140887W
@@ -43,6 +45,10 @@ public class TaskCard extends UiPart<Region> {
     private Label endTime;
     @FXML
     private FlowPane tags;
+    @FXML
+    private ImageView star1;
+    @FXML
+    private ImageView star2;
 
     public TaskCard(ReadOnlyTask task, int displayedIndex) {
         super(FXML);
@@ -75,6 +81,16 @@ public class TaskCard extends UiPart<Region> {
             setStyleToFinished();
         } else {
             checkbox.setSelected(false);
+        }
+
+        //Importance
+        if (task.getPriority().getPriorityLevel().equals(Priority.PriorityLevel.NORMAL)) {
+            // Disable both stars
+            star1.setImage(null);
+            star2.setImage(null);
+        } else if (task.getPriority().getPriorityLevel().equals(Priority.PriorityLevel.IMPORTANT)) {
+            // Disable one star
+            star2.setImage(null);
         }
 
         // Tags
