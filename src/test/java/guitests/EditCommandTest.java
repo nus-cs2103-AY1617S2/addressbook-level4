be0c20f21dcc4e9bc015eded86ca25303ad45d93
is1105 <1551127928@qq.com>
2017-04-06 09:43:06 +0800

    Add editCommand and list command tests

--- src/test/java/guitests/EditCommandTest.java
+++ src/test/java/guitests/EditCommandTest.java
@@ -1,6 +1,5 @@
 package guitests;
 
-//@@author A0141872E
 import static org.junit.Assert.assertTrue;
 import static seedu.taskit.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
@@ -12,6 +11,8 @@ import guitests.guihandles.TaskCardHandle;
 import seedu.taskit.commons.core.Messages;
 import seedu.taskit.logic.commands.EditCommand;
 import seedu.taskit.model.tag.Tag;
+import seedu.taskit.model.task.Date;
+import seedu.taskit.model.task.Priority;
 import seedu.taskit.model.task.Title;
 import seedu.taskit.testutil.TaskBuilder;
 import seedu.taskit.testutil.TestTask;
@@ -23,7 +24,7 @@ public class EditCommandTest extends AddressBookGuiTest {
     // This list is updated with every successful call to assertEditSuccess().
     TestTask[] expectedTasksList = td.getTypicalTasks();
 
-    // @@author A0163996J
+    //@@author A0163996J
 
     public void sortTasksList() {
         Arrays.sort(expectedTasksList);
@@ -42,7 +43,28 @@ public class EditCommandTest extends AddressBookGuiTest {
     }
 
     @Test
+    public void edit_multipleFields_Success() throws Exception {
+        String detailsToEdit = "title Do Homework start 4pm end 6pm priority low tag assignment";
+        int addressBookIndex = 3;
+
+        TestTask editedTask = new TaskBuilder().withTitle("Do Homework").withStart("4pm").withEnd("6pm").withPriority("low").withTags("assignment").build();
+
+        assertEditSuccess(addressBookIndex, addressBookIndex, detailsToEdit, editedTask);
+    }
+
+    @Test
+    public void edit_multipleTags_Success() throws Exception {
+        String detailsToEdit = "tag assignment tag school tag work";
+        int addressBookIndex = 3;
+
+        TestTask editedTask = new TaskBuilder().withTitle("Do Homework").withPriority("medium").withTags("assignment","school","work").build();
+
+        assertEditSuccess(addressBookIndex, addressBookIndex, detailsToEdit, editedTask);
+    }
+
+    @Test
     public void edit_clearTags_success() throws Exception {
+        sortTasksList();
         String detailsToEdit = "tag null";
         int addressBookIndex = 5;
 
@@ -54,6 +76,7 @@ public class EditCommandTest extends AddressBookGuiTest {
 
     @Test
     public void edit_findThenEdit_success() throws Exception {
+        sortTasksList();
         commandBox.runCommand("find Do HW 1");
 
         String detailsToEdit = "title Homework due";
@@ -68,39 +91,54 @@ public class EditCommandTest extends AddressBookGuiTest {
 
     @Test
     public void edit_notAllFieldsSpecified_failure() {
+        sortTasksList();
         commandBox.runCommand("edit 1 ");
         assertResultMessage(EditCommand.MESSAGE_NOT_EDITED);
     }
 
     @Test
     public void edit_missingTaskIndex_failure() {
+        sortTasksList();
         commandBox.runCommand("edit title Homework due");
         assertResultMessage(String.format(MESSAGE_INVALID_COMMAND_FORMAT, EditCommand.MESSAGE_NOT_EDITED));
     }
 
     @Test
     public void edit_invalidTaskIndex_failure() {
+        sortTasksList();
         commandBox.runCommand("edit 99 title Homework due");
         assertResultMessage(Messages.MESSAGE_INVALID_TASK_DISPLAYED_INDEX);
     }
 
     @Test
     public void edit_noFieldsSpecified_failure() {
+        sortTasksList();
         commandBox.runCommand("edit ");
         assertResultMessage(String.format(MESSAGE_INVALID_COMMAND_FORMAT, EditCommand.MESSAGE_NOT_EDITED));
     }
 
     @Test
     public void edit_invalidValues_failure() {
+        sortTasksList();
         //commandBox.runCommand("edit 1 title *&");
         //assertResultMessage(Title.MESSAGE_TITLE_CONSTRAINTS);
 
         commandBox.runCommand("edit 1 tag *&");
         assertResultMessage(Tag.MESSAGE_TAG_CONSTRAINTS);
+
+        commandBox.runCommand("edit 1 start day");
+        assertResultMessage(Date.MESSAGE_DATE_FAIL);
+
+        commandBox.runCommand("edit 1 end day");
+        assertResultMessage(Date.MESSAGE_DATE_FAIL);
+
+        commandBox.runCommand("edit 1 priority very high");
+        assertResultMessage(Priority.MESSAGE_PRIORITY_CONSTRAINTS);
     }
 
     @Test
     public void edit_duplicateTask_failure() {
+        sortTasksList();
         commandBox.runCommand("edit 3 title Do HW 1");
         assertResultMessage(EditCommand.MESSAGE_DUPLICATE_TASK);
     }
