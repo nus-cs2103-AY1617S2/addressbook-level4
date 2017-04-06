70cc43015898811b2136029a847846573fa4fe9a
Wang Yu <stingwangyu@gmail.com>
2017-04-06 11:58:27 +0800

    Fix checkstyle error

--- src/main/java/seedu/bulletjournal/logic/parser/CliSyntax.java
+++ src/main/java/seedu/bulletjournal/logic/parser/CliSyntax.java
@@ -24,7 +24,7 @@ public class CliSyntax {
     /* Patterns definition for show command */
     public static final Pattern KEYWORDS_SHOW_FORMAT =
             Pattern.compile("(done|undone)", Pattern.CASE_INSENSITIVE);
-    
+
     /* Patterns definition for finish command args */
     public static final Pattern FINISH_ARGS_FORMAT =
             Pattern.compile("\\d");
