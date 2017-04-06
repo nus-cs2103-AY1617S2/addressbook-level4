719fcd7014939e2dd57fdf76292854669396116d
M A Rizvi <mrizvi1996@gmail.com>
2017-04-06 23:22:06 +0800

    added clear passed command

--- src/main/java/seedu/address/logic/commands/ClearCommand.java
+++ src/main/java/seedu/address/logic/commands/ClearCommand.java
@@ -22,19 +22,23 @@ public class ClearCommand extends Command {
     
     public static final String KEYWORD_ALL = "all";
     public static final String KEYWORD_COMPLETE = Tag.TAG_COMPLETE;
+    public static final String KEYWORD_PASSED = "passed";
     
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Clears all tasks, "
             + "or only completed tasts if the " + KEYWORD_COMPLETE + " keyword is specified.\n"
-            + "Parameters: [keyword]...\n"
+            + "Parameters: [keyword]\n"
             + "Keywords : "
-            +  KEYWORD_ALL
+            + KEYWORD_ALL
             +", "
             + KEYWORD_COMPLETE
+            + ", "
+            + KEYWORD_PASSED
             + "\n"
             + "Example: " + COMMAND_WORD + " completed";
     
-    public static final String MESSAGE_SUCCESS_COMPLETED = "All competed tasks have been cleared!";
     public static final String MESSAGE_SUCCESS_ALL = "All tasks have been cleared!";
+    public static final String MESSAGE_SUCCESS_COMPLETE = "All competed tasks have been cleared!";
+    public static final String MESSAGE_SUCCESS_PASSSED = "All passed tasks have been cleared!";
     public static final String MESSAGE_FAILURE = "Clear command not given any keywords!\n" + MESSAGE_USAGE;
     
     private Set<String> keywords;
@@ -74,7 +78,26 @@ public class ClearCommand extends Command {
             }
             model.resetData(filteredYTomorrow);
             
-            return new CommandResult(MESSAGE_SUCCESS_COMPLETED);
+            return new CommandResult(MESSAGE_SUCCESS_COMPLETE);
+        }
+        
+        if (keywords.contains(KEYWORD_PASSED)) {
+            
+            List<ReadOnlyPerson> filtered = new ArrayList<ReadOnlyPerson>();
+            for (ReadOnlyPerson task : model.getAddressBook().getPersonList()) {
+                if (!task.hasPassed()) {
+                    filtered.add(task);
+                }
+            }
+            YTomorrow filteredYTomorrow = new YTomorrow();
+            try {
+                filteredYTomorrow.setPersons(filtered);
+            } catch (DuplicatePersonException e) {
+                e.printStackTrace();
+            }
+            model.resetData(filteredYTomorrow);
+            
+            return new CommandResult(MESSAGE_SUCCESS_PASSSED);
         }
         
         return new CommandResult(MESSAGE_FAILURE);
