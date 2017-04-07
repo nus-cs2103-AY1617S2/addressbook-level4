fe7328e0186830a69c4311c471bd53a5263bfc2c
Edwin\admin <torres12318@hotmail.com>
2017-04-07 20:50:44 +0800

    fix import orders

--- src/main/java/seedu/task/storage/XmlSerializableTaskManager.java
+++ src/main/java/seedu/task/storage/XmlSerializableTaskManager.java
@@ -13,8 +13,8 @@ import seedu.task.commons.core.UnmodifiableObservableList;
 import seedu.task.commons.exceptions.IllegalValueException;
 import seedu.task.model.ReadOnlyTaskManager;
 import seedu.task.model.tag.Tag;
-import seedu.task.model.task.Task;
 import seedu.task.model.task.ReadOnlyTask;
+import seedu.task.model.task.Task;
 
 /**
  * An Immutable AddressBook that is serializable to XML format
