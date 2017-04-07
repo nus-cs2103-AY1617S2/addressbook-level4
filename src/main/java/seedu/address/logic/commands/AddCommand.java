161563ff4569045631eebfbc61a43122974f8fbd
M A Rizvi <mrizvi1996@gmail.com>
2017-04-07 17:40:09 +0800

    added add parsing logic

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -1,6 +1,9 @@
 package seedu.address.logic.commands;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 import seedu.address.commons.exceptions.IllegalValueException;
@@ -36,7 +39,125 @@ public class AddCommand extends Command {
     public static final String MESSAGE_PASSED = "The end date of the new task has passed!";
     public static final String MESSAGE_DUPLICATE_TASK = "This task already exists in the todo list";
 
-    private final Task toAdd;
+    private static final String STARTDATE_KEYWORD = "from";
+    private static final String ENDDATE_KEYWORD = "to";
+    private static final String GROUP_KEYWORD = "in";
+    
+    private static List<String> SPECIAL_KEYWORDS;
+    
+    {
+        SPECIAL_KEYWORDS = new ArrayList<String>();
+        SPECIAL_KEYWORDS.add(STARTDATE_KEYWORD);
+        SPECIAL_KEYWORDS.add(ENDDATE_KEYWORD);
+        SPECIAL_KEYWORDS.add(GROUP_KEYWORD);
+    }
+    
+    private Task toAdd = null;
+    
+    //@@author A0163848R
+    public AddCommand(List<String> keywords) {
+        Optional<Name> name = getName(keywords);
+        Optional<StartDate> start = getStartDate(keywords);
+        Optional<EndDate> end = getEndDate(keywords);
+        Optional<Group> group = getGroup(keywords);
+        
+        if (!name.isPresent()
+                || !group.isPresent()
+                || (start.isPresent() && !end.isPresent())) {
+            //TODO syntax error
+        }
+        
+        try {
+            toAdd = Task.factory(
+                    name.get(),
+                    group.get(),
+                    start.isPresent() ? start.get() : null,
+                    end.isPresent() ? end.get() : null
+                    );
+        } catch (IllegalValueException e) {
+            //TODO error
+        }
+        
+    }
+    
+    private static Optional<Name> getName(List<String> keywords) {
+        try {
+            return Optional.of(new Name(concat(keywords.subList(
+                    0,
+                    lastIndexOfAny(keywords, SPECIAL_KEYWORDS)))));
+        } catch (Exception e) {
+            return Optional.empty();
+        }
+    }
+    
+    private static Optional<StartDate> getStartDate(List<String> keywords) {
+        try {
+            return Optional.of(new StartDate(concat(keywords.subList(
+                    keywords.lastIndexOf(STARTDATE_KEYWORD) + 1,
+                    lastIndexOfAny(keywords, except(SPECIAL_KEYWORDS, STARTDATE_KEYWORD))))));
+        } catch (Exception e) {
+            return Optional.empty();
+        }
+    }
+   
+    private static Optional<EndDate> getEndDate(List<String> keywords) {
+        try {
+            return Optional.of(new EndDate(concat(keywords.subList(
+                    keywords.lastIndexOf(ENDDATE_KEYWORD) + 1,
+                    lastIndexOfAny(keywords, except(SPECIAL_KEYWORDS, ENDDATE_KEYWORD))))));
+        } catch (Exception e) {
+            return Optional.empty();
+        }
+    }
+    
+    private static Optional<Group> getGroup(List<String> keywords) {
+        try {
+            return Optional.of(new Group(concat(keywords.subList(
+                    keywords.lastIndexOf(GROUP_KEYWORD) + 1,
+                    lastIndexOfAny(keywords, except(SPECIAL_KEYWORDS, GROUP_KEYWORD))))));
+        } catch (Exception e) {
+            return Optional.empty();
+        }
+    }
+    
+    private static String concat(List<String> strs) {
+        return concat(strs, " ");
+    }
+    
+    private static String concat(List<String> strs, String between) {
+        String cat = "";
+        for (int i = 0; i < strs.size(); i++) {
+            if (i == 0) {
+                cat = strs.get(i);
+                continue;
+            }
+            
+            cat += between + strs.get(i);
+        }
+        return cat;
+    }
+    
+    private static <T> int lastIndexOfAny(List<T> list, List<T> any) {
+        int i = -1;
+        for (T a : any) {
+            int t = list.lastIndexOf(a);
+            if (t < t) {
+                i = t;
+            }
+        }
+        return i;
+    }
+    
+    private static <T> List<T> except(List<T> origin, T except) {
+        List<T> filtered = new ArrayList<T>();
+        for (T el : origin) {
+            if (!el.equals(except)) {
+                filtered.add(el);
+            }
+        }
+        return filtered;
+    }
+    //@@author
 
     /**
      * Creates an AddCommand using raw values. This case is only have the
@@ -45,6 +166,7 @@ public class AddCommand extends Command {
      *             if any of the raw values are invalid
      */
     //@@author A0164032U
+    /*
     public AddCommand(String name, String end, String group) throws IllegalValueException {
         this.toAdd = new DeadlineTask(
                 new Name(name),
@@ -53,11 +175,13 @@ public class AddCommand extends Command {
                 UniqueTagList.build(Tag.TAG_INCOMPLETE)
                 );
     }
+    */
 
     /*
      * Constructor: floating task without starting date and end date
      */
     //@@author A0164032U
+    /*
     public AddCommand(String name, String group) throws IllegalValueException {
         this.toAdd = new FloatingTask(
                 new Name(name),
@@ -76,7 +200,7 @@ public class AddCommand extends Command {
                 UniqueTagList.build(Tag.TAG_INCOMPLETE)
                 );
     }
-
+    */
     @Override
     public CommandResult execute() throws CommandException {
         assert model != null;
@@ -90,5 +214,4 @@ public class AddCommand extends Command {
         }
 
     }
-
 }
--- src/main/java/seedu/address/logic/parser/AddCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddCommandParser.java
@@ -1,15 +1,22 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.KEYWORDS_ARGS_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ENDDATE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_GROUP;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_STARTDATE;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Set;
+import java.util.regex.Matcher;
 
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.Command;
+import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.IncorrectCommand;
 
 /**
@@ -17,11 +24,30 @@ import seedu.address.logic.commands.IncorrectCommand;
  */
 public class AddCommandParser {
 
+    //@@author A0163848R
+    /**
+     * Parses the given {@code String} of arguments in the context of the AddCommand
+     * and returns an AccCommand object for execution.
+     */
+    public Command parse(String args) {
+        final Matcher matcher = KEYWORDS_ARGS_FORMAT.matcher(args.trim());
+        if (!matcher.matches()) {
+            return new IncorrectCommand(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
+        }
+
+        // keywords delimited by whitespace
+        final String[] keywords = matcher.group("keywords").split("\\s+");
+        final List<String> keywordList = Arrays.asList(keywords);
+        return new AddCommand(keywordList);
+    }
+    
     /**
      * Parses the given {@code String} of arguments in the context of the
      * AddCommand and returns an AddCommand object for execution.
      */
     //@@author A0164032U
+    /*
     public Command parse(String args) {
         ArgumentTokenizer argsTokenizer = new ArgumentTokenizer(PREFIX_STARTDATE, PREFIX_ENDDATE, PREFIX_GROUP);
         argsTokenizer.tokenize(args);
@@ -60,5 +86,6 @@ public class AddCommandParser {
             return new IncorrectCommand(ive.getMessage());
         }
     }
+    */
 
 }
