1113816e3a86cebe3974e77e7670f596adc3c11c
XinyiYS <michaelxuus@hotmail.com>
2017-04-07 20:22:29 +0800

    Fixed checkstyle.

--- src/test/java/guitests/DoneCommandTest.java
+++ src/test/java/guitests/DoneCommandTest.java
@@ -6,7 +6,6 @@ import static seedu.task.logic.commands.DoneCommand.MESSAGE_DONE_TASK_SUCCESS;
 import org.junit.Test;
 
 import guitests.guihandles.TaskCardHandle;
-import seedu.task.logic.commands.Command;
 import seedu.task.logic.commands.DoneCommand;
 import seedu.task.logic.commands.ListCommand;
 import seedu.task.testutil.TestTask;
@@ -22,17 +21,18 @@ public class DoneCommandTest extends TaskManagerGuiTest {
         int targetIndex = 1;
         assertIsDoneSuccess(targetIndex, currentList);
 
-        //done last in the list
+        // done last in the list
         targetIndex = currentList.length;
         assertIsDoneSuccess(targetIndex, currentList);
 
-        //invalid index
+        // invalid index
         commandBox.runCommand("done " + currentList.length + 1);
         assertResultMessage("The task index provided is invalid");
 
     }
+
     @Test
-    public void done_incorrectIndex_fail(){
+    public void done_incorrectIndex_fail() {
         commandBox.runCommand(ListCommand.COMMAND_WORD_1);
         commandBox.runCommand("done 0");
         assertResultMessage(String.format(MESSAGE_INVALID_COMMAND_FORMAT, DoneCommand.MESSAGE_USAGE));
@@ -40,19 +40,16 @@ public class DoneCommandTest extends TaskManagerGuiTest {
 
     private void assertIsDoneSuccess(int targetIndex, final TestTask[] currentList) {
         currentList[targetIndex - 1].setIsDone(true);
-        //boolean expectedRemainder = TestUtil.taskIsDone();
+        // boolean expectedRemainder = TestUtil.taskIsDone();
 
         commandBox.runCommand("done " + targetIndex);
 
-        //confirm that task is done
+        // confirm that task is done
         TaskCardHandle editedCard = taskListPanel.navigateToTask(currentList[targetIndex - 1].getName().fullName);
         assertMatching(currentList[targetIndex - 1], editedCard);
 
         assertResultMessage(String.format(MESSAGE_DONE_TASK_SUCCESS, currentList[targetIndex - 1]));
     }
-    
-    
-    
     // @Rule
     // public final ExpectedException thrown = ExpectedException.none();
     //
