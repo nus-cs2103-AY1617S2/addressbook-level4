975488ce779cc29962a2f7df6166e026694e2f40
nuslarry <e0147977@u.nus.edu>
2017-04-08 23:48:24 +0800

    findCommandTest

--- src/test/java/guitests/FindCommandTest.java
+++ src/test/java/guitests/FindCommandTest.java
@@ -1,41 +1,45 @@
 package guitests;
 
-//import static org.junit.Assert.assertTrue;
-//
-//import org.junit.Test;
-//
-//import seedu.task.commons.core.Messages;
-//import seedu.task.testutil.TestPerson;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import seedu.task.commons.core.Messages;
+import seedu.task.testutil.TestTask;
 
 public class FindCommandTest extends AddressBookGuiTest {
 
-    // @Test
-    // public void find_nonEmptyList() {
-    // assertFindResult("find Mark"); // no results
-    // assertFindResult("find Meier", td.benson, td.daniel); // multiple results
+    @Test
+    public void find_nonEmptyList() {
+        commandBox.runCommand(td.benson.getAddCommand());
+        commandBox.runCommand(td.daniel.getAddCommand());
+        assertFindResult("find Mark"); // no results
+        assertFindResult("find Say", td.benson, td.daniel); // multiple results
+
+        //find after deleting one result
+        commandBox.runCommand("delete 1");
+        assertFindResult("find Meier", td.daniel);
+        commandBox.runCommand("undo");
+        assertFindResult("find Say", td.benson, td.daniel);
+    }
+
+    @Test
+    public void find_emptyList() {
+        commandBox.runCommand("clear");
+        assertFindResult("find Jean"); // no results
+    }
     //
-    // //find after deleting one result
-    // commandBox.runCommand("delete 1");
-    // assertFindResult("find Meier", td.daniel);
-    // }
-    //
-    // @Test
-    // public void find_emptyList() {
-    // commandBox.runCommand("clear");
-    // assertFindResult("find Jean"); // no results
-    // }
-    //
-    // @Test
-    // public void find_invalidCommand_fail() {
-    // commandBox.runCommand("findgeorge");
-    // assertResultMessage(Messages.MESSAGE_UNKNOWN_COMMAND);
-    // }
-    //
-    // private void assertFindResult(String command, TestPerson... expectedHits)
-    // {
-    // commandBox.runCommand(command);
-    // assertListSize(expectedHits.length);
-    // assertResultMessage(expectedHits.length + " persons listed!");
-    // assertTrue(personListPanel.isListMatching(expectedHits));
-    // }
+    @Test
+    public void find_invalidCommand_fail() {
+        commandBox.runCommand("findgeorge");
+        assertResultMessage(Messages.MESSAGE_UNKNOWN_COMMAND);
+    }
+
+    private void assertFindResult(String command, TestTask... expectedHits)
+    {
+        commandBox.runCommand(command);
+        assertListSize(expectedHits.length);
+        assertResultMessage(expectedHits.length + " tasks listed!");
+        assertTrue(taskListPanel.isListMatching(expectedHits));
+    }
 }
