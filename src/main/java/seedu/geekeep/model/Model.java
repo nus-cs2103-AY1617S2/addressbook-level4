a9c71f4f5063b07748567f8b627729d834a1cae0
ZhangHanming <zhang.hanming.official@gmail.com>
2017-04-08 16:29:02 +0800

    remove trailing spaces

--- src/main/java/seedu/geekeep/model/Model.java
+++ src/main/java/seedu/geekeep/model/Model.java
@@ -31,31 +31,31 @@ public interface Model {
 
     void deleteTask(ReadOnlyTask target) throws UniqueTaskList.TaskNotFoundException;
 
-    /** 
-     * @return the configuration object 
+    /**
+     * @return the configuration object
      */
     Config getConfig();
 
-    /** 
-     * @return the filtered task list as an {@code UnmodifiableObservableList<ReadOnlyTask>} 
+    /**
+     * @return the filtered task list as an {@code UnmodifiableObservableList<ReadOnlyTask>}
      */
     UnmodifiableObservableList<ReadOnlyTask> getFilteredTaskList();
-    
+
     ReadOnlyGeeKeep getGeeKeep();
 
-    /** 
-     * Clears existing backing model and replaces with the provided new data. 
+    /**
+     * Clears existing backing model and replaces with the provided new data.
      */
     void resetData(ReadOnlyGeeKeep newData);
 
-    /** 
-     * Updates the filter of the filtered task list to show all tasks 
+    /**
+     * Updates the filter of the filtered task list to show all tasks
      */
     void updateFilteredListToShowAll();
 
     /**
      * Updates the filter of the filtered task list to filter by the given keywords, time and tags
-     * 
+     *
      */
     void updateFilteredTaskList(Set<String> keywords, DateTime earlistTime,
             DateTime latestTime, UniqueTagList tags);
@@ -82,7 +82,7 @@ public interface Model {
     void markTaskDone(int filteredTaskListIndex);
 
     void markTaskUndone(int filteredTaskListIndex);
-    
+
     //@@author A0147622H
     String undo() throws NothingToUndoException;
 
