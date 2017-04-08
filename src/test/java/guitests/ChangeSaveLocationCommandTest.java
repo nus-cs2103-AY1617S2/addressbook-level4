e180ec6294b951a21c7d88251e018db1a0c39427
alvinlyj <alvin_loh@u.nus.edu>
2017-04-08 21:02:11 +0800

    Fixing bugs and codacy in ChangeSaveLocation test

--- src/test/java/guitests/ChangeSaveLocationCommandTest.java
+++ src/test/java/guitests/ChangeSaveLocationCommandTest.java
@@ -14,6 +14,8 @@ import seedu.taskmanager.storage.XmlTaskManagerStorage;
 
 public class ChangeSaveLocationCommandTest extends TaskManagerGuiTest {
 
+    public static final String INVALID_SAVE_LOCATION = "Invalid input for save location";
+/*
     @Rule
     public TemporaryFolder testFolder = new TemporaryFolder();
 
@@ -23,35 +25,34 @@ public class ChangeSaveLocationCommandTest extends TaskManagerGuiTest {
     public void setUp() {
         temporaryFilePath = testFolder.getRoot().getPath();
     }
-
-    public static final String INVALID_SAVE_LOCATION = "Invalid input for save location";
-
+*/
     // @@author A0142418L
     @Test
-    public void changeSaveLocation_invalidCommand() {
+    public void changeSaveLocationInvalidCommand() {
         commandBox.runCommand("SAVEdata");
         assertResultMessage(Messages.MESSAGE_UNKNOWN_COMMAND);
     }
 
     @Test
-    public void changeSaveLocation_invalidSaveLocation() {
+    public void changeSaveLocationInvalidSaveLocation() {
         commandBox.runCommand("SAVE hello");
         assertResultMessage(INVALID_SAVE_LOCATION + "\n" + ChangeSaveLocationCommand.MESSAGE_USAGE);
     }
 
     @Test
-    public void changeSaveLocation_invalidCommandFormat() {
+    public void changeSaveLocationInvalidCommandFormat() {
         commandBox.runCommand("SAVE");
         assertResultMessage(String.format(MESSAGE_INVALID_COMMAND_FORMAT, ChangeSaveLocationCommand.MESSAGE_USAGE));
     }
 
     @Test
-    public void changeSaveLocation_validSaveLocation() {
+    public void changeSaveLocationValidSaveLocation() {
+        String temporaryFilePath = "C:\\Users\\Alvin Loh\\Documents\\NUS Sem 4\\CS2103\\Project\\main\\src\\test\\data"
+                + "\\XmlTaskManagerStorageTest";
         commandBox.runCommand("SAVE " + temporaryFilePath);
         String fileLocation = "Save location changed to: " + temporaryFilePath + "\\" + "taskmanager.xml";
         assertResultMessage(fileLocation);
         assertEquals(temporaryFilePath + "\\" + "taskmanager.xml", XmlTaskManagerStorage.giveTaskManagerFilePath());
         commandBox.runCommand("SAVE data");
     }
-
 }
