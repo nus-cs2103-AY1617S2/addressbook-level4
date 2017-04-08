840c2b03d943f02afb19f67bf7ae4c5d85f1e7ff
Unknown <astrid2242@gmail.com>
2017-04-08 18:32:34 +0800

    pass resetTaskManagerData testcase

---
+++ src/test/java/seedu/task/model/task/TaskTest.java
@@ -0,0 +1,46 @@
+package seedu.task.model.task;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import seedu.task.model.tag.UniqueTagList;
+
+public class TaskTest {
+
+    TaskName name = null;
+    TaskDate date = null;
+    TaskTime start = null;
+    TaskTime end = null;
+    String descrip = null;
+    TaskStatus status = null;
+    UniqueTagList tags = null;
+    Task task = null;
+
+    public void setUp() {
+        try {
+            name = new TaskName("task name");
+            date = new TaskDate("030317-040417");
+            start = new TaskTime("0900");
+            end = new TaskTime("1100");
+            descrip = "description";
+            status = new TaskStatus("Ongoing");
+            tags = new UniqueTagList();
+            task = new Task(name, date, start, end, descrip, status, tags);
+        } catch (Exception e) {
+            System.out.println("Construct Sample Task Fail");
+        }
+    }
+
+    @Test
+    public void TaskEquals() {
+        this.setUp();
+        Task another = new Task(name, date, start, end, descrip, status, tags);
+        assertEquals(this.task,another);
+    }
+
+
+}
