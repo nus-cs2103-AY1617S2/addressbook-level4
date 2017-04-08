48806e15a73a4fe8e7fe5b59a7e23404e461a77e
yoshi-1224 <yoshiaki.bb@gmail.com>
2017-04-08 16:21:19 +0800

    Put back helpWindowTest, Sample Data Test

--- src/test/java/guitests/HelpWindowTest.java
+++ src/test/java/guitests/HelpWindowTest.java
@@ -1,40 +1,29 @@
-//package guitests;
-//
-//import static org.junit.Assert.assertFalse;
-//import static org.junit.Assert.assertTrue;
-//
-//import org.junit.Test;
-//
-//import guitests.guihandles.HelpWindowHandle;
-//
-//public class HelpWindowTest extends AddressBookGuiTest {
-//
-//    @Test
-//    public void openHelpWindow() {
-//        //use accelerator
-//        commandBox.clickOnTextField();
-//        assertHelpWindowOpen(mainMenu.openHelpWindowUsingAccelerator());
-//
-//        resultDisplay.clickOnTextArea();
-//        assertHelpWindowOpen(mainMenu.openHelpWindowUsingAccelerator());
-//
-//        personListPanel.clickOnListView();
-//        assertHelpWindowOpen(mainMenu.openHelpWindowUsingAccelerator());
-//
-//        //use menu button
-//        //assertHelpWindowOpen(mainMenu.openHelpWindowUsingMenu());
-//
-//        //use command
-//        //assertHelpWindowOpen(commandBox.runHelpCommand());
-//    }
-//
-//    private void assertHelpWindowOpen(HelpWindowHandle helpWindowHandle) {
-//        assertTrue(helpWindowHandle.isWindowOpen());
-//        helpWindowHandle.closeWindow();
-//    }
-//
-//    private void assertHelpWindowNotOpen(HelpWindowHandle helpWindowHandle) {
-//        assertFalse(helpWindowHandle.isWindowOpen());
-//    }
-//
-//}
+package guitests;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import guitests.guihandles.HelpWindowHandle;
+
+public class HelpWindowTest extends TaskcrusherGuiTest {
+
+    @Test
+    public void openHelpWindow() {
+        //use accelerator
+        commandBox.clickOnTextField();
+        assertHelpWindowOpen(mainMenu.openHelpWindowUsingAccelerator());
+
+        resultDisplay.clickOnTextArea();
+        assertHelpWindowOpen(mainMenu.openHelpWindowUsingAccelerator());
+
+        //use command
+        assertHelpWindowOpen(commandBox.runHelpCommand());
+    }
+
+    private void assertHelpWindowOpen(HelpWindowHandle helpWindowHandle) {
+        assertTrue(helpWindowHandle.isWindowOpen());
+        helpWindowHandle.closeWindow();
+    }
+
+}
--- src/test/java/guitests/SampleDataTest.java
+++ src/test/java/guitests/SampleDataTest.java
@@ -1,30 +1,36 @@
-//package guitests;
-//
-//import static org.junit.Assert.assertTrue;
-//
-//import org.junit.Test;
-//
-//import project.taskcrusher.model.UserInbox;
-//import project.taskcrusher.model.task.Task;
-//import project.taskcrusher.model.util.SampleDataUtil;
-//import project.taskcrusher.testutil.TestUtil;
-//
-//public class SampleDataTest extends AddressBookGuiTest {
-//    @Override
-//    protected UserInbox getInitialData() {
-//        // return null to force test app to load data from file only
-//        return null;
-//    }
-//
-//    @Override
-//    protected String getDataFileLocation() {
-//        // return a non-existent file location to force test app to load sample data
-//        return TestUtil.getFilePathInSandboxFolder("SomeFileThatDoesNotExist1234567890.xml");
-//    }
-//
-//    @Test
-//    public void addressBook_dataFileDoesNotExist_loadSampleData() throws Exception {
-//        Task[] expectedList = SampleDataUtil.getSampleTasks();
-//        assertTrue(userInboxPanel.isListMatching(expectedList));
-//    }
-//}
+package guitests;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.ocpsoft.prettytime.shade.edu.emory.mathcs.backport.java.util.Arrays;
+
+import project.taskcrusher.model.UserInbox;
+import project.taskcrusher.model.event.Event;
+import project.taskcrusher.model.task.Task;
+import project.taskcrusher.model.util.SampleDataUtil;
+import project.taskcrusher.testutil.TestUtil;
+
+public class SampleDataTest extends TaskcrusherGuiTest {
+    @Override
+    protected UserInbox getInitialData() {
+        // return null to force test app to load data from file only
+        return null;
+    }
+
+    @Override
+    protected String getDataFileLocation() {
+        // return a non-existent file location to force test app to load sample data
+        return TestUtil.getFilePathInSandboxFolder("SomeFileThatDoesNotExist1234567890.xml");
+    }
+
+    @Test
+    public void addressBook_dataFileDoesNotExist_loadSampleData() throws Exception {
+        Task[] expectedTaskList = SampleDataUtil.getSampleTasks();
+        Event[] expectedEventList = SampleDataUtil.getSampleEvents();
+        Arrays.sort(expectedTaskList);
+        Arrays.sort(expectedEventList);
+        assertTrue(userInboxPanel.isListMatching(expectedTaskList));
+        assertTrue(userInboxPanel.isListMatching(expectedEventList));
+    }
+}
