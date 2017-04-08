af4c296b5891fbc24222c3e22985d9f9dce2bf8a
mynameisyz <yanzhang_shi@outlook.com>
2017-04-08 23:52:17 +0800

    Add test case for SyncManager

---
+++ src/test/java/seedu/opus/sync/SyncManagerTest.java
@@ -0,0 +1,42 @@
+package seedu.opus.sync;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import seedu.opus.model.task.DateTime;
+import seedu.opus.model.task.Task;
+
+public class SyncManagerTest {
+    private SyncManager syncManager;
+    private SyncService mockService;
+    
+    @Before
+    public void setUp() {
+        mockService = mock(SyncServiceGtask.class);
+        syncManager = new SyncManager(mockService);
+    }
+    
+    @Test
+    public void syncManagerUpdateTaskListExpectSyncServiceUpdateListSuccessful() {
+        Task mockTask = mock(Task.class);
+        Optional<DateTime> mockStartDateTime = Optional.ofNullable(null);
+        Optional<DateTime> mockEndDateTime = Optional.of(mock(DateTime.class));
+        when(mockTask.getStartTime()).thenReturn(mockStartDateTime);
+        when(mockTask.getEndTime()).thenReturn(mockStartDateTime);
+        
+        List<Task> list = new ArrayList();
+        list.add(mockTask);
+        
+        syncManager.updateTaskList(list);
+        verify(mockService).updateTaskList(list);
+    }
+    
+}
