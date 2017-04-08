14b5e172cf990503878ba2b0d30585a2125d48d6
Tyler Rocha <tyler.rocha@utexas.edu>
2017-04-08 23:02:22 +0800

    use task list not address book in TestApp

--- src/test/java/seedu/task/TestApp.java
+++ src/test/java/seedu/task/TestApp.java
@@ -8,7 +8,7 @@ import seedu.task.commons.core.Config;
 import seedu.task.commons.core.GuiSettings;
 import seedu.task.model.ReadOnlyTaskList;
 import seedu.task.model.UserPrefs;
-import seedu.task.storage.XmlSerializableAddressBook;
+import seedu.task.storage.XmlSerializableTaskList;
 import seedu.task.testutil.TestUtil;
 
 /**
@@ -20,8 +20,8 @@ public class TestApp extends MainApp {
     public static final String SAVE_LOCATION_FOR_TESTING = TestUtil.getFilePathInSandboxFolder("sampleData.xml");
     protected static final String DEFAULT_PREF_FILE_LOCATION_FOR_TESTING =
             TestUtil.getFilePathInSandboxFolder("pref_testing.json");
-    public static final String APP_TITLE = "Test App";
-    protected static final String ADDRESS_BOOK_NAME = "Test";
+    public static final String APP_TITLE = "Task Manager Test App";
+    protected static final String TASK_MANAGER_NAME = "Task Manager Test";
     protected Supplier<ReadOnlyTaskList> initialDataSupplier = () -> null;
     protected String saveFileLocation = SAVE_LOCATION_FOR_TESTING;
 
@@ -36,7 +36,7 @@ public class TestApp extends MainApp {
         // If some initial local data has been provided, write those to the file
         if (initialDataSupplier.get() != null) {
             TestUtil.createDataFileWithData(
-                    new XmlSerializableAddressBook(this.initialDataSupplier.get()),
+                    new XmlSerializableTaskList(this.initialDataSupplier.get()),
                     this.saveFileLocation);
         }
     }
@@ -47,7 +47,7 @@ public class TestApp extends MainApp {
         config.setAppTitle(APP_TITLE);
         config.setTaskManagerFilePath(saveFileLocation);
         config.setUserPrefsFilePath(DEFAULT_PREF_FILE_LOCATION_FOR_TESTING);
-        config.setTaskManagerName(ADDRESS_BOOK_NAME);
+        config.setTaskManagerName(TASK_MANAGER_NAME);
         return config;
     }
 
