e08069771ff1fca2df3af77741306dc00d7f0723
Dion Ng Wei Jie <dnwj1994@gmail.com>
2017-04-08 17:09:07 +0800

    updated some tests for the model items

--- src/main/java/org/teamstbf/yats/model/item/SimpleDate.java
+++ src/main/java/org/teamstbf/yats/model/item/SimpleDate.java
@@ -5,55 +5,55 @@ import org.teamstbf.yats.commons.exceptions.IllegalValueException;
 /**
  * Represents a Task's Date in the task manager. Guarantees: immutable; is valid
  * as declared in {@link #isValidDeadline(String)}
- *
+ * NOT USED!!!!!!!!!
  * @Deprecated use {@link #Schedule()} instead
  */
 @Deprecated
 public class SimpleDate {
 
-    public static final String MESSAGE_DEADLINE_CONSTRAINTS = "Task deadline should only be in the format dd/mm/yyyy";
-    public static final String DEADLINE_VALIDATION_REGEX = "(^[0-3][0-9]/[0-3][0-9]/(?:[0-9][0-9])?[0-9][0-9]$|^(?![\\s\\S]))";
+	public static final String MESSAGE_DEADLINE_CONSTRAINTS = "Task deadline should only be in the format dd/mm/yyyy";
+	public static final String DEADLINE_VALIDATION_REGEX = "(^[0-3][0-9]/[0-3][0-9]/(?:[0-9][0-9])?[0-9][0-9]$|^(?![\\s\\S]))";
 
-    public final String value;
+	public final String value;
 
-    /**
-     * Validates given phone number.
-     *
-     * @throws IllegalValueException
-     *             if given phone string is invalid.
-     */
-    public SimpleDate(String deadline) throws IllegalValueException {
-        assert deadline != null;
-        String trimmedDeadline = deadline.trim();
-        if (!isValidDeadline(trimmedDeadline)) {
-            throw new IllegalValueException(MESSAGE_DEADLINE_CONSTRAINTS);
-        }
-        this.value = trimmedDeadline;
-    }
+	/**
+	 * Validates given phone number.
+	 *
+	 * @throws IllegalValueException
+	 *             if given phone string is invalid.
+	 */
+	public SimpleDate(String deadline) throws IllegalValueException {
+		assert deadline != null;
+		String trimmedDeadline = deadline.trim();
+		if (!isValidDeadline(trimmedDeadline)) {
+			throw new IllegalValueException(MESSAGE_DEADLINE_CONSTRAINTS);
+		}
+		this.value = trimmedDeadline;
+	}
 
-    /**
-     * Returns true if a given string is a valid task date.
-     */
-    public static boolean isValidDeadline(String test) {
-        return test.matches(DEADLINE_VALIDATION_REGEX);
-    }
+	/**
+	 * Returns true if a given string is a valid task date.
+	 */
+	public static boolean isValidDeadline(String test) {
+		return test.matches(DEADLINE_VALIDATION_REGEX);
+	}
 
-    @Override
-    public String toString() {
-        return value;
-    }
+	@Override
+	public String toString() {
+		return value;
+	}
 
-    @Override
-    public boolean equals(Object other) {
-        return other == this // short circuit if same object
-                || (other instanceof SimpleDate // instanceof handles nulls
-                        && this.value.equals(((SimpleDate) other).value)); // state
-        // check
-    }
+	@Override
+	public boolean equals(Object other) {
+		return other == this // short circuit if same object
+				|| (other instanceof SimpleDate // instanceof handles nulls
+						&& this.value.equals(((SimpleDate) other).value)); // state
+		// check
+	}
 
-    @Override
-    public int hashCode() {
-        return value.hashCode();
-    }
+	@Override
+	public int hashCode() {
+		return value.hashCode();
+	}
 
 }
