b0df90ab2e40c612941eb7c286cd7f9b742d39e5
Fyelight <kaid.fyelight@hotmail.com>
2017-04-08 22:30:34 +0800

    Fix throw exception for empty p/ and n/

--- src/main/java/seedu/doit/logic/commands/FindCommand.java
+++ src/main/java/seedu/doit/logic/commands/FindCommand.java
@@ -4,17 +4,18 @@ import java.util.Set;
 
 //@@author A0146809W
 /**
- * Finds and lists all tasks in task manager whose name contains any of the argument keywords.
- * Keyword matching is case sensitive.
+ * Finds and lists all tasks in task manager whose name contains any of the
+ * argument keywords. Keyword matching is case sensitive.
  */
 public class FindCommand extends Command {
 
     public static final String COMMAND_WORD = "find";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all tasks whose names contain any of "
-        + "the specified keywords (case-sensitive) and displays them as a list with index numbers.\n"
-        + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
-        + "Example: " + COMMAND_WORD + " Task A Project 3 Print 10 pages";
+            + "the specified keywords (case-sensitive) and displays them as a list with index numbers.\n"
+            + "Parameters: n/TASK NAME p/PRIORITY s/START DATE&TIME e/END DATE&TIME  d/ADDITIONAL DESCRIPTION [t/TAG]...\n"
+            + "Example: " + COMMAND_WORD + " n/Task A p/high e/ 12/1/2020 23:59\n" + COMMAND_WORD
+            + " p/low s/ 12/1/2020 23:59\n" + COMMAND_WORD + " t/project";;
 
     private final Set<String> namekeywords;
     private final Set<String> startTimekeywords;
@@ -24,7 +25,7 @@ public class FindCommand extends Command {
     private final Set<String> desckeywords;
 
     public FindCommand(Set<String> namekeywords, Set<String> startTimekeywords, Set<String> endTimekeywords,
-                       Set<String> prioritykeywords, Set<String> tagskeywords, Set<String> desckeywords) {
+            Set<String> prioritykeywords, Set<String> tagskeywords, Set<String> desckeywords) {
         this.namekeywords = namekeywords;
         this.startTimekeywords = startTimekeywords;
         this.endTimekeywords = endTimekeywords;
@@ -35,9 +36,9 @@ public class FindCommand extends Command {
 
     @Override
     public CommandResult execute() {
-        model.updateFilteredTaskList(namekeywords, prioritykeywords, desckeywords, tagskeywords, startTimekeywords,
-                endTimekeywords);
-        return new CommandResult(getMessageForTaskListShownSummary(model.getFilteredTaskList().size()));
+        this.model.updateFilteredTaskList(this.namekeywords, this.prioritykeywords, this.desckeywords,
+                this.tagskeywords, this.startTimekeywords, this.endTimekeywords);
+        return new CommandResult(getMessageForTaskListShownSummary(this.model.getFilteredTaskList().size()));
     }
 
 }
--- src/main/java/seedu/doit/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/doit/logic/parser/FindCommandParser.java
@@ -11,12 +11,14 @@ import static seedu.doit.logic.parser.CliSyntax.PREFIX_TAG;
 
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.regex.Matcher;
 
 import seedu.doit.logic.commands.Command;
 import seedu.doit.logic.commands.FindCommand;
 import seedu.doit.logic.commands.IncorrectCommand;
+
 //@@author A0146809W
 /**
  * Parses input arguments and creates a new FindCommand object
@@ -34,44 +36,56 @@ public class FindCommandParser implements CommandParser {
         ArgumentTokenizer argsTokenizer = new ArgumentTokenizer(PREFIX_NAME, PREFIX_PRIORITY, PREFIX_START, PREFIX_END,
                 PREFIX_DESCRIPTION, PREFIX_TAG);
         argsTokenizer.tokenize(args);
-        Set<String> nameKeyWordSet = new HashSet<>(
-                argsTokenizer.getAllValuesAfterPrefixAndSpaces(PREFIX_NAME).orElse(Collections.emptyList()));
-        Set<String> priorityKeyWordSet = new HashSet<>(
-                argsTokenizer.getAllValuesAfterPrefixAndSpaces(PREFIX_PRIORITY).orElse(Collections.emptyList()));
-        Set<String> startKeyWordSet = new HashSet<>(
-                argsTokenizer.getAllValuesAfterPrefixAndSpaces(PREFIX_START).orElse(Collections.emptyList()));
-        Set<String> deadlineKeyWordSet = new HashSet<>(
-                argsTokenizer.getAllValuesAfterPrefixAndSpaces(PREFIX_END).orElse(Collections.emptyList()));
-        Set<String> tagsKeyWordSet = new HashSet<>(
-                argsTokenizer.getAllValuesAfterPrefixAndSpaces(PREFIX_TAG).orElse(Collections.emptyList()));
-        Set<String> descKeyWordSet = new HashSet<>(
-                argsTokenizer.getAllValuesAfterPrefixAndSpaces(PREFIX_DESCRIPTION).orElse(Collections.emptyList()));
+        List<String> nameKeyWords = argsTokenizer.getAllValuesAfterPrefixAndSpaces(PREFIX_NAME)
+                .orElse(Collections.emptyList());
+        Set<String> nameKeyWordSet = new HashSet<>(nameKeyWords);
+        List<String> priorityKeyWord = argsTokenizer.getAllValuesAfterPrefixAndSpaces(PREFIX_PRIORITY)
+                .orElse(Collections.emptyList());
+        Set<String> priorityKeyWordSet = new HashSet<>(priorityKeyWord);
+        List<String> startTimeKeyWords = argsTokenizer.getAllValuesAfterPrefixAndSpaces(PREFIX_START)
+                .orElse(Collections.emptyList());
+        Set<String> startKeyWordSet = new HashSet<>(startTimeKeyWords);
+        List<String> endTimeKeyWords = argsTokenizer.getAllValuesAfterPrefixAndSpaces(PREFIX_END)
+                .orElse(Collections.emptyList());
+        Set<String> endKeyWordSet = new HashSet<>(endTimeKeyWords);
+        List<String> tagsKeyWords = argsTokenizer.getAllValuesAfterPrefixAndSpaces(PREFIX_TAG)
+                .orElse(Collections.emptyList());
+        Set<String> tagsKeyWordSet = new HashSet<>(tagsKeyWords);
+        List<String> descriptionKeyWords = argsTokenizer.getAllValuesAfterPrefixAndSpaces(PREFIX_DESCRIPTION)
+                .orElse(Collections.emptyList());
+        Set<String> descKeyWordSet = new HashSet<>(descriptionKeyWords);
 
+        if (isAllKeyWordSetEmpty(nameKeyWordSet, priorityKeyWordSet, startKeyWordSet, endKeyWordSet, tagsKeyWordSet,
+                descKeyWordSet)) {
+            return new IncorrectCommand(String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+        }
 
-        if (isAllKeyWordSetEmpty(nameKeyWordSet, priorityKeyWordSet, startKeyWordSet,
-            deadlineKeyWordSet, tagsKeyWordSet, descKeyWordSet)) {
-            return new IncorrectCommand(
-                String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+        if (nameKeyWords.contains("")) {
+            return new IncorrectCommand(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                    "There must be a name present after n/.\nExample: find n/Project Work"));
+        }
+        if (priorityKeyWord.contains("")) {
+            return new IncorrectCommand(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                    "There must be a priority present after p/.\nExample: find p/high"));
         }
 
         final Matcher matcher = KEYWORDS_ARGS_FORMAT.matcher(args.trim());
         if (!matcher.matches()) {
-            return new IncorrectCommand(
-                String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+            return new IncorrectCommand(String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
         }
 
         // keywords delimited by whitespace
-        return new FindCommand(nameKeyWordSet, startKeyWordSet, deadlineKeyWordSet,
-            priorityKeyWordSet, tagsKeyWordSet, descKeyWordSet);
+        return new FindCommand(nameKeyWordSet, startKeyWordSet, endKeyWordSet, priorityKeyWordSet, tagsKeyWordSet,
+                descKeyWordSet);
 
     }
 
-    private boolean isAllKeyWordSetEmpty (Set<String> nameKeyWordSet, Set<String> priorityKeyWordSet,
-                                          Set<String> startKeyWordSet, Set<String> deadlineKeyWordSet,
-                                          Set<String> tagsKeyWordSet, Set<String> descKeyWordSet) {
+    private boolean isAllKeyWordSetEmpty(Set<String> nameKeyWordSet, Set<String> priorityKeyWordSet,
+            Set<String> startKeyWordSet, Set<String> deadlineKeyWordSet, Set<String> tagsKeyWordSet,
+            Set<String> descKeyWordSet) {
 
         return nameKeyWordSet.isEmpty() && priorityKeyWordSet.isEmpty() && startKeyWordSet.isEmpty()
-            && deadlineKeyWordSet.isEmpty() && tagsKeyWordSet.isEmpty() && descKeyWordSet.isEmpty();
+                && deadlineKeyWordSet.isEmpty() && tagsKeyWordSet.isEmpty() && descKeyWordSet.isEmpty();
     }
 
 }
