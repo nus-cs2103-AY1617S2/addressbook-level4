c36fc13e07894f6c48b0461c8e521741156238ed
mynameisyz <yanzhang_shi@outlook.com>
2017-04-08 22:10:48 +0800

    Moved SyncCommand test cases to new class

---
+++ src/test/java/seedu/opus/logic/SyncCommandTest.java
@@ -0,0 +1,54 @@
+package seedu.opus.logic;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.rules.TemporaryFolder;
+
+import seedu.opus.logic.commands.CommandResult;
+import seedu.opus.logic.commands.SyncCommand;
+import seedu.opus.logic.commands.exceptions.CommandException;
+import seedu.opus.model.Model;
+import seedu.opus.storage.StorageManager;
+
+public class SyncCommandTest {
+
+    @Rule
+    public TemporaryFolder saveFolder = new TemporaryFolder();
+
+    private Logic logic;
+    private Model mockModel = mock(Model.class);
+
+    @Before
+    public void setup() {
+        String tempTaskManagerFile = saveFolder.getRoot().getPath() + "TempTaskManager.xml";
+        String tempPreferencesFile = saveFolder.getRoot().getPath() + "TempPreferences.json";
+        logic = new LogicManager(mockModel, new StorageManager(tempTaskManagerFile, tempPreferencesFile));
+    }
+
+    @Test
+    public void executeSyncCommandWithValidOnArgumentSuccess() throws CommandException {
+        CommandResult result = logic.execute("sync on");
+        assertEquals(result.feedbackToUser, SyncCommand.MESSAGE_SYNC_ON_SUCCESS);
+    }
+
+    @Test
+    public void executeSyncCommandWithValidOffArgumentSuccess() throws CommandException {
+        CommandResult result = logic.execute("sync off");
+        assertEquals(result.feedbackToUser, SyncCommand.MESSAGE_SYNC_OFF_SUCCESS);
+    }
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void executeSyncCommandWithInvalidArgumentRaiseException() throws CommandException {
+        thrown.expect(CommandException.class);
+        thrown.expectMessage(SyncCommand.MESSAGE_USAGE);
+        logic.execute("sync invalid args");
+    }
+}
