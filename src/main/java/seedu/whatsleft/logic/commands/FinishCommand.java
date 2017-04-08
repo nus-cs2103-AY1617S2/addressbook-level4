3a717ba1406291906b749fb3fb13a5da6440dfbd
Zihan Li <lizihan97@gmail.com>
2017-04-09 01:21:31 +0800

    jump to the task when show all

--- src/main/java/seedu/whatsleft/logic/commands/FinishCommand.java
+++ src/main/java/seedu/whatsleft/logic/commands/FinishCommand.java
@@ -1,7 +1,9 @@
 package seedu.whatsleft.logic.commands;
 
+import seedu.whatsleft.commons.core.EventsCenter;
 import seedu.whatsleft.commons.core.Messages;
 import seedu.whatsleft.commons.core.UnmodifiableObservableList;
+import seedu.whatsleft.commons.events.ui.JumpToTaskListRequestEvent;
 import seedu.whatsleft.logic.commands.exceptions.CommandException;
 import seedu.whatsleft.model.ModelManager;
 import seedu.whatsleft.model.ReadOnlyWhatsLeft;
@@ -45,8 +47,12 @@ public class FinishCommand extends Command {
         } catch (TaskNotFoundException pnfe) {
             assert false : "The target task cannot be missing";
         }
+
+        //@@author A0148038A
+        if (model.getDisplayStatus() == "ALL") {
+            EventsCenter.getInstance().post(new JumpToTaskListRequestEvent(filteredActivityListIndex - 1));
+        }
         model.storePreviousCommand("finish");
         return new CommandResult(String.format(MESSAGE_FINISH_TASK_SUCCESS, taskToComplete));
     }
-    //@@author
 }
