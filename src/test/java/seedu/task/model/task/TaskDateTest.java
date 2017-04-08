4b10122052e19ed2236dc08c188809e15d3e7247
Edwin\admin <torres12318@hotmail.com>
2017-04-08 13:06:51 +0800

    fixed indentations

--- src/test/java/seedu/task/model/task/TaskDateTest.java
+++ src/test/java/seedu/task/model/task/TaskDateTest.java
@@ -4,37 +4,36 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 import seedu.task.model.task.TaskDate;
 
-
 public class TaskDateTest {
 
-	@Test
-	public void validDate() {
+    @Test
+    public void validDate() {
 
-		// valid dates
-		try {
-			assertEquals("4/4/17",new TaskDate("040417").toString());
-			assertEquals("4/4/17 - 5/5/17",new TaskDate("040417 050517").toString());
-			assertEquals("4/4/17 - 5/5/17",new TaskDate("040417-050517").toString());
+        // valid dates
+        try {
+            assertEquals("4/4/17", new TaskDate("040417").toString());
+            assertEquals("4/4/17 - 5/5/17", new TaskDate("040417 050517").toString());
+            assertEquals("4/4/17 - 5/5/17", new TaskDate("040417-050517").toString());
 
-			TaskDate date = new TaskDate("tmr");
-			date = new TaskDate("today tmr");
-			date = new TaskDate("next Saturday");
+            TaskDate date = new TaskDate("tmr");
+            date = new TaskDate("today tmr");
+            date = new TaskDate("next Saturday");
 
-		} catch (Exception e) {
-			fail("Valid Dates can not be parsed");
-		}
+        } catch (Exception e) {
+            fail("Valid Dates can not be parsed");
+        }
 
-		// invalid dates
-		try {
-			TaskDate date = new TaskDate("030317");
-			date = new TaskDate("this is an invalid input");
-			date = new TaskDate("999999");
-			date = new TaskDate("030317 020217");
+        // invalid dates
+        try {
+            TaskDate date = new TaskDate("030317");
+            date = new TaskDate("this is an invalid input");
+            date = new TaskDate("999999");
+            date = new TaskDate("030317 020217");
 
-			fail("invalid date is parsed");
-		} catch (Exception e) {
+            fail("invalid date is parsed");
+        } catch (Exception e) {
 
-		}
-	}
+        }
+    }
 
 }
