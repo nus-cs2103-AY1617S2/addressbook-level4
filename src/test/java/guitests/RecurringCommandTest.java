e25f0ac279c42cd2106fc52b95cab35b1b6739ec
Joelleejh <bossleejh@gmail.com>
2017-04-08 18:10:53 +0800

    Fixing coverage V0.5

--- src/test/java/guitests/RecurringCommandTest.java
+++ src/test/java/guitests/RecurringCommandTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import seedu.taskmanager.commons.core.Messages;
 import seedu.taskmanager.logic.commands.RecurringCommand;
 import seedu.taskmanager.testutil.TestTask;
 
@@ -11,11 +12,20 @@ public class RecurringCommandTest extends TaskManagerGuiTest {
 
     // @@author A0141102H
     @Test
-    public void recurFloatingTaskFailure() {
+    public void recurTaskFailure() {
         TestTask[] currentList = td.getTypicalTasks();
         int targetIndex = currentList.length;
         commandBox.runCommand("RECUR " + targetIndex + " 3 days");
         assertResultMessage(RecurringCommand.MESSAGE_RECURRING_FLOATING_TASK_FAILURE);
+
+        commandBox.runCommand("RECUR 1000 4 days");
+        assertResultMessage(Messages.MESSAGE_INVALID_TASK_DISPLAYED_INDEX);
+
+        commandBox.runCommand("RECUR");
+        assertResultMessage(String.format(Messages.MESSAGE_INVALID_COMMAND_FORMAT, RecurringCommand.MESSAGE_USAGE));
+
+        commandBox.runCommand("RECUR 4 DAYS 1000");
+        assertResultMessage(String.format(Messages.MESSAGE_INVALID_COMMAND_FORMAT, RecurringCommand.MESSAGE_USAGE));
     }
 
     @Test
