bc98e3a3511e237231a2539cdea90a63b9926b0e
Edwin\admin <torres12318@hotmail.com>
2017-04-08 23:24:17 +0800

    trailing whitespaces

--- src/main/java/seedu/task/model/task/TaskDateBeforePredicate.java
+++ src/main/java/seedu/task/model/task/TaskDateBeforePredicate.java
@@ -4,33 +4,32 @@ import java.util.function.Predicate;
 
 import seedu.task.commons.exceptions.IllegalValueException;
 
-//import seedu.task.testutil.TaskBuilder;
-
 public class TaskDateBeforePredicate implements Predicate<Object> {
-	public static final String PREDICATE_WORD = "before";
-    //@@author A0163845X
+    public static final String PREDICATE_WORD = "before";
+    // @@author A0163845X
 
-	private TaskDate date;
-	
-	public TaskDateBeforePredicate(String date) throws IllegalValueException {
-		this.date = new TaskDate(date);
-		System.out.println(date);
-	}
-	@Override
-	public boolean test(Object arg0) {
-		
-		Task task = (Task) arg0;
-		System.out.println(date);
+    private TaskDate date;
 
-		System.out.println(task.getTaskDate());
-		if (task.getTaskDate() != null)
-			System.out.println(task.getTaskDate().compareTo(date));
+    public TaskDateBeforePredicate(String date) throws IllegalValueException {
+        this.date = new TaskDate(date);
+        System.out.println(date);
+    }
 
-		if (task.getTaskDate() != null && date.compareTo(task.getTaskDate()) >= 0) {
-			return true;
-		} else {
-			return false;
-		}
-	}
+    @Override
+    public boolean test(Object arg0) {
+
+        Task task = (Task) arg0;
+        System.out.println(date);
+
+        System.out.println(task.getTaskDate());
+        if (task.getTaskDate() != null)
+            System.out.println(task.getTaskDate().compareTo(date));
+
+        if (task.getTaskDate() != null && date.compareTo(task.getTaskDate()) >= 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
 }
