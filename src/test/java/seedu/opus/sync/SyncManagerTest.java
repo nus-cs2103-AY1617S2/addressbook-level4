1c1212d3ed2ba0a25ac1c01c19df30272b81d0fd
mynameisyz <yanzhang_shi@outlook.com>
2017-04-09 00:03:53 +0800

    Add test case for SyncManager

--- src/test/java/seedu/opus/sync/SyncManagerTest.java
+++ src/test/java/seedu/opus/sync/SyncManagerTest.java
@@ -1,8 +1,8 @@
 package seedu.opus.sync;
 
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,30 +13,43 @@ import org.junit.Test;
 
 import seedu.opus.model.task.DateTime;
 import seedu.opus.model.task.Task;
+import seedu.opus.sync.exceptions.SyncException;
 
+//@author A0148087W
 public class SyncManagerTest {
     private SyncManager syncManager;
     private SyncService mockService;
-    
+
     @Before
     public void setUp() {
         mockService = mock(SyncServiceGtask.class);
         syncManager = new SyncManager(mockService);
     }
-    
+
     @Test
-    public void syncManagerUpdateTaskListExpectSyncServiceUpdateListSuccessful() {
+    public void syncManagerStartSyncExpectSyncServiceStart() throws SyncException {
+        syncManager.startSync();
+        verify(mockService).start();
+    }
+
+    @Test
+    public void syncManagerStopSyncExpectSyncServiceStop() throws SyncException {
+        syncManager.stopSync();
+        verify(mockService).stop();
+    }
+
+    @Test
+    public void syncManagerUpdateTaskListWithValidTaskExpectSyncServiceUpdateListSuccessful() {
         Task mockTask = mock(Task.class);
         Optional<DateTime> mockStartDateTime = Optional.ofNullable(null);
         Optional<DateTime> mockEndDateTime = Optional.of(mock(DateTime.class));
         when(mockTask.getStartTime()).thenReturn(mockStartDateTime);
-        when(mockTask.getEndTime()).thenReturn(mockStartDateTime);
-        
-        List<Task> list = new ArrayList();
+        when(mockTask.getEndTime()).thenReturn(mockEndDateTime);
+
+        List<Task> list = new ArrayList<Task>();
         list.add(mockTask);
-        
+
         syncManager.updateTaskList(list);
         verify(mockService).updateTaskList(list);
     }
-    
 }
