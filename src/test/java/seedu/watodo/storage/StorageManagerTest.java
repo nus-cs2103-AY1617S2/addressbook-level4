62eb18c97456b66647f735f56283487a84738972
Megan Quek <meganquek125@gmail.com>
2017-04-09 11:29:24 +0800

    Temp commit to merge update

--- src/test/java/seedu/watodo/storage/StorageManagerTest.java
+++ src/test/java/seedu/watodo/storage/StorageManagerTest.java
@@ -72,7 +72,7 @@ public class StorageManagerTest {
 
     @Test
     public void handleTaskListChangedEvent_exceptionThrown_eventRaised() throws IOException {
-        // Create a StorageManager while injecting a stub that  throws an exception when the save method is called
+        // Create a StorageManager while injecting a stub that throws an exception when the save method is called
         Storage storage = new StorageManager(new XmlTaskListStorageExceptionThrowingStub("dummy"),
                 new JsonUserPrefsStorage("dummy"));
         EventsCollector eventCollector = new EventsCollector();
@@ -95,6 +95,5 @@ public class StorageManagerTest {
             throw new IOException("dummy exception");
         }
     }
-
 
 }
