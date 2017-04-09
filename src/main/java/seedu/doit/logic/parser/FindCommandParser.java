2c3276b025a10edfe3134d4eb4b6065b095a57d2
Fyelight <kaid.fyelight@hotmail.com>
2017-04-09 12:10:15 +0800

    Fix find command with prefix bug and add test cases for failure

--- src/main/java/seedu/doit/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/doit/logic/parser/FindCommandParser.java
@@ -25,6 +25,20 @@ import seedu.doit.logic.commands.IncorrectCommand;
  */
 public class FindCommandParser implements CommandParser {
 
+    public static final String NO_NAME_AFTER_PREFIX = String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+            "There must be a name present after n/.\nExample: find n/Project Work");
+    public static final String NO_PRIORITY_AFTER_PREFIX = String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+            "There must be at least a valid priority present after p/. Valid priorities are high, med and low only.\n"
+                    + "Examples:\nfind p/high\nfind p/high low");
+    public static final String NO_STARTTIME_AFTER_PREFIX = String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+            "There must be at least a valid start time or date present after s/ in a MM/DD/YYYY HH:MM format.\n"
+                    + "Examples:\nfind s/23:59\nfind s/04/31/2020\nfind s/04/31/2020 23:59");
+    public static final String NO_TAGS_AFTER_PREFIX = String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+            "There must be a tag name present after t/.\nExample: find t/Friends");
+    public static final String NO_ENDTIME_AFTER_PREFIX = String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+            "There must be at least a valid end time or date present after e/ in a MM/DD/YYYY HH:MM format.\n"
+                    + "Examples:\nfind e/23:59\nfind e/04/31/2020\nfind e/04/31/2020 23:59");
+
     /**
      * Parses the given {@code String} of arguments in the context of the
      * FindCommand and returns an FindCommand object for execution.
@@ -39,9 +53,9 @@ public class FindCommandParser implements CommandParser {
         List<String> nameKeyWords = argsTokenizer.getAllValuesAfterPrefixAndSpaces(PREFIX_NAME)
                 .orElse(Collections.emptyList());
         Set<String> nameKeyWordSet = new HashSet<>(nameKeyWords);
-        List<String> priorityKeyWord = argsTokenizer.getAllValuesAfterPrefixAndSpaces(PREFIX_PRIORITY)
+        List<String> priorityKeyWords = argsTokenizer.getAllValuesAfterPrefixAndSpaces(PREFIX_PRIORITY)
                 .orElse(Collections.emptyList());
-        Set<String> priorityKeyWordSet = new HashSet<>(priorityKeyWord);
+        Set<String> priorityKeyWordSet = new HashSet<>(priorityKeyWords);
         List<String> startTimeKeyWords = argsTokenizer.getAllValuesAfterPrefixAndSpaces(PREFIX_START)
                 .orElse(Collections.emptyList());
         Set<String> startKeyWordSet = new HashSet<>(startTimeKeyWords);
@@ -61,12 +75,19 @@ public class FindCommandParser implements CommandParser {
         }
 
         if (nameKeyWords.contains("")) {
-            return new IncorrectCommand(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-                    "There must be a name present after n/.\nExample: find n/Project Work"));
+            return new IncorrectCommand(NO_NAME_AFTER_PREFIX);
         }
-        if (priorityKeyWord.contains("")) {
-            return new IncorrectCommand(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-                    "There must be a priority present after p/.\nExample: find p/high"));
+        if (!isValidPriorityKeyWordsPresent(priorityKeyWords, priorityKeyWordSet)) {
+            return new IncorrectCommand(NO_PRIORITY_AFTER_PREFIX);
+        }
+        if (startTimeKeyWords.contains("")) {
+            return new IncorrectCommand(NO_STARTTIME_AFTER_PREFIX);
+        }
+        if (endTimeKeyWords.contains("")) {
+            return new IncorrectCommand(NO_ENDTIME_AFTER_PREFIX);
+        }
+        if (tagsKeyWords.contains("")) {
+            return new IncorrectCommand(NO_TAGS_AFTER_PREFIX);
         }
 
         final Matcher matcher = KEYWORDS_ARGS_FORMAT.matcher(args.trim());
@@ -78,6 +99,27 @@ public class FindCommandParser implements CommandParser {
         return new FindCommand(nameKeyWordSet, startKeyWordSet, endKeyWordSet, priorityKeyWordSet, tagsKeyWordSet,
                 descKeyWordSet);
 
+    }
+
+    /**
+     * Checks if the priority keywords inputed by user is valid
+     *
+     * @param priorityKeyWords
+     * @param priorityKeyWordSet
+     * @return true if high med and low are either present in the priority
+     *         keywords list else false if non of them present.
+     */
+    private boolean isValidPriorityKeyWordsPresent(List<String> priorityKeyWords, Set<String> priorityKeyWordSet) {
+        if (priorityKeyWordSet.isEmpty()) {
+            return true;
+        }
+        if (priorityKeyWords.contains("")) {
+            return false;
+        } else if (priorityKeyWords.contains("high") || priorityKeyWords.contains("med")
+                || priorityKeyWords.contains("low")) {
+            return true;
+        }
+        return false;
     }
 
     private boolean isAllKeyWordSetEmpty(Set<String> nameKeyWordSet, Set<String> priorityKeyWordSet,
