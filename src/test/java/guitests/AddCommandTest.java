b5973cb722dd6823ee2b1b0566e2428d1ec5bbb6
ang-zy <angzy95@yahoo.com.sg>
2017-04-09 22:57:22 +0800

    Update test cases

--- src/test/java/guitests/AddCommandTest.java
+++ src/test/java/guitests/AddCommandTest.java
@@ -6,15 +6,18 @@ import org.junit.Test;
 
 import guitests.guihandles.TaskCardHandle;
 import seedu.tache.commons.core.Messages;
+import seedu.tache.commons.exceptions.IllegalValueException;
 import seedu.tache.logic.commands.AddCommand;
+import seedu.tache.model.recurstate.RecurState.RecurInterval;
 import seedu.tache.model.task.Name;
+import seedu.tache.testutil.TaskBuilder;
 import seedu.tache.testutil.TestTask;
 import seedu.tache.testutil.TestUtil;
 
 public class AddCommandTest extends TaskManagerGuiTest {
 
     @Test
-    public void add() {
+    public void add_floatingTask_success() {
         //add one task
         TestTask[] currentList = td.getTypicalTasks();
         TestTask taskToAdd = td.getFit;
@@ -25,28 +28,47 @@ public class AddCommandTest extends TaskManagerGuiTest {
         taskToAdd = td.findGirlfriend;
         assertAddSuccess(taskToAdd, currentList);
         currentList = TestUtil.addTasksToList(currentList, taskToAdd);
-
+    }
+    //@@author A0150120H
+    @Test
+    public void add_floatingTask_failure() {
+        TestTask[] currentList = td.getTypicalTasks();
         //add duplicate task
-        commandBox.runCommand(td.getFit.getAddCommand());
+        commandBox.runCommand(td.eggsAndBread.getAddCommand());
         assertResultMessage(AddCommand.MESSAGE_DUPLICATE_TASK);
         assertTrue(taskListPanel.isListMatching(currentList));
-        //@@author A0150120H
+        
+        //invalid command
+        commandBox.runCommand("adds Read Newspaper");
+        assertResultMessage(Messages.MESSAGE_UNKNOWN_COMMAND);
+    }
+    
+    @Test
+    public void add_timedTask_success() {
         //add task with end date only
         commandBox.runCommand("clear");
-        currentList = new TestTask[0];
+        TestTask[] currentList = new TestTask[0];
         assertAddSuccess(td.eggsAndBread, currentList);
         currentList = TestUtil.addTasksToList(currentList, td.eggsAndBread);
 
         //add task with both start and end date
         assertAddSuccess(td.visitFriend, currentList);
-
+    }
+    
+    @Test
+    public void add_timedTask_failure() {
         //Invalid format: Start date only
         commandBox.runCommand(td.startDateOnly.getAddCommand());
         assertResultMessage(String.format(Messages.MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
-        //@@author
-        //invalid command
-        commandBox.runCommand("adds Read Newspaper");
-        assertResultMessage(Messages.MESSAGE_UNKNOWN_COMMAND);
+    }
+    
+    @Test
+    public void add_recurringTask_success() throws IllegalValueException {
+        TestTask taskToAdd = new TaskBuilder().withName("Go to school").withStartDateTime("9am")
+                .withEndDateTime("5pm").withRecurringInterval(RecurInterval.DAY).build();
+        commandBox.runCommand(taskToAdd.getAddCommand());
+        TaskCardHandle addedCard = taskListPanel.navigateToTask(taskToAdd.getName().fullName);
+        assertMatching(taskToAdd, addedCard);
     }
 
     //@@author A0142255M
@@ -63,7 +85,6 @@ public class AddCommandTest extends TaskManagerGuiTest {
         assertResultMessage(String.format(AddCommand.MESSAGE_SUCCESS, toAdd.toString()));
     }
     //@@author
-
     private void assertAddSuccess(TestTask taskToAdd, TestTask... currentList) {
         commandBox.runCommand(taskToAdd.getAddCommand());
 
