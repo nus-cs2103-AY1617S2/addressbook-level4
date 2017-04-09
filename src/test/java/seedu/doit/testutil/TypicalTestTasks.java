ef9908a318dd0a77a2819f644ea7e3a4d5bcfe57
Fyelight <kaid.fyelight@hotmail.com>
2017-04-09 14:35:49 +0800

    Change indentation to fix travis

--- src/test/java/seedu/doit/testutil/TypicalTestTasks.java
+++ src/test/java/seedu/doit/testutil/TypicalTestTasks.java
@@ -60,7 +60,7 @@ public class TypicalTestTasks {
 
     public TestTask[] getTypicalTasks() {
         return new TestTask[] { this.alice, this.benson, this.carl, this.daniel, this.elle, this.fiona, this.george,
-                this.aE, this.bE, this.cE, this.aF, this.bF, this.cF };
+            this.aE, this.bE, this.cE, this.aF, this.bF, this.cF };
     }
 
     public static TestTask getEventTestTask() throws IllegalValueException {
