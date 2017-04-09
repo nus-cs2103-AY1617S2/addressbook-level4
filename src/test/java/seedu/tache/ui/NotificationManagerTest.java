d9b1cbc084b782c852c31d4d3406a3d295948941
Crsytral <shouyu2@hotmail.com>
2017-04-10 00:33:04 +0800

    Update Unit Test for NotificationManagerTest

--- src/test/java/seedu/tache/ui/NotificationManagerTest.java
+++ src/test/java/seedu/tache/ui/NotificationManagerTest.java
@@ -23,15 +23,15 @@ public class NotificationManagerTest {
 
     @Test
     public void showSystemTrayNotification_validInput_success() throws IllegalValueException {
+        NotificationManager testNotification = new NotificationManager();
+        Timer initialTimer = testNotification.getNotificationTimer();
+        Task event = new Task(new Name("Event"), Optional.of(new DateTime("2 hours later")),
+                Optional.of(new DateTime("2 days later")), new UniqueTagList("TestTag"), true,
+                RecurInterval.NONE, new ArrayList<Date>());
+        Task deadline = new Task(new Name("Deadline"), null,
+                Optional.of(new DateTime("2 hours later")), new UniqueTagList("TestTag"), true,
+                RecurInterval.NONE, new ArrayList<Date>());
         if (SystemTray.isSupported()) {
-            NotificationManager testNotification = new NotificationManager();
-            Timer initialTimer = testNotification.getNotificationTimer();
-            Task event = new Task(new Name("Event"), Optional.of(new DateTime("2 hours later")),
-                    Optional.of(new DateTime("2 days later")), new UniqueTagList("TestTag"), true,
-                    RecurInterval.NONE, new ArrayList<Date>());
-            Task deadline = new Task(new Name("Deadline"), null,
-                    Optional.of(new DateTime("2 hours later")), new UniqueTagList("TestTag"), true,
-                    RecurInterval.NONE, new ArrayList<Date>());
             testNotification.showSystemTrayNotification(event, 0);
             testNotification.showSystemTrayNotification(deadline, 1);
             assertNotSame(testNotification.getNotificationTimer().toString(), initialTimer.toString());
