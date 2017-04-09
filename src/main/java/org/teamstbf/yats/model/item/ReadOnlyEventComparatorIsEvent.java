aa6fdd55c499668db27a9161dacf84d175082401
HORIZON\riveria94 <timelesschaos94@gmail.com>
2017-04-09 22:55:25 +0800

    Code quality improvement

--- src/main/java/org/teamstbf/yats/model/item/ReadOnlyEventComparatorIsEvent.java
+++ src/main/java/org/teamstbf/yats/model/item/ReadOnlyEventComparatorIsEvent.java
@@ -5,14 +5,14 @@ import java.util.Comparator;
 //@@ author A0102778B
 
 public class ReadOnlyEventComparatorIsEvent implements Comparator<ReadOnlyEvent> {
-	@Override
-	public int compare(ReadOnlyEvent o1, ReadOnlyEvent o2) {
-		if (o1.getStartTime().toString().equals("")) {
-			return 1;
-		} else if (o2.getStartTime().toString().equals("")) {
-			return -1;
-		}
-		return 0;
-	}
+    @Override
+    public int compare(ReadOnlyEvent o1, ReadOnlyEvent o2) {
+        if (o1.getStartTime().toString().equals("")) {
+            return 1;
+        } else if (o2.getStartTime().toString().equals("")) {
+            return -1;
+        }
+        return 0;
+    }
 
 }
--- src/test/java/guitests/ListCommandTest.java
+++ src/test/java/guitests/ListCommandTest.java
@@ -11,54 +11,54 @@ import org.teamstbf.yats.testutil.TestEvent;
 
 public class ListCommandTest extends TaskManagerGuiTest {
 
-	@Test
-	public void list_nonEmptyList() {
-		/** lists all tasks in the list */
-		assertListAllResult("list", td.abdicate, td.boop, td.oxygen, td.cower, td.duck);
+    @Test
+    public void list_nonEmptyList() {
+        /** lists all tasks in the list */
+        assertListAllResult("list", td.abdicate, td.boop, td.oxygen, td.cower, td.duck);
 
-		/** lists all done tasks in the list */
-		assertListResult("list done");
+        /** lists all done tasks in the list */
+        assertListResult("list done");
 
-		/**
-		 * lists all tasks in the primary list that starts on 9th April
-		 */
-		assertListResult("list by start 08/04/2017", td.abdicate, td.oxygen, td.cower);
+        /**
+         * lists all tasks in the primary list that starts on 9th April
+         */
+        assertListResult("list by start 08/04/2017", td.abdicate, td.oxygen, td.cower);
 
-		/** lists all tasks in the primary list that ends on 8th April */
-		assertListResult("list by end 08/04/2017", td.abdicate, td.oxygen, td.cower);
+        /** lists all tasks in the primary list that ends on 8th April */
+        assertListResult("list by end 08/04/2017", td.abdicate, td.oxygen, td.cower);
 
-		/** lists all tasks in the primary list that has deadline 8th April */
-		assertListResult("list by deadline 08/04/2017", td.boop, td.duck);
+        /** lists all tasks in the primary list that has deadline 8th April */
+        assertListResult("list by deadline 08/04/2017", td.boop, td.duck);
 
-		/** lists all tasks in the primary list that has the location NUS */
-		assertListResult("list by location The Wall", td.cower);
+        /** lists all tasks in the primary list that has the location NUS */
+        assertListResult("list by location The Wall", td.cower);
 
-		/** lists all tasks in the primary list that has the tag Tag */
-		assertListResult("list by tag Tag ONN", td.oxygen);
-	}
+        /** lists all tasks in the primary list that has the tag Tag */
+        assertListResult("list by tag Tag ONN", td.oxygen);
+    }
 
-	@Test
-	public void list_emptyList() {
-		commandBox.runCommand("reset");
-		assertListAllResult("list"); // no result
-	}
+    @Test
+    public void list_emptyList() {
+        commandBox.runCommand("reset");
+        assertListAllResult("list"); // no result
+    }
 
-	@Test
-	public void list_invalidCommand_fail() {
-		commandBox.runCommand("listby start");
-		assertResultMessage(Messages.MESSAGE_UNKNOWN_COMMAND);
-	}
+    @Test
+    public void list_invalidCommand_fail() {
+        commandBox.runCommand("listby start");
+        assertResultMessage(Messages.MESSAGE_UNKNOWN_COMMAND);
+    }
 
-	private void assertListResult(String command, TestEvent... expectedHits) {
-		commandBox.runCommand(command);
-		assertListSize(expectedHits.length);
-		assertResultMessage(expectedHits.length + " tasks listed!");
-		assertTrue(taskListPanel.isListMatching(expectedHits));
-	}
+    private void assertListResult(String command, TestEvent... expectedHits) {
+        commandBox.runCommand(command);
+        assertListSize(expectedHits.length);
+        assertResultMessage(expectedHits.length + " tasks listed!");
+        assertTrue(taskListPanel.isListMatching(expectedHits));
+    }
 
-	private void assertListAllResult(String command, TestEvent... expectedHits) {
-		commandBox.runCommand(command);
-		assertListSize(expectedHits.length);
-		assertResultMessage(ListCommand.MESSAGE_SUCCESS);
-	}
+    private void assertListAllResult(String command, TestEvent... expectedHits) {
+        commandBox.runCommand(command);
+        assertListSize(expectedHits.length);
+        assertResultMessage(ListCommand.MESSAGE_SUCCESS);
+    }
 }
