cd3ae3cf4a27697d8961c2bd568827e900554005
Aditya Hendraatmaja <ahendraatmaja@gmail.com>
2017-04-10 04:20:44 +0800

    Change Taskmanager Logo

--- src/main/java/seedu/taskmanager/ui/MainWindow.java
+++ src/main/java/seedu/taskmanager/ui/MainWindow.java
@@ -24,7 +24,7 @@ import seedu.taskmanager.model.UserPrefs;
  */
 public class MainWindow extends UiPart<Region> {
 
-    private static final String ICON = "/images/taskmanager.jpg";
+    private static final String ICON = "/images/taskmanager.png";
     private static final String FXML = "MainWindow.fxml";
     private static final int MIN_HEIGHT = 800;
     private static final int MIN_WIDTH = 600;
