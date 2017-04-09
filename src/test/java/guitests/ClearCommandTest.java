a676db00ee24e8c43f37daf33bc7d5dcb9bef170
LEUNG Cheuk Ting <e0148074@u.nus.edu>
2017-04-10 03:43:35 +0800

    fix DeleteTest Problem

--- src/test/java/guitests/ClearCommandTest.java
+++ src/test/java/guitests/ClearCommandTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-import seedu.address.logic.commands.ClearCommand;
+import seedu.task.logic.commands.ClearCommand;
 
 public class ClearCommandTest extends AddressBookGuiTest {
 
