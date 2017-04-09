ab73c5d7451f0f7313d48b68eeea387d0106b931
Jun Wei Koh <kohjw1994@gmail.com>
2017-04-09 12:11:11 +0800

    Fixing lapses

--- src/test/java/seedu/taskmanager/testutil/TypicalTestTasks.java
+++ src/test/java/seedu/taskmanager/testutil/TypicalTestTasks.java
@@ -113,14 +113,7 @@ public class TypicalTestTasks {
             completedEatBreakfast = new TaskBuilder().withTaskName("Eat breakfast with mom").withStartDate("03/03/17")
                     .withStartTime("1000").withEndDate("03/03/17").withEndTime("1100").withCompletion(true)
                     .withCategories("just", "friends").build(); // event
-        } catch (IllegalValueException e) {
-            e.printStackTrace();
-            assert false : "not possible";
-        }
-    }
 
-    public void TypicalRecurringEventsForDays() {
-        try {
             recurTestDay = new TaskBuilder().withTaskName("I'm recurring").withStartDate("03/03/17")
                     .withStartTime("1000").withEndDate("03/03/17").withEndTime("1100").withCompletion(false)
                     .withCategories("just", "friends").build();
