cff102ca941df27cdd8ca566bacb991707df1310
Evan Yeung <evan.yeung1@gmail.com>
2017-04-09 12:24:27 +0800

    Update DeveloperGuide to use Task/TaskList
    Removes all mention of address book and person from the developer guide.
    Also refactors `handleAddressBookChangedEvent` to `handleTaskListChangedEvent`.

--- src/main/java/seedu/task/storage/Storage.java
+++ src/main/java/seedu/task/storage/Storage.java
@@ -34,5 +34,5 @@ public interface Storage extends TaskListStorage, UserPrefsStorage {
      *   Creates the data file if it is missing.
      * Raises {@link DataSavingExceptionEvent} if there was an error during saving.
      */
-    void handleAddressBookChangedEvent(TaskListChangedEvent abce);
+    void handleTaskListChangedEvent(TaskListChangedEvent abce);
 }
