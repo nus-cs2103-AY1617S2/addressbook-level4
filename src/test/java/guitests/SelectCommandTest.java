8cef68b7954bbcdd87909ed0a41bcc5df3729828
Jacob Levy <levyjr@bu.edu>
2017-04-09 20:13:36 +0800

    Tests

--- src/test/java/guitests/SelectCommandTest.java
+++ src/test/java/guitests/SelectCommandTest.java
@@ -2,33 +2,33 @@ package guitests;
 
 import static org.junit.Assert.assertEquals;
 
-//import org.junit.Test;
+import org.junit.Test;
 
 import seedu.task.model.task.ReadOnlyTask;
 
 public class SelectCommandTest extends TaskManagerGuiTest {
 
-    // @Test
-    // public void selectPerson_nonEmptyList() {
-    //
-    // assertSelectionInvalid(10); // invalid index
-    // assertNoPersonSelected();
-    //
-    // assertSelectionSuccess(1); // first person in the list
-    // int personCount = td.getTypicalTasks().length;
-    // assertSelectionSuccess(personCount); // last person in the list
-    // int middleIndex = personCount / 2;
-    // assertSelectionSuccess(middleIndex); // a person in the middle of the
-    // // list
-    //
-    // assertSelectionInvalid(personCount + 1); // invalid index
-    // assertPersonSelected(middleIndex); // assert previous selection remains
-    //
-    // /*
-    // * Testing other invalid indexes such as -1 should be done when testing
-    // the SelectCommand
-    // */
-    // }
+     @Test
+     public void selectPerson_nonEmptyList() {
+    
+     assertSelectionInvalid(10); // invalid index
+     assertNoTaskSelected();
+    
+     assertSelectionSuccess(1); // first person in the list
+     int taskCount = td.getTypicalTasks().length;
+     assertSelectionSuccess(taskCount); // last person in the list
+     int middleIndex = taskCount / 2;
+     assertSelectionSuccess(middleIndex); // a person in the middle of the
+     // list
+    
+     assertSelectionInvalid(taskCount + 1); // invalid index
+     assertTaskSelected(middleIndex); // assert previous selection remains
+    
+     /*
+     * Testing other invalid indexes such as -1 should be done when testing
+     the SelectCommand
+     */
+     }
 
     // @Test
     // public void selectPerson_emptyList() {
