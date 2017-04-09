419d8a8cdb3b7528efb084302018a36e959dd2dc
Yue Deng <yue0068@gmail.com>
2017-04-09 21:53:00 +0800

    Add List Parser and modify list command

--- src/main/java/seedu/jobs/logic/commands/ListCommand.java
+++ src/main/java/seedu/jobs/logic/commands/ListCommand.java
@@ -1,19 +1,43 @@
 package seedu.jobs.logic.commands;
 
+import java.util.Set;
 
+//@@author A0164440M
 /**
- * Lists all tasks in the JOBS to the user.
+ * list all tasks with or without arguments
  */
+
 public class ListCommand extends Command {
 
     public static final String COMMAND_WORD = "list";
-
     public static final String MESSAGE_SUCCESS = "Listed all tasks";
 
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": list all tasks whose names contain any of "
+            + "the specified keywords (case-sensitive) and displays them as a list with index numbers.\n"
+            + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
+            + "Example: " + COMMAND_WORD + " alice bob charlie";
 
+    private final Set<String> keywords;
+    
+    public ListCommand() {
+        this.keywords = null;
+    }
+    
+    public ListCommand(Set<String> keywords) {
+        this.keywords = keywords;
+    }
+
+    //@@author A0164440M
     @Override
     public CommandResult execute() {
-        model.updateFilteredListToShowAll();
-        return new CommandResult(MESSAGE_SUCCESS);
+        if(keywords == null) {
+            model.updateFilteredListToShowAll();
+            return new CommandResult(MESSAGE_SUCCESS);
+        } else {
+            model.updateFilteredListToShowAll(keywords);
+        }
+        return new CommandResult(getMessageForTaskListShownSummary(model.getFilteredTaskList().size()));
     }
+    //@@author
+
 }
