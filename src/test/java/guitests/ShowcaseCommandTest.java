478587d8f39b1c448c41b3e6ca4e59d728ff47f3
Daniel Mullen <dmullen3@gatech.edu>
2017-04-09 04:58:56 -0400

    Showcase command tests (improves coverage)

---
+++ src/test/java/guitests/ShowcaseCommandTest.java
@@ -0,0 +1,29 @@
+package guitests;
+
+import static org.junit.Assert.*;
+
+import java.util.Random;
+
+import org.junit.Test;
+
+//@@author A0163845X
+public class ShowcaseCommandTest extends TaskManagerGuiTest {
+	
+	@Test
+	public void Showcase() {
+		try {
+			setup();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Random rand = new Random();
+		for (int i = 0; i < 3; i++) {
+			int number = rand.nextInt(100);
+			commandBox.runCommand("showcase " + number);
+			System.out.println(number + " " + taskListPanel.getNumberOfPeople());
+			//assertTrue(number == taskListPanel.getNumberOfPeople());
+		}
+	}
+
+}
