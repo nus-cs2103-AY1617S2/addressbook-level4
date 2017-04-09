e6e2c0806803e096acd81a547b50be52e8a696f1
Malik Jabati <malikjabati@gmail.com>
2017-04-09 17:00:40 +0800

    Fix UI tests

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -8,7 +8,8 @@ import seedu.address.logic.parser.ArgumentTokenizer.Prefix;
  * Contains Command Line Interface (CLI) syntax definitions common to multiple commands
  */
 public class CliSyntax {
-
+    
+    //@@author A0164466X-reused
     /* Prefix definitions */
     public static final Prefix PREFIX_GROUP = new Prefix("g/");
     public static final Prefix PREFIX_END_DATE = new Prefix("d/");
