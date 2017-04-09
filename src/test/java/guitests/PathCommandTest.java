8710340d4a5856431ba2122c9ca441d5b90ef5cc
Daniel Mullen <dmullen3@gatech.edu>
2017-04-09 09:54:00 -0400

    covering path

---
+++ src/test/java/guitests/PathCommandTest.java
@@ -0,0 +1,30 @@
+package guitests;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+// @@author A0163845X
+public class PathCommandTest extends TaskManagerGuiTest {
+
+    @Test
+    public void path() {
+        try {
+            setup();
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        commandBox.runCommand("showcase 75");
+        commandBox.runCommand("path TaskManager1.xml");
+        commandBox.runCommand("showcase 50");
+        commandBox.runCommand("load TaskManager.xml");
+        assertTrue(taskListPanel.getNumberOfTasks() == 75);
+        commandBox.runCommand("load TaskManager1.xml");
+        assertTrue(taskListPanel.getNumberOfTasks() == 50);
+        commandBox.runCommand("load TaskManager.xml");
+        
+        // @@author
+    }
+}
