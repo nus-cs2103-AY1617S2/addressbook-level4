d0fb714efecfa689b9d0bcceba6a574301ae63fd
Li Heng <liheng6567@gmail.com>
2017-04-10 08:35:17 +0800

    Test for clear done clear undone. Fix #80 #81

--- src/main/java/seedu/onetwodo/logic/commands/ListCommand.java
+++ src/main/java/seedu/onetwodo/logic/commands/ListCommand.java
@@ -19,6 +19,8 @@ public class ListCommand extends Command {
 
     private static final String DATES_ARE_INVALID = "Dates are invalid";
     public static final String COMMAND_WORD = "list";
+    public static final String COMMAND_LIST_DONE = COMMAND_WORD + " done";
+    public static final String COMMAND_LIST_UNDONE = COMMAND_WORD + " undone";
     public static final String SHORT_COMMAND_WORD = "ls";
     public static final String MESSAGE_LIST_DONE_SUCCESS = "Listed completed tasks";
     public static final String MESSAGE_LIST_UNDONE_SUCCESS = "Listed incompleted tasks";
--- src/test/java/guitests/ClearCommandTest.java
+++ src/test/java/guitests/ClearCommandTest.java
@@ -8,6 +8,8 @@ import org.junit.Test;
 
 import seedu.onetwodo.logic.commands.ClearCommand;
 import seedu.onetwodo.logic.commands.DeleteCommand;
+import seedu.onetwodo.logic.commands.DoneCommand;
+import seedu.onetwodo.logic.commands.ListCommand;
 import seedu.onetwodo.model.task.TaskType;
 import seedu.onetwodo.testutil.TestTask;
 
@@ -17,12 +19,12 @@ public class ClearCommandTest extends ToDoListGuiTest {
 
     @Test
     public void clear_invalid_input_failure () {
-        commandBox.runCommand(ClearCommand.COMMAND_WORD + " e9999");
+        commandBox.runCommand(ClearCommand.COMMAND_WORD + " ddoonee");
         assertResultMessage(String.format(MESSAGE_INVALID_COMMAND_FORMAT, ClearCommand.MESSAGE_USAGE));
     }
 
     @Test
-    public void clear_all_sucess() {
+    public void clear_all_success() {
         //verify a non-empty list can be cleared
         TaskType taskATaskType = td.taskA.getTaskType();
 
@@ -34,16 +36,68 @@ public class ClearCommandTest extends ToDoListGuiTest {
         assertTrue(taskListPanel.isListMatching(td.task1.getTaskType(), td.task1));
         commandBox.runCommand(DeleteCommand.COMMAND_WORD + " e1");
         assertListSize(0);
-
-        //verify clear command works when the list is empty
-        assertClearAllCommandSuccess();
     }
 
+    @Test
+    public void clear_success() {
+        assertClearCommandSuccess();
+    }
+
+    @Test
+    public void clear_empty_list_failure() {
+        commandBox.runCommand(ClearCommand.COMMAND_WORD);
+        commandBox.runCommand(ClearCommand.COMMAND_WORD);
+        assertResultMessage(ClearCommand.MESSAGE_NO_MORE_TASK);
+    }
+
+    @Test
     public void clear_done_success () {
-
+        commandBox.runCommand(DoneCommand.COMMAND_WORD + " t1");
+        commandBox.runCommand(DoneCommand.COMMAND_WORD + " t1");
+        commandBox.runCommand(ClearCommand.COMMAND_CLEAR_DONE);
+        assertResultMessage(ClearCommand.MESSAGE_CLEAR_DONE_SUCCESS);
+        commandBox.runCommand(ListCommand.COMMAND_LIST_DONE);
+        assertListSize(0);
     }
 
+    @Test
+    public void double_clear_done_failure() {
+        commandBox.runCommand(DoneCommand.COMMAND_WORD + " t1");
+        commandBox.runCommand(ClearCommand.COMMAND_CLEAR_DONE);
+        commandBox.runCommand(ClearCommand.COMMAND_CLEAR_DONE);
+        assertResultMessage(ClearCommand.MESSAGE_NO_MORE_DONE_TASK);
+    }
+
+    @Test
+    public void clear_undone_success () {
+        commandBox.runCommand(DoneCommand.COMMAND_WORD + " t1");
+        commandBox.runCommand(DoneCommand.COMMAND_WORD + " t1");
+        commandBox.runCommand(ClearCommand.COMMAND_CLEAR_UNDONE);
+        assertResultMessage(ClearCommand.MESSAGE_CLEAR_UNDONE_SUCCESS);
+        assertListSize(0);
+    }
+
+    @Test
+    public void double_clear_undone_failure() {
+        commandBox.runCommand(DoneCommand.COMMAND_WORD + " t1");
+        commandBox.runCommand(ClearCommand.COMMAND_CLEAR_UNDONE);
+        commandBox.runCommand(ClearCommand.COMMAND_CLEAR_UNDONE);
+        assertResultMessage(ClearCommand.MESSAGE_NO_MORE_UNDONE_TASK);
+    }
+
+    /**
+     * Runs the clear_all command and confirms the result is correct.
+     */
     private void assertClearAllCommandSuccess() {
+        commandBox.runCommand(ClearCommand.COMMAND_CLEAR_ALL);
+        assertListSize(0);
+        assertResultMessage(ClearCommand.MESSAGE_CLEAR_ALL_SUCCESS);
+    }
+
+    /**
+     * Runs the clear command and confirms the result is correct.
+     */
+    private void assertClearCommandSuccess() {
         commandBox.runCommand(ClearCommand.COMMAND_WORD);
         assertListSize(0);
         assertResultMessage(ClearCommand.MESSAGE_CLEAR_ALL_SUCCESS);
